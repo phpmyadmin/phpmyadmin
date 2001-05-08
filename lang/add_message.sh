595bf27acf2e6946295ef13aaf21c094a2cb991d
Loïc Chapeaux <lolo@phpheaven.net>
2001-05-08 17:22:15 +0000

    Lem9: added a shell script that adds a message to all message files

---
+++ lang/add_message.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Shell script that adds a message to all message files (Lem9)
+#
+# Example:  add_message '$strNewMessage' 'new message contents'
+#
+for file in *.inc.php3
+do
+        echo $file " "
+        grep -v '?>' ${file} > ${file}.new
+        echo "$1=\""  $2  '";  //to translate' >> ${file}.new
+        echo "?>" >> ${file}.new
+        rm $file
+        mv ${file}.new $file
+done
+echo " "
+echo "This script also added the new message to english.inc.php3"
