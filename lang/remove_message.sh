4f30b9042a735aa24f78430e2063a5cbcc3ebe08
Marc Delisle <marc@infomarc.info>
2001-07-01 15:10:23 +0000

    new script

---
+++ lang/remove_message.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Shell script that removes a message from all message files (Lem9)
+# it checks for the message, followed by a space
+#
+# Example:  remove_message.sh 'strMessageToRemove' 
+#
+for file in *.inc.php3
+do
+	echo "lines before:" `wc -l $file`
+        grep -v "$1 " ${file} > ${file}.new
+        rm $file
+        mv ${file}.new $file
+	echo " lines after:" `wc -l $file`
+done
+echo " "
