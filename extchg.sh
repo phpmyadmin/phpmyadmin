48997a8ee669c46c8b39a3a2659540c706b75832
Marc Delisle <marc@infomarc.info>
2001-07-08 01:49:29 +0000

    extchg.sh and sub-dir

--- extchg.sh
+++ extchg.sh
@@ -6,38 +6,22 @@
 # modified by Tobias Ratschiller to allow any file extension
 # part of the phpMyAdmin distribution <http://phpwizard.net/phpMyAdmin>
 
-if [$1 eq ""]
+# 2001-07-07, lem9@users.sourceforge.net:
+# - supports sub-directories
+# - no more bak directory
+
+if [ $# != 2 ]
 then
-  echo "Missing first parameter (extension to be changed)"
-  echo "Usage: extchg <extension to change from> <extension to change to>"
+  echo "Usage: extchg.sh <extension to change from> <extension to change to>"
+  echo ""
+  echo "Example: extchg.sh php3 php"
   exit
 fi
 
-if [$2 eq ""]
-then
-  echo "Missing second parameter (extension to change to)"
-  echo "Usage: extchg <extension to change from> <extension to change to>"
-  exit
-fi
-
-
-if test ! -s *.$1
-then
-  echo 'Nothing to convert! Try to copy the script to the directory where you have files to convert.' 
-  exit
-fi
-
-
-if  test ! -d bak
-then
-	mkdir bak
-else
-	echo 'Directory bak is already there - will try to use it to backup your files...'
-fi
-
-for i in *.$1
+for i in `find . -name "*.$1"`
 	 do 
+	 echo $i
 	 sed -e 's/'$1'/'$2'/g' $i > `ls $i|sed -e 's/'$1'/'$2'/g'`
-	 mv $i bak/$i
+	 rm $i
 	done;
 
