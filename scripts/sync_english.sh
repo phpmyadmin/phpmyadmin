06b97e46dd4bb8fda209ab20406f036444c19d80
Marc Delisle <marc@infomarc.info>
2002-06-14 14:38:58 +0000

    iso8859-1

--- scripts/sync_english.sh
+++ scripts/sync_english.sh
@@ -3,7 +3,7 @@
 # Shell script that synchronises all english translations using iso-8859-1 as basic
 
 echo -n "Creating utf-8 ... "
-iconv -f iso8859-2 -t utf-8 english.inc.php3| sed -e 's/iso-8859-1/utf-8/' -e '/\$charset/a\
+iconv -f iso8859-1 -t utf-8 english.inc.php3| sed -e 's/iso-8859-1/utf-8/' -e '/\$charset/a\
 $allow_recoding = TRUE;' > english-utf8.inc.php3
 echo done
 
