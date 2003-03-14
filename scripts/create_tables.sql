25ff6df409903951a089ee44f536039edbfe5e48
Robin Johnson <robbat2@users.sourceforge.net>
2003-03-14 10:29:55 +0000

    sql cleanup

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,89 +1,97 @@
-#########################################################
-#                                                       #
-# SQL Commands to set up the pmadb as described in      #
-# Documentation.txt.                                    #
-#                                                       #
-# This script expects the user pma to allready be       #
-# existing. (if we would put a line here to create him  #
-# too many users might just use this script and end     #
-# up with having the same password for the controluser) #
-#                                                       #
-# Please dont forget to set up the tablenames in        #
-# config.inc.php3                                       #
-#                                                       #
-#########################################################
+-- ########################################################
+--                                                        #
+--  SQL Commands to set up the pmadb as described in      #
+--  Documentation.txt.                                    #
+--                                                        #
+--  This script expects the user pma to allready be       #
+--  existing. (if we would put a line here to create him  #
+--  too many users might just use this script and end     #
+--  up with having the same password for the controluser) #
+--                                                        #
+--  Please dont forget to set up the tablenames in        #
+--  config.inc.php3                                       #
+--                                                        #
+-- ########################################################
 
-CREATE DATABASE `phpmyadmin`;
+DROP DATABASE IF EXISTS `phpmyadmin`;
+CREATE DATABASE IF NOT EXISTS `phpmyadmin`;
 
 USE `phpmyadmin`;
 
-GRANT SELECT, INSERT, DELETE ON `phpmyadmin`.* TO
+GRANT SELECT, INSERT, DELETE, UPDATE ON `phpmyadmin`.* TO
     'pma'@localhost;
 
+DROP TABLE IF EXISTS `PMA_bookmark`;
 CREATE TABLE `PMA_bookmark` (
-    id int(11) DEFAULT '0' NOT NULL auto_increment,
-    dbase varchar(255) NOT NULL,
-    user varchar(255) NOT NULL,
-    label varchar(255) NOT NULL,
-    query text NOT NULL,
-    PRIMARY KEY (id)
+    `id` int(11) DEFAULT '0' NOT NULL auto_increment,
+    `dbase` VARCHAR(255) NOT NULL,
+    `user` VARCHAR(255) NOT NULL,
+    `label` VARCHAR(255) NOT NULL,
+    `query` TEXT NOT NULL,
+    PRIMARY KEY (`id`)
 ) TYPE=MyISAM COMMENT='Bookmarks';
 
+DROP TABLE IF EXISTS `PMA_relation`;
 CREATE TABLE `PMA_relation` (
-    `master_db` varchar(64) NOT NULL default '',
-    `master_table` varchar(64) NOT NULL default '',
-    `master_field` varchar(64) NOT NULL default '',
-    `foreign_db` varchar(64) NOT NULL default '',
-    `foreign_table` varchar(64) NOT NULL default '',
-    `foreign_field` varchar(64) NOT NULL default '',
+    `master_db` VARCHAR(64) NOT NULL DEFAULT '',
+    `master_table` VARCHAR(64) NOT NULL DEFAULT '',
+    `master_field` VARCHAR(64) NOT NULL DEFAULT '',
+    `foreign_db` VARCHAR(64) NOT NULL DEFAULT '',
+    `foreign_table` VARCHAR(64) NOT NULL DEFAULT '',
+    `foreign_field` VARCHAR(64) NOT NULL DEFAULT '',
     PRIMARY KEY (`master_db`, `master_table`,`master_field`),
-    KEY foreign_field (foreign_db, foreign_table)
-    ) TYPE=MyISAM COMMENT='Relation table';
+    KEY `foreign_field` (`foreign_db`, `foreign_table`)
+) TYPE=MyISAM COMMENT='Relation table';
 
+DROP TABLE IF EXISTS `PMA_table_info`;
 CREATE TABLE `PMA_table_info` (
-    `db_name` varchar(64) NOT NULL default '',
-    `table_name` varchar(64) NOT NULL default '',
-    `display_field` varchar(64) NOT NULL default '',
+    `db_name` VARCHAR(64) NOT NULL DEFAULT '',
+    `table_name` VARCHAR(64) NOT NULL DEFAULT '',
+    `display_field` VARCHAR(64) NOT NULL DEFAULT '',
     PRIMARY KEY (`db_name`, `table_name`)
-    ) TYPE=MyISAM COMMENT='Table information for phpMyAdmin';
+) TYPE=MyISAM COMMENT='Table information for phpMyAdmin';
 
+DROP TABLE IF EXISTS `PMA_table_coords`;
 CREATE TABLE `PMA_table_coords` (
-    `db_name` varchar(64) NOT NULL default '',
-    `table_name` varchar(64) NOT NULL default '',
-    `pdf_page_number` int NOT NULL default '0',
-    `x` float unsigned NOT NULL default '0',
-    `y` float unsigned NOT NULL default '0',
+    `db_name` VARCHAR(64) NOT NULL DEFAULT '',
+    `table_name` VARCHAR(64) NOT NULL DEFAULT '',
+    `pdf_page_number` int NOT NULL DEFAULT '0',
+    `x` float unsigned NOT NULL DEFAULT '0',
+    `y` float unsigned NOT NULL DEFAULT '0',
     PRIMARY KEY (`db_name`, `table_name`, `pdf_page_number`)
-    ) TYPE=MyISAM COMMENT='Table coordinates for phpMyAdmin PDF output';
+) TYPE=MyISAM COMMENT='Table coordinates for phpMyAdmin PDF output';
 
+DROP TABLE IF EXISTS `PMA_pdf_pages`;
 CREATE TABLE `PMA_pdf_pages` (
-    `db_name` varchar(64) NOT NULL default '',
-    `page_nr` int(10) unsigned NOT NULL auto_increment,
-    `page_descr` varchar(50) NOT NULL default '',
-    PRIMARY KEY (page_nr),
-    KEY (db_name)
-    ) TYPE=MyISAM COMMENT='PDF Relationpages for PMA';
+    `db_name` VARCHAR(64) NOT NULL DEFAULT '',
+    `page_nr` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `page_descr` VARCHAR(50) NOT NULL DEFAULT '',
+    PRIMARY KEY (`page_nr`),
+    KEY (`db_name`)
+) TYPE=MyISAM COMMENT='PDF Relationpages for PMA';
 
+DROP TABLE IF EXISTS `PMA_column_info`;
 CREATE TABLE `PMA_column_info` (
-    id int(5) unsigned NOT NULL auto_increment,
-    db_name varchar(64) NOT NULL default '',
-    table_name varchar(64) NOT NULL default '',
-    column_name varchar(64) NOT NULL default '',
-    `comment` varchar(255) NOT NULL default '',
-    mimetype varchar(255) NOT NULL default '',
-    transformation varchar(255) NOT NULL default '',
-    transformation_options varchar(255) NOT NULL default '',
-    PRIMARY KEY (id),
-    UNIQUE KEY db_name (db_name, table_name, column_name)
-    ) TYPE=MyISAM COMMENT='Column Information for phpMyAdmin';
+    `id` INT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `db_name` VARCHAR(64) NOT NULL DEFAULT '',
+    `table_name` VARCHAR(64) NOT NULL DEFAULT '',
+    `column_name` VARCHAR(64) NOT NULL DEFAULT '',
+    `comment` VARCHAR(255) NOT NULL DEFAULT '',
+    `mimetype` VARCHAR(255) NOT NULL DEFAULT '',
+    `transformation` VARCHAR(255) NOT NULL DEFAULT '',
+    `transformation_options` VARCHAR(255) NOT NULL DEFAULT '',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `db_name` (`db_name`, `table_name`, `column_name`)
+) TYPE=MyISAM COMMENT='Column Information for phpMyAdmin';
 
+DROP TABLE IF EXISTS `PMA_history`;
 CREATE TABLE `PMA_history` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR( 64 ) NOT NULL ,
-    `db` VARCHAR( 64 ) NOT NULL ,
-    `table` VARCHAR( 64 ) NOT NULL ,
+    `username` VARCHAR(64) NOT NULL ,
+    `db` VARCHAR(64) NOT NULL ,
+    `table` VARCHAR(64) NOT NULL ,
     `timevalue` TIMESTAMP NOT NULL ,
     `sqlquery` TEXT NOT NULL ,
-    PRIMARY KEY ( `id` ) ,
-    INDEX ( `username` , `db` , `table` , `timevalue` )
-    ) TYPE=MyISAM COMMENT='SQL history';
+    PRIMARY KEY (`id`) ,
+    KEY `username` (`username`, `db`, `table`, `timevalue`)
+) TYPE=MyISAM COMMENT='SQL history';
