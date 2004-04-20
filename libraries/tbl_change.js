ff02ffe2fc5303b89d89751639d839b43c85b944
Marc Delisle <marc@infomarc.info>
2004-04-20 19:46:36 +0000

    date problem

--- libraries/tbl_change.js
+++ libraries/tbl_change.js
@@ -254,9 +254,11 @@ function initCalendar() {
         dispmonth = 1 + month;
         
         if (window.opener.dateType == 'datetime' || window.opener.dateType == 'date') {
-		    actVal = formatNum4(year) + "-" + formatNum2(dispmonth) + "-" + formatNum2(day);
+//		    actVal = formatNum4(year) + "-" + formatNum2(dispmonth) + "-" + formatNum2(day);
+		    actVal = formatNum4(year) + "-" + formatNum2(dispmonth) + "-" + formatNum2(i);
         } else {
-		    actVal = "" + formatNum4(year) + formatNum2(dispmonth) + formatNum2(day);
+//		    actVal = "" + formatNum4(year) + formatNum2(dispmonth) + formatNum2(day);
+		    actVal = "" + formatNum4(year) + formatNum2(dispmonth) + formatNum2(i);
         }
         if (i == day) {
             style = ' class="selected"';
