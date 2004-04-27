ea13babc3a0219236b75b9d6945f2dce6e6523ed
Garvin Hicking <me@supergarv.de>
2004-04-27 11:32:54 +0000

    RFE #867284 - Click on result row      to mark the checkbox.

--- libraries/display_tbl.lib.php
+++ libraries/display_tbl.lib.php
@@ -1165,6 +1165,8 @@ function PMA_displayTableBody(&$dt_result, &$is_display, $map, $analyzed_sql)
                 $column_style = '';
             }
 
+            $column_style .= ' onmousedown="document.getElementById(\'id_rows_to_delete' . $row_no . '\').checked = (document.getElementById(\'id_rows_to_delete' . $row_no . '\').checked ? false : true);" ';
+
             // garvin: Wrap MIME-transformations. [MIME]
             $default_function = 'default_function'; // default_function
             $transform_function = $default_function;
