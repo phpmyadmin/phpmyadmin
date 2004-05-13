28a220fb3c5e29670801176d3a2bf7b6b10e3517
Michal Čihař <michal@cihar.com>
2004-05-13 19:54:42 +0000

    Do not fail on broken table.

--- tbl_properties_table_info.php
+++ tbl_properties_table_info.php
@@ -17,13 +17,13 @@ PMA_checkParameters(array('db', 'table'));
 // The 'show table' statement works correct since 3.23.03
 $table_info_result   = PMA_DBI_query('SHOW TABLE STATUS LIKE \'' . PMA_sqlAddslashes($table, TRUE) . '\';');
 $showtable           = PMA_DBI_fetch_assoc($table_info_result);
-$tbl_type            = strtoupper($showtable['Type']);
+$tbl_type            = isset($showtable['Type']) ? strtoupper($showtable['Type']) : '';
 $tbl_collation       = empty($showtable['Collation']) ? '' : $showtable['Collation'];
 $table_info_num_rows = (isset($showtable['Rows']) ? $showtable['Rows'] : 0);
 $show_comment        = (isset($showtable['Comment']) ? $showtable['Comment'] : '');
 $auto_increment      = (isset($showtable['Auto_increment']) ? $showtable['Auto_increment'] : '');
 
-$tmp                 = explode(' ', $showtable['Create_options']);
+$tmp                 = isset($showtable['Create_options']) ? explode(' ', $showtable['Create_options']) : array();
 $tmp_cnt             = count($tmp);
 for ($i = 0; $i < $tmp_cnt; $i++) {
     $tmp1            = explode('=', $tmp[$i]);
