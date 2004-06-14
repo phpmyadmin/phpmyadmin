798947fa5538560b843260c3b97bdaf672c660a2
Marc Delisle <marc@infomarc.info>
2004-06-14 20:15:46 +0000

    improve background image for th background on error section

--- db_details_structure.php
+++ db_details_structure.php
@@ -758,9 +758,10 @@ if (PMA_MYSQL_INT_VERSION >= 40101) {
 
 if ($num_tables > 0
     && !$cfgRelation['allworks'] && $cfg['PmaNoRelation_DisableWarning'] == FALSE) {
-    echo '<tr><td colspan="3"><div class="error"><div class="head">' . $strError . '</div>'
-        . '<div class="text">' . sprintf($strRelationNotWorking, '<a href="' . $cfg['PmaAbsoluteUri'] . 'chk_rel.php?' . $url_query . '">',  '</a>')
-        . '</div></div></td></tr>';
+    echo '<tr><th colspan="3" class="tblHeadError"><div class="errorhead">' . $strError . '</div></th></tr>'
+        . '<tr><td colspan="2" class="tblError">'
+        . sprintf(wordwrap($strRelationNotWorking,65,'<br />'), '<a href="' . $cfg['PmaAbsoluteUri'] . 'chk_rel.php?' . $url_query . '">',  '</a>')
+        . '</td></tr>';
 } // end if
 ?>
 </table>
