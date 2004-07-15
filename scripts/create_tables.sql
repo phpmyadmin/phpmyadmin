de237b4c82bc7fcf902eb177494ec973fffb579b
Alexander M. Turek <me@derrabus.de>
2004-07-15 21:18:29 +0000

    db, table and field names are case-sensitive.

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -25,7 +25,7 @@
 -- 
 DROP DATABASE IF EXISTS `phpmyadmin`;
 CREATE DATABASE `phpmyadmin`
-  DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+  DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 USE phpmyadmin;
 
 -- --------------------------------------------------------
@@ -46,12 +46,12 @@ CREATE TABLE `pma_bookmark` (
   `id` int(11) NOT NULL auto_increment,
   `dbase` varchar(255) NOT NULL default '',
   `user` varchar(255) NOT NULL default '',
-  `label` varchar(255) NOT NULL default '',
+  `label` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL default '',
   `query` text NOT NULL,
   PRIMARY KEY  (`id`)
 )
   ENGINE=MyISAM COMMENT='Bookmarks'
-  DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+  DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -64,15 +64,15 @@ CREATE TABLE `pma_column_info` (
   `db_name` varchar(64) NOT NULL default '',
   `table_name` varchar(64) NOT NULL default '',
   `column_name` varchar(64) NOT NULL default '',
-  `comment` varchar(255) NOT NULL default '',
-  `mimetype` varchar(255) NOT NULL default '',
+  `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL default '',
+  `mimetype` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL default '',
   `transformation` varchar(255) NOT NULL default '',
   `transformation_options` varchar(255) NOT NULL default '',
   PRIMARY KEY  (`id`),
   UNIQUE KEY `db_name` (`db_name`,`table_name`,`column_name`)
 )
   ENGINE=MyISAM COMMENT='Column information for phpMyAdmin'
-  DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+  DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -91,7 +91,7 @@ CREATE TABLE `pma_history` (
   KEY `username` (`username`,`db`,`table`,`timevalue`)
 )
   ENGINE=MyISAM COMMENT='SQL history for phpMyAdmin'
-  DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+  DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -102,12 +102,12 @@ CREATE TABLE `pma_history` (
 CREATE TABLE `pma_pdf_pages` (
   `db_name` varchar(64) NOT NULL default '',
   `page_nr` int(10) unsigned NOT NULL auto_increment,
-  `page_descr` varchar(50) NOT NULL default '',
+  `page_descr` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL default '',
   PRIMARY KEY  (`page_nr`),
   KEY `db_name` (`db_name`)
 )
   ENGINE=MyISAM COMMENT='PDF relation pages for phpMyAdmin'
-  DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+  DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -126,7 +126,7 @@ CREATE TABLE `pma_relation` (
   KEY `foreign_field` (`foreign_db`,`foreign_table`)
 )
   ENGINE=MyISAM COMMENT='Relation table'
-  DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+  DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -143,7 +143,7 @@ CREATE TABLE `pma_table_coords` (
   PRIMARY KEY  (`db_name`,`table_name`,`pdf_page_number`)
 )
   ENGINE=MyISAM COMMENT='Table coordinates for phpMyAdmin PDF output'
-  DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+  DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -158,4 +158,4 @@ CREATE TABLE `pma_table_info` (
   PRIMARY KEY  (`db_name`,`table_name`)
 )
   ENGINE=MyISAM COMMENT='Table information for phpMyAdmin'
-  DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+  DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
