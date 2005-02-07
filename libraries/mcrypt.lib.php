71c02259968aa24626796910b5e889cd0b0e23e1
Alexander M. Turek <me@derrabus.de>
2005-02-07 16:46:02 +0000

    bug #1117907

--- libraries/mcrypt.lib.php
+++ libraries/mcrypt.lib.php
@@ -12,7 +12,7 @@
 // per server so I don't put the server number in the cookie name.
 
 if (!isset($_COOKIE['pma_mcrypt_iv'])) {
-    srand();
+    srand((double) microtime() * 1000000);
     $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_CBC), MCRYPT_RAND);
     setcookie('pma_mcrypt_iv',
         base64_encode($iv),
