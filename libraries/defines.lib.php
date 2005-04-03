bc51ea41c3995f4868c7fe6c0107e76a322b1584
Marc Delisle <marc@infomarc.info>
2005-04-03 22:43:39 +0000

    2.6.2-rc1

--- libraries/defines.lib.php
+++ libraries/defines.lib.php
@@ -20,7 +20,7 @@
  */
 // phpMyAdmin release
 if (!defined('PMA_VERSION')) {
-    define('PMA_VERSION', '2.6.2-beta2');
+    define('PMA_VERSION', '2.6.2-rc1');
 }
 
 if (!defined('PMA_THEME_VERSION')) {
