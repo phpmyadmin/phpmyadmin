4bae3aaca491b64e093f44f116d6312e4fcd9785
Marc Delisle <marc@infomarc.info>
2005-11-03 20:07:43 +0000

    favicon on login panel

--- libraries/header_meta_style.inc.php
+++ libraries/header_meta_style.inc.php
@@ -22,7 +22,7 @@ echo "<?xml version=\"1.0\" encoding=\"" . $GLOBALS['charset'] . "\"?".">";
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $GLOBALS['available_languages'][$GLOBALS['lang']][2]; ?>" lang="<?php echo $GLOBALS['available_languages'][$GLOBALS['lang']][2]; ?>" dir="<?php echo $GLOBALS['text_dir']; ?>">
-
+    <link rel="shortcut icon" href="favicon.ico" type="image/ico" />
 <head>
 <title><?php 
     if(!empty($page_title)) 
