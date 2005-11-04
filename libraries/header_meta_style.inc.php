eb3f4f5d30fcf690ce46caf917cfc3e20c118676
Sebastian Mendel <cybot_tm@users.sourceforge.net>
2005-11-04 13:20:40 +0000

    fixed position of link meta tag

--- libraries/header_meta_style.inc.php
+++ libraries/header_meta_style.inc.php
@@ -22,8 +22,8 @@ echo "<?xml version=\"1.0\" encoding=\"" . $GLOBALS['charset'] . "\"?".">";
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $GLOBALS['available_languages'][$GLOBALS['lang']][2]; ?>" lang="<?php echo $GLOBALS['available_languages'][$GLOBALS['lang']][2]; ?>" dir="<?php echo $GLOBALS['text_dir']; ?>">
-    <link rel="shortcut icon" href="favicon.ico" type="image/ico" />
 <head>
+<link rel="shortcut icon" href="favicon.ico" type="image/ico" />
 <title><?php 
     if(!empty($page_title)) 
         echo htmlspecialchars($page_title); 
