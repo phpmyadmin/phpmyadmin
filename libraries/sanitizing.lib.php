a27ae8e91ea81b2486c3eead5e535eed786aa693
Sebastian Mendel <cybot_tm@users.sourceforge.net>
2005-11-17 13:12:58 +0000

    dont use deprecated tags

--- libraries/sanitizing.lib.php
+++ libraries/sanitizing.lib.php
@@ -15,16 +15,24 @@
 function PMA_sanitize($message)
 {
     $replace_pairs = array(
-        '<'     => '&lt;',
-        '>'     => '&gt;',
-        '[i]'   => '<i>',
-        '[/i]'  => '</i>',
-        '[b]'   => '<b>',
-        '[/b]'  => '</b>',
-        '[tt]'   => '<tt>',
-        '[/tt]'  => '</tt>',
-        '[br]'  => '<br />',
-        '[/a]'  => '</a>',
+        '<'         => '&lt;',
+        '>'         => '&gt;',
+        '[i]'       => '<em>',      // deprecated by em
+        '[/i]'      => '</em>',     // deprecated by em
+        '[em]'      => '<em>',
+        '[/em]'     => '</em>',
+        '[b]'       => '<strong>',  // deprecated by strong
+        '[/b]'      => '</strong>', // deprecated by strong
+        '[strong]'  => '<strong>',
+        '[/strong]' => '</strong>',
+        '[tt]'      => '<code>',    // deprecated by CODE or KBD
+        '[/tt]'     => '</code>',   // deprecated by CODE or KBD
+        '[code]'    => '<code>',
+        '[/code]'   => '</code>',
+        '[kbd]'     => '<kbd>',
+        '[/kbd]'    => '</kbd>',
+        '[br]'      => '<br />',
+        '[/a]'      => '</a>',
     );
     return preg_replace('/\[a@([^"@]*)@([^]"]*)\]/', '<a href="\1" target="\2">', strtr($message, $replace_pairs));
 }
