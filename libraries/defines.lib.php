942542815794e1cd023c64a5904eef10670ae4e7
Marc Delisle <marc@infomarc.info>
2005-11-20 13:38:23 +0000

    going to 2.7.1-dev

--- libraries/defines.lib.php
+++ libraries/defines.lib.php
@@ -20,7 +20,7 @@
  */
 // phpMyAdmin release
 if (!defined('PMA_VERSION')) {
-    define('PMA_VERSION', '2.7.0-rc1');
+    define('PMA_VERSION', '2.7.1-dev');
 }
 
 if (!defined('PMA_THEME_VERSION')) {
