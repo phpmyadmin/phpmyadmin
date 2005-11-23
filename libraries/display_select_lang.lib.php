37dec301be913fa20a48057838c59f64a70ced1f
Sebastian Mendel <cybot_tm@users.sourceforge.net>
2005-11-23 15:10:19 +0000

    *REVERTED* correct lang definition

--- libraries/display_select_lang.lib.php
+++ libraries/display_select_lang.lib.php
@@ -23,33 +23,41 @@ function PMA_language_cmp( &$a, &$b ) {
  */
 function PMA_select_language($use_fieldset = FALSE) {
     global $cfg, $lang;
-?>
+    ?>
+
 <form method="post" action="index.php" target="_parent">
-        <bdo xml:lang="en" dir="ltr">
+    <bdo xml:lang="en" dir="ltr">
     <?php
     if (isset($GLOBALS['collation_connection'])) {
-        echo '            <input type="hidden" name="collation_connection" value="' . htmlspecialchars($GLOBALS['collation_connection']) . '" />' . "\n";
+        echo '            <input type="hidden" name="collation_connection" value="'
+            . htmlspecialchars($GLOBALS['collation_connection']) . '" />' . "\n";
     }
     if (isset($GLOBALS['convcharset'])) {
-        echo '            <input type="hidden" name="convcharset" value="' . htmlspecialchars($GLOBALS['convcharset']) . '" />' . "\n";
+        echo '            <input type="hidden" name="convcharset" value="'
+            . htmlspecialchars($GLOBALS['convcharset']) . '" />' . "\n";
     }
     if (isset($GLOBALS['db'])) {
-        echo '            <input type="hidden" name="db" value="' . htmlspecialchars($GLOBALS['db']) . '" />' . "\n";
+        echo '            <input type="hidden" name="db" value="'
+            . htmlspecialchars($GLOBALS['db']) . '" />' . "\n";
     }
     if (isset($GLOBALS['table'])) {
-        echo '            <input type="hidden" name="table" value="' . htmlspecialchars($GLOBALS['table']) . '" />' . "\n";
+        echo '            <input type="hidden" name="table" value="'
+            . htmlspecialchars($GLOBALS['table']) . '" />' . "\n";
     }
     if (isset($GLOBALS['server'])) {
-        echo '            <input type="hidden" name="server" value="' . ((int)$GLOBALS['server']) . '" />' . "\n";
+        echo '            <input type="hidden" name="server" value="'
+            . ((int)$GLOBALS['server']) . '" />' . "\n";
     }
     if ($use_fieldset) {
         echo '<fieldset>';
         echo '<legend>';
     }
     ?>
+
             Language <a href="./translators.html" target="documentation"><?php
             if ( $cfg['ReplaceHelpImg'] ) {
-                echo '<img class="icon" src="' . $GLOBALS['pmaThemeImage'] . 'b_info.png" width="11" height="11" alt="Info" />';
+                echo '<img class="icon" src="' . $GLOBALS['pmaThemeImage']
+                    . 'b_info.png" width="11" height="11" alt="Info" />';
             } else { echo '(*)'; }
 ?></a>
     <?php
@@ -59,8 +67,8 @@ function PMA_select_language($use_fieldset = FALSE) {
         echo ':';
     }
     ?>
-    <select name="lang" onchange="this.form.submit();">
 
+    <select name="lang" onchange="this.form.submit();">
     <?php
 
     uasort($GLOBALS['available_languages'], 'PMA_language_cmp');
@@ -69,8 +77,8 @@ function PMA_select_language($use_fieldset = FALSE) {
 
         // Include native name if non empty
         if (!empty($tmplang[3])) {
-            $lang_name = '<bdo dir="ltr" lang="' . $tmplang[2] . '">'
-                . htmlentities($tmplang[3], ENT_COMPAT, 'UTF-8') . '</bdo> - ' . $lang_name;
+            $lang_name = htmlentities($tmplang[3], ENT_COMPAT, 'UTF-8') . ' - '
+                . $lang_name;
         }
 
         // Include charset if it makes sense
@@ -85,31 +93,36 @@ function PMA_select_language($use_fieldset = FALSE) {
             $selected = '';
         }
 
-        echo '                        ';
-        echo '<option value="' . $id . '"' . $selected . '>' . $lang_name . '</option>' . "\n";
+        echo '        ';
+        echo '<option value="' . $id . '"' . $selected . '>' . $lang_name
+            . '</option>' . "\n";
     }
     ?>
-                </select>
+
+    </select>
     <?php
     if ($use_fieldset) {
         echo '</fieldset>';
     }
     ?>
-                <noscript>
+
+    <noscript>
     <?php
     if ($use_fieldset) {
         echo '<fieldset class="tblFooters">';
     }
     ?>
-                <input type="submit" value="Go" />
+
+        <input type="submit" value="Go" />
     <?php
     if ($use_fieldset) {
         echo '</fieldset>';
     }
     ?>
-                </noscript>
-            </bdo>
-            </form>
-<?php
+
+    </noscript>
+    </bdo>
+</form>
+    <?php
 } // End of function PMA_select_language
 ?>
