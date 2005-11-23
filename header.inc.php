c55e55c478cce47c776c45f0843f833a711437b3
Michal Čihař <michal@cihar.com>
2005-11-23 18:30:23 +0000

    Remove unused code.

--- header.inc.php
+++ header.inc.php
@@ -24,23 +24,8 @@ if (empty($GLOBALS['is_header_sent'])) {
 
     require_once('./libraries/header_http.inc.php');
     require_once('./libraries/header_meta_style.inc.php');
-    /* replaced 2004-05-05 by Michael Keck (mkkeck)
-    $title     = '';
-    if (isset($GLOBALS['db'])) {
-        $title .= str_replace('\'', '\\\'', $GLOBALS['db']);
-    }
-    if (isset($GLOBALS['table'])) {
-        $title .= (empty($title) ? '' : '.') . str_replace('\'', '\\\'', $GLOBALS['table']);
-    }
-    if (!empty($GLOBALS['cfg']['Server']) && isset($GLOBALS['cfg']['Server']['host'])) {
-        $title .= (empty($title) ? 'phpMyAdmin ' : ' ')
-               . sprintf($GLOBALS['strRunning'], (empty($GLOBALS['cfg']['Server']['verbose']) ? str_replace('\'', '\\\'', $GLOBALS['cfg']['Server']['host']) : str_replace('\'', '\\\'', $GLOBALS['cfg']['Server']['verbose'])));
-    }
-    $title     .= (empty($title) ? '' : ' - ') . 'phpMyAdmin ' . PMA_VERSION;
-    */
-    /* the new one
-     * 2004-05-05: replaced by Michael Keck (mkkeck)
-     */
+
+    // generate title
     $title     = '';
     if ($cfg['ShowHttpHostTitle']) {
         $title .= (empty($GLOBALS['cfg']['SetHttpHostTitle']) && isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $GLOBALS['cfg']['SetHttpHostTitle']) . ' / ';
