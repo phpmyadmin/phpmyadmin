6ac06df7f13cc611aa6d88101c73e4921d307d22
Sebastian Mendel <cybot_tm@users.sourceforge.net>
2005-11-29 12:49:35 +0000

    make use of new PMA_Theme and PMA_Theme_Manager classes

--- themes.php
+++ themes.php
@@ -31,39 +31,7 @@ function takeThis(what){
 <body id="bodythemes">
 <h1>phpMyAdmin - <?php echo $strTheme; ?></h1>
 <?php
-foreach ($available_themes_choices AS $PMA_Theme) {
-    $screen_directory = $path_to_themes . $PMA_Theme;
-
-    // check for theme requires/name
-    unset($theme_name, $theme_generation, $theme_version);
-    @include($path_to_themes . $PMA_Theme . '/info.inc.php');
-
-    // did it set correctly?
-    if (!isset($theme_name, $theme_generation, $theme_version))
-        continue; // invalid theme
-
-    if ($theme_generation != PMA_THEME_GENERATION)
-        continue; // different generation
-
-    if ($theme_version < PMA_THEME_VERSION)
-        continue; // too old version
-
-    if (is_dir($screen_directory) && @file_exists($screen_directory.'/screen.png')) {
-        // if screen exists then output
-        ?>
-<h2><?php echo htmlspecialchars( $theme_name ); ?></h2>
-
-<p> <a href="index.php?set_theme=<?php echo $PMA_Theme; ?>&amp;<?php echo PMA_generate_common_url(); ?>"
-        target="_top"
-        onclick="takeThis('<?php echo addslashes( $PMA_Theme ); ?>'); return false;">
-    <img src="<?php echo $screen_directory; ?>/screen.png" border="1"
-        alt="<?php echo htmlspecialchars( $theme_name ); ?>"
-        title="<?php echo htmlspecialchars( $theme_name ); ?>" /><br />
-    [ <b><?php echo $strTakeIt; ?></b> ]</a>
-</p>
-        <?php
-    } // end 'screen output'
-} // end 'open themes'
+$_SESSION['PMA_Theme_Manager']->printPreviews();
 ?>
 </body>
 </html>
