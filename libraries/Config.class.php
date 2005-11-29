687ea83a627924066e0c8a3398730063bb616709
Sebastian Mendel <cybot_tm@users.sourceforge.net>
2005-11-29 17:41:27 +0000

    added checkPmaAbsoluteUri()

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -15,11 +15,6 @@ class PMA_Config {
     var $settings = array();
 
     /**
-     * @var boolean
-     */
-    var $success_apply_user_config = true;
-
-    /**
      * @var string  config source
      */
     var $source = '';
@@ -32,7 +27,17 @@ class PMA_Config {
     /**
      * @var boolean
      */
-    var $cannot_load_configuration_defaults = false;
+    var $error_config_file = false;
+
+    /**
+     * @var boolean
+     */
+    var $error_config_default_file = false;
+
+    /**
+     * @var boolean
+     */
+    var $error_pma_uri = false;
 
     /**
      * @var array
@@ -59,14 +64,14 @@ class PMA_Config {
      *
      * @uses    file_exists()
      * @uses    $this->default_source
-     * @uses    $this->cannot_load_configuration_defaults
+     * @uses    $this->error_config_default_file
      * @uses    $this->cfg
      * @return  boolean     success
      */
     function loadDefaults() {
         $cfg = array();
         if ( ! file_exists( $this->default_source ) ) {
-            $this->cannot_load_configuration_defaults = true;
+            $this->error_config_default_file = true;
             return false;
         }
         include $this->default_source;
@@ -91,15 +96,15 @@ class PMA_Config {
          */
         $old_error_reporting = error_reporting( 0 );
         if ( function_exists( 'file_get_contents' ) ) {
-            $this->success_apply_user_config =
+            $this->error_config_file =
                 eval( '?>' . file_get_contents( $this->setSource() ) );
         } else {
-            $this->success_apply_user_config =
+            $this->error_config_file =
                 eval( '?>' . implode( '\n', file( $this->setSource() ) ) );
         }
         error_reporting( $old_error_reporting );
 
-        if ( $this->success_apply_user_config ) {
+        if ( $this->error_config_file ) {
             $this->source_mtime = filemtime( $this->getSource() );
         }
     }
@@ -132,6 +137,10 @@ class PMA_Config {
         return NULL;
     }
 
+    function set( $setting, $value ) {
+        $this->cfg[$setting] = $value;
+    }
+
     /**
      * returns source for current config
      * @return  string  config source
@@ -147,6 +156,130 @@ class PMA_Config {
      */
     function PMA_Config( $source ) {
         $this->__construct( $source );
+    }
+
+    /**
+     * $cfg['PmaAbsoluteUri'] is a required directive else cookies won't be
+     * set properly and, depending on browsers, inserting or updating a
+     * record might fail
+     */
+    function checkPmaAbsoluteUri() {
+
+        // Setup a default value to let the people and lazy syadmins work anyway,
+        // they'll get an error if the autodetect code doesn't work
+        $pma_absolute_uri = $this->get('PmaAbsoluteUri');
+        if ( strlen( $pma_absolute_uri ) < 0 ) {
+
+            $url = array();
+
+            // At first we try to parse REQUEST_URI, it might contain full URI
+            if ( ! empty($_SERVER['REQUEST_URI'] ) ) {
+                $url = parse_url( $_SERVER['REQUEST_URI'] );
+            }
+
+            // If we don't have scheme, we didn't have full URL so we need to
+            // dig deeper
+            if ( empty( $url['scheme'] ) ) {
+                // Scheme
+                if ( ! empty( $_SERVER['HTTP_SCHEME'] ) ) {
+                    $url['scheme'] = $_SERVER['HTTP_SCHEME'];
+                } else {
+                    $url['scheme'] =
+                        !empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) != 'off'
+                            ? 'https'
+                            : 'http';
+                }
+
+                // Host and port
+                if ( ! empty( $_SERVER['HTTP_HOST'] ) ) {
+                    if ( strpos( $_SERVER['HTTP_HOST'], ':' ) !== false ) {
+                        list( $url['host'], $url['port'] ) =
+                            explode( ':', $_SERVER['HTTP_HOST'] );
+                    } else {
+                        $url['host'] = $_SERVER['HTTP_HOST'];
+                    }
+                } elseif ( ! empty( $_SERVER['SERVER_NAME'] ) ) {
+                    $url['host'] = $_SERVER['SERVER_NAME'];
+                } else {
+                    $this->error_pma_uri = true;
+                    return false;
+                }
+
+                // If we didn't set port yet...
+                if ( empty( $url['port'] ) && ! empty( $_SERVER['SERVER_PORT'] ) ) {
+                    $url['port'] = $_SERVER['SERVER_PORT'];
+                }
+
+                // And finally the path could be already set from REQUEST_URI
+                if ( empty( $url['path'] ) ) {
+                    if (!empty($_SERVER['PATH_INFO'])) {
+                        $path = parse_url($_SERVER['PATH_INFO']);
+                    } else {
+                        // PHP_SELF in CGI often points to cgi executable, so use it
+                        // as last choice
+                        $path = parse_url($_SERVER['PHP_SELF']);
+                    }
+                    $url['path'] = $path['path'];
+                }
+            }
+
+            // Make url from parts we have
+            $pma_absolute_uri = $url['scheme'] . '://';
+            // Was there user information?
+            if (!empty($url['user'])) {
+                $pma_absolute_uri .= $url['user'];
+                if (!empty($url['pass'])) {
+                    $pma_absolute_uri .= ':' . $url['pass'];
+                }
+                $pma_absolute_uri .= '@';
+            }
+            // Add hostname
+            $pma_absolute_uri .= $url['host'];
+            // Add port, if it not the default one
+            if ( ! empty( $url['port'] )
+              && ( ( $url['scheme'] == 'http' && $url['port'] != 80 )
+                || ( $url['scheme'] == 'https' && $url['port'] != 443 ) ) ) {
+                $pma_absolute_uri .= ':' . $url['port'];
+            }
+            // And finally path, without script name, the 'a' is there not to
+            // strip our directory, when path is only /pmadir/ without filename
+            $path = dirname($url['path'] . 'a');
+            // To work correctly within transformations overview:
+            if (defined('PMA_PATH_TO_BASEDIR') && PMA_PATH_TO_BASEDIR == '../../') {
+                $path = dirname(dirname($path));
+            }
+            $pma_absolute_uri .= $path . '/';
+
+            // We used to display a warning if PmaAbsoluteUri wasn't set, but now
+            // the autodetect code works well enough that we don't display the
+            // warning at all. The user can still set PmaAbsoluteUri manually.
+            // See
+            // http://sf.net/tracker/?func=detail&aid=1257134&group_id=23067&atid=377411
+
+        } else {
+            // The URI is specified, however users do often specify this
+            // wrongly, so we try to fix this.
+
+            // Adds a trailing slash et the end of the phpMyAdmin uri if it
+            // does not exist.
+            if (substr($pma_absolute_uri, -1) != '/') {
+                $pma_absolute_uri .= '/';
+            }
+
+            // If URI doesn't start with http:// or https://, we will add
+            // this.
+            if ( substr($pma_absolute_uri, 0, 7) != 'http://'
+              && substr($pma_absolute_uri, 0, 8) != 'https://' ) {
+                $pma_absolute_uri =
+                    (!empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) != 'off'
+                        ? 'https'
+                        : 'http')
+                    . ':' . (substr($pma_absolute_uri, 0, 2) == '//' ? '' : '//')
+                    . $pma_absolute_uri;
+            }
+        }
+
+        $this->set( 'PmaAbsoluteUri', $pma_absolute_uri );
     }
 
     /**
