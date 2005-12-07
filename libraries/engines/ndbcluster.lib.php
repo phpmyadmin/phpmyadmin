b15044a6ab0bab365ac08517d37aa9d5858cbf13
Sebastian Mendel <cybot_tm@users.sourceforge.net>
2005-12-07 10:41:35 +0000

    NEW

---
+++ libraries/engines/ndbcluster.lib.php
@@ -0,0 +1,38 @@
+<?php
+/* $Id$ */
+// vim: expandtab sw=4 ts=4 sts=4:
+
+class PMA_StorageEngine_ndbcluster extends PMA_StorageEngine
+{
+    /**
+     * @return  array
+     */
+    function getVariables()
+    {
+        return array(
+            'ndb_connectstring' => array(
+            ),
+         );
+    }
+
+    /**
+     * @return  string  SQL query LIKE pattern
+     */
+    function getVariablesLikePattern()
+    {
+        return 'ndb\\_%';
+    }
+
+    /**
+     * returns string with filename for the MySQL helppage
+     * about this storage engne
+     *
+     * @return  string  mysql helppage filename
+     */
+    function getMysqlHelpPage()
+    {
+        return 'ndbcluster';
+    }
+}
+
+?>
