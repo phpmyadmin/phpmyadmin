87abe9a4776add2cc367232717145c8f1ed05367
Sebastian Mendel <cybot_tm@users.sourceforge.net>
2005-12-07 10:55:34 +0000

    NEW

---
+++ libraries/engines/binlog.lib.php
@@ -0,0 +1,19 @@
+<?php
+/* $Id$ */
+// vim: expandtab sw=4 ts=4 sts=4:
+
+class PMA_StorageEngine_binlog extends PMA_StorageEngine
+{
+    /**
+     * returns string with filename for the MySQL helppage
+     * about this storage engne
+     *
+     * @return  string  mysql helppage filename
+     */
+    function getMysqlHelpPage()
+    {
+        return 'binary-log';
+    }
+}
+
+?>
