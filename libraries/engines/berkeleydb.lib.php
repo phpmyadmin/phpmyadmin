00cc9dd4310ffab6b40f8d2e6f1cb261d0a3800c
Sebastian Mendel <cybot_tm@users.sourceforge.net>
2005-12-07 10:28:50 +0000

    - PEAR coding standard - added comments - added more status variables

--- libraries/engines/berkeleydb.lib.php
+++ libraries/engines/berkeleydb.lib.php
@@ -2,7 +2,10 @@
 /* $Id$ */
 // vim: expandtab sw=4 ts=4 sts=4:
 
-include_once('./libraries/engines/bdb.lib.php');
-class PMA_StorageEngine_berkeleydb extends PMA_StorageEngine_bdb {}
+include_once './libraries/engines/bdb.lib.php';
+
+class PMA_StorageEngine_berkeleydb extends PMA_StorageEngine_bdb
+{
+}
 
 ?>
