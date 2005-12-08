f8024290043c08f1933dcd3d8a0c28d758b35d4a
Sebastian Mendel <cybot_tm@users.sourceforge.net>
2005-12-08 12:14:34 +0000

    added some comments and made some code more readable

--- libraries/tbl_properties_table_info.inc.php
+++ libraries/tbl_properties_table_info.inc.php
@@ -2,9 +2,16 @@
 /* $Id$ */
 // vim: expandtab sw=4 ts=4 sts=4:
 
-// this should be recoded as functions, to avoid messing with global
-// variables
+/**
+ * extracts table properties from create statement
+ *
+ * @TODO this should be recoded as functions,
+ * to avoid messing with global variables
+ */
 
+/**
+ * requirements
+ */
 require_once('./libraries/common.lib.php');
 
 // Check parameters
@@ -20,13 +27,14 @@ global $showtable, $tbl_is_view, $tbl_type, $show_comment, $tbl_collation,
 /**
  * Gets table informations
  */
-
 // Seems we need to do this in MySQL 5.0.2,
 // otherwise error #1046, no database selected
-PMA_DBI_select_db($db);
+PMA_DBI_select_db($GLOBALS['db']);
 
 // The 'show table' statement works correct since 3.23.03
-$table_info_result   = PMA_DBI_query('SHOW TABLE STATUS LIKE \'' . PMA_sqlAddslashes($table, TRUE) . '\';', NULL, PMA_DBI_QUERY_STORE);
+$table_info_result   = PMA_DBI_query(
+    'SHOW TABLE STATUS LIKE \'' . PMA_sqlAddslashes($GLOBALS['table'], true) . '\';',
+    null, PMA_DBI_QUERY_STORE);
 
 // need this test because when we are creating a table, we get 0 rows
 // from the SHOW TABLE query
@@ -34,43 +42,59 @@ $table_info_result   = PMA_DBI_query('SHOW TABLE STATUS LIKE \'' . PMA_sqlAddsla
 
 if ($table_info_result && PMA_DBI_num_rows($table_info_result) > 0) {
     $showtable           = PMA_DBI_fetch_assoc($table_info_result);
+    PMA_DBI_free_result($table_info_result);
+    unset( $table_info_result );
+
     if (!isset($showtable['Type']) && isset($showtable['Engine'])) {
         $showtable['Type'] =& $showtable['Engine'];
     }
     // MySQL < 5.0.13 returns "view", >= 5.0.13 returns "VIEW"
-    if (PMA_MYSQL_INT_VERSION >= 50000 && !isset($showtable['Type']) && isset($showtable['Comment']) && strtoupper($showtable['Comment']) == 'VIEW') {
-        $tbl_is_view     = TRUE;
-        $tbl_type        = $strView;
-        $show_comment    = NULL;
+    if ( PMA_MYSQL_INT_VERSION >= 50000 && !isset($showtable['Type'])
+      && isset($showtable['Comment'])
+      && strtoupper($showtable['Comment']) == 'VIEW' ) {
+        $tbl_is_view     = true;
+        $tbl_type        = $GLOBALS['strView'];
+        $show_comment    = null;
     } else {
-        $tbl_is_view     = FALSE;
-        $tbl_type        = isset($showtable['Type']) ? strtoupper($showtable['Type']) : '';
+        $tbl_is_view     = false;
+        $tbl_type        = isset($showtable['Type'])
+            ? strtoupper($showtable['Type'])
+            : '';
         // a new comment could be coming from tbl_properties_operations.php
         // and we want to show it in the header
         if (isset($submitcomment) && isset($comment)) {
             $show_comment = $comment;
         } else {
-            $show_comment    = (isset($showtable['Comment']) ? $showtable['Comment'] : '');
+            $show_comment    = isset($showtable['Comment'])
+                ? $showtable['Comment']
+                : '';
         }
     }
-    $tbl_collation       = empty($showtable['Collation']) ? '' : $showtable['Collation'];
-    
-    if ( NULL === $showtable['Rows'] ) {
-        $showtable['Rows']   = PMA_countRecords( $GLOBALS['db'],
-            $showtable['Name'], $return = true, $force_exact = true );
-    }
-    $table_info_num_rows = (isset($showtable['Rows']) ? $showtable['Rows'] : 0);
-    $auto_increment      = (isset($showtable['Auto_increment']) ? $showtable['Auto_increment'] : '');
+    $tbl_collation       = empty($showtable['Collation'])
+        ? ''
+        : $showtable['Collation'];
 
-    $tmp                 = isset($showtable['Create_options']) ? explode(' ', $showtable['Create_options']) : array();
-    $tmp_cnt             = count($tmp);
-    for ($i = 0; $i < $tmp_cnt; $i++) {
-        $tmp1            = explode('=', $tmp[$i]);
-        if (isset($tmp1[1])) {
-            $$tmp1[0]    = $tmp1[1];
+    if ( null === $showtable['Rows'] ) {
+        $showtable['Rows']   = PMA_countRecords( $GLOBALS['db'],
+            $showtable['Name'], true, true );
+    }
+    $table_info_num_rows = isset($showtable['Rows']) ? $showtable['Rows'] : 0;
+    $auto_increment      = isset($showtable['Auto_increment'])
+        ? $showtable['Auto_increment']
+        : '';
+
+    $create_options      = isset($showtable['Create_options'])
+        ? explode(' ', $showtable['Create_options'])
+        : array();
+
+    // export create options by its name as variables into gloabel namespace
+    // f.e. pack_keys=1 becomes available as $pack_keys with value of '1'
+    foreach ( $create_options as $each_create_option ) {
+        $each_create_option = explode('=', $each_create_option);
+        if ( isset( $each_create_option[1] ) ) {
+            $$each_create_option[0]    = $each_create_option[1];
         }
-    } // end for
-    PMA_DBI_free_result($table_info_result);
-    unset($tmp1, $tmp, $table_info_result);
+    }
+    unset( $create_options, $each_create_option );
 } // end if
 ?>
