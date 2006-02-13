f4b8fae9cf9046ea44c510696f7693062643c835
Sebastian Mendel <cybot_tm@users.sourceforge.net>
2006-02-13 13:21:18 +0000

    fixed bug #1409893 - links in navigation frame opens new window

--- libraries/footer.inc.php
+++ libraries/footer.inc.php
@@ -10,62 +10,63 @@
  * 
  */
 
-require_once('./libraries/relation.lib.php'); // for PMA_setHistory()
+require_once './libraries/relation.lib.php'; // for PMA_setHistory()
 
 /**
- * Query window
+ * updates javascript variables in index.php for coorect working with querywindow
+ * and navigation frame refreshing
  */
-
-// If query window is open, update with latest selected db/table.
-    ?>
+?>
 <script type="text/javascript" language="javascript">
 //<![CDATA[
-    <?php
-    if ( ! isset( $GLOBALS['no_history'] ) && isset( $GLOBALS['db'] ) && strlen($GLOBALS['db']) && empty( $GLOBALS['error_message'] ) ) {
-        $table = isset( $GLOBALS['table'] ) ? $GLOBALS['table'] : '';
-        // updates current settings
-        ?>
-    window.parent.setAll( '<?php echo $GLOBALS['lang']; ?>', '<?php echo $GLOBALS['collation_connection']; ?>', '<?php echo $GLOBALS['server']; ?>', '<?php echo $GLOBALS['db']; ?>', '<?php echo $table; ?>' );
-        <?php
-    }
-    
-    if ( ! empty( $GLOBALS['reload'] ) ) {
-        ?>
-    window.parent.refreshLeft();
-        <?php
-    }
+<?php
+if (! isset($GLOBALS['no_history']) && isset($GLOBALS['db'])
+  && strlen($GLOBALS['db']) && empty($GLOBALS['error_message'])) {
+    $table = isset($GLOBALS['table']) ? $GLOBALS['table'] : ''; ?>
+// updates current settings
+window.parent.setAll('<?php echo $GLOBALS['lang']; ?>', '<?php echo $GLOBALS['collation_connection']; ?>', '<?php echo $GLOBALS['server']; ?>', '<?php echo $GLOBALS['db']; ?>', '<?php echo $table; ?>');
+<?php } ?>
 
-    if ( ! isset( $GLOBALS['no_history'] ) && empty( $GLOBALS['error_message'] ) ) {
-        if ( isset( $GLOBALS['LockFromUpdate'] ) && $GLOBALS['LockFromUpdate'] == '1' && isset( $GLOBALS['sql_query'] ) ) {
-            // When the button 'LockFromUpdate' was selected in the querywindow,
-            // it does not submit it's contents to
-            // itself. So we create a SQL-history entry here.
-            if ($GLOBALS['cfg']['QueryHistoryDB'] && $GLOBALS['cfgRelation']['historywork']) {
-                PMA_setHistory( ( isset( $GLOBALS['db'] ) ? $GLOBALS['db'] : '' ),
-                    ( isset( $GLOBALS['table'] ) ? $GLOBALS['table'] : '' ),
-                    $GLOBALS['cfg']['Server']['user'],
-                    $GLOBALS['sql_query'] );
-            }
+<?php if (! empty($GLOBALS['reload'])) { ?>
+// refresh navigation frame content
+window.parent.refreshLeft();
+<?php } ?>
+
+<?php
+if (! isset($GLOBALS['no_history']) && empty($GLOBALS['error_message'])) {
+    if (isset($GLOBALS['LockFromUpdate']) && $GLOBALS['LockFromUpdate'] == '1'
+      && isset($GLOBALS['sql_query'])) {
+        // When the button 'LockFromUpdate' was selected in the querywindow,
+        // it does not submit it's contents to
+        // itself. So we create a SQL-history entry here.
+        if ($GLOBALS['cfg']['QueryHistoryDB'] && $GLOBALS['cfgRelation']['historywork']) {
+            PMA_setHistory((isset($GLOBALS['db']) ? $GLOBALS['db'] : ''),
+                (isset($GLOBALS['table']) ? $GLOBALS['table'] : ''),
+                $GLOBALS['cfg']['Server']['user'],
+                $GLOBALS['sql_query']);
         }
-        ?>
-    window.parent.reload_querywindow(
-        "<?php echo isset( $GLOBALS['db'] ) ? addslashes( $GLOBALS['db'] ) : '' ?>",
-        "<?php echo isset( $GLOBALS['table'] ) ? addslashes( $GLOBALS['table'] ) : '' ?>",
-        "<?php echo isset( $GLOBALS['sql_query'] ) ? urlencode( $GLOBALS['sql_query'] ) : ''; ?>" );
-        <?php
-    }
-
-    if ( ! empty( $GLOBALS['focus_querywindow'] ) ) {
-        ?>
-    if ( parent.querywindow && !parent.querywindow.closed && parent.querywindow.location) {
-        self.focus();
-    }
-        <?php
     }
     ?>
+// set current db, table and sql query in the querywindow
+window.parent.reload_querywindow(
+    "<?php echo isset($GLOBALS['db']) ? addslashes($GLOBALS['db']) : '' ?>",
+    "<?php echo isset($GLOBALS['table']) ? addslashes($GLOBALS['table']) : '' ?>",
+    "<?php echo isset($GLOBALS['sql_query']) ? urlencode($GLOBALS['sql_query']) : ''; ?>");
+<?php } ?>
+
+<?php if (! empty($GLOBALS['focus_querywindow'])) { ?>
+// set focus to the querywindow
+if (parent.querywindow && !parent.querywindow.closed && parent.querywindow.location) {
+    self.focus();
+}
+<?php } ?>
+
+// reset content frame name, as querywindow needs to set a unique name
+// before submitting form data, and navigation frame needs the original name
+window.parent.frames[1].name = 'frame_content';
 //]]>
 </script>
-    <?php
+<?php
 
 // Link to itself to replicate windows including frameset
 if (!isset($GLOBALS['checked_special'])) {
@@ -86,26 +87,26 @@ if (isset($_SERVER['SCRIPT_NAME']) && empty($_POST) && !$GLOBALS['checked_specia
 /**
  * Close database connections
  */
-if ( isset( $GLOBALS['controllink'] ) && $GLOBALS['controllink'] ) {
-    @PMA_DBI_close( $GLOBALS['controllink'] );
+if (isset($GLOBALS['controllink']) && $GLOBALS['controllink']) {
+    @PMA_DBI_close($GLOBALS['controllink']);
 }
-if ( isset( $GLOBALS['userlink'] ) && $GLOBALS['userlink'] ) {
-    @PMA_DBI_close( $GLOBALS['userlink'] );
+if (isset($GLOBALS['userlink']) && $GLOBALS['userlink']) {
+    @PMA_DBI_close($GLOBALS['userlink']);
 }
 
 // Include possible custom footers
-require_once('./libraries/footer_custom.inc.php');
+require_once './libraries/footer_custom.inc.php';
 
 /**
  * Generates profiling data if requested
  */
-if ( ! empty( $GLOBALS['cfg']['DBG']['enable'] )
-  && ! empty( $GLOBALS['cfg']['DBG']['profile']['enable'] ) ) {
+if (! empty($GLOBALS['cfg']['DBG']['enable'])
+  && ! empty($GLOBALS['cfg']['DBG']['profile']['enable'])) {
     //run the basic setup code first
-    require_once('./libraries/dbg/setup.php');
+    require_once './libraries/dbg/setup.php';
     //if the setup ran fine, then do the profiling
-    if ( ! empty( $GLOBALS['DBG'] ) ) {
-        require_once('./libraries/dbg/profiling.php');
+    if (! empty($GLOBALS['DBG'])) {
+        require_once './libraries/dbg/profiling.php';
         dbg_dump_profiling_results();
     }
 }
@@ -117,9 +118,9 @@ if ( ! empty( $GLOBALS['cfg']['DBG']['enable'] )
 /**
  * Sends bufferized data
  */
-if ( ! empty( $GLOBALS['cfg']['OBGzip'] )
-  && ! empty( $GLOBALS['ob_mode'] ) ) {
-    PMA_outBufferPost( $GLOBALS['ob_mode'] );
+if (! empty($GLOBALS['cfg']['OBGzip'])
+  && ! empty($GLOBALS['ob_mode'])) {
+    PMA_outBufferPost($GLOBALS['ob_mode']);
 }
 
 /**
