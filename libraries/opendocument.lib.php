77d003c05de7f969469b186a8d85bba83453b117
Michal Čihař <michal@cihar.com>
2006-04-28 09:07:46 +0000

    Add document metainformation.

--- libraries/opendocument.lib.php
+++ libraries/opendocument.lib.php
@@ -22,10 +22,23 @@ function PMA_createOpenDocument($mime, $data) {
     $zipfile = new zipfile();
     $zipfile -> addFile($mime, 'mimetype');
     $zipfile -> addFile($data, 'content.xml');
-    $zipfile -> addFile('<?xml version="1.0" encoding="UTF-8"?>'
+    $zipfile -> addFile('<?xml version="1.0" encoding="UTF-8"?'. '>'
+        . '<office:document-meta '
+            . 'xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" '
+            . 'xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" '
+            . 'office:version="1.0">'
+        . '<office:meta>'
+        . '<meta:generator>phpMyAdmin ' . PMA_VERSION . '</meta:generator>'
+        . '<meta:initial-creator>phpMyAdmin ' . PMA_VERSION . '</meta:initial-creator>'
+        . '<meta:creation-date>' . strftime('%Y-%m-%dT%H:%M:%S') . '</meta:creation-date>'
+        . '</office:meta>'
+        . '</office:document-meta>'
+        , 'meta.xml');
+    $zipfile -> addFile('<?xml version="1.0" encoding="UTF-8"?' . '>'
         . '<manifest:manifest xmlns:manifest="urn:oasis:names:tc:opendocument:xmlns:manifest:1.0">'
         . '<manifest:file-entry manifest:media-type="' . $mime . '" manifest:full-path="/"/>'
         . '<manifest:file-entry manifest:media-type="text/xml" manifest:full-path="content.xml"/>'
+        . '<manifest:file-entry manifest:media-type="text/xml" manifest:full-path="meta.xml"/>'
         . '</manifest:manifest>'
         , 'META-INF/manifest.xml');
     return $zipfile -> file();
