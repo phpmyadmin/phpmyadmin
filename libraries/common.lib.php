39c52ac207f3889be490c735b6a03080bd6d8817
Sebastian Mendel <cybot_tm@users.sourceforge.net>
2006-08-02 12:18:09 +0000

    now really (forgot to return the unquoted  string)

--- libraries/common.lib.php
+++ libraries/common.lib.php
@@ -750,6 +750,7 @@ if (!defined('PMA_MINIMUM_COMMON')) {
                  $unquoted_string = substr($quoted_string, 1, -1);
                  // replace escaped quotes
                  $unquoted_string = str_replace($quote . $quote, $quote, $unquoted_string);
+                 return $unquoted_string;
              }
         }
 
