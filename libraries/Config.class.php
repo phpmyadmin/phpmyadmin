3ab9bb963b121d003804880994a1ea162b7e21a4
Marc Delisle <marc@infomarc.info>
2006-08-10 11:50:48 +0000

    oops 2.10.0-dev

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -80,7 +80,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '2.9.10-dev');
+        $this->set('PMA_VERSION', '2.10.0-dev');
         /**
          * @deprecated
          */
