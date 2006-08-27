6c409172a2f85a4c1b397d53a55791bc65920673
Marc Delisle <marc@infomarc.info>
2006-08-27 12:36:35 +0000

    patch #1523543, possibility of vertical line between frames

--- themes/darkblue_orange/layout.inc.php
+++ themes/darkblue_orange/layout.inc.php
@@ -30,7 +30,7 @@ $GLOBALS['cfg']['MainColor']                = '#000000';
 
 // background for the main frame
 $GLOBALS['cfg']['MainBackground']           = '#ffffff';
-//$GLOBALS['cfg']['MainBackground']       = 'url(../' . $_SESSION['PMA_Theme']->getImgPath() . 'vertical_line.png)';
+//$GLOBALS['cfg']['MainBackground']       = '#ffffff url(../' . $_SESSION['PMA_Theme']->getImgPath() . 'vertical_line.png) repeat-y';
 
 // foreground (text) color of the pointer in browse mode
 $GLOBALS['cfg']['BrowsePointerColor']       = '#000000';
@@ -57,12 +57,6 @@ $GLOBALS['cfg']['FontFamily']           = 'Verdana, Arial, Helvetica, sans-serif
  * fixed width font family, used in textarea
  */
 $GLOBALS['cfg']['FontFamilyFixed']      = 'monospace';
-/**
- * font size as a valid css font size value,
- * if not set the browser default will be used
- * (depending on browser, DTD and system settings)
- */
-$GLOBALS['cfg']['FontSize']             = '';
 
 /**
  * tables
