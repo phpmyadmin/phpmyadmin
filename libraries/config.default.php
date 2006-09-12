27a600dba8c84c4bfcae064fea05ea569e9991c8
Marc Delisle <marc@infomarc.info>
2006-09-12 17:15:14 +0000

    bug #1556934, SQL compatibility mode on export

--- libraries/config.default.php
+++ libraries/config.default.php
@@ -310,7 +310,7 @@ $cfg['Export']['latex_structure_label']     = 'tab:__TABLE__-structure';
 
 $cfg['Export']['sql_structure']             = TRUE;
 $cfg['Export']['sql_data']                  = TRUE;
-$cfg['Export']['sql_compat']                = 'NONE';
+$cfg['Export']['sql_compatibility']         = 'NONE';
 $cfg['Export']['sql_disable_fk']            = FALSE;
 $cfg['Export']['sql_use_transaction']       = FALSE;
 $cfg['Export']['sql_drop_database']         = FALSE;
