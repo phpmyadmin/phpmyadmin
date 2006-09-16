c825c227c179c4609ac2a4b1836957383534a2c0
Marc Delisle <marc@infomarc.info>
2006-09-16 20:03:30 +0000

    new

---
+++ graphivore/info.inc.php
@@ -0,0 +1,11 @@
+<?php
+/* $Id$ */
+/* Theme information */
+/*
+ * created 2005-09-15 by Yves De Troeyer (ydt@ydtconcept.org)
+ * for 2.6.1 and higher
+*/
+$theme_name = 'Graphivore';
+$theme_version = 1;
+$theme_generation = 2;
+?>
