e190478d01567f9f2bd8d3f0b4eb4447e577fa39
Marc Delisle <marc@infomarc.info>
2007-03-16 16:38:42 +0000

    vim complains with a */ at the end of options

--- libraries/auth/cookie.auth.lib.php
+++ libraries/auth/cookie.auth.lib.php
@@ -1,5 +1,5 @@
 <?php
-/* vim: expandtab sw=4 ts=4 sts=4: */
+// vim: expandtab sw=4 ts=4 sts=4:
 /**
  * Set of functions used to run cookie based authentication.
  * Thanks to Piotr Roszatycki <d3xter at users.sourceforge.net> and
