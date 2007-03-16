5272b1be7da995c62d9ed6a1bd64f95304dc6957
Sebastian Mendel <cybot_tm@users.sourceforge.net>
2007-03-16 11:51:57 +0000

    test some environment wrap test output

--- test/AllTests.php
+++ test/AllTests.php
@@ -18,16 +18,22 @@ if (! defined('PMA_MAIN_METHOD')) {
  */
 require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
+//require_once 'PHPUnit/Util/TestDox/ResultPrinter.php.';
 require_once './test/FailTest.php';
 require_once './test/PMA_get_real_size_test.php';
 require_once './test/PMA_sanitize_test.php';
 require_once './test/PMA_pow_test.php';
+require_once './test/Environment_test.php';
 
 class AllTests
 {
     public static function main()
     {
-        PHPUnit_TextUI_TestRunner::run(self::suite());
+        $parameters = array();
+        //$parameters['testdoxHTMLFile'] = true;
+        //$parameters['printer'] = PHPUnit_Util_TestDox_ResultPrinter::factory('HTML');
+        //$parameters['printer'] = PHPUnit_Util_TestDox_ResultPrinter::factory('Text');
+        PHPUnit_TextUI_TestRunner::run(self::suite(), $parameters);
     }
 
     public static function suite()
@@ -35,6 +41,7 @@ class AllTests
         $suite = new PHPUnit_Framework_TestSuite('phpMyAdmin');
 
         //$suite->addTestSuite('FailTest');
+        $suite->addTestSuite('Environment_test');
         $suite->addTestSuite('PMA_get_real_size_test');
         $suite->addTestSuite('PMA_sanitize_test');
         $suite->addTestSuite('PMA_pow_test');
@@ -43,6 +50,8 @@ class AllTests
 }
 
 if (PMA_MAIN_METHOD == 'AllTests::main') {
+    echo '<pre>';
     AllTests::main();
+    echo '</pre>';
 }
 ?>
