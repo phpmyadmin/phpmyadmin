49ea233e65ce05b2796329a96162924140c9ebfa
Marc Delisle <marc@infomarc.info>
2007-05-13 00:37:11 +0000

    initial dark_lime

---
+++ dark_lime/info.inc.php
@@ -0,0 +1,19 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Theme information
+ *
+ * made by GamBit 
+ *
+ * @version $Id$
+ * @package phpMyAdmin-theme
+ * @subpackage Dark_lime
+ */
+
+/**
+  *
+ */
+
+$theme_name = 'dark_lime';
+$theme_full_version = '2.9';
+?>
