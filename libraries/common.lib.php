0588579a3f4f5294f1fc8339b2ed15b5f3b4abcb
Marc Delisle <marc@infomarc.info>
2007-06-24 10:42:02 +0000

    bug #1727138 HTML not encoded (more than 1000 characters)

--- libraries/common.lib.php
+++ libraries/common.lib.php
@@ -1407,6 +1407,7 @@ if (typeof(window.parent) != 'undefined'
             $max_characters = 1000;
             if (! defined('PMA_QUERY_TOO_BIG') && strlen($query_base) > $max_characters) {
                 define('PMA_QUERY_TOO_BIG',1);
+                $query_base = htmlspecialchars($sql_query);
             }
 
             // Parse SQL if needed
