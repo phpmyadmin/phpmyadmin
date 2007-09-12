5b5bb08c8497472c15790586390ab7141eb88684
Marc Delisle <marc@infomarc.info>
2007-09-12 09:40:59 +0000

    change class of message about MySQL lib to "notice"

--- main.php
+++ main.php
@@ -299,11 +299,6 @@ if (! empty($GLOBALS['PMA_errors']) && is_array($GLOBALS['PMA_errors'])) {
 }
 
 /**
- * Removed the "empty $cfg['PmaAbsoluteUri']" warning on 2005-08-23
- * See https://sourceforge.net/tracker/index.php?func=detail&aid=1257134&group_id=23067&atid=377411
- */
-
-/**
  * Warning if using the default MySQL privileged account
  * modified: 2004-05-05 mkkeck
  */
@@ -349,7 +344,7 @@ if (defined('PMA_MYSQL_INT_VERSION') && PMA_MYSQL_INT_VERSION < 50000) {
  * (a difference on the third digit does not count)
  */
 if ($server > 0 && substr(PMA_MYSQL_CLIENT_API, 0, 3) != substr(PMA_MYSQL_INT_VERSION, 0, 3)) {
-    echo '<div class="warning">'
+    echo '<div class="notice">'
      . PMA_sanitize(sprintf($strMysqlLibDiffersServerVersion,
             PMA_DBI_get_client_info(),
             substr(PMA_MYSQL_STR_VERSION, 0, strpos(PMA_MYSQL_STR_VERSION . '-', '-'))))
