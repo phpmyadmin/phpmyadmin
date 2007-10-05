113b4fbcf79ba3ebadc27987fb4ed523344e9a77
Sebastian Mendel <cybot_tm@users.sourceforge.net>
2007-10-05 09:46:30 +0000

    bug #1807923 [login] Login with html entities in password fails

--- libraries/cleanup.lib.php
+++ libraries/cleanup.lib.php
@@ -14,36 +14,29 @@
  * @access public
  * @author  Michal Cihar (michal@cihar.com)
  */
-function PMA_remove_request_vars(&$whitelist) {
+function PMA_remove_request_vars(&$whitelist)
+{
     // do not check only $_REQUEST because it could have been overwritten
     // and use type casting because the variables could have become
     // strings
     $keys = array_keys(array_merge((array)$_REQUEST, (array)$_GET, (array)$_POST, (array)$_COOKIE));
 
     foreach($keys as $key) {
-        if (!in_array($key, $whitelist)) {
+        if (! in_array($key, $whitelist)) {
             unset($_REQUEST[$key], $_GET[$key], $_POST[$key], $GLOBALS[$key]);
         } else {
             // allowed stuff could be compromised so escape it
             // we require it to be a string
-            if (isset($_REQUEST[$key]) && is_string($_REQUEST[$key])) {
-                $_REQUEST[$key] = htmlspecialchars($_REQUEST[$key], ENT_QUOTES);
-            } else {
+            if (isset($_REQUEST[$key]) && ! is_string($_REQUEST[$key])) {
                 unset($_REQUEST[$key]);
             }
-            if (isset($_POST[$key]) && is_string($_POST[$key])) {
-                $_POST[$key] = htmlspecialchars($_POST[$key], ENT_QUOTES);
-            } else {
+            if (isset($_POST[$key]) && ! is_string($_POST[$key])) {
                 unset($_POST[$key]);
             }
-            if (isset($_COOKIE[$key]) && is_string($_COOKIE[$key])) {
-                $_COOKIE[$key] = htmlspecialchars($_COOKIE[$key], ENT_QUOTES);
-            } else {
+            if (isset($_COOKIE[$key]) && ! is_string($_COOKIE[$key])) {
                 unset($_COOKIE[$key]);
             }
-            if (isset($_GET[$key]) && is_string($_GET[$key])) {
-                $_GET[$key] = htmlspecialchars($_GET[$key], ENT_QUOTES);
-            } else {
+            if (isset($_GET[$key]) && ! is_string($_GET[$key])) {
                 unset($_GET[$key]);
             }
         }
