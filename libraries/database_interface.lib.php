142154b4a8d22128574af3d8ba520705eedb57dd
Marc Delisle <marc@infomarc.info>
2007-10-06 20:03:52 +0000

    undefined variable when creating a table that exists

--- libraries/database_interface.lib.php
+++ libraries/database_interface.lib.php
@@ -611,7 +611,7 @@ function PMA_DBI_get_columns($database, $table, $full = false, $link = null)
     $fields = PMA_DBI_fetch_result(
         'SHOW ' . ($full ? 'FULL' : '') . ' COLUMNS
         FROM ' . PMA_backquote($database) . '.' . PMA_backquote($table),
-        'Fields', ($full ? null : 'Fields'), $link);
+        'Field', ($full ? null : 'Field'), $link);
     if (! is_array($fields) || count($fields) < 1) {
         return false;
     }
