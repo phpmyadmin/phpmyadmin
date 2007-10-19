8415b0049ef9b083f2b225e4fc9cccc5c32144f3
Marc Delisle <marc@infomarc.info>
2007-10-19 17:14:36 +0000

    do not display $strMysqlLibDiffersServerVersion if the client is mysqlnd

--- main.php
+++ main.php
@@ -287,14 +287,18 @@ if ($GLOBALS['using_mb_charset'] && !@extension_loaded('mbstring')) {
 
 /**
  * Warning about different MySQL library and server version
- * (a difference on the third digit does not count)
+ * (a difference on the third digit does not count).
+ * If someday there is a constant that we can check about mysqlnd, we can use it instead
+ * of strpos().
  */
-if ($server > 0 && substr(PMA_MYSQL_CLIENT_API, 0, 3) != substr(PMA_MYSQL_INT_VERSION, 0, 3)) {
+$_client_info = PMA_DBI_get_client_info();
+if ($server > 0 && strpos($_client_info, 'mysqlnd') === false && substr(PMA_MYSQL_CLIENT_API, 0, 3) != substr(PMA_MYSQL_INT_VERSION, 0, 3)) {
     trigger_error(PMA_sanitize(sprintf($strMysqlLibDiffersServerVersion,
-            PMA_DBI_get_client_info(),
+            $_client_info,
             substr(PMA_MYSQL_STR_VERSION, 0, strpos(PMA_MYSQL_STR_VERSION . '-', '-')))),
         E_USER_NOTICE);
 }
+unset($_client_info);
 
 /**
  * prints list item for main page
