1410002c8695ab03dd12656d3cc5adad50cbb0c7
Marc Delisle <marc@infomarc.info>
2007-11-02 16:45:49 +0000

    wrong default charset in case of broken session

--- libraries/select_lang.lib.php
+++ libraries/select_lang.lib.php
@@ -343,8 +343,9 @@ if (empty($GLOBALS['convcharset'])) {
     if (isset($_COOKIE['pma_charset'])) {
         $GLOBALS['convcharset'] = $_COOKIE['pma_charset'];
     } else {
-        // session.save_path might point to a bad folder
-        $GLOBALS['convcharset'] = $GLOBALS['cfg']['DefaultCharset'];
+        // session.save_path might point to a bad folder, in which case
+        // $GLOBALS['cfg'] would not exist
+        $GLOBALS['convcharset'] = isset($GLOBALS['cfg']['DefaultCharset']) ? $GLOBALS['cfg']['DefaultCharset'] : 'utf-8';
     }
 }
 
