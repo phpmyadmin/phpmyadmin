4b7628f6997ea171e699c07e3226779aed6fb1b1
Marc Delisle <marc@infomarc.info>
2007-11-15 17:51:46 +0000

    bug #1828265 [privileges] No weird characters in generated password

--- js/server_privileges.js
+++ js/server_privileges.js
@@ -79,7 +79,10 @@ function checkAddUser(the_form)
  * @return  boolean  always true
  */
 function suggestPassword() {
-    var pwchars = "abcdefhjmnpqrstuvwxyz23456789ABCDEFGHJKLMNPQRSTUVWYXZ.,:";
+    // restrict the password to just letters and numbers to avoid problems:
+    // "editors and viewers regard the password as multiple words and
+    // things like double click no longer work"
+    var pwchars = "abcdefhjmnpqrstuvwxyz23456789ABCDEFGHJKLMNPQRSTUVWYXZ";
     var passwordlength = 16;    // do we want that to be dynamic?  no, keep it simple :)
     var passwd = document.getElementById('generated_pw');
     passwd.value = '';
