17269bc15c5d8d7316a0f2304f640fb95af57c10
Marc Delisle <marc@infomarc.info>
2008-01-05 14:46:01 +0000

    2.11.4-rc1

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -85,7 +85,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '2.11.4-dev');
+        $this->set('PMA_VERSION', '2.11.4-rc1');
         /**
          * @deprecated
          */
