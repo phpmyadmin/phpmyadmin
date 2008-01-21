32ba2d44bfacfb9e2513fe369f6156be75b29abc
Sebastian Mendel <cybot_tm@users.sourceforge.net>
2008-01-21 11:44:37 +0000

    no need to call PMA_profilingSupported() if isset($_REQUEST['profiling']) is false

--- libraries/common.inc.php
+++ libraries/common.inc.php
@@ -912,8 +912,7 @@ if (! defined('PMA_MINIMUM_COMMON')) {
      * check if profiling was requested and remember it
      * (note: when $cfg['ServerDefault'] = 0, constant is not defined)
      */
-
-    if (PMA_profilingSupported() && isset($_REQUEST['profiling'])) {
+    if (isset($_REQUEST['profiling']) && PMA_profilingSupported()) {
         $_SESSION['profiling'] = true;
     } elseif (isset($_REQUEST['profiling_form'])) {
         // the checkbox was unchecked
