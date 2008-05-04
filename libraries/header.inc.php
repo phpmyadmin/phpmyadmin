f0cd8feebec711d9eb813bbf49a5e2644c60aa2f
Marc Delisle <marc@infomarc.info>
2008-05-04 12:41:26 +0000

    undefined $server

--- libraries/header.inc.php
+++ libraries/header.inc.php
@@ -64,7 +64,7 @@ if (empty($GLOBALS['is_header_sent'])) {
      * Display heading if needed. Design can be set in css file.
      */
 
-    if (PMA_DISPLAY_HEADING && $server > 0) {
+    if (PMA_DISPLAY_HEADING && $GLOBALS['server'] > 0) {
         $server_info = (!empty($GLOBALS['cfg']['Server']['verbose'])
                         ? $GLOBALS['cfg']['Server']['verbose']
                         : $GLOBALS['cfg']['Server']['host'] . (empty($GLOBALS['cfg']['Server']['port'])
