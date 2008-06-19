f221eabcc300a8446db0eb84b96088120abc4a64
Marc Delisle <marc@infomarc.info>
2008-06-19 12:32:07 +0000

    typo

--- export.php
+++ export.php
@@ -586,7 +586,7 @@ if (!empty($asfile)) {
     }
 
     // Do the compression
-    // 1. as a gzipped file
+    // 1. as a zipped file
     if ($compression == 'zip') {
         if (@function_exists('gzcompress')) {
             $zipfile = new zipfile();
