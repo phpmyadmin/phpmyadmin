654499029ca72c736d998d68a2fdead3eb9c52a8
Marc Delisle <marc@infomarc.info>
2008-07-28 16:40:56 +0000

    2.11.8

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -85,7 +85,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '2.11.8-rc1');
+        $this->set('PMA_VERSION', '2.11.8');
         /**
          * @deprecated
          */
