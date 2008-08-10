4c68c91ffbccfc61a89e5b799e7a7c2e01eeeae3
Marc Delisle <marc@infomarc.info>
2008-08-10 12:20:11 +0000

    going to 3.0.0-beta

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -85,7 +85,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '3.0.0-alpha');
+        $this->set('PMA_VERSION', '3.0.0-beta');
         /**
          * @deprecated
          */
