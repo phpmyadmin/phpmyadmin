7a958e11a2112858ce376afd8a4e090c3a818133
Marc Delisle <marc@infomarc.info>
2008-09-22 16:39:00 +0000

    2.11.9.2

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -85,7 +85,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '2.11.9.1');
+        $this->set('PMA_VERSION', '2.11.9.2');
         /**
          * @deprecated
          */
