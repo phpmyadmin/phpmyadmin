37ca1e466cf2ed7ed55361a8c4aedbc110b9fe99
Marc Delisle <marc@infomarc.info>
2008-10-06 21:36:08 +0000

    bug #1914066 [core] ForceSSL generates incorrectly escaped redirections, this time with the correct fix

--- libraries/common.inc.php
+++ libraries/common.inc.php
@@ -534,7 +534,7 @@ if ($_SESSION['PMA_Config']->get('ForceSSL')
     PMA_sendHeaderLocation(
         preg_replace('/^http/', 'https',
             $_SESSION['PMA_Config']->get('PmaAbsoluteUri'))
-        . PMA_generate_common_url($_GET, '', '&'));
+        . PMA_generate_common_url($_GET, '&');
     exit;
 }
 
