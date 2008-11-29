3428cd3489c66b9da94931562a75e1079797d5b4
Marc Delisle <marc@infomarc.info>
2008-11-29 19:28:18 +0000

    typos

--- libraries/config.default.php
+++ libraries/config.default.php
@@ -563,8 +563,8 @@ $cfg['AllowArbitraryServer'] = false;
 /*******************************************************************************
  * Error handler configuration
  *
- * this configures phpMyAdmins own error handler, it is used to avoid information
- * dislcosure, gather errors for logging, reporting and displaying
+ * this configures phpMyAdmin's own error handler, it is used to avoid information
+ * disclosure, gather errors for logging, reporting and displaying
  *
  * @global array $cfg['Error_Handler']
  */
