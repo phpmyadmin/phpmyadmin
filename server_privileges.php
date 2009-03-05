01dbe95dffe5c448d49eae317371b98271a7906f
Marc Delisle <marc@infomarc.info>
2009-03-05 16:57:43 +0000

    add reference to mysql_upgrade command

--- server_privileges.php
+++ server_privileges.php
@@ -1374,7 +1374,8 @@ if (empty($_REQUEST['adduser']) && (! isset($checkprivs) || ! strlen($checkprivs
                 // a automatic repair feature soon.
                 $raw = 'Your privilege table structure seems to be older than'
                     . ' this MySQL version!<br />'
-                    . 'Please run the script <tt>mysql_fix_privilege_tables</tt>'
+                    . 'Please run the <tt>mysql_upgrade</tt> command'
+                    . '(<tt>mysql_fix_privilege_tables</tt> on older systems)'
                     . ' that should be included in your MySQL server distribution'
                     . ' to solve this problem!';
                 PMA_Message::rawError($raw)->display();
