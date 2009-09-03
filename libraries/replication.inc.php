b8447fec8ab8fce66f4f5bc3e847297d9d0c22b9
Tomas Srnka <tomas.srnka@gmail.com>
2009-09-03 10:36:32 +0000

    Fixed PHP notices undes PHP 5.3.0 (moved from split to explode)

--- libraries/replication.inc.php
+++ libraries/replication.inc.php
@@ -97,17 +97,17 @@ foreach ($replication_types as $type) {
 
   if (${"server_{$type}_status"}) {
     if ($type=="master") {
-      ${"server_{$type}_Do_DB"} = split (",", $server_master_replication[0]["Binlog_Do_DB"]);
-      ${"server_{$type}_Ignore_DB"} = split (",", $server_master_replication[0]["Binlog_Ignore_DB"]);
+      ${"server_{$type}_Do_DB"} = explode (",", $server_master_replication[0]["Binlog_Do_DB"]);
+      ${"server_{$type}_Ignore_DB"} = explode (",", $server_master_replication[0]["Binlog_Ignore_DB"]);
     } elseif ($type=="slave") {
-      ${"server_{$type}_Do_DB"} = split (",", $server_slave_replication[0]["Replicate_Do_DB"]);
-      ${"server_{$type}_Ignore_DB"} = split (",", $server_slave_replication[0]["Replicate_Ignore_DB"]);
+      ${"server_{$type}_Do_DB"} = explode (",", $server_slave_replication[0]["Replicate_Do_DB"]);
+      ${"server_{$type}_Ignore_DB"} = explode (",", $server_slave_replication[0]["Replicate_Ignore_DB"]);
       
-      ${"server_{$type}_Do_Table"} = split (",", $server_slave_replication[0]["Replicate_Do_Table"]);
-      ${"server_{$type}_Ignore_Table"} = split (",", $server_slave_replication[0]["Replicate_Ignore_Table"]);
+      ${"server_{$type}_Do_Table"} = explode (",", $server_slave_replication[0]["Replicate_Do_Table"]);
+      ${"server_{$type}_Ignore_Table"} = explode (",", $server_slave_replication[0]["Replicate_Ignore_Table"]);
       
-      ${"server_{$type}_Wild_Do_Table"} = split(",", $server_slave_replication[0]["Replicate_Wild_Do_Table"]);
-      ${"server_{$type}_Wild_Ignore_Table"} = split(",", $server_slave_replication[0]["Replicate_Wild_Ignore_Table"]);
+      ${"server_{$type}_Wild_Do_Table"} = explode (",", $server_slave_replication[0]["Replicate_Wild_Do_Table"]);
+      ${"server_{$type}_Wild_Ignore_Table"} = explode (",", $server_slave_replication[0]["Replicate_Wild_Ignore_Table"]);
     }
   }
 }
