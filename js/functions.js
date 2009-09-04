54b463ad6f7fd276c08f4f3aa369cf0a0640baf9
Marc Delisle <marc@infomarc.info>
2009-09-04 16:39:03 +0000

    bug [structure] Missing validation for BINARY and VARBINARY

--- js/functions.js
+++ js/functions.js
@@ -390,7 +390,7 @@ function checkTableEditForm(theForm, fieldsCnt)
     {
         id = "field_" + i + "_2";
         elm = getElement(id);
-        if (elm.value == 'VARCHAR' || elm.value == 'CHAR' || elm.value == 'BIT') {
+        if (elm.value == 'VARCHAR' || elm.value == 'CHAR' || elm.value == 'BIT' || elm.value == 'VARBINARY' || elm.value == 'BINARY') {
             elm2 = getElement("field_" + i + "_3");
             val = parseInt(elm2.value);
             elm3 = getElement("field_" + i + "_1");
