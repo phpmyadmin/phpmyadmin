a2fa8219c85f49b1aeadc5edf37ad4afb89ae6cf
Marc Delisle <marc@infomarc.info>
2009-12-31 13:27:55 +0000

    advance version numbers

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -92,7 +92,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '3.3.0-dev');
+        $this->set('PMA_VERSION', '3.3.0-alpha1');
         /**
          * @deprecated
          */
