94007b6705f95ae341e4b3f9b60cfb95ce2998c0
Michal Čihař <michal@cihar.com>
2010-01-22 21:48:04 +0000

    Disable hash loading code for now.
    I'll try to fix this properly, but probably not earlier than on Monday.

--- libraries/footer.inc.php
+++ libraries/footer.inc.php
@@ -167,7 +167,9 @@ if (PMA_getenv('SCRIPT_NAME') && empty($_POST) && !$GLOBALS['checked_special']) 
     echo '<div id="selflink" class="print_ignore">' . "\n";
     $url_params['target'] = basename(PMA_getenv('SCRIPT_NAME'));
     /* Store current location in hash part of URL to allow direct bookmarking */
-    echo '<script>parent.location.hash = "' . PMA_generate_common_url($url_params, 'text', '') . '";</script>';
+    // Disabled for now, causes infinite loop with some Chrome based browsers
+    // Should be converted to use mootools and onload event
+    //echo '<script>parent.location.hash = "' . PMA_generate_common_url($url_params, 'text', '') . '";</script>';
 
     echo '<a href="index.php' . PMA_generate_common_url($url_params) . '"'
         . ' title="' . $GLOBALS['strOpenNewWindow'] . '" target="_blank">';
