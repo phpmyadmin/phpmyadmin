6d947f20b36f6b301b7395dbdd49794f3a7616fa
Michal Čihař <michal@cihar.com>
2010-01-22 16:27:50 +0000

    Improve field download.
    - Check errors.
    - Properly select database.
    - Add some comments.

--- tbl_get_field.php
+++ tbl_get_field.php
@@ -1,29 +1,38 @@
 <?php
 /* vim: set expandtab sw=4 ts=4 sts=4: */
 /**
- * Displays table structure infos like fields/columns, indexes, size, rows
- * and allows manipulation of indexes and columns/fields
+ * Provides download to a given field defined in parameters.
  * @version $Id$
  * @package phpMyAdmin
  */
 
 /**
- *
+ * Common functions.
  */
 require_once './libraries/common.inc.php';
 
-/**
- * Gets tables informations
- */
-require_once './libraries/tbl_info.inc.php';
-
+/* Check parameters */
 PMA_checkParameters(array('db', 'table', 'where_clause', 'transform_key'));
 
+/* Select database */
+if (!PMA_DBI_select_db($db)) {
+    PMA_mysqlDie(sprintf($GLOBALS['strDatabaseNotExisting'], htmlspecialchars($db)),
+        '', '');
+}
+
+/* Check if table exists */
 if (!PMA_DBI_get_columns($db, $table)) {
     PMA_mysqlDie($strInvalidTableName);
 }
 
-$result = PMA_DBI_fetch_value('SELECT ' . PMA_backquote($transform_key) . ' FROM ' . PMA_backquote($table) . ' WHERE ' . $where_clause . ';');
+/* Grab data */
+$sql = 'SELECT ' . PMA_backquote($transform_key) . ' FROM ' . PMA_backquote($table) . ' WHERE ' . $where_clause . ';';
+$result = PMA_DBI_fetch_value($sql);
+
+/* Check return code */
+if ($result === false) {
+    PMA_mysqlDie($strEmptyResultSet, $sql);
+}
 
 /* Avoid corrupting data */
 @ini_set('url_rewriter.tags','');
