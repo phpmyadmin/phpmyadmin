1711f1b029fb188adf94343d15879b935d7c4615
Marc Delisle <marc@infomarc.info>
2010-03-07 12:38:50 +0000

    new theme

---
+++ smooth_yellow/info.inc.php
@@ -0,0 +1,15 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Theme information
+ *
+ *
+ * @version $Id$
+ * @package phpMyAdmin-theme
+ * @subpackage Smooth_Yellow
+ */
+
+$theme_name = 'Smooth Yellow';
+$theme_version = 3;
+$theme_generation = 3;
+?>
