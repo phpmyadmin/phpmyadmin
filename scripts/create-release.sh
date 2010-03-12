c4bdb187033c908982e814af1b254d28ab3b1d3e
Michal Čihař <mcihar@novell.com>
2010-03-12 13:49:27 +0100

    Better logging of progress.

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -93,6 +93,7 @@ fi
 LC_ALL=C date -u > RELEASE-DATE-${version}
 
 # Building Documentation.txt
+echo "* Generating Documentation.txt"
 LC_ALL=C w3m -dump Documentation.html > Documentation.txt
 
 # Remove test directory from package to avoid Path disclosure messages
@@ -119,28 +120,28 @@ for kit in $KITS ; do
     for comp in $COMPRESSIONS ; do
         case $comp in
             tbz|tgz)
-                echo "Creating $name.tar"
+                echo "* Creating $name.tar"
                 tar cf $name.tar $name
                 if [ $comp = tbz ] ; then
-                    echo "Creating $name.tar.bz2"
+                    echo "* Creating $name.tar.bz2"
                     bzip2 -9k $name.tar
                 fi
                 if [ $comp = tgz ] ; then
-                    echo "Creating $name.tar.gz"
+                    echo "* Creating $name.tar.gz"
                     gzip -9c $name.tar > $name.tar.gz
                 fi
                 rm $name.tar
                 ;;
             zip)
-                echo "Creating $name.zip"
+                echo "* Creating $name.zip"
                 zip -q -9 -r $name.zip $name
                 ;;
             zip-7z)
-                echo "Creating $name.zip"
+                echo "* Creating $name.zip"
                 7za a -bd -tzip $name.zip $name > /dev/null
                 ;;
             7z)
-                echo "Creating $name.7z"
+                echo "* Creating $name.7z"
                 7za a -bd $name.7z $name > /dev/null
                 ;;
             *)
