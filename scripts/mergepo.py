dcf52474a3426f07cc2c908b20a4d5f63cd146d3
Michal Čihař <mcihar@novell.com>
2010-03-15 13:47:26 +0100

    Update translations
    Another update for strings already translated in php code and were
    missing in gettext.

--- scripts/mergepo.py
+++ scripts/mergepo.py
@@ -12,6 +12,11 @@ for origentry in po.fuzzy_entries():
             origentry.msgstr = updateentry.msgstr
             origentry.flags.remove('fuzzy')
             break
+for origentry in po.untranslated_entries():
+    for updateentry in poupdate.translated_entries():
+        if origentry.msgctxt is None and origentry.msgid == updateentry.msgid:
+            origentry.msgstr = updateentry.msgstr
+            break
 
 po.save()
 
