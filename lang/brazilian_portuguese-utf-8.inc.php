6745ee722446dd84a3c95f4a89f81ac35344fc49
Michal Čihař <mcihar@novell.com>
2010-05-25 16:04:36 +0200

    Fix syntax errors.

--- lang/brazilian_portuguese-utf-8.inc.php
+++ lang/brazilian_portuguese-utf-8.inc.php
@@ -20,8 +20,6 @@ $datefmt = '%B %d, %Y as %I:%M %p';
 
 $timespanfmt = '%s dias, %s horas, %s minutos e %s segundos';
 
- DESC.';
-
 $strAbortedClients = 'Abortado';
 $strAccessDenied = 'Acesso negado';
 $strAccessDeniedCreateConfig = 'A provável razão para isso é que você não criou o arquivo de configuração. Você deve usar o %1$ssetup script%2$s para criar um.';
@@ -787,7 +785,7 @@ $strShowStatusHandler_discoverDescr = 'O servidor MySQL pode perguntar ao motor 
 $strShowStatusHandler_read_firstDescr = 'O número de vezes que a primeira entrada foi lida de um índice. Se isto for alto, sugere que o usuário está fazendo muitas varreduras completas do índice; por exemplo, SELECT col1 FROM foo, supondo que col1 é um índice.';
 $strShowStatusHandler_read_keyDescr = 'O número de requisições para ler uma linha baseada em uma chave. Se isto for alto, é uma boa indicação de que suas consultas e tabelas estejam corretamente indexadas.';
 $strShowStatusHandler_read_nextDescr = 'O número de requisições para ler a linha seguinte na ordem da chave. Isto é incrementado se você estiver consultando uma coluna do índice com uma restrição da escala ou se você estiver fazendo uma varredura do índice.';
-$strShowStatusHandler_read_prevDescr = 'O número de requisições para ler a linha precedente na ordem da chave. Este método de leitura é usado principalmente para otimizar ORDER B';
+$strShowStatusHandler_read_prevDescr = 'O número de requisições para ler a linha precedente na ordem da chave. Este método de leitura é usado principalmente para otimizar ORDER BY ... DESC.';
 $strShowStatusHandler_read_rndDescr = 'O número de requisições pra ler uma linha baseada em uma posição fixa. Isto é alto se você estiver fazendo muitas consultas que requerem a ordenação do resultado. Você tem provavelmente muitas consultas que requerem que o MySQL faça a varredura de tabelas inteiras ou você tem junções que não usam as chaves corretamente.';
 $strShowStatusHandler_read_rnd_nextDescr = 'O número de requisições para ler a linha seguinte no arquivo de dados. Isto é alto se você estiver fazendo muitas varreduras da tabela. Geralmente isto sugere que suas tabelas não estão corretamente indexadas ou que suas consultas não estão escritas para tomar vantagem dos índices que você têm.';
 $strShowStatusHandler_rollbackDescr = 'O número de enunciados ROLLBACK internos.';
@@ -1084,7 +1082,7 @@ $strCouldNotConnectTarget = 'Could not connect to the target';  //to translate
 $strCurrentServer = 'Current server';  //to translate
 $strCustomColor = 'Custom color';  //to translate
 
-$strDatabaseNotExisting = '\'%s\' database does not exist.';  //to translate 
+$strDatabaseNotExisting = '\'%s\' database does not exist.';  //to translate
 $strDatabase_src = 'Source database';  //to translate
 $strDatabase_trg = 'Target database';  //to translate
 $strDataDiff = 'Data Difference';  //to translate
