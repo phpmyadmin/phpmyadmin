d775f341a4791c108dab4839d094b4f654d97f32
ninadsp <ninadsp16289@gmail.com>
2010-05-28 13:06:51 +0530

    Added new js files for Ajax events on db_operations.php and tbl_operations.php

---
+++ js/db_operations.js
@@ -0,0 +1,15 @@
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * function used in server privilege pages
+ *
+ * @version $Id$
+ */
+
+/**
+ * Add Ajax event handlers here for db_operations.php
+ *
+ * Create table (will this also be necessary on tbl_operations.php?
+ * Rename Database
+ * Copy Database
+ * Change Collation
+ */
---
+++ js/tbl_operations.js
@@ -0,0 +1,21 @@
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * function used in server privilege pages
+ *
+ * @version $Id$
+ */
+
+/**
+ * Add all AJAX scripts for tbl_operations.php page here.
+ *
+ * Alter table order
+ * Move Table
+ * Table Options
+ * Copy Table
+ * Table Maintenance
+ *  Check
+ *  Repair
+ *  Analyze
+ *  Flush
+ *  Optimize
+ */
