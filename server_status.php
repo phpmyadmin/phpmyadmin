473b9b38dcc8200b7efb9a52c8f24aa00d171ec6
Martynas Mickevicius <mmartynas@gmail.com>
2010-06-15 20:13:12 +0300

    fixed the layout of the chart in the server status page

--- server_status.php
+++ server_status.php
@@ -587,22 +587,19 @@ foreach ($used_queries as $name => $value) {
 ?>
     </tbody>
     </table>
+    <div class="clearfloat"></div>
 </div>
 
-<div>
-    <table>
-        <tr><td>
-            <?php
-            // format keys which will be shown in the chart
-            $chart_data = array();
-            foreach($used_queries as $key => $value) {
-                $key = str_replace(array('Com_', '_'), array('', ' '), $key);
-                $chart_data[ucwords($key)] = (int)$value;
-            }
-            echo PMA_chart_pie(__('Query type'), $chart_data);
-            ?>
-        </td></tr>
-    </table>
+<div>    
+    <?php
+    // format keys which will be shown in the chart
+    $chart_data = array();
+    foreach($used_queries as $key => $value) {
+        $key = str_replace(array('Com_', '_'), array('', ' '), $key);
+        $chart_data[ucwords($key)] = (int)$value;
+    }
+    echo PMA_chart_pie(__('Query type'), $chart_data);
+    ?>
 </div>
 
 <div id="serverstatussection">
