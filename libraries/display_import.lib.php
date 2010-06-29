f368184a12c2fc619ef442e588e07be58c3c6085
Marc Delisle <marc@infomarc.info>
2010-06-29 07:57:45 -0400

    fix typo; remove Id

--- libraries/display_import.lib.php
+++ libraries/display_import.lib.php
@@ -2,7 +2,6 @@
 /* vim: set expandtab sw=4 ts=4 sts=4: */
 /**
  *
- * @version $Id$
  * @package phpMyAdmin
  */
 if (! defined('PHPMYADMIN')) {
@@ -94,7 +93,7 @@ if ($_SESSION[$SESSION_KEY]["handler"]!="noplugin") {
                 window.setTimeout(perform_upload, 1000);
 
     <?php
-} else { // no plugin avaliable
+} else { // no plugin available
     ?>
                         $('#upload_form_status_info').html('<img src="<?php echo $GLOBALS['pmaThemeImage'];?>ajax_clock_small.gif" alt="ajax clock" /> <?php echo PMA_jsFormat(__('Please be patient, the file is being uploaded. Details about the upload are not available.')) . PMA_showDocu('faq2_9'); ?>');
                         $('#upload_form_status').css("display", "none");
