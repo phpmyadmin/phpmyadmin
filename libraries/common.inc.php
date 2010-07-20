7b99e3b0e4e3963e216458afd8b13065e6ad1df5
ninadsp <ninadsp16289@gmail.com>
2010-07-21 00:10:46 +0530

    Added a parameter for inline edit requests where truncated and transformed values are retrieved

--- libraries/common.inc.php
+++ libraries/common.inc.php
@@ -994,6 +994,19 @@ if (isset($_REQUEST['ajax_request']) && $_REQUEST['ajax_request'] == true) {
 	$GLOBALS['is_ajax_request'] = false;
 }
 
+/**
+ * @global  boolean $GLOBALS['inline_edit']
+ *
+ * Set to true if this is a request made during an inline edit process.  This
+ * request is made to retrieve the non-truncated/transformed values.
+ */
+if(isset($_REQUEST['inline_edit']) && $_REQUEST['inline_edit'] == true) {
+    $GLOBALS['inline_edit'] = true;
+}
+else {
+    $GLOBALS['inline_edit'] = false;
+}
+
 if (!empty($__redirect) && in_array($__redirect, $goto_whitelist)) {
     /**
      * include subform target page
