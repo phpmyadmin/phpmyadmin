eeae8bec2159e2be2b25cf704fa25d1ac1d33c33
Michal Čihař <mcihar@novell.com>
2010-07-21 09:21:58 +0200

    Get rid of inline javascript in login form.

--- js/functions.js
+++ js/functions.js
@@ -1726,5 +1726,12 @@ $(document).ready(function(){
         return false;
     });
     $('#sqlquery').focus();
+    if ($('#input_username')) {
+        if ($('#input_username').val() == '') {
+            $('#input_username').focus();
+        } else {
+            $('#input_password').focus();
+        }
+    }
 });
 
