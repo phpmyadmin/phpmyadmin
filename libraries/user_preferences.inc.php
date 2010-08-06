60329f14cbc865e5c312602529966e128a0402da
Crack <piotrprz@gmail.com>
2010-08-06 14:36:26 +0200

    blacklist and settings debug code shown if $cfg['DBG']['php'] is true

--- libraries/user_preferences.inc.php
+++ libraries/user_preferences.inc.php
@@ -48,18 +48,20 @@ if (!empty($_GET['saved'])) {
     $message->display();
 }
 
-// todo: debug - remove
-$arr = ConfigFile::getInstance()->getConfigArray();
-$arr2 = array();
-foreach ($arr as $k => $v) {
-    $arr2[] = "<b>$k</b> " . var_export($v, true);
+// debug info
+if ($cfg['DBG']['php']) {
+    $arr = ConfigFile::getInstance()->getConfigArray();
+    $arr2 = array();
+    foreach ($arr as $k => $v) {
+        $arr2[] = "<b>$k</b> " . var_export($v, true);
+    }
+    $arr2 = implode(', ', $arr2);
+    $arr2 .= '<br />Blacklist: ' . (empty($cfg['UserprefsDisallow'])
+            ? '<i>empty</i>'
+            : implode(', ', $cfg['UserprefsDisallow']));
+    $msg = PMA_Message::notice('Settings: ' . $arr2);
+    $msg->display();
 }
-$arr2 = implode(', ', $arr2);
-$arr2 .= '<br />Blacklist: ' . (empty($cfg['UserprefsDisallow'])
-        ? '<i>empty</i>'
-        : implode(', ', $cfg['UserprefsDisallow']));
-$msg = PMA_Message::notice('Debug: ' . $arr2);
-$msg->display();
 
 // warn about using session storage for settings
 $cfgRelation = PMA_getRelationsParam();
