1406bd83b045e3e195b379ffb02848dd4ca7d7e1
Marc Delisle <marc@infomarc.info>
2010-10-09 09:33:57 -0400

    use PMA_getIcon()

--- libraries/build_action_titles.inc.php
+++ libraries/build_action_titles.inc.php
@@ -9,43 +9,17 @@ if (! defined('PHPMYADMIN')) {
 }
 
 $titles = array();
-if (true == $cfg['PropertiesIconic']) {
-    $titles['Browse']     = '<img class="icon" width="16" height="16" src="' .$pmaThemeImage . 'b_browse.png" alt="' . __('Browse') . '" title="' . __('Browse') . '" />';
-    $titles['NoBrowse']   = '<img class="icon" width="16" height="16" src="' .$pmaThemeImage . 'bd_browse.png" alt="' . __('Browse') . '" title="' . __('Browse') . '" />';
-    $titles['Search']     = '<img class="icon" width="16" height="16" src="' .$pmaThemeImage . 'b_select.png" alt="' . __('Search') . '" title="' . __('Search') . '" />';
-    $titles['NoSearch']   = '<img class="icon" width="16" height="16" src="' .$pmaThemeImage . 'bd_select.png" alt="' . __('Search') . '" title="' . __('Search') . '" />';
-    $titles['Insert']     = '<img class="icon" width="16" height="16" src="' .$pmaThemeImage . 'b_insrow.png" alt="' . __('Insert') . '" title="' . __('Insert') . '" />';
-    $titles['NoInsert']   = '<img class="icon" width="16" height="16" src="' .$pmaThemeImage . 'bd_insrow.png" alt="' . __('Insert') . '" title="' . __('Insert') . '" />';
-    $titles['Structure']  = '<img class="icon" width="16" height="16" src="' .$pmaThemeImage . 'b_props.png" alt="' . __('Structure') . '" title="' . __('Structure') . '" />';
-    $titles['Drop']       = '<img class="icon" width="16" height="16" src="' .$pmaThemeImage . 'b_drop.png" alt="' . __('Drop') . '" title="' . __('Drop') . '" />';
-    $titles['NoDrop']     = '<img class="icon" width="16" height="16" src="' .$pmaThemeImage . 'bd_drop.png" alt="' . __('Drop') . '" title="' . __('Drop') . '" />';
-    $titles['Empty']      = '<img class="icon" width="16" height="16" src="' .$pmaThemeImage . 'b_empty.png" alt="' . __('Empty') . '" title="' . __('Empty') . '" />';
-    $titles['NoEmpty']    = '<img class="icon" width="16" height="16" src="' .$pmaThemeImage . 'bd_empty.png" alt="' . __('Empty') . '" title="' . __('Empty') . '" />';
 
-    if ('both' === $cfg['PropertiesIconic']) {
-        $titles['Browse']     .= __('Browse');
-        $titles['Search']     .= __('Search');
-        $titles['NoBrowse']   .= __('Browse');
-        $titles['NoSearch']   .= __('Search');
-        $titles['Insert']     .= __('Insert');
-        $titles['NoInsert']   .= __('Insert');
-        $titles['Structure']  .= __('Structure');
-        $titles['Drop']       .= __('Drop');
-        $titles['NoDrop']     .= __('Drop');
-        $titles['Empty']      .= __('Empty');
-        $titles['NoEmpty']    .= __('Empty');
-    }
-} else {
-    $titles['Browse']     = __('Browse');
-    $titles['Search']     = __('Search');
-    $titles['NoBrowse']   = __('Browse');
-    $titles['NoSearch']   = __('Search');
-    $titles['Insert']     = __('Insert');
-    $titles['NoInsert']   = __('Insert');
-    $titles['Structure']  = __('Structure');
-    $titles['Drop']       = __('Drop');
-    $titles['NoDrop']     = __('Drop');
-    $titles['Empty']      = __('Empty');
-    $titles['NoEmpty']    = __('Empty');
-}
+$titles['Browse']     = PMA_getIcon('b_browse.png', __('Browse'), true);
+$titles['NoBrowse']   = PMA_getIcon('bd_browse.png', __('Browse'), true);
+$titles['Search']     = PMA_getIcon('b_select.png', __('Search'), true);
+$titles['NoSearch']   = PMA_getIcon('bd_select.png', __('Search'), true);
+$titles['Insert']     = PMA_getIcon('b_insrow.png', __('Insert'), true);
+$titles['NoInsert']   = PMA_getIcon('bd_insrow.png', __('Insert'), true);
+$titles['Structure']  = PMA_getIcon('b_props.png', __('Structure'), true);
+$titles['Drop']       = PMA_getIcon('b_drop.png', __('Drop'), true);
+$titles['NoDrop']     = PMA_getIcon('bd_drop.png', __('Drop'), true);
+$titles['Empty']      = PMA_getIcon('b_empty.png', __('Empty'), true);
+$titles['NoEmpty']    = PMA_getIcon('bd_empty.png', __('Empty'), true);
+
 ?>
