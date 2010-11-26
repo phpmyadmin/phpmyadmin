a6e79391b92a48ffef307fa107ce1a61965e4244
Marc Delisle <marc@infomarc.info>
2010-11-26 09:17:30 -0500

    2.11.11.1 release

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -85,7 +85,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '2.11.11');
+        $this->set('PMA_VERSION', '2.11.11.1');
         /**
          * @deprecated
          */
