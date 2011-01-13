d9f862a8c115811062f71d818c7d98e027c4f2c4
Marc Delisle <marc@infomarc.info>
2011-01-13 16:29:14 -0500

    Clarify datepicker parameter

--- tbl_change.php
+++ tbl_change.php
@@ -986,7 +986,7 @@ $(function() {
         stepMinutes: 1,
         stepHours: 1,
         <?php echo ($field['pma_type'] == 'date' ? "showTime: false,":"showTime: true,"); ?>
-        dateFormat: 'yy-mm-dd',
+        dateFormat: 'yy-mm-dd', // yy means year with four digits
         altTimeField: '',
         constrainInput: false
      });
