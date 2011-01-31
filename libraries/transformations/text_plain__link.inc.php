acc405e3faa5763b4c30877762182e077a9befea
Michal Čihař <mcihar@novell.com>
2011-01-31 15:35:08 +0100

    Wrap URL in transformation as well

--- libraries/transformations/text_plain__link.inc.php
+++ libraries/transformations/text_plain__link.inc.php
@@ -18,7 +18,7 @@ function PMA_transformation_text_plain__link($buffer, $options = array(), $meta 
 
 //    $transform_options = array ('string' => '<a href="' . (isset($options[0]) ? $options[0] : '') . '%1$s" title="' . (isset($options[1]) ? $options[1] : '%1$s') . '">' . (isset($options[1]) ? $options[1] : '%1$s') . '</a>');
 
-    $transform_options = array ('string' => '<a href="' . (isset($options[0]) ? $options[0] : '') . $buffer . '" title="' . (isset($options[1]) ? $options[1] : '') . '">' . (isset($options[1]) ? $options[1] : $buffer) . '</a>');
+    $transform_options = array ('string' => '<a href="' . PMA_linkURL((isset($options[0]) ? $options[0] : '') . $buffer) . '" title="' . (isset($options[1]) ? $options[1] : '') . '">' . (isset($options[1]) ? $options[1] : $buffer) . '</a>');
 
     $buffer = PMA_transformation_global_html_replace($buffer, $transform_options);
 
