2f7c7aaf669bedacbb0a06f2141919c72a0c18ec
Michal Čihař <mcihar@novell.com>
2011-02-07 13:34:06 +0100

    Make disabled actions look more like a disabled

--- img/make-bd.sh
+++ img/make-bd.sh
@@ -3,7 +3,7 @@
 for f in bd* ; do
     orig=b_${f##bd_}
     if [ -f $f ] ; then
-        convert $orig -colorspace Gray $f
+        convert $orig -modulate 90,90 -colorspace Gray $f
     fi
 done
 convert eye.png -colorspace Gray eye_grey.png
