5f4a8e6a971e88135380a79ae09dc7ebd28010fb
Marc Delisle <marc@infomarc.info>
2011-05-14 08:26:00 -0400

    Update to jQuery 1.6.1

--- setup/index.php
+++ setup/index.php
@@ -37,7 +37,7 @@ require './libraries/header_http.inc.php';
 <link href="../favicon.ico" rel="icon" type="image/x-icon" />
 <link href="../favicon.ico" rel="shortcut icon" type="image/x-icon" />
 <link href="styles.css" rel="stylesheet" type="text/css" />
-<script type="text/javascript" src="../js/jquery/jquery-1.4.4.js"></script>
+<script type="text/javascript" src="../js/jquery/jquery-1.6.1.js"></script>
 <script type="text/javascript" src="../js/jquery/jquery-ui-1.8.custom.js"></script>
 <script type="text/javascript" src="../js/jquery/jquery.json-2.2.js"></script>
 <script type="text/javascript" src="../js/config.js"></script>
