aac58fd425e4ee4064fa5dbbe666304c5e2b6f2c
root <root@localhost.localdomain>
2011-05-18 22:40:33 +0530

    testing for push

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie
  */
-$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'a8b7c6d'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /*
  * Servers configuration
