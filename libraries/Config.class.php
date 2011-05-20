8c1fbf450500a6903aab062c837139baf3a7296b
Marc Delisle <marc@infomarc.info>
2011-05-20 13:16:42 -0400

    3.4.1

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -96,7 +96,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '3.4.1-rc1');
+        $this->set('PMA_VERSION', '3.4.1');
         /**
          * @deprecated
          */
