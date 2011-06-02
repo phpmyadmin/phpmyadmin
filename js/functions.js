42e1fc1533941c86b6f1017cf899ca41f29dbb06
Michal Čihař <mcihar@novell.com>
2011-06-02 17:01:17 +0200

    Fix detection whether there is SQL editor to handle

--- js/functions.js
+++ js/functions.js
@@ -2396,7 +2396,7 @@ $(document).ready(function() {
  */
 $(document).ready(function() {
     var elm = $('#sqlquery');
-    if (elm) {
+    if (elm.length > 0) {
         codemirror_editor = CodeMirror.fromTextArea(elm[0], {lineNumbers: true, matchBrackets: true, indentUnit: 4, mode: "text/x-mysql"});
     }
 })
