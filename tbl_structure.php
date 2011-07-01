0cb77d14bad36196664f953d62e247193e1eb30d
Thilanka Kaushalya <lgtkaushalya@gmail.com>
2011-07-01 23:39:23 +0530

    Ajaxify Index edit in table structure

--- tbl_structure.php
+++ tbl_structure.php
@@ -14,7 +14,7 @@ require_once './libraries/mysql_charsets.lib.php';
 
 $GLOBALS['js_include'][] = 'jquery/jquery-ui-1.8.custom.js';
 $GLOBALS['js_include'][] = 'tbl_structure.js';
-
+$GLOBALS['js_include'][] = 'indexes.js';
 /**
  * handle multiple field commands if required
  *
