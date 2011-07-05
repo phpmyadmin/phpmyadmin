5d48637abc34fa0c54d4a88e0e979616aa359f3c
Rouslan Placella <rouslan@placella.com>
2011-07-05 16:25:08 +0100

    Added validation for the Events editor

--- js/rte/events.js
+++ js/rte/events.js
@@ -1,6 +1,26 @@
 /* vim: set expandtab sw=4 ts=4 sts=4: */
 
 RTE.validateCustom = function () {
+    /**
+     * @var    $elm    a jQuery object containing the reference
+     *                 to an element that is being validated.
+     */
+    var $elm = null;
+    if ($('select[name=item_type]').find(':selected').val() === 'RECURRING') {
+        $elm = $('input[name=item_interval_value]');
+        if ($elm.val() === '') {
+            $elm.focus();
+            alert(PMA_messages['strFormEmpty']);
+            return false;
+        }
+    } else {
+        $elm = $('input[name=item_execute_at]');
+        if ($elm.val() === '') {
+            $elm.focus();
+            alert(PMA_messages['strFormEmpty']);
+            return false;
+        }
+    }
     return true;
 };
 RTE.postDialogShow = function () {};
