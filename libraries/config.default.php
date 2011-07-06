866d1da95a853cd35920253e53432c4f55ad61bf
Piotr Przybylski <piotrprz@gmail.com>
2011-07-06 17:11:04 +0200

    Move ColumnTypes, AttributeTypes, Functions, RestrictColumnTypes, RestrictFunctions and DefaultFunctions to a separate file

--- libraries/config.default.php
+++ libraries/config.default.php
@@ -2740,392 +2740,59 @@ $cfg['DBG']['sql'] = false;
  */
 
 /**
- * Column types;
- * VARCHAR, TINYINT, TEXT and DATE are listed first, based on estimated popularity
+ * Column types
+ *
+ * Fill in this array to overwrite data from data_*.inc.php files
  *
  * @global array $cfg['ColumnTypes']
  */
-$cfg['ColumnTypes'] = array(
-    // most used
-    'INT',
-    'VARCHAR',
-    'TEXT',
-    'DATE',
-
-    // numeric
-    'NUMERIC' => array(
-        'TINYINT',
-        'SMALLINT',
-        'MEDIUMINT',
-        'INT',
-        'BIGINT',
-        '-',
-        'DECIMAL',
-        'FLOAT',
-        'DOUBLE',
-        'REAL',
-        '-',
-        'BIT',
-        'BOOLEAN',
-        'SERIAL',
-    ),
-
-
-    // Date/Time
-    'DATE and TIME' => array(
-        'DATE',
-        'DATETIME',
-        'TIMESTAMP',
-        'TIME',
-        'YEAR',
-    ),
-
-    // Text
-    'STRING' => array(
-        'CHAR',
-        'VARCHAR',
-        '-',
-        'TINYTEXT',
-        'TEXT',
-        'MEDIUMTEXT',
-        'LONGTEXT',
-        '-',
-        'BINARY',
-        'VARBINARY',
-        '-',
-        'TINYBLOB',
-        'MEDIUMBLOB',
-        'BLOB',
-        'LONGBLOB',
-        '-',
-        'ENUM',
-        'SET',
-    ),
-
-    'SPATIAL' => array(
-        'GEOMETRY',
-        'POINT',
-        'LINESTRING',
-        'POLYGON',
-        'MULTIPOINT',
-        'MULTILINESTRING',
-        'MULTIPOLYGON',
-        'GEOMETRYCOLLECTION',
-    ),
-);
+$cfg['ColumnTypes'] = array();
 
 /**
  * Attributes
  *
+ * Fill in this array to overwrite data from data_*.inc.php files
+ *
  * @global array $cfg['AttributeTypes']
  */
-$cfg['AttributeTypes'] = array(
-   '',
-   'BINARY',
-   'UNSIGNED',
-   'UNSIGNED ZEROFILL',
-   'on update CURRENT_TIMESTAMP',
-);
-
+$cfg['AttributeTypes'] = array();
 
 if ($cfg['ShowFunctionFields']) {
     /**
      * Available functions
      *
+     * Fill in this array to overwrite data from data_*.inc.php files
+     *
      * @global array $cfg['Functions']
      */
-    $cfg['Functions'] = array(
-        'ABS',
-        'ACOS',
-        'ASCII',
-        'ASIN',
-        'ATAN',
-        'BIN',
-        'BIT_COUNT',
-        'BIT_LENGTH',
-        'CEILING',
-        'CHAR',
-        'CHAR_LENGTH',
-        'COMPRESS',
-        'CONNECTION_ID',
-        'COS',
-        'COT',
-        'CRC32',
-        'CURDATE',
-        'CURRENT_USER',
-        'CURTIME',
-        'DATABASE',
-        'DATE',
-        'DAYNAME',
-        'DAYOFMONTH',
-        'DAYOFWEEK',
-        'DAYOFYEAR',
-        'DEGREES',
-        'DES_DECRYPT',
-        'DES_ENCRYPT',
-        'ENCRYPT',
-        'EXP',
-        'FLOOR',
-        'FROM_DAYS',
-        'FROM_UNIXTIME',
-        'HEX',
-        'HOUR',
-        'INET_ATON',
-        'INET_NTOA',
-        'LAST_DAY',
-        'LENGTH',
-        'LN',
-        'LOAD_FILE',
-        'LOG',
-        'LOG10',
-        'LOG2',
-        'LOWER',
-        'LTRIM',
-        'MD5',
-        'MICROSECOND',
-        'MINUTE',
-        'MONTH',
-        'MONTHNAME',
-        'NOW',
-        'OCT',
-        'OLD_PASSWORD',
-        'ORD',
-        'PASSWORD',
-        'PI',
-        'QUARTER',
-        'QUOTE',
-        'RADIANS',
-        'RAND',
-        'REVERSE',
-        'ROUND',
-        'RTRIM',
-        'SECOND',
-        'SEC_TO_TIME',
-        'SHA1',
-        'SIGN',
-        'SIN',
-        'SOUNDEX',
-        'SPACE',
-        'SQRT',
-        'SYSDATE',
-        'TAN',
-        'TIME',
-        'TIMESTAMP',
-        'TIME_TO_SEC',
-        'TO_DAYS',
-        'TRIM',
-        'UNCOMPRESS',
-        'UNCOMPRESSED_LENGTH',
-        'UNHEX',
-        'UNIX_TIMESTAMP',
-        'UPPER',
-        'USER',
-        'UTC_DATE',
-        'UTC_TIME',
-        'UTC_TIMESTAMP',
-        'UUID',
-        'VERSION',
-        'WEEK',
-        'WEEKDAY',
-        'WEEKOFYEAR',
-        'YEAR',
-        'YEARWEEK',
-    );
+    $cfg['Functions'] = array();
 
     /**
      * Which column types will be mapped to which Group?
      *
+     * Fill in this array to overwrite data from data_*.inc.php files
+     *
      * @global array $cfg['RestrictColumnTypes']
      */
-    $cfg['RestrictColumnTypes'] = array(
-        'TINYINT'   => 'FUNC_NUMBER',
-        'SMALLINT'  => 'FUNC_NUMBER',
-        'MEDIUMINT' => 'FUNC_NUMBER',
-        'INT'       => 'FUNC_NUMBER',
-        'BIGINT'    => 'FUNC_NUMBER',
-        'DECIMAL'   => 'FUNC_NUMBER',
-        'FLOAT'     => 'FUNC_NUMBER',
-        'DOUBLE'    => 'FUNC_NUMBER',
-        'REAL'      => 'FUNC_NUMBER',
-        'BIT'       => 'FUNC_NUMBER',
-        'BOOLEAN'   => 'FUNC_NUMBER',
-        'SERIAL'    => 'FUNC_NUMBER',
-
-        'DATE'      => 'FUNC_DATE',
-        'DATETIME'  => 'FUNC_DATE',
-        'TIMESTAMP' => 'FUNC_DATE',
-        'TIME'      => 'FUNC_DATE',
-        'YEAR'      => 'FUNC_DATE',
-
-        'CHAR'       => 'FUNC_CHAR',
-        'VARCHAR'    => 'FUNC_CHAR',
-        'TINYTEXT'   => 'FUNC_CHAR',
-        'TEXT'       => 'FUNC_CHAR',
-        'MEDIUMTEXT' => 'FUNC_CHAR',
-        'LONGTEXT'   => 'FUNC_CHAR',
-        'BINARY'     => 'FUNC_CHAR',
-        'VARBINARY'  => 'FUNC_CHAR',
-        'TINYBLOB'   => 'FUNC_CHAR',
-        'MEDIUMBLOB' => 'FUNC_CHAR',
-        'BLOB'       => 'FUNC_CHAR',
-        'LONGBLOB'   => 'FUNC_CHAR',
-        'ENUM'       => '',
-        'SET'        => '',
-
-        'GEOMETRY'           => 'FUNC_SPATIAL',
-        'POINT'              => 'FUNC_SPATIAL',
-        'LINESTRING'         => 'FUNC_SPATIAL',
-        'POLYGON'            => 'FUNC_SPATIAL',
-        'MULTIPOINT'         => 'FUNC_SPATIAL',
-        'MULTILINESTRING'    => 'FUNC_SPATIAL',
-        'MULTIPOLYGON'       => 'FUNC_SPATIAL',
-        'GEOMETRYCOLLECTION' => 'FUNC_SPATIAL',
-
-    );
+    $cfg['RestrictColumnTypes'] = array();
 
     /**
      * Map above defined groups to any function
      *
+     * Fill in this array to overwrite data from data_*.inc.php files
+     *
      * @global array $cfg['RestrictFunctions']
      */
-    $cfg['RestrictFunctions'] = array(
-        'FUNC_CHAR' => array(
-            'BIN',
-            'CHAR',
-            'CURRENT_USER',
-            'COMPRESS',
-            'DATABASE',
-            'DAYNAME',
-            'DES_DECRYPT',
-            'DES_ENCRYPT',
-            'ENCRYPT',
-            'HEX',
-            'INET_NTOA',
-            'LOAD_FILE',
-            'LOWER',
-            'LTRIM',
-            'MD5',
-            'MONTHNAME',
-            'OLD_PASSWORD',
-            'PASSWORD',
-            'QUOTE',
-            'REVERSE',
-            'RTRIM',
-            'SHA1',
-            'SOUNDEX',
-            'SPACE',
-            'TRIM',
-            'UNCOMPRESS',
-            'UNHEX',
-            'UPPER',
-            'USER',
-            'UUID',
-        ),
-
-        'FUNC_DATE' => array(
-            'CURDATE',
-            'CURTIME',
-            'DATE',
-            'FROM_DAYS',
-            'FROM_UNIXTIME',
-            'LAST_DAY',
-            'NOW',
-            'SEC_TO_TIME',
-            'SYSDATE',
-            'TIME',
-            'TIMESTAMP',
-            'UTC_DATE',
-            'UTC_TIME',
-            'UTC_TIMESTAMP',
-            'YEAR',
-        ),
-
-        'FUNC_NUMBER' => array(
-            'ABS',
-            'ACOS',
-            'ASCII',
-            'ASIN',
-            'ATAN',
-            'BIT_LENGTH',
-            'BIT_COUNT',
-            'CEILING',
-            'CHAR_LENGTH',
-            'CONNECTION_ID',
-            'COS',
-            'COT',
-            'CRC32',
-            'DAYOFMONTH',
-            'DAYOFWEEK',
-            'DAYOFYEAR',
-            'DEGREES',
-            'EXP',
-            'FLOOR',
-            'HOUR',
-            'INET_ATON',
-            'LENGTH',
-            'LN',
-            'LOG',
-            'LOG2',
-            'LOG10',
-            'MICROSECOND',
-            'MINUTE',
-            'MONTH',
-            'OCT',
-            'ORD',
-            'PI',
-            'QUARTER',
-            'RADIANS',
-            'RAND',
-            'ROUND',
-            'SECOND',
-            'SIGN',
-            'SIN',
-            'SQRT',
-            'TAN',
-            'TO_DAYS',
-            'TIME_TO_SEC',
-            'UNCOMPRESSED_LENGTH',
-            'UNIX_TIMESTAMP',
-            'WEEK',
-            'WEEKDAY',
-            'WEEKOFYEAR',
-            'YEARWEEK',
-        ),
-
-        'FUNC_SPATIAL' => array(
-            'GeomFromText',
-            'GeomFromWKB',
-
-            'GeomCollFromText',
-            'LineFromText',
-            'MLineFromText',
-            'PointFromText',
-            'MPointFromText',
-            'PolyFromText',
-            'MPolyFromText',
-
-            'GeomCollFromWKB',
-            'LineFromWKB',
-            'MLineFromWKB',
-            'PointFromWKB',
-            'MPointFromWKB',
-            'PolyFromWKB',
-            'MPolyFromWKB',
-        ),
-    );
+    $cfg['RestrictFunctions'] = array();
 
     /**
      * Default functions for above defined groups
      *
+     * Fill in this array to overwrite data from data_*.inc.php files
+     *
      * @global array $cfg['DefaultFunctions']
      */
-    $cfg['DefaultFunctions'] = array(
-        'FUNC_CHAR' => '',
-        'FUNC_DATE' => '',
-        'FUNC_NUMBER' => '',
-        'first_timestamp' => 'NOW',
-        'pk_char36' => 'UUID',
-    );
-} // end if
+    $cfg['DefaultFunctions'] = array();
+}
 
 ?>
