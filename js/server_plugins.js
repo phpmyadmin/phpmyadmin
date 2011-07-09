26038eb6b5263f34e542611d3b667079e81f4b91
Piotr Przybylski <piotrprz@gmail.com>
2011-07-09 17:59:49 +0200

    Fix page height Optimize selector

--- js/server_plugins.js
+++ js/server_plugins.js
@@ -9,13 +9,18 @@ $(function() {
     $('#pluginsTabs').tabs({
         // Tab persistence
         cookie: { name: 'pma_serverStatusTabs', expires: 1 },
-        // Fixes line break in the menu bar when the page overflows and scrollbar appears
-        show: function() { menuResize(); }
+        show: function(event, ui) {
+            // Fixes line break in the menu bar when the page overflows and scrollbar appears
+            menuResize();
+            // 'Plugins' tab is too high due to hiding of 'Modules' by negative left position,
+            // hide tabs by changing display to fix it
+            $(ui.panel).closest('.ui-tabs').find('> div').not(ui.panel).css('display', 'none');
+            $(ui.panel).css('display', 'block');
+        }
     });
 
     // Make columns sortable, but only for tables with more than 1 data row
-    var $tables = $('#plugins_plugins table:has(tbody tr + tr:first)')
-        /*.add('#plugins_modules table')*/;
+    var $tables = $('#plugins_plugins table:has(tbody tr + tr)');
     $tables.tablesorter({
         sortList: [[0,0]],
         widgets: ['zebra']
