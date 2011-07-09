e808b244d72c2d8ab0120340489ec8fd5e35e92e
Piotr Przybylski <piotrprz@gmail.com>
2011-07-09 23:38:29 +0200

    Remove unused addEvent and removeEvent

--- js/common.js
+++ js/common.js
@@ -15,44 +15,6 @@ var querywindow = '';
 var query_to_load = '';
 
 /**
- * attach a function to object event
- *
- * <code>
- * addEvent(window, 'load', PMA_initPage);
- * </code>
- * @param object or id
- * @param string event type (load, mouseover, focus, ...)
- * @param function to be attached
- */
-function addEvent(obj, type, fn)
-{
-    if (obj.attachEvent) {
-        obj['e' + type + fn] = fn;
-        obj[type + fn] = function() {obj['e' + type + fn](window.event);}
-        obj.attachEvent('on' + type, obj[type + fn]);
-    } else {
-        obj.addEventListener(type, fn, false);
-    }
-}
-
-/**
- * detach/remove a function from an object event
- *
- * @param object or id
- * @param event type (load, mouseover, focus, ...)
- * @param function naem of function to be attached
- */
-function removeEvent(obj, type, fn)
-{
-    if (obj.detachEvent) {
-        obj.detachEvent('on' + type, obj[type + fn]);
-        obj[type + fn] = null;
-    } else {
-        obj.removeEventListener(type, fn, false);
-    }
-}
-
-/**
  * get DOM elements by html class
  *
  * @param string class_name - name of class
