05a79cbbf8d366a72e17e941dc52a070d296ff1b
Zarubin Stas <zarubin@zsv.org.ua>
2011-07-14 07:34:38 +0300

    Test for PMA_langDetails_test(), PMA_langList_test() and PMA_langName_test() from select_lang.lib.php

---
+++ test/libraries/select_lang/PMA_langName_test.php
@@ -0,0 +1,35 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Test for PMA_langName from select_lang.lib.php
+ *
+ * @package phpMyAdmin-test
+ * @version $Id: PMA_langName_test.php
+ * @group select_lang.lib-tests
+ */
+
+/*
+ * Include to test.
+ */
+require_once 'libraries/select_lang.lib.php';
+
+class PMA_langName_test extends PHPUnit_Framework_TestCase
+{
+    function dataProvider()
+    {
+        return array(
+            array(array('en|english', 'en', ''),'English'),
+            array(array('fr|french', 'fr', 'Fran&ccedil;ais'), 'Fran&ccedil;ais - French'),
+            array(array('zh|chinese simplified', 'zh', '&#20013;&#25991;'), '&#20013;&#25991; - Chinese simplified'),
+        );
+    }
+
+    /**
+     * @dataProvider dataProvider
+     * @return void
+     */
+    function testLangName($test, $result)
+    {
+        $this->assertEquals($result, PMA_langName($test));
+    }
+}
