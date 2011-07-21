578bb63d76378363bc7f10443dc6cde80d24171f
Michal Čihař <mcihar@suse.cz>
2011-07-21 11:46:14 +0200

    Another variable needed for test

--- test/libraries/common/PMA_checkParameters_test.php
+++ test/libraries/common/PMA_checkParameters_test.php
@@ -20,7 +20,7 @@ class PMA_checkParameters_test extends PHPUnit_Extensions_OutputTestCase
     {
         $GLOBALS['PMA_Config'] = new PMA_Config();
         $_SESSION['PMA_Theme'] = new PMA_Theme();
-        $GLOBALS['cfg'] = array('ReplaceHelpImg' => true);
+        $GLOBALS['cfg'] = array('ReplaceHelpImg' => true, 'ServerDefault' => 1);
         $GLOBALS['pmaThemeImage'] = 'theme/';
     }
 
