41b23ebf4ad737ac894c09f09aa7f9507c8c587f
Michal Čihař <mcihar@suse.cz>
2011-07-21 16:50:12 +0200

    Wrap long lines

--- test/libraries/common/PMA_showPHPDocu_test.php
+++ test/libraries/common/PMA_showPHPDocu_test.php
@@ -21,7 +21,9 @@ class PMA_showPHPDocu_test extends PHPUnit_Framework_TestCase
 
         $target = "docu";
         $lang = _pgettext('PHP documentation language', 'en');
-        $expected = '<a href="http://php.net/manual/' . $lang . '/' . $target . '" target="documentation"><img class="icon ic_b_help_s" src="themes/dot.gif" alt="' . __('Documentation') . '" title="' . __('Documentation') . '" /></a>';
+        $expected = '<a href="http://php.net/manual/' . $lang . '/' . $target 
+            . '" target="documentation"><img class="icon ic_b_help_s" src="themes/dot.gif" alt="' 
+            . __('Documentation') . '" title="' . __('Documentation') . '" /></a>';
 
         $this->assertEquals($expected, PMA_showPHPDocu($target));
     }
@@ -32,7 +34,8 @@ class PMA_showPHPDocu_test extends PHPUnit_Framework_TestCase
 
         $target = "docu";
         $lang = _pgettext('PHP documentation language', 'en');
-        $expected = '[<a href="http://php.net/manual/' . $lang . '/' . $target . '" target="documentation">' . __('Documentation') . '</a>]';
+        $expected = '[<a href="http://php.net/manual/' . $lang . '/' . $target 
+            . '" target="documentation">' . __('Documentation') . '</a>]';
 
         $this->assertEquals($expected, PMA_showPHPDocu($target));
     }
