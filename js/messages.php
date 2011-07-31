e83f3acd26fbfb51366cae9b184e69e5376ae10d
Ammar Yasir <ammaryasir.88@gmail.com>
2011-07-31 12:35:07 +0530

    Added to 'How to use' message content, strings are converted into integers for plotting purpose.

--- js/messages.php
+++ js/messages.php
@@ -213,7 +213,7 @@ $js_messages['strShowSearchCriteria'] = __('Show search criteria');
 
 /* For tbl_zoom_plot.js */
 $js_messages['strZoomSearch'] = __('Zoom Search');
-$js_messages['strDisplayHelp'] =  __('* Each point represents a data row.<br>* Hovering over a point will show its label.<br>* Drag and select an area in the plot to zoom into it.<br>* Click reset zoom link to come back to original state.<br>*  Click a data point to view and possibly edit the data row.<br>* The plot can be resized by dragging it along the bottom right corner.<br>');
+$js_messages['strDisplayHelp'] =  __('* Each point represents a data row.<br>* Hovering over a point will show its label.<br>* Drag and select an area in the plot to zoom into it.<br>* Click reset zoom link to come back to original state.<br>*  Click a data point to view and possibly edit the data row.<br>* The plot can be resized by dragging it along the bottom right corner.<br>* Strings are converted into integer for plotting');
 $js_messages['strInputNull'] = __('<b>Select two columns</b>');
 $js_messages['strSameInputs'] = __('<b>Select two different columns</b>');
 
