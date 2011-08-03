68e21b05e46f144735faa849b80734d83812c7e6
Madhura Jayaratne <madhura.cj@gmail.com>
2011-08-04 00:04:16 +0530

    Changelog entry for bug #3383711. Some formatting

--- libraries/display_tbl.lib.php
+++ libraries/display_tbl.lib.php
@@ -796,8 +796,9 @@ function PMA_displayTableHeaders(&$is_display, &$fields_meta, $fields_cnt = 0, $
             // 2.1.3 Check the field name for a bracket.
             //       If it contains one, it's probably a function column
             //       like 'COUNT(`field`)'
-            // This check is not sufficient, see bug #3383711, it still might be a column name of a view, check is_orgname
-            if (strpos($name_to_use_in_sort, '(') !== false && !$is_orgname) {
+            //       It still might be a column name of a view. See bug #3383711
+            //       Check is_orgname.
+            if (strpos($name_to_use_in_sort, '(') !== false && ! $is_orgname) {
                 $sort_order = ' ORDER BY ' . $name_to_use_in_sort . ' ';
             } else {
                 $sort_order = ' ORDER BY ' . $sort_tbl . PMA_backquote($name_to_use_in_sort) . ' ';
