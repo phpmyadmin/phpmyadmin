3fa5e89bb29bb481576a2cf81d3364402bef8dcf
Piotr Przybylski <piotrprz@gmail.com>
2011-08-04 14:14:05 +0200

    Drizzle DBI: default to fully buffered query, like in mysql and mysqli

--- libraries/dbi/drizzle.dbi.lib.php
+++ libraries/dbi/drizzle.dbi.lib.php
@@ -159,9 +159,9 @@ function PMA_DBI_select_db($dbname, $link = null)
  */
 function PMA_DBI_real_query($query, $link, $options)
 {
-    $buffer_mode = $options & PMA_DBI_QUERY_STORE
-        ? PMA_Drizzle::BUFFER_RESULT
-        : PMA_Drizzle::BUFFER_ROW;
+    $buffer_mode = $options & PMA_DBI_QUERY_UNBUFFERED
+        ? PMA_Drizzle::BUFFER_ROW
+        : PMA_Drizzle::BUFFER_RESULT;
     $res = $link->query($query, $buffer_mode);
     return $res;
 }
