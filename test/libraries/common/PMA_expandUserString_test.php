46cd0d6afad6ea9f2682420f1aa1c1da65fb6c1f
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2011-08-04 19:24:15 +0200

    fix typo

--- test/libraries/common/PMA_expandUserString_test.php
+++ test/libraries/common/PMA_expandUserString_test.php
@@ -38,7 +38,7 @@ class PMA_expandUserString_test extends PHPUnit_Extensions_OutputTestCase
     }
 
     /**
-     * Test case for parsing SHOW COLUNS output
+     * Test case for parsing SHOW COLUMNS output
      *
      * @dataProvider provider
      */
@@ -48,7 +48,7 @@ class PMA_expandUserString_test extends PHPUnit_Extensions_OutputTestCase
     }
 
     /**
-     * Test case for parsing SHOW COLUNS output
+     * Test case for parsing SHOW COLUMNS output
      *
      * @dataProvider provider
      */
