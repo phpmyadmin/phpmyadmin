79c5b6b0e269f8bfa153f9bc6e61325804f99cc0
Michal Čihař <mcihar@suse.cz>
2011-08-10 10:57:44 +0200

    Replace all possibly dangerous chars in filename of downloaded file

--- libraries/core.lib.php
+++ libraries/core.lib.php
@@ -576,6 +576,8 @@ function PMA_download_header($filename, $mimetype, $length = 0, $no_cache = true
     if ($no_cache) {
         PMA_no_cache_header();
     }
+    /* Replace all possibly dangerous chars in filename */
+    $filename = str_replace(array(';', '"', "\n", "\r"), '-', $filename);
     header('Content-Description: File Transfer');
     header('Content-Disposition: attachment; filename="' . $filename . '"');
     header('Content-Type: ' . $mimetype);
