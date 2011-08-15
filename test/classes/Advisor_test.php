74764f3cf1c238fed2cdb29e62fb59bbcf771c69
Michal Čihař <mcihar@suse.cz>
2011-08-15 11:24:22 +0200

    Add test case for failed eval

--- test/classes/Advisor_test.php
+++ test/classes/Advisor_test.php
@@ -50,15 +50,15 @@ class Advisor_test extends PHPUnit_Framework_TestCase
      * @depends testParse
      * @dataProvider rulesProvider
      */
-    public function testAddRule($rule, $expected, $errors)
+    public function testAddRule($rule, $expected, $error)
     {
         $advisor = new Advisor();
         $parseResult = $advisor->parseRulesFile();
         $this->assertEquals($parseResult['errors'], array());
         $advisor->variables['value'] = 0;
         $advisor->addRule('fired', $rule);
-        if (isset($advisor->runResult['errors']) || $errors != array()) {
-            $this->assertEquals($advisor->runResult['errors'], array());
+        if (isset($advisor->runResult['errors']) || !is_null($error)) {
+            $this->assertEquals($advisor->runResult['errors'], array($error));
         }
         if (isset($advisor->runResult['fired']) || $expected != array()) {
             $this->assertEquals($advisor->runResult['fired'], array($expected));
@@ -71,27 +71,32 @@ class Advisor_test extends PHPUnit_Framework_TestCase
             array(
                 array('justification' => 'foo', 'name' => 'Basic', 'issue' => 'issue', 'recommendation' => 'Recommend'),
                 array('justification' => 'foo', 'name' => 'Basic', 'issue' => 'issue', 'recommendation' => 'Recommend'),
-                array(),
+                null,
             ),
             array(
                 array('justification' => 'foo', 'name' => 'Variable', 'issue' => 'issue', 'recommendation' => 'Recommend {status_var}'),
                 array('justification' => 'foo', 'name' => 'Variable', 'issue' => 'issue', 'recommendation' => 'Recommend <a href="server_variables.php?lang=en&amp;token=token#filter=status_var">status_var</a>'),
-                array(),
+                null,
             ),
             array(
                 array('justification' => '%s foo | value', 'name' => 'Format', 'issue' => 'issue', 'recommendation' => 'Recommend'),
                 array('justification' => '0 foo', 'name' => 'Format', 'issue' => 'issue', 'recommendation' => 'Recommend'),
-                array(),
+                null,
             ),
             array(
                 array('justification' => '%s% foo | value', 'name' => 'Percent', 'issue' => 'issue', 'recommendation' => 'Recommend'),
                 array('justification' => '0% foo', 'name' => 'Percent', 'issue' => 'issue', 'recommendation' => 'Recommend'),
-                array(),
+                null,
             ),
             array(
                 array('justification' => '"\'foo', 'name' => 'Quotes', 'issue' => 'issue', 'recommendation' => 'Recommend"\''),
                 array('justification' => '"\'foo', 'name' => 'Quotes', 'issue' => 'issue', 'recommendation' => 'Recommend"\''),
+                null,
+            ),
+            array(
+                array('justification' => 'foo | fsafdsa', 'name' => 'Failure', 'issue' => 'issue', 'recommendation' => 'Recommend'),
                 array(),
+                'Failed formattingstring for rule \'Failure\'. PHP threw following error: Use of undefined constant fsafdsa - assumed \'fsafdsa\'',
             ),
         );
     }
