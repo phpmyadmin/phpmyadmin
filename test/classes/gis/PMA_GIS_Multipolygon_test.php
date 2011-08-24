1fddc1891bdef030fc10eb66eaf78eb2a2d42335
Madhura Jayaratne <madhura.cj@gmail.com>
2011-08-24 10:26:47 +0530

    more tests for scaleRow function for better test coverage

--- test/classes/gis/PMA_GIS_Multipolygon_test.php
+++ test/classes/gis/PMA_GIS_Multipolygon_test.php
@@ -243,6 +243,16 @@ class PMA_GIS_MultipolygonTest extends PMA_GIS_GeomTest
                     'minY' => 0,
                     'maxY' => 83
                 )
+            ),
+            array(
+                'MULTIPOLYGON(((35 10,10 20,15 40,45 45,35 10),(20 30,35 32,30 20'
+                    . ',20 30)),((105 0,56 20,78 73,105 0)))',
+                array(
+                    'minX' => 10,
+                    'maxX' => 105,
+                    'minY' => 0,
+                    'maxY' => 73
+                )
             )
         );
     }
