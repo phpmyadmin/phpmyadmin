2a14fdec556a78a5682f57625611831f381c63e2
Rouslan Placella <rouslan@placella.com>
2011-10-30 20:41:35 +0000

    Scroll to results after clicking "Browse" in database search

--- js/db_search.js
+++ js/db_search.js
@@ -37,6 +37,10 @@ function loadResult(result_path, table_name, link, ajaxEnable)
             $('#table-link').attr({"href" : 'sql.php?'+link }).text(table_name);
             var url = result_path + " #sqlqueryresults";
             $('#browse-results').load(url, null, function() {
+                $('html, body')
+                    .animate({
+                        scrollTop: $("#browse-results").offset().top
+                    }, 1000);
                 PMA_ajaxRemoveMessage($msg);
                 // because under db_search, window.parent.table is not defined yet,
                 // we assign it manually from #table-link
