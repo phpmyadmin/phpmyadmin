phpmyadmin/phpmyadmin

commit f62c4a1809458008c778afac98e481f54def216b

coding style : control structures

server_databases.php
@@ -181,8 +181,7 @@ if ($databases_count > 0) {
         }
     }
 
-    foreach ($replication_types as $type)
-    {
+    foreach ($replication_types as $type) {
       if ($type=="master")
         $name = __('Master replication');
       elseif ($type == "slave")
@@ -245,10 +244,10 @@ if ($databases_count > 0) {
         }
     }
 
-    foreach ($replication_types as $type)
-    {
-      if (${"server_{$type}_status"})
-        echo '    <th></th>' . "\n";
+    foreach ($replication_types as $type) {
+        if (${"server_{$type}_status"}) {
+            echo '    <th></th>' . "\n";
+        {
     }
 
     if ($is_superuser) {

enum_editor.php
@@ -93,7 +93,7 @@ require_once './libraries/header_meta_style.inc.php';
                 // Remove a value, given a valid index, from the list
                 // of values, if there was a request to do so.
                 if (isset($_GET['drop']) && is_array($_GET['drop'])) {
-                    foreach ($_GET['drop'] as $index => $value){
+                    foreach ($_GET['drop'] as $index => $value) {
                         if ((int)$index == $index
                             && $index > 0
                             && $index <= count($values)

libraries/header.inc.php
@@ -208,8 +208,8 @@ if (isset($GLOBALS['is_ajax_request']) && !$GLOBALS['is_ajax_request']) {
          * Sets a variable to remember headers have been sent
          */
         $GLOBALS['is_header_sent'] = true;
-} //end if (!$GLOBALS['is_ajax_request'])
-else {
+//end if (!$GLOBALS['is_ajax_request'])
+} else {
     if (empty($GLOBALS['is_header_sent'])) {
         include_once './libraries/header_http.inc.php';
         $GLOBALS['is_header_sent'] = true;

libraries/header_scripts.inc.php
@@ -64,7 +64,7 @@ if (typeof(parent.document) != 'undefined' && typeof(parent.document) != 'unknow
     parent.document.title = '<?php echo (isset($title) ? PMA_sanitize(PMA_escapeJsString(htmlspecialchars($title))) : ''); ?>';
 }
 <?php
-if(count($GLOBALS['js_script']) > 0) {
+if (count($GLOBALS['js_script']) > 0) {
     echo implode("\n", $GLOBALS['js_script'])."\n";
 }
 

libraries/import/sql.php
@@ -106,7 +106,7 @@ while (!($GLOBALS['finished'] && $i >= $len) && !$error && !$timeout_passed) {
         // free memory
         unset($data);
         // Do not parse string when we're not at the end and don't have ; inside
-        if ((strpos($buffer, $sql_delimiter, $i) === false) && !$GLOBALS['finished'])  {
+        if ((strpos($buffer, $sql_delimiter, $i) === false) && !$GLOBALS['finished']) {
             continue;
         }
     }

libraries/mult_submits.inc.php
@@ -262,8 +262,7 @@ if (!empty($submit_mult) && !empty($what)) {
                 <button type="submit" name="mult_btn" value="<?php echo __('Yes'); ?>" id="buttonYes"><?php echo __('Submit'); ?></button>
         </fieldset>
     <?php
-        }
-        elseif ($what == 'add_prefix_tbl') { ?>
+        } elseif ($what == 'add_prefix_tbl') { ?>
         <fieldset class = "input">
                 <legend><?php echo __('Add table prefix') ?>:</legend>
                 <table>
@@ -276,8 +275,7 @@ if (!empty($submit_mult) && !empty($what)) {
                 <button type="submit" name="mult_btn" value="<?php echo __('Yes'); ?>" id="buttonYes"><?php echo __('Submit'); ?></button>
         </fieldset>
     <?php
-        }
-        else { ?>
+        } else { ?>
     <fieldset class="confirmation">
         <legend><?php echo ($what == 'drop_db' ? __('You are about to DESTROY a complete database!') . '&nbsp;' : '') . __('Do you really want to '); ?>:</legend>
         <tt><?php echo $full_query; ?></tt>
@@ -290,14 +288,10 @@ if (!empty($submit_mult) && !empty($what)) {
     }
     include './libraries/footer.inc.php';
 
-} // end if
-
-
+} elseif ($mult_btn == __('Yes')) {
 /**
  * Executes the query - dropping rows, columns/fields, tables or dbs
  */
-elseif ($mult_btn == __('Yes')) {
-
     if ($query_type == 'drop_db' || $query_type == 'drop_tbl' || $query_type == 'drop_fld') {
         include_once './libraries/relation_cleanup.lib.php';
     }

libraries/svg_plot/pma_scatter_plot.php
@@ -118,13 +118,13 @@ class PMA_Scatter_Plot
     {
         $this->_dataPoints = array();
         if (! is_null($this->_userSpecifiedSettings)) {
-            foreach (array_keys($this->_userSpecifiedSettings) as $key){
-            $this->_settings[$key] = $this->_userSpecifiedSettings[$key];
-         }
+            foreach (array_keys($this->_userSpecifiedSettings) as $key) {
+                $this->_settings[$key] = $this->_userSpecifiedSettings[$key];
+            }
         }
         if ($this->_settings['dataLabel'] == '') {
             $labels = array_keys($this->_data[0]);
-        $this->_settings['dataLabel'] = $labels[0];
+            $this->_settings['dataLabel'] = $labels[0];
         }
     }
 
@@ -213,8 +213,7 @@ class PMA_Scatter_Plot
                 for ($j = 0 ; $j < count($coordinates[$i]) ; $j++) {
                      $coordinates[$i][$j] = $map[$coordinates[$i][$j]];
                 }
-            }
-            else if (is_numeric($coordinates[$i][0])) {
+            } else if (is_numeric($coordinates[$i][0])) {
                 $maxC = max($coordinates[$i]);
                 for ($j = 0 ; $j < count($coordinates[$i]) ; $j++) {
                     if ($i == 0) {

libraries/tbl_links.inc.php
@@ -122,8 +122,7 @@ if ($table_info_num_rows == 0 && !$tbl_is_view) {
 echo PMA_generate_html_tabs($tabs, $url_params);
 unset($tabs);
 
-if (PMA_Tracker::isActive() and PMA_Tracker::isTracked($GLOBALS["db"], $GLOBALS["table"]))
-{
+if (PMA_Tracker::isActive() and PMA_Tracker::isTracked($GLOBALS["db"], $GLOBALS["table"])) {
     $msg = PMA_Message::notice('<a href="tbl_tracking.php?'.$url_query.'">'.sprintf(__('Tracking of %s.%s is activated.'), htmlspecialchars($GLOBALS["db"]), htmlspecialchars($GLOBALS["table"])).'</a>');
     $msg->display();
 }

commit 03fd5593823d6999e85792a910d460902f844db3

Fix syntax error introduced by f62c4a1809458008c778afac98e481f54def216b

server_databases.php
@@ -247,7 +247,7 @@ if ($databases_count > 0) {
     foreach ($replication_types as $type) {
         if (${"server_{$type}_status"}) {
             echo '    <th></th>' . "\n";
-        {
+        }
     }
 
     if ($is_superuser) {