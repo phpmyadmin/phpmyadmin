1a98c838e6142664d3f2d41543b08ee1ed0dada3
Marc Delisle <marc@infomarc.info>
2012-01-06 05:53:16 -0500

    3.5.0-alpha1

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -98,7 +98,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '3.5.0-dev');
+        $this->set('PMA_VERSION', '3.5.0-alpha1');
         /**
          * @deprecated
          */
