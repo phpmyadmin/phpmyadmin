6fc336b2ddf86e3de51e5c21efccbf25ff4ba386
Marc Delisle <marc@infomarc.info>
2012-01-25 05:49:35 -0500

    Starting 3.5.0-beta2

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -98,7 +98,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '3.5.0-beta1');
+        $this->set('PMA_VERSION', '3.5.0-beta2');
         /**
          * @deprecated
          */
