d4eaf57b88584d0b863b44a50b849014c6d18afc
Michal Čihař <mcihar@suse.cz>
2012-01-27 14:16:09 +0100

    Prevent warnings from ini_set

--- test/classes/PMA_Config_test.php
+++ test/classes/PMA_Config_test.php
@@ -69,11 +69,11 @@ class PMA_ConfigTest extends PHPUnit_Framework_TestCase
         $this->object->checkOutputCompression();
         $this->assertEquals('auto', $this->object->get("OBGzip"));
 
-        ini_set('zlib.output_compression', 'Off');
+        @ini_set('zlib.output_compression', 'Off');
         $this->object->checkOutputCompression();
         $this->assertFalse($this->object->get("OBGzip"));
 
-        ini_set('zlib.output_compression', 'On');
+        @ini_set('zlib.output_compression', 'On');
     }
 
     public function testCheckClient()
