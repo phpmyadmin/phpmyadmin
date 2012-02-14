c748c3bf5af1b803049b904f590f255323c975bc
Michal Čihař <mcihar@suse.cz>
2012-02-14 16:15:18 +0100

    Compatibility with 3.4 branch

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -147,7 +147,19 @@ for kit in $KITS ; do
     # Cleanup translations
     cd phpMyAdmin-$version-$kit
     scripts/lang-cleanup.sh $kit
-    rm -rf scripts
+    if [ -f examples/create_tables.sql ] ; then
+        # 3.5 and newer
+        rm -rf scripts
+    else
+        # 3.4 and older
+        # Remove javascript compiler, no need to ship it
+        rm -rf scripts/google-javascript-compiler/
+
+        # Remove scripts which are not useful for user
+        for s in compress-js create-release.sh generate-mo mergepo.py php2gettext.sh remove_control_m.sh update-po upload-release pending-po pendingpo.py ; do
+            rm -f scripts/$s
+        done
+    fi
     cd ..
 
     # Remove tar file possibly left from previous run
