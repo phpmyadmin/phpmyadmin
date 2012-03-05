b916cdb4aafa81c14666f2a7c99c379a27774d7d
Onur Avsar <avsaro42@gmail.com>
2012-03-05 12:05:36 +0200

    Added feature request 'clearing DEFAULT NULL' (rfe:3484063).

--- libraries/Table.class.php
+++ libraries/Table.class.php
@@ -415,6 +415,11 @@ class PMA_Table
             }
             break;
         case 'NULL' :
+            //If user uncheck null checkbox and not change default value null,
+            //default value will be ignored.
+            if ($null !== false && $null != 'NULL') {
+                break;
+            }
         case 'CURRENT_TIMESTAMP' :
             $query .= ' DEFAULT ' . $default_type;
             break;
