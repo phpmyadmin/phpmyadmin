ce5ed096e6d88ccd6b663d8ac0f0af2611e70625
Alex Marin <alex.ukf@gmail.com>
2012-04-01 13:25:02 +0300

    remove unused functions PMA_transformation_global_plain and PMA_transformation_global_html

--- libraries/transformations/global.inc.php
+++ libraries/transformations/global.inc.php
@@ -25,16 +25,6 @@
 /**
  *
  */
-function PMA_transformation_global_plain($buffer, $options = array(), $meta = '')
-{
-    return htmlspecialchars($buffer);
-}
-
-function PMA_transformation_global_html($buffer, $options = array(), $meta = '')
-{
-    return $buffer;
-}
-
 function PMA_transformation_global_html_replace($buffer, $options = array(), $meta = '')
 {
     if (!isset($options['string'])) {
