3f13a99c3c22d93a2eee689fe9be6d4676b5b55c
Jo Michael <me@mynetx.net>
2012-04-08 19:13:33 +0200

    Fix typo in '...is lower that cookie validity...'

--- main.php
+++ main.php
@@ -289,7 +289,7 @@ if (! @extension_loaded('mbstring')) {
  */
 $gc_time = (int)@ini_get('session.gc_maxlifetime');
 if ($gc_time < $GLOBALS['cfg']['LoginCookieValidity'] ) {
-    trigger_error(PMA_Message::decodeBB(__('Your PHP parameter [a@http://php.net/manual/en/session.configuration.php#ini.session.gc-maxlifetime@]session.gc_maxlifetime[/a] is lower that cookie validity configured in phpMyAdmin, because of this, your login will expire sooner than configured in phpMyAdmin.')), E_USER_WARNING);
+    trigger_error(PMA_Message::decodeBB(__('Your PHP parameter [a@http://php.net/manual/en/session.configuration.php#ini.session.gc-maxlifetime@]session.gc_maxlifetime[/a] is lower than cookie validity configured in phpMyAdmin, because of this, your login will expire sooner than configured in phpMyAdmin.')), E_USER_WARNING);
 }
 
 /**
