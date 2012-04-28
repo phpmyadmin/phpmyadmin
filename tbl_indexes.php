phpmyadmin/phpmyadmin

commit 03cb36142033f0b2d9fcfe60b83de3e493d73a53

Add trailing ; after ALTER TABLE (rfe #2009500)

tbl_indexes.php
@@ -47,7 +47,7 @@ if (isset($_REQUEST['do_save_data'])) {
     $error = false;
 
     // $sql_query is the one displayed in the query box
-    $sql_query = 'ALTER TABLE ' . PMA_backquote($db) . '.' . PMA_backquote($table);
+    $sql_query = 'ALTER TABLE ' . PMA_backquote($db) . '.' . PMA_backquote($table) . ';';
 
     // Drops the old index
     if (! empty($_REQUEST['old_index'])) {

js/tbl_structure.js
@@ -45,7 +45,7 @@ $(document).ready(function() {
         /**
          * @var question    String containing the question to be asked for confirmation
          */
-        var question = $.sprintf(PMA_messages['strDoYouReally'], 'ALTER TABLE `' + escapeHtml(curr_table_name) + '` DROP `' + escapeHtml(curr_column_name) + '`');
+        var question = $.sprintf(PMA_messages['strDoYouReally'], 'ALTER TABLE `' + escapeHtml(curr_table_name) + '` DROP `' + escapeHtml(curr_column_name) + '`;');
 
         $(this).PMA_confirm(question, $(this).attr('href'), function(url) {
 
@@ -91,7 +91,7 @@ $(document).ready(function() {
         /**
          * @var question    String containing the question to be asked for confirmation
          */
-        var question = $.sprintf(PMA_messages['strDoYouReally'], 'ALTER TABLE `' + escapeHtml(curr_table_name) + '` ADD PRIMARY KEY(`' + escapeHtml(curr_column_name) + '`)');
+        var question = $.sprintf(PMA_messages['strDoYouReally'], 'ALTER TABLE `' + escapeHtml(curr_table_name) + '` ADD PRIMARY KEY(`' + escapeHtml(curr_column_name) + '`);');
 
         $(this).PMA_confirm(question, $(this).attr('href'), function(url) {
 
@@ -624,7 +624,7 @@ function moreOptsMenuResize() {
 
     // remove some links if current width is wider than maximum allowed
     if (cur_width > max_width && $table.find("td.more_opts").length != 0) {
-        while (cur_width > max_width 
+        while (cur_width > max_width
             && $(".replaced_by_more:visible").length > 0) {
 
             // hide last visible element

libraries/Index.class.php
@@ -487,13 +487,13 @@ class PMA_Index
 
                 $this_params = $GLOBALS['url_params'];
                 if ($index->getName() == 'PRIMARY') {
-                    $this_params['sql_query'] = 'ALTER TABLE ' . PMA_backquote($table) . ' DROP PRIMARY KEY';
+                    $this_params['sql_query'] = 'ALTER TABLE ' . PMA_backquote($table) . ' DROP PRIMARY KEY;';
                     $this_params['message_to_show'] = __('The primary key has been dropped');
                     $js_msg      = PMA_jsFormat('ALTER TABLE ' . $table . ' DROP PRIMARY KEY');
                 } else {
-                    $this_params['sql_query'] = 'ALTER TABLE ' . PMA_backquote($table) . ' DROP INDEX ' . PMA_backquote($index->getName());
+                    $this_params['sql_query'] = 'ALTER TABLE ' . PMA_backquote($table) . ' DROP INDEX ' . PMA_backquote($index->getName()) . ';';
                     $this_params['message_to_show'] = sprintf(__('Index %s has been dropped'), $index->getName());
-                    $js_msg      = PMA_jsFormat('ALTER TABLE ' . $table . ' DROP INDEX ' . $index->getName());
+                    $js_msg      = PMA_jsFormat('ALTER TABLE ' . $table . ' DROP INDEX ' . $index->getName()) . ';';
                 }
 
                 $r .= '<td ' . $row_span . '>';

pmd_relation_new.php
@@ -57,6 +57,7 @@ if (PMA_foreignkey_supported($type_T1) && PMA_foreignkey_supported($type_T2) && 
         if ($on_update != 'nix') {
             $upd_query   .= ' ON UPDATE ' . $on_update;
         }
+        $upd_query .= ';';
         PMA_DBI_try_query($upd_query) or PMD_return_new(0, __('Error: Relation not added.'));
     PMD_return_new(1, __('FOREIGN KEY relation added'));
     }

pmd_relation_upd.php
@@ -31,7 +31,8 @@ if (PMA_foreignkey_supported($type_T1) && PMA_foreignkey_supported($type_T2) && 
     if (isset($existrel_foreign[$F2]['constraint'])) {
         $upd_query  = 'ALTER TABLE ' . PMA_backquote($T2)
                   . ' DROP FOREIGN KEY '
-                  . PMA_backquote($existrel_foreign[$F2]['constraint']);
+                  . PMA_backquote($existrel_foreign[$F2]['constraint'])
+                  . ';';
         $upd_rs     = PMA_DBI_query($upd_query);
     } else {
         // there can be an internal relation even if InnoDB

tbl_addfield.php
@@ -158,7 +158,7 @@ if (isset($_REQUEST['do_save_data'])) {
     // To allow replication, we first select the db to use and then run queries
     // on this db.
     PMA_DBI_select_db($db) or PMA_mysqlDie(PMA_getError(), 'USE ' . PMA_backquotes($db), '', $err_url);
-    $sql_query    = 'ALTER TABLE ' . PMA_backquote($table) . ' ' . implode(', ', $definitions);
+    $sql_query    = 'ALTER TABLE ' . PMA_backquote($table) . ' ' . implode(', ', $definitions) . ';';
     $result = PMA_DBI_try_query($sql_query);
 
     if ($result === true) {

tbl_alter.php
@@ -105,6 +105,7 @@ if (isset($_REQUEST['do_save_data'])) {
     }
     $sql_query = 'ALTER TABLE ' . PMA_backquote($table) . ' ';
     $sql_query .= implode(', ', $changes) . $key_query;
+    $sql_query .= ';';
     $result    = PMA_DBI_try_query($sql_query);
 
     if ($result !== false) {

tbl_operations.php
@@ -164,6 +164,7 @@ if (isset($_REQUEST['submitoptions'])) {
     if (count($table_alters) > 0) {
         $sql_query      = 'ALTER TABLE ' . PMA_backquote($GLOBALS['table']);
         $sql_query     .= "\r\n" . implode("\r\n", $table_alters);
+        $sql_query     .= ';';
         $result        .= PMA_DBI_query($sql_query) ? true : false;
         $reread_info    = true;
         unset($table_alters);
@@ -191,6 +192,7 @@ if (isset($_REQUEST['submitorderby']) && ! empty($_REQUEST['order_field'])) {
     if (isset($_REQUEST['order_order']) && $_REQUEST['order_order'] === 'desc') {
         $sql_query .= ' DESC';
     }
+    $sql_query .= ';';
     $result = PMA_DBI_query($sql_query);
 } // end if
 
@@ -198,7 +200,7 @@ if (isset($_REQUEST['submitorderby']) && ! empty($_REQUEST['order_field'])) {
  * A partition operation has been requested by the user
  */
 if (isset($_REQUEST['submit_partition']) && ! empty($_REQUEST['partition_operation'])) {
-    $sql_query = 'ALTER TABLE ' . PMA_backquote($GLOBALS['table']) . ' ' . $_REQUEST['partition_operation'] . ' PARTITION ' . $_REQUEST['partition_name'];
+    $sql_query = 'ALTER TABLE ' . PMA_backquote($GLOBALS['table']) . ' ' . $_REQUEST['partition_operation'] . ' PARTITION ' . $_REQUEST['partition_name'] . ';';
     $result = PMA_DBI_query($sql_query);
 } // end if
 
@@ -613,7 +615,7 @@ if ($is_myisam_or_aria || $is_innodb || $is_berkeleydb) {
     if ($is_innodb) {
         $this_url_params = array_merge(
             $url_params,
-            array('sql_query' => 'ALTER TABLE ' . PMA_backquote($GLOBALS['table']) . ' ENGINE = InnoDB')
+            array('sql_query' => 'ALTER TABLE ' . PMA_backquote($GLOBALS['table']) . ' ENGINE = InnoDB;')
         );
         ?>
     <li><a class='maintain_action' href="sql.php<?php echo PMA_generate_common_url($this_url_params); ?>">
@@ -775,7 +777,7 @@ if (! (isset($db_is_information_schema) && $db_is_information_schema)) {
     $this_url_params = array_merge(
         $url_params,
         array(
-            'sql_query' => 'ALTER TABLE ' . PMA_backquote($GLOBALS['table']) . ' REMOVE PARTITIONING'
+            'sql_query' => 'ALTER TABLE ' . PMA_backquote($GLOBALS['table']) . ' REMOVE PARTITIONING;'
             )
         );
 ?>

commit 6281b4f9418323c6bb8b661a0885a2c509b750e3

Fixed query generation for index editor (was broken in 03cb36142033f0b2d9fcfe60b83de3e493d73a53)

tbl_indexes.php
@@ -47,7 +47,7 @@ if (isset($_REQUEST['do_save_data'])) {
     $error = false;
 
     // $sql_query is the one displayed in the query box
-    $sql_query = 'ALTER TABLE ' . PMA_backquote($db) . '.' . PMA_backquote($table) . ';';
+    $sql_query = 'ALTER TABLE ' . PMA_backquote($db) . '.' . PMA_backquote($table);
 
     // Drops the old index
     if (! empty($_REQUEST['old_index'])) {
@@ -93,6 +93,8 @@ if (isset($_REQUEST['do_save_data'])) {
     } else {
         $sql_query .= ' (' . implode(', ', $index_fields) . ')';
     }
+
+    $sql_query .= ';';
 
     if (! $error) {
         PMA_DBI_query($sql_query);