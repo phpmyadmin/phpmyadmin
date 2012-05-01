1a526147e6ea45414321f0140df585b07e0d80de
Alex Marin <alex.ukf@gmail.com>
2012-05-01 20:33:55 +0300

    Export xml coding style: wrap long lines

--- libraries/export/xml.php
+++ libraries/export/xml.php
@@ -20,66 +20,74 @@ if (isset($plugin_list)) {
         'extension' => 'xml',
         'mime_type' => 'text/xml',
         'options' => array(
-            array('type' => 'begin_group', 'name' => 'general_opts'),
-            array('type' => 'hidden', 'name' => 'structure_or_data'),
-            array('type' => 'end_group')
+            array(
+                'type' => 'begin_group',
+                'name' => 'general_opts'
             ),
+            array(
+                'type' => 'hidden',
+                'name' => 'structure_or_data'
+            ),
+            array(
+                'type' => 'end_group'
+            )
+        ),
         'options_text' => __('Options')
-        );
+    );
 
     /* Export structure */
     $plugin_list['xml']['options'][] = array(
         'type' => 'begin_group',
         'name' => 'structure',
         'text' => __('Object creation options (all are recommended)')
-        );
+    );
     if (! PMA_DRIZZLE) {
         $plugin_list['xml']['options'][] = array(
             'type' => 'bool',
             'name' => 'export_functions',
             'text' => __('Functions')
-            );
+        );
         $plugin_list['xml']['options'][] = array(
             'type' => 'bool',
             'name' => 'export_procedures',
             'text' => __('Procedures')
-            );
+        );
     }
     $plugin_list['xml']['options'][] = array(
         'type' => 'bool',
         'name' => 'export_tables',
         'text' => __('Tables')
-        );
+    );
     if (! PMA_DRIZZLE) {
         $plugin_list['xml']['options'][] = array(
             'type' => 'bool',
             'name' => 'export_triggers',
             'text' => __('Triggers')
-            );
+        );
         $plugin_list['xml']['options'][] = array(
             'type' => 'bool',
             'name' => 'export_views',
             'text' => __('Views')
-            );
+        );
     }
     $plugin_list['xml']['options'][] = array(
         'type' => 'end_group'
-        );
+    );
 
     /* Data */
     $plugin_list['xml']['options'][] = array(
         'type' => 'begin_group',
         'name' => 'data',
         'text' => __('Data dump options')
-        );
+    );
     $plugin_list['xml']['options'][] = array(
         'type' => 'bool',
         'name' => 'export_contents',
         'text' => __('Export contents')
-        );
+    );
     $plugin_list['xml']['options'][] = array(
         'type' => 'end_group'
-        );
+    );
 } else {
 
     /**
@@ -111,8 +119,10 @@ if (isset($plugin_list)) {
         global $table;
         global $tables;
 
-        $export_struct = isset($GLOBALS['xml_export_functions']) || isset($GLOBALS['xml_export_procedures'])
-            || isset($GLOBALS['xml_export_tables']) || isset($GLOBALS['xml_export_triggers'])
+        $export_struct = isset($GLOBALS['xml_export_functions'])
+            || isset($GLOBALS['xml_export_procedures'])
+            || isset($GLOBALS['xml_export_tables'])
+            || isset($GLOBALS['xml_export_triggers'])
             || isset($GLOBALS['xml_export_views']);
         $export_data = isset($GLOBALS['xml_export_contents']) ? true : false;
 
@@ -138,7 +148,11 @@ if (isset($plugin_list)) {
                .  '- ' . __('PHP Version') . ': ' . phpversion() . $crlf
                .  '-->' . $crlf . $crlf;
 
-        $head .= '<pma_xml_export version="1.0"' . (($export_struct) ? ' xmlns:pma="http://www.phpmyadmin.net/some_doc_url/"' : '') . '>' . $crlf;
+        $head .= '<pma_xml_export version="1.0"'
+            . (($export_struct)
+            ? ' xmlns:pma="http://www.phpmyadmin.net/some_doc_url/"'
+            : '')
+            . '>' . $crlf;
 
         if ($export_struct) {
             if (PMA_DRIZZLE) {
@@ -150,7 +164,11 @@ if (isset($plugin_list)) {
                     WHERE SCHEMA_NAME = '" . PMA_sqlAddSlashes($db) . "'"
                 );
             } else {
-                $result = PMA_DBI_fetch_result('SELECT `DEFAULT_CHARACTER_SET_NAME`, `DEFAULT_COLLATION_NAME` FROM `information_schema`.`SCHEMATA` WHERE `SCHEMA_NAME` = \''.PMA_sqlAddSlashes($db).'\' LIMIT 1');
+                $result = PMA_DBI_fetch_result(
+                    'SELECT `DEFAULT_CHARACTER_SET_NAME`, `DEFAULT_COLLATION_NAME`'
+                    . ' FROM `information_schema`.`SCHEMATA` WHERE `SCHEMA_NAME`'
+                    . ' = \''.PMA_sqlAddSlashes($db).'\' LIMIT 1'
+                );
             }
             $db_collation = $result[0]['DEFAULT_COLLATION_NAME'];
             $db_charset = $result[0]['DEFAULT_CHARACTER_SET_NAME'];
@@ -159,7 +177,9 @@ if (isset($plugin_list)) {
             $head .= '    - Structure schemas' . $crlf;
             $head .= '    -->' . $crlf;
             $head .= '    <pma:structure_schemas>' . $crlf;
-            $head .= '        <pma:database name="' . htmlspecialchars($db) . '" collation="' . $db_collation . '" charset="' . $db_charset . '">' . $crlf;
+            $head .= '        <pma:database name="' . htmlspecialchars($db)
+                . '" collation="' . $db_collation . '" charset="' . $db_charset
+                . '">' . $crlf;
 
             if (count($tables) == 0) {
                 $tables[] = $table;
@@ -167,7 +187,11 @@ if (isset($plugin_list)) {
 
             foreach ($tables as $table) {
                 // Export tables and views
-                $result = PMA_DBI_fetch_result('SHOW CREATE TABLE ' . PMA_backquote($db) . '.' . PMA_backquote($table), 0);
+                $result = PMA_DBI_fetch_result(
+                    'SHOW CREATE TABLE ' . PMA_backquote($db) . '.'
+                    . PMA_backquote($table),
+                    0
+                );
                 $tbl =  $result[$table][1];
 
                 $is_view = PMA_Table::isView($db, $table);
@@ -186,7 +210,8 @@ if (isset($plugin_list)) {
                     continue;
                 }
 
-                $head .= '            <pma:' . $type . ' name="' . $table . '">' . $crlf;
+                $head .= '            <pma:' . $type . ' name="' . $table . '">'
+                    . $crlf;
 
                 $tbl = "                " . htmlspecialchars($tbl);
                 $tbl = str_replace("\n", "\n                ", $tbl);
@@ -194,13 +219,15 @@ if (isset($plugin_list)) {
                 $head .= $tbl . ';' . $crlf;
                 $head .= '            </pma:' . $type . '>' . $crlf;
 
-                if (isset($GLOBALS['xml_export_triggers']) && $GLOBALS['xml_export_triggers']) {
+                if (isset($GLOBALS['xml_export_triggers'])
+                    && $GLOBALS['xml_export_triggers']) {
                     // Export triggers
                     $triggers = PMA_DBI_get_triggers($db, $table);
                     if ($triggers) {
                         foreach ($triggers as $trigger) {
                             $code = $trigger['create'];
-                            $head .= '            <pma:trigger name="' . $trigger['name'] . '">' . $crlf;
+                            $head .= '            <pma:trigger name="'
+                                . $trigger['name'] . '">' . $crlf;
 
                             // Do some formatting
                             $code = substr(rtrim($code), 0, -3);
@@ -217,12 +244,15 @@ if (isset($plugin_list)) {
                 }
             }
 
-            if (isset($GLOBALS['xml_export_functions']) && $GLOBALS['xml_export_functions']) {
+            if (isset($GLOBALS['xml_export_functions'])
+                && $GLOBALS['xml_export_functions']
+            ) {
                 // Export functions
                 $functions = PMA_DBI_get_procedures_or_functions($db, 'FUNCTION');
                 if ($functions) {
                     foreach ($functions as $function) {
-                        $head .= '            <pma:function name="' . $function . '">' . $crlf;
+                        $head .= '            <pma:function name="'
+                            . $function . '">' . $crlf;
 
                         // Do some formatting
                         $sql = PMA_DBI_get_definition($db, 'FUNCTION', $function);
@@ -240,12 +270,15 @@ if (isset($plugin_list)) {
                 }
             }
 
-            if (isset($GLOBALS['xml_export_procedures']) && $GLOBALS['xml_export_procedures']) {
+            if (isset($GLOBALS['xml_export_procedures'])
+                && $GLOBALS['xml_export_procedures']
+            ) {
                 // Export procedures
                 $procedures = PMA_DBI_get_procedures_or_functions($db, 'PROCEDURE');
                 if ($procedures) {
                     foreach ($procedures as $procedure) {
-                        $head .= '            <pma:procedure name="' . $procedure . '">' . $crlf;
+                        $head .= '            <pma:procedure name="'
+                            . $procedure . '">' . $crlf;
 
                         // Do some formatting
                         $sql = PMA_DBI_get_definition($db, 'PROCEDURE', $procedure);
@@ -289,7 +322,9 @@ if (isset($plugin_list)) {
     {
         global $crlf;
 
-        if (isset($GLOBALS['xml_export_contents']) && $GLOBALS['xml_export_contents']) {
+        if (isset($GLOBALS['xml_export_contents'])
+            && $GLOBALS['xml_export_contents']
+        ) {
             $head = '    <!--' . $crlf
                   . '    - ' . __('Database') . ': ' .  '\'' . $db . '\'' . $crlf
                   . '    -->' . $crlf
@@ -314,7 +349,9 @@ if (isset($plugin_list)) {
     {
         global $crlf;
 
-        if (isset($GLOBALS['xml_export_contents']) && $GLOBALS['xml_export_contents']) {
+        if (isset($GLOBALS['xml_export_contents'])
+            && $GLOBALS['xml_export_contents']
+        ) {
             return PMA_exportOutputHandler('    </database>' . $crlf);
         } else {
             return true;
@@ -351,8 +388,10 @@ if (isset($plugin_list)) {
     function PMA_exportData($db, $table, $crlf, $error_url, $sql_query)
     {
 
-        if (isset($GLOBALS['xml_export_contents']) && $GLOBALS['xml_export_contents']) {
-            $result      = PMA_DBI_query($sql_query, null, PMA_DBI_QUERY_UNBUFFERED);
+        if (isset($GLOBALS['xml_export_contents'])
+            && $GLOBALS['xml_export_contents']
+        ) {
+            $result = PMA_DBI_query($sql_query, null, PMA_DBI_QUERY_UNBUFFERED);
 
             $columns_cnt = PMA_DBI_num_fields($result);
             $columns = array();
@@ -367,14 +406,18 @@ if (isset($plugin_list)) {
             }
 
             while ($record = PMA_DBI_fetch_row($result)) {
-                $buffer      = '        <table name="' . htmlspecialchars($table) . '">' . $crlf;
+                $buffer = '        <table name="'
+                    . htmlspecialchars($table) . '">' . $crlf;
                 for ($i = 0; $i < $columns_cnt; $i++) {
-                    // If a cell is NULL, still export it to preserve the XML structure
+                    // If a cell is NULL, still export it to preserve
+                    // the XML structure
                     if (! isset($record[$i]) || is_null($record[$i])) {
                         $record[$i] = 'NULL';
                     }
-                    $buffer .= '            <column name="' . htmlspecialchars($columns[$i]) . '">' . htmlspecialchars((string)$record[$i])
-                            .  '</column>' . $crlf;
+                    $buffer .= '            <column name="'
+                        . htmlspecialchars($columns[$i]) . '">'
+                        . htmlspecialchars((string)$record[$i])
+                        .  '</column>' . $crlf;
                 }
                 $buffer     .= '        </table>' . $crlf;
 
