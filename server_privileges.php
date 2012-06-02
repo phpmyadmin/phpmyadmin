9605deb78e54e20ceacbf47f3967f28907189615
Rouslan Placella <rouslan@placella.com>
2012-06-02 18:06:11 +0100

    Fixed missing globals

--- server_privileges.php
+++ server_privileges.php
@@ -28,13 +28,15 @@ $get_params = array(
     'dbname',
     'hostname',
     'initial',
+    'old_username',
+    'old_hostname',
     'tablename',
     'username',
     'viewing_mode'
 );
 foreach ($get_params as $one_get_param) {
-    if (isset($_GET[$one_get_param])) {
-        $GLOBALS[$one_get_param] = $_GET[$one_get_param];
+    if (isset($_REQUEST[$one_get_param])) {
+        $GLOBALS[$one_get_param] = $_REQUEST[$one_get_param];
     }
 }
 
@@ -1425,7 +1427,7 @@ if (isset($_REQUEST['change_pw'])) {
     // similar logic in user_password.php
     $message = '';
 
-    if ($nopass == 0 && isset($pma_pw) && isset($pma_pw2)) {
+    if (empty($_REQUEST['nopass']) && isset($pma_pw) && isset($pma_pw2)) {
         if ($pma_pw != $pma_pw2) {
             $message = PMA_Message::error(__('The passwords aren\'t the same!'));
         } elseif (empty($pma_pw) || empty($pma_pw2)) {
