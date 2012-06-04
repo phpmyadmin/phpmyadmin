0df919c0f60b49dfc1f804566b49d493a9708ea5
Madhura Jayaratne <madhura.cj@gmail.com>
2012-06-04 21:25:58 +0530

    Fix indentation

--- libraries/import/upload/noplugin.php
+++ libraries/import/upload/noplugin.php
@@ -29,12 +29,12 @@ function PMA_getUploadStatus($id)
     }
     if (! array_key_exists($id, $_SESSION[$SESSION_KEY])) {
         $_SESSION[$SESSION_KEY][$id] = array(
-                    'id'       => $id,
-                    'finished' => false,
-                    'percent'  => 0,
-                    'total'    => 0,
-                    'complete' => 0,
-                    'plugin'   => $ID_KEY
+            'id'       => $id,
+            'finished' => false,
+            'percent'  => 0,
+            'total'    => 0,
+            'complete' => 0,
+            'plugin'   => $ID_KEY
         );
     }
     $ret = $_SESSION[$SESSION_KEY][$id];
