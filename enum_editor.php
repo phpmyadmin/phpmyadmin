ddd1eac48069ebd0d66acaea802cdcb3ee0fdeac
Rouslan Placella <rouslan@placella.com>
2012-06-11 14:00:18 +0100

    Fixed bug #3534311 - Grid editing incorrectly parses ENUM/SET values

--- enum_editor.php
+++ enum_editor.php
@@ -43,39 +43,9 @@ require_once './libraries/header_meta_style.inc.php';
                     foreach ($values as $key => $value) {
                         $values[$key] = htmlentities($value);
                     }
-                    // If the values are in a string
                 } elseif (isset($_GET['values']) && is_string($_GET['values'])) {
-                    // then this page was called via a link from some external page
-                    $values_string = htmlentities($_GET['values']);
-                    // There is a JS port of the below parser in functions.js
-                    // If you are fixing something here,
-                    // you need to also update the JS port.
-                    $values = array();
-                    $in_string = false;
-                    $buffer = '';
-                    for ($i=0; $i<strlen($values_string); $i++) {
-                        $curr = $values_string[$i];
-                        $next = $i == strlen($values_string)-1 ? '' : $values_string[$i+1];
-                        if (! $in_string && $curr == "'") {
-                            $in_string = true;
-                        } else if ($in_string && $curr == "\\" && $next == "\\") {
-                            $buffer .= "&#92;";
-                            $i++;
-                        } else if ($in_string && $next == "'" && ($curr == "'" || $curr == "\\")) {
-                            $buffer .= "&#39;";
-                            $i++;
-                        } else if ($in_string && $curr == "'") {
-                            $in_string = false;
-                            $values[] = $buffer;
-                            $buffer = '';
-                        } else if ($in_string) {
-                             $buffer .= $curr;
-                        }
-                    }
-                    if (strlen($buffer) > 0) {
-                        // The leftovers in the buffer are the last value (if any)
-                        $values[] = $buffer;
-                    }
+                    // Parse the values from a string
+                    $values = PMA_parseEnumSetValues($_GET['values']);
                 }
                 // Escape double quotes
                 foreach ($values as $key => $value) {
