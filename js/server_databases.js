11a5cd8194cbedd111b6f435fc579ef6b874d094
Rouslan Placella <rouslan@placella.com>
2012-06-15 19:53:45 +0100

    Validate database name on client side when creating one

--- js/server_databases.js
+++ js/server_databases.js
@@ -85,6 +85,13 @@ $(function() {
 
         $form = $(this);
 
+        var newDbNameInput = $form.find('input[name=new_db]');
+        if (newDbNameInput.val() === '') {
+            newDbNameInput.focus();
+            alert(PMA_messages['strFormEmpty']);
+            return;
+        }
+
         PMA_ajaxShowMessage(PMA_messages['strProcessingRequest']);
         PMA_prepareForAjaxRequest($form);
 
