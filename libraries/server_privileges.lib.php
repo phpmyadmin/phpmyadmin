e3941295bf7a5c0774a9fe935e0555db17d1f167
Thilina Buddika <thilinaabeyrathna@gmail.com>
2012-07-03 17:21:31 +0530

    function implementations for user form for privilees

--- libraries/server_privileges.lib.php
+++ libraries/server_privileges.lib.php
@@ -1262,4 +1262,244 @@ function PMA_getHtmlForAddUser($random_n, $dbname)
     
     return $html_output;
 }
+
+/**
+ * Get the list of privileges and list of compared privileges as strings
+ * and return a array that contains both strings
+ * 
+ * @return array  $list_of_privileges, $list_of_compared_privileges
+ */
+function PMA_getListOfPrivilegesAndComparedPrivileges()
+{
+    $list_of_privileges
+        = '`User`, '
+        . '`Host`, '
+        . '`Select_priv`, '
+        . '`Insert_priv`, '
+        . '`Update_priv`, '
+        . '`Delete_priv`, '
+        . '`Create_priv`, '
+        . '`Drop_priv`, '
+        . '`Grant_priv`, '
+        . '`Index_priv`, '
+        . '`Alter_priv`, '
+        . '`References_priv`, '
+        . '`Create_tmp_table_priv`, '
+        . '`Lock_tables_priv`, '
+        . '`Create_view_priv`, '
+        . '`Show_view_priv`, '
+        . '`Create_routine_priv`, '
+        . '`Alter_routine_priv`, '
+        . '`Execute_priv`';
+
+    $list_of_compared_privileges
+        = '`Select_priv` = \'N\''
+        . ' AND `Insert_priv` = \'N\''
+        . ' AND `Update_priv` = \'N\''
+        . ' AND `Delete_priv` = \'N\''
+        . ' AND `Create_priv` = \'N\''
+        . ' AND `Drop_priv` = \'N\''
+        . ' AND `Grant_priv` = \'N\''
+        . ' AND `References_priv` = \'N\''
+        . ' AND `Create_tmp_table_priv` = \'N\''
+        . ' AND `Lock_tables_priv` = \'N\''
+        . ' AND `Create_view_priv` = \'N\''
+        . ' AND `Show_view_priv` = \'N\''
+        . ' AND `Create_routine_priv` = \'N\''
+        . ' AND `Alter_routine_priv` = \'N\''
+        . ' AND `Execute_priv` = \'N\'';
+
+    if (PMA_MYSQL_INT_VERSION >= 50106) {
+        $list_of_privileges .=
+            ', `Event_priv`, '
+            . '`Trigger_priv`';
+        $list_of_compared_privileges .=
+            ' AND `Event_priv` = \'N\''
+            . ' AND `Trigger_priv` = \'N\'';
+    }
+    return array($list_of_privileges, $list_of_compared_privileges);
+}
+
+/**
+ * Get the HTML for user form and check the privileges for a particular database.
+ * 
+ * @param string $checkprivs        check privileges
+ * @param string $link_edit         standard link for edit
+ * @param string $conditional_class if ajaxable 'Ajax' otherwise ''
+ * 
+ * @return string $html_output
+ */
+function PMA_getUserForm($checkprivs, $link_edit, $conditional_class)
+{
+    $common_functions = PMA_CommonFunctions::getInstance();
+    // check the privileges for a particular database.
+    $html_output = '<form id="usersForm" action="server_privileges.php"><fieldset>' . "\n";
+    $html_output .= '<legend>' . "\n"
+        . $common_functions->getIcon('b_usrcheck.png')
+        . '    '
+        . sprintf(
+           __('Users having access to &quot;%s&quot;'),
+           '<a href="' . $GLOBALS['cfg']['DefaultTabDatabase'] . '?' . PMA_generate_common_url($checkprivs) . '">' .  htmlspecialchars($checkprivs) . '</a>'
+        )
+        . "\n"
+        . '</legend>' . "\n";
+    
+    $html_output .= '<table id="dbspecificuserrights" class="data">' . "\n"
+        . '<thead>' . "\n"
+        . '<tr><th>' . __('User') . '</th>' . "\n"
+        . '<th>' . __('Host') . '</th>' . "\n"
+        . '<th>' . __('Type') . '</th>' . "\n"
+        . '<th>' . __('Privileges') . '</th>' . "\n"
+        . '<th>' . __('Grant') . '</th>' . "\n"
+        . '<th>' . __('Action') . '</th>' . "\n"
+        . '</tr>' . "\n"
+        . '</thead>' . "\n";
+    $odd_row = true;
+    // now, we build the table...
+    list($list_of_privileges, $list_of_compared_privileges)
+        = PMA_getListOfPrivilegesAndComparedPrivileges();
+
+    $sql_query = '(SELECT ' . $list_of_privileges . ', `Db`'
+        .' FROM `mysql`.`db`'
+        .' WHERE \'' . $common_functions->sqlAddSlashes($checkprivs) . "'"
+        .' LIKE `Db`'
+        .' AND NOT (' . $list_of_compared_privileges. ')) '
+        .'UNION '
+        .'(SELECT ' . $list_of_privileges . ', \'*\' AS `Db`'
+        .' FROM `mysql`.`user` '
+        .' WHERE NOT (' . $list_of_compared_privileges . ')) '
+        .' ORDER BY `User` ASC,'
+        .'  `Host` ASC,'
+        .'  `Db` ASC;';
+    $res = PMA_DBI_query($sql_query);
+    $row = PMA_DBI_fetch_assoc($res);
+    if ($row) {
+        $found = true;
+    }
+    $html_output .= PMA_getUserFormTableBody(
+        $found, $row, $odd_row, $link_edit, $res
+    );
+    $html_output .= '</table>'
+        . '</fieldset>'
+        . '</form>' . "\n";
+
+    if ($GLOBALS['is_ajax_request'] == true) {
+        $message = PMA_Message::success(__('User has been added.'));
+        $response = PMA_Response::getInstance();
+        $response->addJSON('message', $message);
+        $response->addJSON('user_form', $html_output);
+        exit;
+    } else {
+        // Offer to create a new user for the current database
+        $html_output .= '<fieldset id="fieldset_add_user">' . "\n"
+           . '<legend>' . __('New') . '</legend>' . "\n";
+        
+        $html_output .= '<a href="server_privileges.php?'
+            . $GLOBALS['url_query'] . '&amp;adduser=1&amp;dbname=' . htmlspecialchars($checkprivs)
+            .'" rel="'.'checkprivs='.htmlspecialchars($checkprivs). '&amp;'.$GLOBALS['url_query']
+            . '" class="'.$conditional_class
+            .'" name="db_specific">' . "\n"
+            . $common_functions->getIcon('b_usradd.png')
+            . '        ' . __('Add user') . '</a>' . "\n";
+                
+        $html_output .= '</fieldset>' . "\n";
+    }
+    return $html_output;
+}
+
+/**
+ * Get HTML snippet for table body of user form
+ * 
+ * @param boolean $found        whether user found or not
+ * @param array $row            array of rows from mysql , db table with list of privileges  
+ * @param boolean $odd_row      whether odd or not
+ * @param string $link_edit     standard link for edit
+ * @param string $res           ran sql query
+ *            
+ * @return string $html_output
+ */
+function PMA_getUserFormTableBody($found, $row, $odd_row, $link_edit, $res)
+{
+    $html_output = '<tbody>' . "\n";
+    if ($found) {
+        while (true) {
+            // prepare the current user
+            $current_privileges = array();
+            $current_user = $row['User'];
+            $current_host = $row['Host'];
+            while ($row && $current_user == $row['User'] && $current_host == $row['Host']) {
+                $current_privileges[] = $row;
+                $row = PMA_DBI_fetch_assoc($res);
+            }
+            $html_output .= '<tr class="noclick ' . ($odd_row ? 'odd' : 'even') . '">' . "\n"
+                . '<td';
+            if (count($current_privileges) > 1) {
+                $html_output .= ' rowspan="' . count($current_privileges) . '"';
+            }
+            $html_output .= '>' 
+                . (empty($current_user)
+                    ? '<span style="color: #FF0000">' . __('Any') . '</span>'
+                    : htmlspecialchars($current_user)) . "\n"
+                . '</td>' . "\n";
+            
+            $html_output .= '<td';
+            if (count($current_privileges) > 1) {
+                $html_output .= ' rowspan="' . count($current_privileges) . '"';
+            }
+            $html_output .= '>'
+                . htmlspecialchars($current_host) . '</td>' . "\n";
+            for ($i = 0; $i < count($current_privileges); $i++) {
+                $current = $current_privileges[$i];
+                $html_output .= '<td>' . "\n"
+                   . '            ';
+                if (! isset($current['Db']) || $current['Db'] == '*') {
+                    $html_output .= __('global');
+                } elseif ($current['Db'] == PMA_CommonFunctions::getInstance()->escapeMysqlWildcards($checkprivs)) {
+                    $html_output .= __('database-specific');
+                } else {
+                    $html_output .= __('wildcard'). ': <code>' . htmlspecialchars($current['Db']) . '</code>';
+                }
+                $html_output .= "\n"
+                   . '</td>' . "\n";
+                
+                $html_output .='<td>' . "\n"
+                   . '<code>' . "\n"
+                   . '' . join(',' . "\n" . '                ', PMA_extractPrivInfo($current, true)) . "\n"
+                   . '</code>' . "\n"
+                   . '</td>' . "\n";
+                
+                $html_output .= '<td>' . "\n"
+                   . '' . ($current['Grant_priv'] == 'Y' ? __('Yes') : __('No')) . "\n"
+                   . '</td>' . "\n"
+                   . '<td>' . "\n";
+                $html_output .= sprintf(
+                    $link_edit,
+                    urlencode($current_user),
+                    urlencode($current_host),
+                    urlencode(! isset($current['Db']) || $current['Db'] == '*' ? '' : $current['Db']),
+                    ''
+                );
+                $html_output .= '</td>' . "\n"
+                   . '    </tr>' . "\n";
+                if (($i + 1) < count($current_privileges)) {
+                    $html_output .= '<tr class="noclick ' . ($odd_row ? 'odd' : 'even') . '">' . "\n";
+                }
+            }
+            if (empty($row)) {
+                break;
+            }
+            $odd_row = ! $odd_row;
+        }
+    } else {
+        $html_output .= '<tr class="odd">' . "\n"
+           . '<td colspan="6">' . "\n"
+           . '            ' . __('No user found.') . "\n"
+           . '</td>' . "\n"
+           . '</tr>' . "\n";
+    }
+    $html_output .= '</tbody>' . "\n";
+    
+    return $html_output;
+}
+
 ?>
