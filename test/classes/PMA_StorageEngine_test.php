201ec230f63ff39ee367b5dfa054c4c1b06a421f
Yasitha Pandithawatta <yasitha4@gmail.com>
2012-07-08 20:03:05 +0530

    Improve PMA_StorageEngine and fixed some failing test cases

--- test/classes/PMA_StorageEngine_test.php
+++ test/classes/PMA_StorageEngine_test.php
@@ -10,7 +10,8 @@
  */
 
 require_once 'libraries/StorageEngine.class.php';
-require_once 'libraries/dbi/mysql.dbi.lib.php';
+require_once 'libraries/php-gettext/gettext.inc';
+require_once 'libraries/CommonFunctions.class.php';
 
 class PMA_StorageEngine_test extends PHPUnit_Framework_TestCase
 {
@@ -110,4 +111,130 @@ class PMA_StorageEngine_test extends PHPUnit_Framework_TestCase
         );
     }
 
+    /**
+     * Test for getPage
+     */
+    public function testGetPage(){
+
+        $this->assertFalse(
+            $this->object->getPage(1)
+        );
+    }
+
+    /**
+     * Test for getInfoPages
+     */
+    public function testGetInfoPages(){
+
+        $this->assertEquals(
+            $this->object->getInfoPages(),
+            array()
+        );
+    }
+
+    /**
+     * Test for getVariablesLikePattern
+     */
+    public function testGetVariablesLikePattern(){
+
+        $this->assertFalse(
+            $this->object->getVariablesLikePattern()
+        );
+    }
+
+    /**
+     * Test for getMysqlHelpPage
+     */
+    public function testGetMysqlHelpPage(){
+
+        $this->assertEquals(
+            $this->object->getMysqlHelpPage(),
+            'dummy-storage-engine'
+        );
+    }
+
+    /**
+     * Test for getVariables
+     */
+    public function testGetVariables(){
+
+        $this->assertEquals(
+            $this->object->getVariables(),
+            array()
+        );
+    }
+
+    /**
+     * Test for getSupportInformationMessage
+     */
+    public function testGetSupportInformationMessage(){
+        $this->assertEquals(
+            $this->object->getSupportInformationMessage(),
+            'This MySQL server does not support the t storage engine.'
+        );
+
+        $this->object->support = 1;
+        $this->assertEquals(
+            $this->object->getSupportInformationMessage(),
+            't has been disabled for this MySQL server.'
+        );
+
+        $this->object->support = 2;
+        $this->assertEquals(
+            $this->object->getSupportInformationMessage(),
+            't is available on this MySQL server.'
+        );
+
+        $this->object->support = 3;
+        $this->assertEquals(
+            $this->object->getSupportInformationMessage(),
+            't is the default storage engine on this MySQL server.'
+        );
+    }
+
+    /**
+     * Test for getComment
+     */
+    public function testGetComment(){
+
+        $this->assertEquals(
+            $this->object->getComment(),
+            't'
+        );
+    }
+
+    /**
+     * Test for getTitle
+     */
+    public function testGetTitle(){
+
+        $this->assertEquals(
+            $this->object->getTitle(),
+            't'
+        );
+    }
+
+    /**
+     * Test for engine_init
+     */
+    public function testEngine_init(){
+
+        $this->assertNull(
+            $this->object->engine_init()
+        );
+    }
+
+    /**
+     * Test for resolveTypeSize
+     */
+    public function testResolveTypeSize(){
+
+        $this->assertEquals(
+            $this->object->resolveTypeSize(12),
+            array(
+                0 => 12,
+                1 => 'B'
+            )
+        );
+    }
 }
