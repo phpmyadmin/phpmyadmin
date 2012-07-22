9dece4fbef60c31bde2ab21aeb445e6d3c569b96
Atul Pratap Singh <atulpratapsingh05@gmail.com>
2012-07-22 18:37:30 +0530

    Add wrongly removed query execution part

--- db_qbe.php
+++ db_qbe.php
@@ -16,6 +16,31 @@ $response = PMA_Response::getInstance();
 // Gets the relation settings
 $cfgRelation = PMA_getRelationsParam();
 
+/**
+ * A query has been submitted -> (maybe) execute it
+ */
+$message_to_display = false;
+if (isset($_REQUEST['submit_sql']) && ! empty($sql_query)) {
+    if (! preg_match('@^SELECT@i', $sql_query)) {
+        $message_to_display = true;
+    } else {
+        $goto      = 'db_sql.php';
+        include 'sql.php';
+        exit;
+    }
+}
+
+$sub_part  = '_qbe';
+require 'libraries/db_common.inc.php';
+$url_query .= '&amp;goto=db_qbe.php';
+$url_params['goto'] = 'db_qbe.php';
+require 'libraries/db_info.inc.php';
+
+if ($message_to_display) {
+    PMA_Message::error(__('You have to choose at least one column to display'))->display();
+}
+unset($message_to_display);
+
 // create new qbe search instance
 $db_qbe = new PMA_DBQbe($GLOBALS['db']);
 
