55c27dd333943612bc0417c432e4a8608bb66d36
Alex Marin <alex.ukf@gmail.com>
2012-07-30 14:27:14 +0300

    oop: fix undefined method bug for import plug-ins

--- libraries/plugin_interface.lib.php
+++ libraries/plugin_interface.lib.php
@@ -199,13 +199,16 @@ function PMA_pluginGetChoice($section, $name, &$list, $cfgname = null)
         $plugin_name = strtolower(substr(get_class($plugin), strlen($section)));
         $ret .= '<input type="hidden" id="force_file_' . $plugin_name
             . '" value="';
-        if ($plugin->getProperties()->getForceFile() != null) {
+        if ( ! strcmp($section, 'Import') 
+            || $plugin->getProperties()->getForceFile() != null
+        ) {
             $ret .= 'true';
         } else {
             $ret .= 'false';
         }
         $ret .= '" />'. "\n";
     }
+    
     return $ret;
 }
 
