51482c02edebdf32d5a51ad2d7ecad43f0c79433
Thilina Buddika <thilinaabeyrathna@gmail.com>
2012-08-06 22:01:58 +0530

    lib file for structure section

---
+++ libraries/structure.lib.php
@@ -0,0 +1,15 @@
+<?php
+
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * set of functions for structure section in pma
+ *
+ * @package PhpMyAdmin
+ */
+
+if (! defined('PHPMYADMIN')) {
+    exit;
+}
+
+
+?>
