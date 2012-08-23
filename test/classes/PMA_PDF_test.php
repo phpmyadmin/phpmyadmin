6a02f774659140eb71d7db4631c122102ecfeaf0
Michal Čihař <mcihar@suse.cz>
2012-08-23 10:48:34 +0200

    Add more complext test for PMA_PDF class

--- test/classes/PMA_PDF_test.php
+++ test/classes/PMA_PDF_test.php
@@ -40,5 +40,26 @@ class PMA_PDF_test extends PHPUnit_Framework_TestCase
         $arr->SetAlias('{00}', '32');
         $this->assertContains('PDF', $arr->getPDFData());
     }
+
+    /**
+     * @group large
+     */
+    public function testDocument()
+    {
+        $pdf = new PMA_PDF();
+        $pdf->SetTitle('Title');
+        $pdf->Open();
+        $pdf->SetAutoPageBreak('auto');
+        $pdf->Addpage();
+        $pdf->SetFont(PMA_PDF_FONT, 'B', 14);
+        $pdf->Cell(0, 6, 'Cell', 'B', 1, 'C');
+        $pdf->Ln();
+        $pdf->Addpage();
+        $pdf->Bookmark('Bookmark');
+        $pdf->SetMargins(0, 0);
+        $pdf->SetDrawColor(200, 200, 200);
+        $pdf->line(0, 0, 100, 100);
+        $this->assertContains('PDF', $pdf->getPDFData());
+    }
 }
 ?>
