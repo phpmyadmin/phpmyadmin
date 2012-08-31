286c8a7dbb52e4be7f09aa0624c8d1ebd0e4118c
Marc Delisle <marc@infomarc.info>
2012-08-31 14:18:02 -0400

    Initial porting to jqplot

--- js/tbl_chart.js
+++ js/tbl_chart.js
@@ -1,3 +1,5 @@
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+
 var chart_xaxis_idx = -1;
 var chart_series;
 var chart_series_index = -1;
@@ -9,6 +11,7 @@ $(document).ready(function() {
     chart_series = 'columns';
     chart_xaxis_idx = $('select[name="chartXAxis"]').attr('value');
 
+    // from jQuery UI
     $('#resizer').resizable({
         minHeight:240,
         minWidth:300,
@@ -17,53 +20,67 @@ $(document).ready(function() {
         // content, the redrawing might be slow. In that case, we recommend
         // that you use the 'stop' event instead of 'resize'.
         resize: function() {
-            currentChart.setSize(
-                this.offsetWidth - 20,
-                this.offsetHeight - 20,
-                false
-            );
+            //currentChart.setSize(
+            //    this.offsetWidth - 20,
+            //    this.offsetHeight - 20,
+            //    false
+            //);
         }
     });
 
-    var currentSettings = {
+    var nonJqplotSettings = {
         chart: {
             type: 'line',
             width: $('#resizer').width() - 20,
             height: $('#resizer').height() - 20
-        },
-        xAxis: {
-            title: { text: $('input[name="xaxis_label"]').attr('value') }
-        },
-        yAxis: {
-            title: { text: $('input[name="yaxis_label"]').attr('value') }
+        }
+    }
+
+    var currentSettings = {
+        //xAxis: {
+        //    title: { text: $('input[name="xaxis_label"]').attr('value') }
+        //},
+        //yAxis: {
+        //    title: { text: $('input[name="yaxis_label"]').attr('value') }
+        //},
+        axes: {
+            xaxis: {
+                label: $('input[name="xaxis_label"]').val(),
+                labelRenderer: $.jqplot.CanvasAxisLabelRenderer
+            },
+            yaxis: {
+                label: $('input[name="yaxis_label"]').val(),
+                labelRenderer: $.jqplot.CanvasAxisLabelRenderer
+            },
         },
         title: {
-            text: $('input[name="chartTitle"]').attr('value'),
-            margin:20
+            text: $('input[name="chartTitle"]').attr('value')
+            //margin:20
         },
-        plotOptions: {
-            series: {}
-        }
+        //plotOptions: {
+        //    series: {}
+        //}
     }
 
     $('#querychart').html('');
 
     $('input[name="chartType"]').click(function() {
-        currentSettings.chart.type = $(this).attr('value');
+        nonJqplotSettings.chart.type = $(this).attr('value');
 
         drawChart();
 
-        if($(this).attr('value') == 'bar' || $(this).attr('value') == 'column')
+        if ($(this).attr('value') == 'bar' || $(this).attr('value') == 'column')
             $('span.barStacked').show();
         else
             $('span.barStacked').hide();
     });
 
     $('input[name="barStacked"]').click(function() {
-        if(this.checked)
-            $.extend(true,currentSettings,{ plotOptions: { series: { stacking:'normal' } } });
-        else
-            $.extend(true,currentSettings,{ plotOptions: { series: { stacking:null } } });
+        if(this.checked) {
+            //$.extend(true,currentSettings,{ plotOptions: { series: { stacking:'normal' } } });
+        } else {
+            //$.extend(true,currentSettings,{ plotOptions: { series: { stacking:null } } });
+        }
         drawChart();
     });
 
@@ -72,8 +89,8 @@ $(document).ready(function() {
     });
     $('input[name="chartTitle"]').keyup(function() {
         var title = $(this).attr('value');
-        if(title.length == 0) title = ' ';
-        currentChart.setTitle({ text: title });
+        if (title.length == 0) title = ' ';
+        //currentChart.setTitle({ text: title });
     });
     $('input[name="chartTitle"]').blur(function() {
         if ($(this).val() != temp_chart_title) {
@@ -93,23 +110,25 @@ $(document).ready(function() {
 
     /* Sucks, we cannot just set axis labels, we have to redraw the chart completely */
     $('input[name="xaxis_label"]').keyup(function() {
-        currentSettings.xAxis.title.text = $(this).attr('value');
+        //currentSettings.xAxis.title.text = $(this).attr('value');
+        currentSettings.axes.xaxis.label = $(this).attr('value');
         drawChart(true);
     });
     $('input[name="yaxis_label"]').keyup(function() {
-        currentSettings.yAxis.title.text = $(this).attr('value');
+        //currentSettings.yAxis.title.text = $(this).attr('value');
+        currentSettings.axes.yaxis.label = $(this).attr('value');
         drawChart(true);
     });
 
     function drawChart(noAnimation) {
-        currentSettings.chart.width = $('#resizer').width() - 20;
-        currentSettings.chart.height = $('#resizer').height() - 20;
+        nonJqplotSettings.chart.width = $('#resizer').width() - 20;
+        nonJqplotSettings.chart.height = $('#resizer').height() - 20;
 
         if(currentChart != null) currentChart.destroy();
 
-        if(noAnimation) currentSettings.plotOptions.series.animation = false;
-        currentChart = PMA_queryChart(chart_data,currentSettings);
-        if(noAnimation) currentSettings.plotOptions.series.animation = true;
+        //if(noAnimation) currentSettings.plotOptions.series.animation = false;
+        currentChart = PMA_queryChart(chart_data, currentSettings, nonJqplotSettings);
+        //if(noAnimation) currentSettings.plotOptions.series.animation = true;
     }
 
     drawChart();
@@ -123,9 +142,9 @@ function in_array(element,array)
     return false;
 }
 
-function PMA_queryChart(data,passedSettings)
+function PMA_queryChart(data, passedSettings, passedNonJqplotSettings)
 {
-    if($('#querychart').length == 0) return;
+    if ($('#querychart').length == 0) return;
 
     var columnNames = Array();
 
@@ -137,7 +156,7 @@ function PMA_queryChart(data,passedSettings)
         columnNames.push(index);
     });
 
-    switch(passedSettings.chart.type) {
+    switch(passedNonJqplotSettings.chart.type) {
         case 'column':
         case 'spline':
         case 'line':
@@ -150,10 +169,14 @@ function PMA_queryChart(data,passedSettings)
                     if(i != chart_xaxis_idx) {
                         series[j] = new Object();
                         series[j].data = new Array();
+                        // for jqplot:
+                        series[j].data[0] = new Array();
                         series[j].name = columnNames[i];
 
                         $.each(data,function(key,value) {
-                            series[j].data.push(parseFloat(value[columnNames[i]]));
+                            //series[j].data.push(parseFloat(value[columnNames[i]]));
+                            // jqplot needs an extra level of array here
+                            series[j].data[0].push(parseFloat(value[columnNames[i]]));
                             if( j== 0 && chart_xaxis_idx != -1 && ! xaxis.categories[value[columnNames[chart_xaxis_idx]]])
                                 xaxis.categories.push(value[columnNames[chart_xaxis_idx]]);
                         });
@@ -207,45 +230,50 @@ function PMA_queryChart(data,passedSettings)
     }
 
     // Prevent the user from seeing the JSON code
-    $('div#profilingchart').html('').show();
+    //$('div#profilingchart').html('').show();
 
     var settings = {
-        chart: {
-            renderTo: 'querychart'
-        },
+        //chart: {
+        //    renderTo: 'querychart'
+        //},
+        //title: {
+        //    text: '',
+        //    margin: 0
+        //},
         title: {
-            text: '',
-            margin: 0
+            text: '' 
+            //margin:20
         },
         series: series,
-        xAxis: xaxis,
-        yAxis: yaxis,
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    distance: 35,
-                    formatter: function() {
-                        return '<b>'+ this.point.name +'</b><br/>'+ Highcharts.numberFormat(this.percentage, 2) +' %';
-                   }
-                }
-            }
-        },
-        tooltip: {
-            formatter: function() {
-                if(this.point.name) return '<b>'+this.series.name+'</b><br/>'+this.point.name+'<br/>'+this.y;
-                return '<b>'+this.series.name+'</b><br/>'+this.y;
-            }
-        }
+        //xAxis: xaxis,
+        //yAxis: yaxis,
+        //plotOptions: {
+        //    pie: {
+        //        allowPointSelect: true,
+        //        cursor: 'pointer',
+        //        dataLabels: {
+        //            enabled: true,
+        //            distance: 35,
+        //            formatter: function() {
+                        //return '<b>'+ this.point.name +'</b><br/>'+ Highcharts.numberFormat(this.percentage, 2) +' %';
+        //           }
+        //        }
+        //    }
+        //},
+        //tooltip: {
+        //    formatter: function() {
+        //        if(this.point.name) return '<b>'+this.series.name+'</b><br/>'+this.point.name+'<br/>'+this.y;
+        //        return '<b>'+this.series.name+'</b><br/>'+this.y;
+        //    }
+        //}
     };
 
-    if(passedSettings.chart.type == 'pie')
-        settings.tooltip.formatter = function() { return '<b>'+columnNames[0]+'</b><br/>'+this.y; }
-
+    if (passedNonJqplotSettings.chart.type == 'pie') {
+        //settings.tooltip.formatter = function() { return '<b>'+columnNames[0]+'</b><br/>'+this.y; }
+    }
     // Overwrite/Merge default settings with passedsettings
-    $.extend(true,settings,passedSettings);
+    $.extend(true, settings, passedSettings);
 
-    return PMA_createChart(settings);
+    //return PMA_createChart(settings);
+    $.jqplot('querychart', series[0].data, settings);
 }
