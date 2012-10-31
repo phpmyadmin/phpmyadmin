1c147142c77922d675a6ed5f2d56cc5bcef6f6b8
Rouslan Placella <rouslan@placella.com>
2012-10-31 09:56:59 +0000

    Fixed "Theme preview" test case

--- test/classes/PMA_Theme_test.php
+++ test/classes/PMA_Theme_test.php
@@ -245,7 +245,7 @@ class PMA_ThemeTest extends PHPUnit_Framework_TestCase
     {
         $this->assertEquals(
             $this->object->getPrintPreview(),
-            '<div class="theme_preview"><h2> (0.0.0.0) </h2><p><a target="_top" class="take_theme" name="" href="index.php?set_theme=&amp;server=99&amp;lang=en&amp;token=token">No preview available.[ <strong>take it</strong> ]</a></p></div>'
+            '<div class="theme_preview"><h2> (0.0.0.0) </h2><p><a class="take_theme" name="" href="index.php?set_theme=&amp;server=99&amp;lang=en&amp;token=token">No preview available.[ <strong>take it</strong> ]</a></p></div>'
         );
     }
 
