041eab50f315a753e8908019a28cbadd0b2d1ac9
Michal Čihař <mcihar@suse.cz>
2012-11-09 10:01:18 +0100

    Improve rendering of configuration

--- conf.py
+++ conf.py
@@ -17,6 +17,7 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "_ext")))
 
 # -- General configuration -----------------------------------------------------
 
@@ -25,7 +26,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['configext']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
