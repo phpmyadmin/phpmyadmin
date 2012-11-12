b07a6c4ee2e3d622d77fd0faf82d75bbe9e05a38
Michal Čihař <mcihar@suse.cz>
2012-11-12 09:18:59 +0100

    Build doc directly in doc folder

--- doc/conf.py
+++ doc/conf.py
@@ -65,7 +65,7 @@ release = '4.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'html', 'doctrees']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
