bad1163dd4f26cefb63cc252b6c58db919590a5a
Marc Delisle <marc@infomarc.info>
2013-01-20 13:11:07 -0500

    3.5.6-rc1

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -98,7 +98,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '3.5.6-dev');
+        $this->set('PMA_VERSION', '3.5.6-rc1');
         /**
          * @deprecated
          */
