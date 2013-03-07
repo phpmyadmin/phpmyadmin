ec7dec415423b57b306effd23b3542b7c184962b
Samith Dassanayake <hisamith@gmail.com>
2013-03-07 17:32:29 +0530

    [Contest-4] Automatically create index when creating foreign key. Add option to select index type(unique or index) and length for non-indexed fields. It offers all the fields and if the fields are non-indexed, user should select the index type. User can create indexes in the "Relational View" for the fields which are is non-indexed.

--- js/tbl_relation.js
+++ js/tbl_relation.js
@@ -15,11 +15,30 @@ function show_hide_clauses($thisDropdown)
     }
 }
 
+function show_hide_foreign_key($thisDropdown)
+{
+    
+    if ($thisDropdown.val() != '') {
+        // show length field
+        $thisDropdown.parent().next('span').show();
+        // hide "No index defined!" span in "Foreign key constraint" column"
+        $thisDropdown.parent().parent().next('td').find('span[class^="no_index"]').hide();
+        $thisDropdown.parent().parent().next('td').find('select[class^="referenced_column_dropdown"]').show();
+    } else { 
+        $thisDropdown.parent().next('span').find('input[name^="index_length"]').val('');
+        $thisDropdown.parent().next('span').hide();
+        $thisDropdown.parent().parent().next('td').find('span[class^="no_index"]').show();
+        $thisDropdown.parent().parent().next('td').find('select[class^="referenced_column_dropdown"]').hide();
+        $thisDropdown.parent().parent().next('td').find('select[class^="referenced_column_dropdown"]').val('').change();
+    }
+}
+
 /**
  * Unbind all event handlers before tearing down a page
  */
 AJAX.registerTeardown('tbl_relation.js', function() {
     $('select.referenced_column_dropdown').unbind('change');
+    $('select.index_column_dropdown').unbind('change');
 });
 
 AJAX.registerOnload('tbl_relation.js', function() {
@@ -30,5 +49,13 @@ AJAX.registerOnload('tbl_relation.js', function() {
     // change
     $('select.referenced_column_dropdown').change(function() {
         show_hide_clauses($(this));
+    });
+    
+    $('select.index_column_dropdown').each(function(index, one_dropdown) {
+        show_hide_foreign_key($(one_dropdown));
+    });
+    // change
+    $('select.index_column_dropdown').change(function() {
+        show_hide_foreign_key($(this));
     });
 });
