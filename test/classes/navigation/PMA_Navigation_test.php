51c556340c5a4ac87f4de70a7618135bdf27073d
adamgsoc2013 <adamgsoc2013@gmail.com>
2013-04-13 02:06:14 +0800

    add Class PMA_Navigation_test to cover navigation test

---
+++ test/classes/navigation/PMA_Navigation_test.php
@@ -0,0 +1,45 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * tests for PMA_Navigation class
+ *
+ * @package PhpMyAdmin-test
+ */
+
+require_once 'libraries/navigation/Navigation.class.php';
+require_once 'libraries/Util.class.php';
+require_once 'libraries/Theme.class.php';
+
+
+class PMA_Navigation_test extends PHPUnit_Framework_TestCase
+{
+    public function setup()
+    {
+        $GLOBALS['server'] = 0;
+        $GLOBALS['token'] = 'token';
+    }
+
+    /**
+     * Test for GetDisplay
+     *
+     * @return void
+     */
+    public function testGetDisplay()
+    {
+        $navigation = new PMA_Navigation();
+        $html_navigation = $navigation->getDisplay();
+        $this->assertContains(
+            "pma_navigation",
+            $html_navigation
+        );
+        $this->assertContains(
+            "pma_navigation_resizer",
+            $html_navigation
+        );
+        $this->assertContains(
+            "pma_navigation_collapser",
+            $html_navigation
+        );
+    }
+}
+?>
