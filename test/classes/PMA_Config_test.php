d13fbbbc414d0cbaf4646d6f547e21c6f3fe764d
adamgsoc2013 <adamgsoc2013@gmail.com>
2013-04-16 13:51:31 +0800

    add test case for Config.class.php: 1 testIsGitRevision 2 testCheckHTTP
    per http://ci.phpmyadmin.net/job/phpMyAdmin/cloverphp/libraries_Config.class.php.html
    It can increase a lot of code coverage

--- test/classes/PMA_Config_test.php
+++ test/classes/PMA_Config_test.php
@@ -36,6 +36,7 @@ class PMA_ConfigTest extends PHPUnit_Framework_TestCase
     {
         $this->object = new PMA_Config;
         $GLOBALS['server'] = 0;
+        $_SESSION['is_git_revision'] = true;
     }
 
     /**
@@ -792,6 +793,37 @@ class PMA_ConfigTest extends PHPUnit_Framework_TestCase
             )
         );
 
+    }
+
+    /**
+     * Test for isGitRevision
+     *
+     * @return void
+     */
+    public function testIsGitRevision()
+    {
+      $this->assertTrue(
+            $this->object->isGitRevision()
+        );
+    }
+
+    /**
+     * Test for Check HTTP
+     *
+     * @return void
+     */
+    public function testCheckHTTP()
+    {
+      $this->assertTrue(
+            $this->object->checkHTTP("http://www.phpmyadmin.net/test/data")
+      );
+      $this->assertContains(
+            "TEST DATA",
+            $this->object->checkHTTP("http://www.phpmyadmin.net/test/data",true)
+      );
+      $this->assertFalse(
+            $this->object->checkHTTP("http://www.phpmyadmin.net/test/nothing")
+      );
     }
 
     /**
