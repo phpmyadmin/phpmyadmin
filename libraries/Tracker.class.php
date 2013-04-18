1cba93204b0ef72c7ce09525ac8d5076f3a273b3
Marc Delisle <marc@infomarc.info>
2013-04-18 05:53:58 -0400

    Prevent null-byte injection in preg_replace()

--- libraries/Tracker.class.php
+++ libraries/Tracker.class.php
@@ -941,6 +941,9 @@ class PMA_Tracker
         if (empty($dbname)) {
             return;
         }
+        // Remove null bytes (preg_replace() is vulnerable in some
+        // PHP versions)
+        $dbname = str_replace("\0", "", $dbname);
 
         // If we found a valid statement
         if (isset($result['identifier'])) {
