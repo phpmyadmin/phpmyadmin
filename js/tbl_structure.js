phpmyadmin/phpmyadmin

commit 14eb3c32ca1eec74bbb6afff7efd9682a559c83e

Add rfe #1422 [interface] Provide feedback if no columns selected for multi-submit

js/tbl_structure.js
@@ -80,26 +80,6 @@ AJAX.registerOnload('tbl_structure.js', function() {
     }); // end change table button "do_save_data"
 
     /**
-     * Attach Event Handler for 'Change Column'
-     */
-    $("a.change_column_anchor.ajax").live('click', function(event) {
-        event.preventDefault();
-        var $msg = PMA_ajaxShowMessage();
-        $('#page_content').hide();
-        $.get($(this).attr('href'), {'ajax_request': true}, function(data) {
-            PMA_ajaxRemoveMessage($msg);
-            if (data.success) {
-                $('<div id="change_column_dialog"></div>')
-                    .html(data.message)
-                    .insertBefore('#page_content');
-                PMA_verifyColumnsProperties();
-            } else {
-                PMA_ajaxShowMessage(PMA_messages['strErrorProcessingRequest'] + " : " + data.error, false);
-            }
-        });
-    });
-
-    /**
      * Attach Event Handler for 'Change multiple columns'
      */
     $("button.change_columns_anchor.ajax, input.change_columns_anchor.ajax").live('click', function(event) {
@@ -111,12 +91,16 @@ AJAX.registerOnload('tbl_structure.js', function() {
         $.post($form.prop("action"), params, function (data) {
             PMA_ajaxRemoveMessage($msg);
             if (data.success) {
-                $('<div id="change_column_dialog"></div>')
-                    .html(data.message)
-                    .insertBefore('#page_content');
-                PMA_verifyColumnsProperties();
+                $('#page_content')
+                    .empty()
+                    .append(
+                        $('<div id="change_column_dialog"></div>')
+                            .html(data.message)
+                    )
+                    .show();
             } else {
-                PMA_ajaxShowMessage(PMA_messages['strErrorProcessingRequest'] + " : " + data.error, false);
+                $('#page_content').show();
+                PMA_ajaxShowMessage(data.error);
             }
         });
     });

libraries/structure.lib.php
@@ -1311,7 +1311,7 @@ function PMA_getHtmlForDropColumn($tbl_is_view, $db_is_information_schema,
 
     if (! $tbl_is_view && ! $db_is_information_schema) {
         $html_output .= '<td class="edit center">'
-            . '<a class="change_column_anchor ajax"'
+            . '<a class="change_column_anchor"'
             . ' href="tbl_structure.php?' 
             . $url_query . '&amp;field=' . $field_encoded 
             . '&amp;change_column=1">'

tbl_structure.php
@@ -87,37 +87,42 @@ if (isset($_REQUEST['submit_mult_change_x'])) {
         $_REQUEST['selected_fld'] = $_REQUEST['selected'];
     }
 }
+if (! empty($submit_mult)) {
+    if (isset($_REQUEST['selected_fld'])) {
+        $err_url = 'tbl_structure.php?' . PMA_generate_common_url($db, $table);
+        if ($submit_mult == 'browse') {
+            // browsing the table displaying only selected fields/columns
+            $GLOBALS['active_page'] = 'sql.php';
+            $sql_query = '';
+            foreach ($_REQUEST['selected_fld'] as $idx => $sval) {
+                if ($sql_query == '') {
+                    $sql_query .= 'SELECT ' . PMA_Util::backquote($sval);
+                } else {
+                    $sql_query .=  ', ' . PMA_Util::backquote($sval);
+                }
+            }
+            $sql_query .= ' FROM ' . PMA_Util::backquote($db)
+            . '.' . PMA_Util::backquote($table);
+            include 'sql.php';
+            exit;
+        } else {
+            // handle multiple field commands
+            // handle confirmation of deleting multiple fields/columns
+            $action = 'tbl_structure.php';
+            include 'libraries/mult_submits.inc.php';
+            /**
+             * if $submit_mult == 'change', execution will have stopped
+             * at this point
+             */
 
-if (! empty($submit_mult) && isset($_REQUEST['selected_fld'])) {
-    $err_url = 'tbl_structure.php?' . PMA_generate_common_url($db, $table);
-    if ($submit_mult == 'browse') {
-        // browsing the table displaying only selected fields/columns
-        $GLOBALS['active_page'] = 'sql.php';
-        $sql_query = '';
-        foreach ($_REQUEST['selected_fld'] as $idx => $sval) {
-            if ($sql_query == '') {
-                $sql_query .= 'SELECT ' . PMA_Util::backquote($sval);
-            } else {
-                $sql_query .=  ', ' . PMA_Util::backquote($sval);
+            if (empty($message)) {
+                $message = PMA_Message::success();
             }
         }
-        $sql_query .= ' FROM ' . PMA_Util::backquote($db)
-        . '.' . PMA_Util::backquote($table);
-        include 'sql.php';
-        exit;
     } else {
-        // handle multiple field commands
-        // handle confirmation of deleting multiple fields/columns
-        $action = 'tbl_structure.php';
-        include 'libraries/mult_submits.inc.php';
-        /**
-         * if $submit_mult == 'change', execution will have stopped
-         * at this point
-         */
-
-        if (empty($message)) {
-            $message = PMA_Message::success();
-        }
+        $response = PMA_Response::getInstance();
+        $response->isSuccess(false);
+        $response->addJSON('message', __('No column selected.'));
     }
 }
 

commit add86949b9349fbe18dd8dfefd16964af2d59f1a

Fix Ajax problems introduced with 14eb3c32, correct spacing for column editor

js/tbl_structure.js
@@ -35,7 +35,7 @@ AJAX.registerOnload('tbl_structure.js', function() {
     /**
      *Ajax action for submitting the "Column Change" and "Add Column" form
      */
-    $(".append_fields_form.ajax").bind('submit', function(event) {
+    $(".append_fields_form.ajax").live('submit', function(event) {
         event.preventDefault();
         /**
          * @var    the_form    object referring to the export form
@@ -53,24 +53,21 @@ AJAX.registerOnload('tbl_structure.js', function() {
             // OK, form passed validation step
             PMA_prepareForAjaxRequest($form);
             //User wants to submit the form
-            PMA_ajaxShowMessage();
+            $msg = PMA_ajaxShowMessage();
             $.post($form.attr('action'), $form.serialize() + '&do_save_data=1', function(data) {
                 if ($("#sqlqueryresults").length != 0) {
                     $("#sqlqueryresults").remove();
-                } else if ($(".error").length != 0) {
-                    $(".error").remove();
+                } else if ($(".error:not(.tab)").length != 0) {
+                    $(".error:not(.tab)").remove();
                 }
                 if (data.success == true) {
-                    $("<div id='sqlqueryresults'></div>").prependTo("#page_content");
-                    $("#sqlqueryresults").html(data.sql_query);
+                    $("#page_content")
+                        .empty()
+                        .append(data.message)
+                        .show();
                     $("#result_query .notice").remove();
-                    $("#result_query").prepend(data.message);
-                    /* Reload the field form */
-                    if ($("#fieldsForm").length) {
-                        reloadFieldForm(data.message);
-                    } else {
-                        PMA_ajaxShowMessage(data.message);
-                    }
+                    $form.remove();
+                    PMA_ajaxRemoveMessage($msg);
                     PMA_reloadNavigation();
                 } else {
                     PMA_ajaxShowMessage(data.error, false);
@@ -78,6 +75,25 @@ AJAX.registerOnload('tbl_structure.js', function() {
             }); // end $.post()
         }
     }); // end change table button "do_save_data"
+
+    /**
+     * Attach Event Handler for 'Change Column'
+     */
+    $("a.change_column_anchor.ajax").live('click', function(event) {
+        event.preventDefault();
+        var $msg = PMA_ajaxShowMessage();
+        $('#page_content').hide();
+        $.get($(this).attr('href'), {'ajax_request': true}, function (data) {
+            PMA_ajaxRemoveMessage($msg);
+            if (data.success) {
+                $('<div id="change_column_dialog" class="margin"></div>')
+                    .html(data.message)
+                    .insertBefore('#page_content');
+            } else {
+                PMA_ajaxShowMessage(PMA_messages['strErrorProcessingRequest'] + " : " + data.error, false);
+            }
+        });
+    });
 
     /**
      * Attach Event Handler for 'Change multiple columns'

libraries/structure.lib.php
@@ -1311,7 +1311,7 @@ function PMA_getHtmlForDropColumn($tbl_is_view, $db_is_information_schema,
 
     if (! $tbl_is_view && ! $db_is_information_schema) {
         $html_output .= '<td class="edit center">'
-            . '<a class="change_column_anchor"'
+            . '<a class="change_column_anchor ajax"'
             . ' href="tbl_structure.php?' 
             . $url_query . '&amp;field=' . $field_encoded 
             . '&amp;change_column=1">'

themes/original/css/common.css.php
@@ -1611,6 +1611,10 @@ input#input_import_file {
     margin: 0.1em;
 }
 
+.margin#change_column_dialog {
+    margin: 0 .5em;
+}
+
 /**
  * Indexes
  */

themes/pmahomme/css/common.css.php
@@ -2121,6 +2121,10 @@ table#index_columns select {
     border-radius: 2px;
 }
 
+.margin#change_column_dialog {
+    margin: 0 .5em;
+}
+
 /* config forms */
 .config-form ul.tabs {
     margin: 1.1em .2em 0;