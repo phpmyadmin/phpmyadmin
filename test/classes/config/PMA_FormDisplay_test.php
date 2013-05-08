6b416048a19b5dda5e84133366db334ca81138f3
adamgsoc2013 <adamgsoc2013@gmail.com>
2013-05-08 13:16:03 +0800

    add PMA_FormDisplay_test

---
+++ test/classes/config/PMA_FormDisplay_test.php
@@ -0,0 +1,23 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * tests for Form class in config folder
+ *
+ * @package PhpMyAdmin-test
+ */
+
+require_once 'libraries/config/ConfigFile.class.php';
+require_once 'libraries/config/Form.class.php';
+require_once 'libraries/config/FormDisplay.class.php';
+require_once 'libraries/Util.class.php';
+
+
+class PMA_FormDisplay_Test extends PHPUnit_Framework_TestCase
+{
+    public function testContructor()
+    { 
+        $form_display = new FormDisplay();
+        $this->assertFalse($form_display->hasErrors());
+    }
+}
+?>
