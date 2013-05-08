8858281c69cfc97893aa195754ff330d787ddebc
adamgsoc2013 <adamgsoc2013@gmail.com>
2013-05-08 12:58:44 +0800

    change form to an array

--- test/classes/config/PMA_FormDisplay_test.php
+++ test/classes/config/PMA_FormDisplay_test.php
@@ -20,7 +20,7 @@ class PMA_FormDisplay_Test extends PHPUnit_Framework_TestCase
         $form = new Form($form_name, array('pma_form1','pma_form2'), 1);
        
         $form_display = new FormDisplay();
-        $form_display->registerForm($form_name, $form);
+        $form_display->registerForm($form_name, array($form));
 
         $this->assertFalse($form_display->hasErrors());
     }
