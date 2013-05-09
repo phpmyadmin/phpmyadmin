e147de0ed38e15ed709451ba5023cc40c3270d3a
adamgsoc2013 <adamgsoc2013@gmail.com>
2013-05-10 00:00:05 +0800

    add PMA_Index_test

---
+++ test/classes/PMA_Index_test.php
@@ -0,0 +1,71 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Test for PMA_Index class
+ *
+ * @package PhpMyAdmin-test
+ */
+
+/*
+ * Include to test.
+ */
+require_once 'libraries/Index.class.php';
+
+/**
+ * Test for PMA_Index class
+ *
+ * @package PhpMyAdmin-test
+ */
+class PMA_Index_Test extends PHPUnit_Framework_TestCase
+{
+    protected $params;
+
+    /**
+     * Configures parameters.
+     *
+     * @return void
+     */
+    function setup()
+    {
+		$params = array();
+		$params['columns'] = "PMA_columns";
+		$params['Schema'] = "PMA_Schema";
+		$params['Table'] = "PMA_Table";
+		$params['Key_name'] = "PMA_Key_name";
+		$params['Index_type'] = "PMA_Index_type";
+		$params['Comment'] = "PMA_Comment";
+		$params['Index_comment'] = "PMA_Index_comment";
+		$params['Non_unique'] = "PMA_Non_unique";
+		$params['Packed'] = "PMA_Packed";
+    }
+
+    /**
+     * Test for Constructor
+     *
+     * @return void
+     */
+    public function testConstructor()
+    {
+        $index = new PMA_Index($params);
+        $this->assertEquals(
+            'PMA_Index_comment',
+            $index->getComment()
+        );
+        $this->assertEquals(
+            'PMA_Comment',
+            $index->getRemarks()
+        );
+        $this->assertEquals(
+            'PMA_Index_type',
+            $index->getType()
+        );
+        $this->assertEquals(
+            'PMA_Packed',
+            $index->getPacked()
+        );
+        $this->assertEquals(
+            'PMA_Non_unique',
+            $index->getNonUnique()
+        );
+    }
+}
