af177aca2b03efce5eb3b98be000edd02c93bbfe
Madhura Jayaratne <madhura.cj@gmail.com>
2013-05-23 23:34:15 +0530

    Wrap more long lines

--- libraries/replication.inc.php
+++ libraries/replication.inc.php
@@ -111,29 +111,48 @@ foreach ($replication_types as $type) {
     }
     if (${"server_{$type}_status"}) {
         if ($type == "master") {
-            ${"server_{$type}_Do_DB"} = explode(",", $server_master_replication[0]["Binlog_Do_DB"]);
+            ${"server_{$type}_Do_DB"} = explode(
+                ",", $server_master_replication[0]["Binlog_Do_DB"]
+            );
             $replication_info[$type]['Do_DB'] = ${"server_{$type}_Do_DB"};
 
-            ${"server_{$type}_Ignore_DB"} = explode(",", $server_master_replication[0]["Binlog_Ignore_DB"]);
+            ${"server_{$type}_Ignore_DB"} = explode(
+                ",", $server_master_replication[0]["Binlog_Ignore_DB"]
+            );
             $replication_info[$type]['Ignore_DB'] = ${"server_{$type}_Ignore_DB"};
         } elseif ($type == "slave") {
-            ${"server_{$type}_Do_DB"} = explode(",", $server_slave_replication[0]["Replicate_Do_DB"]);
+            ${"server_{$type}_Do_DB"} = explode(
+                ",", $server_slave_replication[0]["Replicate_Do_DB"]
+            );
             $replication_info[$type]['Do_DB'] = ${"server_{$type}_Do_DB"};
 
-            ${"server_{$type}_Ignore_DB"} = explode(",", $server_slave_replication[0]["Replicate_Ignore_DB"]);
+            ${"server_{$type}_Ignore_DB"} = explode(
+                ",", $server_slave_replication[0]["Replicate_Ignore_DB"]
+            );
             $replication_info[$type]['Ignore_DB'] = ${"server_{$type}_Ignore_DB"};
 
-            ${"server_{$type}_Do_Table"} = explode(",", $server_slave_replication[0]["Replicate_Do_Table"]);
+            ${"server_{$type}_Do_Table"} = explode(
+                ",", $server_slave_replication[0]["Replicate_Do_Table"]
+            );
             $replication_info[$type]['Do_Table'] = ${"server_{$type}_Do_Table"};
 
-            ${"server_{$type}_Ignore_Table"} = explode(",", $server_slave_replication[0]["Replicate_Ignore_Table"]);
-            $replication_info[$type]['Ignore_Table'] = ${"server_{$type}_Ignore_Table"};
+            ${"server_{$type}_Ignore_Table"} = explode(
+                ",", $server_slave_replication[0]["Replicate_Ignore_Table"]
+            );
+            $replication_info[$type]['Ignore_Table']
+                = ${"server_{$type}_Ignore_Table"};
 
-            ${"server_{$type}_Wild_Do_Table"} = explode(",", $server_slave_replication[0]["Replicate_Wild_Do_Table"]);
-            $replication_info[$type]['Wild_Do_Table'] = ${"server_{$type}_Wild_Do_Table"};
+            ${"server_{$type}_Wild_Do_Table"} = explode(
+                ",", $server_slave_replication[0]["Replicate_Wild_Do_Table"]
+            );
+            $replication_info[$type]['Wild_Do_Table']
+                = ${"server_{$type}_Wild_Do_Table"};
 
-            ${"server_{$type}_Wild_Ignore_Table"} = explode(",", $server_slave_replication[0]["Replicate_Wild_Ignore_Table"]);
-            $replication_info[$type]['Wild_Ignore_Table'] = ${"server_{$type}_Wild_Ignore_Table"};
+            ${"server_{$type}_Wild_Ignore_Table"} = explode(
+                ",", $server_slave_replication[0]["Replicate_Wild_Ignore_Table"]
+            );
+            $replication_info[$type]['Wild_Ignore_Table']
+                = ${"server_{$type}_Wild_Ignore_Table"};
         }
     }
 }
