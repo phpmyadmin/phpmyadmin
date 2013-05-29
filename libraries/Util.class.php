d448add28e369b05eb5e6edd9a821e2aad4f2d38
Marc Delisle <marc@infomarc.info>
2013-05-29 17:23:35 -0400

    bug #3579 NOW() function incorrectly selected (partial regression)

--- libraries/Util.class.php
+++ libraries/Util.class.php
@@ -3743,16 +3743,12 @@ class PMA_Util
         // and the column does not have the
         // ON UPDATE DEFAULT TIMESTAMP attribute.
         if (($field['True_Type'] == 'timestamp')
+            && $field['first_timestamp']
             && empty($field['Default'])
             && empty($data)
             && ! isset($analyzed_sql_field_array['on_update_current_timestamp'])
             && ($analyzed_sql_field_array['default_value'] != 'NULL')
         ) {
-            $default_function = $cfg['DefaultFunctions']['first_timestamp'];
-        }
-
-        // Default for first timestamp field
-        if ($field['first_timestamp']) {
             $default_function = $cfg['DefaultFunctions']['first_timestamp'];
         }
 
