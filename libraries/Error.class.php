08665f2ca7d746eb173ce2cee78a6a8efa47de32
Michal Čihař <mcihar@suse.cz>
2013-06-10 15:04:54 +0200

    Hide params from DBI connect methods as well

--- libraries/Error.class.php
+++ libraries/Error.class.php
@@ -305,6 +305,7 @@ class PMA_Error extends PMA_Message
             'mysql_pconnect',
             'mysqli_connect',
             'mysqli_real_connect',
+            'connect',
         );
 
         if (in_array($function, $include_functions)) {
