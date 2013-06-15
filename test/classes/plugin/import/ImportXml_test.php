43a9886aa3121eac21991a613fca0c9cdce5b097
adamgsoc2013 <adamgsoc2013@gmail.com>
2013-06-15 22:06:15 +0800

    add doImport real case

--- test/classes/plugin/import/ImportXml_test.php
+++ test/classes/plugin/import/ImportXml_test.php
@@ -12,7 +12,7 @@
 /* Each PluginObserver instance contains a PluginManager instance */
 require_once 'libraries/plugins/import/ImportXml.class.php';
 require_once 'libraries/Util.class.php';
-
+require_once 'libraries/database_interface.inc.php';
 
 /**
  * Tests for ImportXml class
@@ -35,7 +35,23 @@ class ImportXml_Test extends PHPUnit_Framework_TestCase
      */
     protected function setUp()
     {
-        $this->object = new ImportXml(); 
+        global $compression, $import_handle, $read_multiply, $cfg;
+        
+        $this->object = new ImportXml();    
+
+        //setting        
+        $GLOBALS['finished'] = false;
+        $GLOBALS['read_limit'] = 100000000;
+        $GLOBALS['import_file'] = 'test/classes/plugin/import/phpmyadmin_importXML_For_Testing.xml';
+        $GLOBALS['import_text'] = 'ImportXml_Test';
+        $compression = 'none'; 
+        $GLOBALS['offset'] = 0;
+        $read_multiply = 10;
+        $import_file = 'test/classes/plugin/import/phpmyadmin_importXML_For_Testing.xml';
+        $import_type = 'Xml';
+        $import_handle = @fopen($import_file, 'r');
+        $cfg['AllowUserDropDatabase'] = false;
+        $GLOBALS['cfg']['Server']['DisableIS'] = true;
     }
 
     /**
@@ -79,6 +95,62 @@ class ImportXml_Test extends PHPUnit_Framework_TestCase
         $this->assertEquals(
             __('Options'),
             $properties->getOptionsText()
+        ); 
+    
+    }
+    
+    /**
+     * Test for doImport
+     *
+     * @return void
+     *
+     * @group medium
+     */
+    public function testDoImport()
+    {
+        global $import_notice;
+        $dbi = $this->getMockBuilder('PMA_DatabaseInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $dbi->expects($this->once())
+             ->method('isSuperuser')
+             ->with('true');
+        $GLOBALS['dbi'] = $dbi;
+        $this->object->doImport();
+ 
+        //If import successfully, PMA will show all database and tables imported as following HTML Page
+        /*
+           The following structures have either been created or altered. Here you can:
+           View a structure's contents by clicking on its name
+           Change any of its settings by clicking the corresponding "Options" link
+           Edit structure by following the "Structure" link
+
+           phpmyadmin (Options)
+           p ma_bookmark (Structure) (Options)
+        */      
+        $import_succesful_display_message =
+               '<br /><br /><strong>The following structures have either '
+             . 'been created or altered. Here you can:</strong><br /><ul><li>View a structure\'s '
+             . 'contents by clicking on its name</li><li>Change any of its settings by clicking '
+             . 'the corresponding "Options" link</li><li>Edit structure by following the "Structure'
+             . '" link</li><br /><li><a href="db_structure.php?db=phpmyadmin&amp;lang=en&amp;'
+             . 'token=token" title="Go to database: `phpmyadmin`">phpmyadmin</a> (<a href="db_op'
+             . 'erations.php?db=phpmyadmin&amp;lang=en&amp;token=token" title="Edit settings for'
+             . ' `phpmyadmin`">Options</a>)</li><ul><li><a href="sql.php?db=phpmyadmin&amp;table'
+             . '=pma_bookmark&amp;lang=en&amp;token=token" title="Go to table: `pma_bookmark`">pma_bookmark'
+             . '</a> (<a href="tbl_structure.php?db=phpmyadmin&amp;table=pma_bookmark'
+             . '&amp;lang=en&amp;token=token" title="Structure of `pma_bookmark`">Structure</a>)'
+             . ' (<a href="tbl_operations.php?db=phpmyadmin&amp;table=pma_bookmark&amp;lang=en&amp;'
+             . 'token=token" title="Edit settings for `pma_bookmark`">Options</a>)</li></ul></ul>';
+        
+        //asset that all databases and tables are imported
+        $this->assertEquals(
+            $import_succesful_display_message,
+            $import_notice
+        );         
+        $this->assertEquals(
+            true,
+            $GLOBALS['finished']
         ); 
     
     }
