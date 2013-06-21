fe93b9234004d74644b3800a742cad781c5df494
ayushchd <ayushchd@gmail.com>
2013-06-21 13:16:47 +0545

    Share code between StringMB and StringNative tests

--- test/libraries/PMA_StringMB_test.php
+++ test/libraries/PMA_StringMB_test.php
@@ -10,13 +10,14 @@
  * Include to test.
  */
 require_once 'libraries/StringMB.class.php';
+require_once 'test/libraries/PMA_StringNative_test.php';
 
 /**
  * Tests for Specialized String Functions (multi-byte) for phpMyAdmin
  *
  * @package PhpMyAdmin-test
  */
-class PMA_String_Mb_Test extends PHPUnit_Framework_TestCase
+class PMA_String_Mb_Test extends PMA_StringNative_Test
 {
     protected $internal_encoding;
 
@@ -29,6 +30,9 @@ class PMA_String_Mb_Test extends PHPUnit_Framework_TestCase
     protected function setUp() 
     {
         $this->internal_encoding = mb_internal_encoding();
+        mb_internal_encoding("UTF-8");
+
+        $this->testClass = new PMA_StringMB;
     }
 
     /**
@@ -41,144 +45,5 @@ class PMA_String_Mb_Test extends PHPUnit_Framework_TestCase
     {
         mb_internal_encoding($this->internal_encoding);
     }
-
-    /**
-     * Test for PMA_StringMB::strlen
-     * 
-     * @param integer $length   Length of the string
-     * @param string  $str      String to check for
-     * @param string  $encoding Encoding of the string
-     * 
-     * @return void
-     * @test
-     * @dataProvider mbStrlenData
-     */
-    public function testMbStrlen($length, $str, $encoding)
-    {   
-        mb_internal_encoding($encoding);
-        $this->assertEquals(
-            $length,
-            PMA_StringMB::strlen($str)
-        );
-    }
-
-    /**
-     * Data provider for testMbStrlen
-     * 
-     * @return array Test data
-     */
-    public function mbStrlenData() 
-    {
-        return array(
-            array(2, "ab", "UTF-8"),
-            array(9, "åèö - doo", "UTF-8"),
-            array(12, "åèö - doo", "ISO-8859-1")
-        );
-    }
-
-    /**
-     * Test for PMA_StringMB::substr
-     * 
-     * @param string $str      Expected substring
-     * @param string $haystack String to check in
-     * @param int    $start    Starting position of substring
-     * @param int    $length   Length of substring
-     * @param string $encoding Encoding of the string
-     * 
-     * @return void
-     * @test
-     * @dataProvider mbSubStrData
-     */
-    public function testMbSubStr($str, $haystack, $start, $length, $encoding)
-    {   
-        mb_internal_encoding($encoding);
-        $this->assertEquals(
-            $str,
-            PMA_StringMB::substr($haystack, $start, $length)
-        );
-    }
-
-    /**
-     * Data provider for testMbSubStr
-     * 
-     * @return array Test data
-     */
-    public function mbSubStrData() 
-    {
-        return array(
-            array("b", "ab", 1, 1, "UTF-8"),
-            array("èö", "åèö - doo", 1, 2, "UTF-8"),
-        );
-    }
-
-    /**
-     * Test for PMA_StringMB::strpos
-     * 
-     * @param int    $pos      Expected position
-     * @param string $haystack String to search in
-     * @param string $needle   String to search for
-     * @param int    $offset   Search offset
-     * @param string $encoding Encoding to test against
-     * 
-     * @return void
-     * @test
-     * @dataProvider mbStrposData
-     */
-    public function testMbStrpos($pos, $haystack, $needle, $offset, $encoding)
-    {   
-        mb_internal_encoding($encoding);
-        $this->assertEquals(
-            $pos,
-            PMA_StringMB::strpos($haystack, $needle, $offset)
-        );
-    }
-
-    /**
-     * Data provider for testMbStrpos
-     * 
-     * @return array Test data
-     */
-    public function mbStrposData() 
-    {
-        return array(
-            array(1, "ab", "b", 0, "UTF-8"),
-            array(2, "åèö - doo", "ö", 0, "UTF-8"),
-            array(6, "åèöè", "è", 3, "ISO-8859-1")
-        );
-    }
-
-    /**
-     * Test for PMA_StringMB::strtolower
-     * 
-     * @param string $expected Expected lowercased string
-     * @param string $string   String to convert to lowercase
-     * @param string $encoding Encoding to test against
-     * 
-     * @return void
-     * @test
-     * @dataProvider mbStrToLowerData
-     */
-    public function testMbStrToLower($expected, $string, $encoding)
-    {   
-        mb_internal_encoding($encoding);
-        $this->assertEquals(
-            $expected,
-            PMA_StringMB::strtolower($string)
-        );
-    }
-
-    /**
-     * Data provider for testMbStrpos
-     * 
-     * @return array Test data
-     */
-    public function mbStrToLowerData() 
-    {
-        return array(
-            array("mary had a", "Mary Had A", "UTF-8"),
-            array("τάχιστη", "Τάχιστη", "UTF-8")
-        );
-    }
-
 }
 ?>
