239067f35ff67bc2d707054ed17bc490e6ecef6b
xmujay <xmujay@gmail.com>
2013-06-22 15:28:50 +0800

    refactor server_replication 1. refactor session  "Handling control requests" 2. fix code style problems

--- server_replication.php
+++ server_replication.php
@@ -40,87 +40,7 @@ if (isset($_REQUEST['url_params'])) {
 /**
  * Handling control requests
  */
-if (isset($_REQUEST['sr_take_action'])) {
-    $refresh = false;
-    if (isset($_REQUEST['slave_changemaster'])) {
-        $_SESSION['replication']['m_username'] = $sr['username'] = PMA_Util::sqlAddSlashes($_REQUEST['username']);
-        $_SESSION['replication']['m_password'] = $sr['pma_pw']   = PMA_Util::sqlAddSlashes($_REQUEST['pma_pw']);
-        $_SESSION['replication']['m_hostname'] = $sr['hostname'] = PMA_Util::sqlAddSlashes($_REQUEST['hostname']);
-        $_SESSION['replication']['m_port']     = $sr['port']     = PMA_Util::sqlAddSlashes($_REQUEST['port']);
-        $_SESSION['replication']['m_correct']  = '';
-        $_SESSION['replication']['sr_action_status'] = 'error';
-        $_SESSION['replication']['sr_action_info'] = __('Unknown error');
-
-        // Attempt to connect to the new master server
-        $link_to_master = PMA_Replication_connectToMaster(
-            $sr['username'], $sr['pma_pw'], $sr['hostname'], $sr['port']
-        );
-
-        if (! $link_to_master) {
-            $_SESSION['replication']['sr_action_status'] = 'error';
-            $_SESSION['replication']['sr_action_info'] = sprintf(
-                __('Unable to connect to master %s.'),
-                htmlspecialchars($sr['hostname'])
-            );
-        } else {
-            // Read the current master position
-            $position = PMA_Replication_Slave_binLogMaster($link_to_master);
-
-            if (empty($position)) {
-                $_SESSION['replication']['sr_action_status'] = 'error';
-                $_SESSION['replication']['sr_action_info'] = 
-                    __('Unable to read master log position. Possible privilege problem on master.');
-            } else {
-                $_SESSION['replication']['m_correct']  = true;
-
-                if (! PMA_Replication_Slave_changeMaster(
-                          $sr['username'], 
-                          $sr['pma_pw'], 
-                          $sr['hostname'], 
-                          $sr['port'], 
-                          $position, 
-                          true, 
-                          false)) {
-                    $_SESSION['replication']['sr_action_status'] = 'error';
-                    $_SESSION['replication']['sr_action_info'] = __('Unable to change master');
-                } else {
-                    $_SESSION['replication']['sr_action_status'] = 'success';
-                    $_SESSION['replication']['sr_action_info'] = sprintf(
-                        __('Master server changed successfully to %s'),
-                        htmlspecialchars($sr['hostname'])
-                    );
-                }
-            }
-        }
-    } elseif (isset($_REQUEST['sr_slave_server_control'])) {
-        if ($_REQUEST['sr_slave_action'] == 'reset') {
-            PMA_Replication_Slave_control("STOP");
-            $GLOBALS['dbi']->tryQuery("RESET SLAVE;");
-            PMA_Replication_Slave_control("START");
-        } else {
-            PMA_Replication_Slave_control(
-                $_REQUEST['sr_slave_action'],
-                $_REQUEST['sr_slave_control_parm']
-            );
-        }
-        $refresh = true;
-
-    } elseif (isset($_REQUEST['sr_slave_skip_error'])) {
-        $count = 1;
-        if (isset($_REQUEST['sr_skip_errors_count'])) {
-            $count = $_REQUEST['sr_skip_errors_count'] * 1;
-        }
-        PMA_Replication_Slave_control("STOP");
-        $GLOBALS['dbi']->tryQuery("SET GLOBAL SQL_SLAVE_SKIP_COUNTER = ".$count.";");
-        PMA_Replication_Slave_control("START");
-
-    }
-
-    if ($refresh) {
-        Header("Location: server_replication.php" . PMA_generate_common_url($GLOBALS['url_params']));
-    }
-    unset($refresh);
-}
+PMA_handleControlRequest();
 
 /**
  * start output
@@ -133,7 +53,8 @@ $response->addHTML(PMA_getHtmlForErrorMessage());
 
 if ($server_master_status) {
     $response->addHTML(PMA_getHtmlForMasterReplication());
-} elseif (! isset($_REQUEST['mr_configure']) && ! isset($_REQUEST['repl_clear_scr'])) {
+} elseif (! isset($_REQUEST['mr_configure']) && 
+          ! isset($_REQUEST['repl_clear_scr'])) {
     $response->addHTML(PMA_getHtmlForNotServerReplication());
 }
 
@@ -147,7 +68,12 @@ $response->addHTML('</div>');
 
 if (! isset($_REQUEST['repl_clear_scr'])) {
     // Render the 'Slave configuration' section
-    $response->addHTML(PMA_getHtmlForSlaveConfiguration($server_slave_status, $server_slave_replication));
+    $response->addHTML(
+        PMA_getHtmlForSlaveConfiguration(
+            $server_slave_status, 
+            $server_slave_replication
+        )
+    );
 }
 if (isset($_REQUEST['sl_configure'])) {
     $response->addHTML(PMA_getHtmlForReplicationChangeMaster("slave_changemaster"));
