a35e214e2e12d075d9d5eeee6675eb7b032d43bf
xmujay <xmujay@gmail.com>
2013-06-25 22:28:49 +0800

    change $cfg['PropertiesIconic'] to $cfg['TabsMode']

--- libraries/server_databases.lib.php
+++ libraries/server_databases.lib.php
@@ -80,7 +80,7 @@ function PMA_getHtmlForDatabase(
     $html .= PMA_getHtmlForReplicationType(
         $is_superuser, 
         $replication_types, 
-        $cfg['PropertiesIconic']
+        $cfg['TabsMode']
     );  
     
     $html .= '</tr>' . "\n"
