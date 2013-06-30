b75d450a9811ac3d23a81b50fc4bb507ea0b5c50
Spun Nakandala <supun.nakandala@gmail.com>
2013-06-30 08:36:05 +0530

    $drooped_column is never initialized. Therefore this code removed

--- sql.php
+++ sql.php
@@ -344,7 +344,7 @@ if (isset($GLOBALS['show_as_php']) || ! empty($GLOBALS['validatequery'])) {
     } // end rows total count
 
     // if a table or database gets dropped, check column comments.
-    if (isset($purge) && $purge == '1') {
+    if (isset($_REQUEST['purge']) && $_REQUEST['purge'] == '1') {
         /**
          * Cleanup relations.
          */
@@ -359,17 +359,6 @@ if (isset($GLOBALS['show_as_php']) || ! empty($GLOBALS['validatequery'])) {
         } // end if relation-stuff
     } // end if ($purge)
 
-    // If a column gets dropped, do relation magic.
-    if (isset($dropped_column)
-        && strlen($db)
-        && strlen($table)
-        && ! empty($dropped_column)
-    ) {
-        include_once 'libraries/relation_cleanup.lib.php';
-        PMA_relationsCleanupColumn($db, $table, $dropped_column);
-        // to refresh the list of indexes (Ajax mode)
-        $extra_data['indexes_list'] = PMA_Index::getView($table, $db);
-    } // end if column was dropped
 } // end else "didn't ask to see php code"
 
 // No rows returned -> move back to the calling page
