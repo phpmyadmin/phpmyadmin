e0c1ed02a11b4ed46d82a31b7f95030854865add
Marc Delisle <marc@infomarc.info>
2013-06-30 06:58:32 -0400

    4.0.4.1 release

--- doc/conf.py
+++ doc/conf.py
@@ -49,7 +49,7 @@ copyright = u'2012 - 2013, The phpMyAdmin devel team'
 # built documents.
 #
 # The short X.Y version.
-version = '4.0.4'
+version = '4.0.4.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -101,7 +101,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '4.0.4');
+        $this->set('PMA_VERSION', '4.0.4.1');
         /**
          * @deprecated
          */
