01d35b3558e47fba947719857bd71f6fd9e5dce8
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2013-07-05 19:19:23 +0200

    fix self-XSS, thanks to Michał Bentkowski for reporting, see PMASA-2013-8

--- libraries/display_tbl.lib.php
+++ libraries/display_tbl.lib.php
@@ -2362,7 +2362,7 @@ function PMA_displayTable(&$dt_result, &$the_disp_mode, $analyzed_sql)
         }
 
         $message = PMA_Message::success(__('Showing rows'));
-        $message->addMessage($first_shown_rec);
+        $message->addMessage(htmlspecialchars($first_shown_rec));
         if ($message_view_warning) {
             $message->addMessage('...', ' - ');
             $message->addMessage($message_view_warning);
