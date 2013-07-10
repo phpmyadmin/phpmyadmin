1427bbd7104803a32407144a40bb0741bd39c5f0
Michal Čihař <mcihar@suse.cz>
2013-07-10 15:14:55 +0200

    Fix typo

--- libraries/PDF.class.php
+++ libraries/PDF.class.php
@@ -41,7 +41,7 @@ class PMA_PDF extends TCPDF
      * @access public
      */
     public function __construct($orientation = 'P', $unit = 'mm', $format = 'A4',
-        $unicode = true, $encoding = 'UTF-8', $diskcache = false, , $pdfa=false
+        $unicode = true, $encoding = 'UTF-8', $diskcache = false, $pdfa=false
     ) {
         parent::__construct(
             $orientation, $unit, $format, $unicode,
