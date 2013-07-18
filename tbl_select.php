f6838ff7ce25531226c8218a0dc7c6a2f17e91d3
Spun Nakandala <supun.nakandala@gmail.com>
2013-07-18 09:26:44 +0530

    removed include sql.php from tbl_select.php

--- tbl_select.php
+++ tbl_select.php
@@ -15,6 +15,12 @@
 require_once 'libraries/common.inc.php';
 require_once 'libraries/mysql_charsets.inc.php';
 require_once 'libraries/TableSearch.class.php';
+require_once 'libraries/Table.class.php';
+require_once 'libraries/Header.class.php';
+require_once 'libraries/check_user_privileges.lib.php';
+require_once 'libraries/bookmark.lib.php';
+require_once 'libraries/sql.lib.php';
+require_once 'libraries/sqlparser.lib.php';
 
 $response = PMA_Response::getInstance();
 $header   = $response->getHeader();
@@ -65,6 +71,67 @@ if (! isset($_POST['columnsToDisplay']) && ! isset($_POST['displayAllColumns']))
      * Selection criteria have been submitted -> do the work
      */
     $sql_query = $table_search->buildSqlQuery();
-    include 'sql.php';
+
+    /**
+     * Parse and analyze the query
+     */
+    require_once 'libraries/parse_analyze.inc.php';
+
+    // Include PMA_Index class for use in PMA_DisplayResults class
+    require_once './libraries/Index.class.php';
+
+    require_once 'libraries/DisplayResults.class.php';
+
+    $displayResultsObject = new PMA_DisplayResults(
+        $GLOBALS['db'], $GLOBALS['table'], $GLOBALS['goto'], $GLOBALS['sql_query']
+    );
+
+    $displayResultsObject->setConfigParamsForDisplayTable();
+    
+    // assign default full_sql_query
+    $full_sql_query = $sql_query;
+
+    // Handle remembered sorting order, only for single table query
+    if (PMA_isRememberSortingOrder($analyzed_sql_results)) {
+        PMA_handleSortOrder($db, $table, $analyzed_sql, $full_sql_query);
+    }
+
+    // Do append a "LIMIT" clause?
+    if (PMA_isAppendLimitClause($analyzed_sql_results)) {
+        list($sql_limit_to_append, $full_sql_query,
+            $analyzed_display_query, $display_query
+        ) = PMA_appendLimitClause(
+            $full_sql_query, $analyzed_sql, isset($display_query)
+        );
+    } else {
+        $sql_limit_to_append = '';
+    }
+
+    $reload = PMA_hasCurrentDbChanged($db);
+
+    // Execute the query
+    list($result, $num_rows, $unlim_num_rows, $profiling_results,
+        $justBrowsing, $extra_data
+    ) = PMA_executeTheQuery(
+        $analyzed_sql_results, $full_sql_query, false, $db, $table,
+        null, null, $cfg['Bookmark']['user'], null
+    );
+
+    if ((0 == $num_rows && 0 == $unlim_num_rows) || $is_affected) {
+        // No rows returned -> move back to the calling page
+        PMA_sendResponseForNoResultsReturned(
+            $analyzed_sql_results, $db, $table, null,
+            $num_rows, $displayResultsObject, $extra_data, $cfg
+        );
+    } else {
+        // At least one row is returned -> displays a table with results    
+        PMA_sendResponseForResultsReturned(
+            $result, $justBrowsing, $analyzed_sql_results, $db, $table, null, null,
+            null, $displayResultsObject, $goto, $pmaThemeImage,
+            $sql_limit_to_append, $unlim_num_rows, $num_rows, $querytime,
+            $full_sql_query, null, null, $profiling_results, null, null, $sql_query,
+            null, $cfg
+        );
+    } // end rows returned
 }
 ?>
