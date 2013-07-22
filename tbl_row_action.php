6a053b562796929fb77adba454d7b971d24f6d4c
Spun Nakandala <supun.nakandala@gmail.com>
2013-07-22 22:52:57 +0530

    replace include 'sql.php' with method calls to sql.lib.php

--- tbl_row_action.php
+++ tbl_row_action.php
@@ -11,6 +11,7 @@
  */
 require_once 'libraries/common.inc.php';
 require_once 'libraries/mysql_charsets.inc.php';
+require_once 'libraries/sql.lib.php';
 
 if (isset($_REQUEST['submit_mult'])) {
     $submit_mult = $_REQUEST['submit_mult'];
@@ -124,8 +125,16 @@ if (!empty($submit_mult)) {
         unset($submit_mult, $_REQUEST['mult_btn']);
 
         $active_page = 'sql.php';
-        include 'sql.php';
-        break;
+        /**
+         * Parse and analyze the query
+         */
+        require_once 'libraries/parse_analyze.inc.php';
+
+        PMA_executeQueryAndSendQueryResponse(
+            $analyzed_sql_results, false, $db, $table, null, null, null, false, null,
+            null, null, null, $goto, $pmaThemeImage, null, null, null, $sql_query,
+            null, null
+        );
     }
 }
 ?>
