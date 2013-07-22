1c0fd2fdfef304bc7b1d2eacbf321a7b1950db86
adamgsoc2013 <adamgsoc2013@gmail.com>
2013-07-22 23:12:36 +0800

    add update sql statement for testing

--- test/libraries/PMA_SQL_parse_analyze_test.php
+++ test/libraries/PMA_SQL_parse_analyze_test.php
@@ -9,12 +9,14 @@
 /*
  * Include to test.
  */
+require_once 'libraries/Util.class.php';
 require_once 'libraries/sqlparser.lib.php';
 
 class PMA_SQLParserAnalyze_Test extends PHPUnit_Framework_TestCase
 {
     public function testPMA_SQP_getParserAnalyzeMa()
     {
+        //select statement
         $sql_query = "select * from PMA.PMAParse";
         $db = "PMA";
         $data = PMA_SQP_getParserAnalyzeMap($sql_query, $db);
@@ -32,10 +34,33 @@ class PMA_SQLParserAnalyze_Test extends PHPUnit_Framework_TestCase
             'select * from PMA.PMAParse',
             $data['analyzed_sql'][0]['unsorted_query']
         );
-        $this->assertFalse($data['reload']);
+
+        $this->assertTrue($data['is_select']);
         $this->assertFalse($data['is_group']);
         $this->assertFalse($data['is_show']);
+        
+        //update statement
+        $sql_query = "UPDATE `11`.`pma_bookmark` SET `id` = '2' WHERE `pma_bookmark`.`id` = 1;";
+        $db = "PMA";
+        $data = PMA_SQP_getParserAnalyzeMap($sql_query, $db);
+        
+        $this->assertEquals(
+            array(
+                'type' => 'alpha_reservedWord',
+                'data' => 'UPDATE',
+                'pos' => 6,
+                'forbidden' => true,
+            ),
+            $data['parsed_sql'][0]
+        );
+        $this->assertEquals(
+            $sql_query,
+            $data['analyzed_sql'][0]['unsorted_query']
+        );
         $this->assertFalse($data['is_group']);
+        $this->assertFalse($data['is_show']);
+        $this->assertTrue($data['is_affected']);
+        $this->assertFalse($data['is_select']);        
     }
 }
 
