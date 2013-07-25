2666eb81a5e6dd62d95c4bfc9b8440e555e94927
Fatih Üstündağ <fatih.ustundag@gmail.com>
2013-07-25 14:19:22 +0300

    verbose md5 check fix

--- libraries/common.inc.php
+++ libraries/common.inc.php
@@ -749,7 +749,7 @@ if (! defined('PMA_MINIMUM_COMMON')) {
             if ($server['host'] == $_REQUEST['server']
                 || $server['verbose'] == $_REQUEST['server']
                 || $PMA_String::strtolower($server['verbose']) == $PMA_String::strtolower($_REQUEST['server'])
-                || md5($PMA_String::strtolower($server['verbose'])) == md5($PMA_String::strtolower($_REQUEST['server']))
+                || md5($PMA_String::strtolower($server['verbose'])) == $PMA_String::strtolower($_REQUEST['server'])
             ) {
                 $_REQUEST['server'] = $i;
                 break;
