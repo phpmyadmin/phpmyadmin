333d82d3271b2a1b445134bb6bbb15ae8c9ba8a6
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2013-07-27 14:52:22 +0200

    [security] Fix unencoded json object, see PMASA-2013-11

--- version_check.php
+++ version_check.php
@@ -9,10 +9,27 @@
 header('Content-Type: application/json');
 
 $file = 'http://www.phpmyadmin.net/home_page/version.json';
+
+$response = '{}';
+
 if (ini_get('allow_url_fopen')) {
-    echo file_get_contents($file);
+    $response = file_get_contents($file);
 } else if (function_exists('curl_init')) {
-    curl_exec(curl_init($file));
+    $curl_handle = curl_init($file);
+    curl_setopt(
+        $curl_handle,
+        CURLOPT_RETURNTRANSFER,
+        true
+    );
+    $response = curl_exec($curl_handle);
 }
 
+// check if the retrieved version.json file is a valid json object,
+// before encoding it again and sending it to the browser
+$data = json_decode($response);
+if (is_object($data) && strlen($data->version) && strlen($data->date)) {
+    echo json_encode(
+        array('version' => $data->version, 'date' => $data->date)
+    );
+}
 ?>
