a28f43b288d2162981784b3c79f0af628c76640f
Kasun Chathuranga <chathuranga.jayaneththi@gmail.com>
2013-07-31 22:09:46 +0530

    Add new JS file for server_user_groups

--- js/server_privileges.js
+++ js/server_privileges.js
@@ -301,33 +301,6 @@ AJAX.registerOnload('server_privileges.js', function () {
         }); // end $.post()
     }); // end Revoke User
 
-    $("a.deleteUserGroup.ajax").live('click', function (event) {
-        event.preventDefault();
-        var $link = $(this);
-        var groupName = $link.parents('tr').find('td:first').text();
-        var buttonOptions = {};
-        buttonOptions[PMA_messages.strGo] = function () {
-            $(this).dialog("close");
-            $link.removeClass('ajax').trigger('click');
-        };
-        buttonOptions[PMA_messages.strClose] = function () {
-            $(this).dialog("close");
-        };
-        $('<div/>')
-            .attr('id', 'confirmUserGroupDeleteDialog')
-            .append($.sprintf(PMA_messages.strDropUserGroupWarning, escapeHtml(groupName)))
-            .dialog({
-                width: 300,
-                minWidth: 200,
-                modal: true,
-                buttons: buttonOptions,
-                title: PMA_messages.strConfirm,
-                close: function () {
-                    $(this).remove();
-                }
-            });
-    });
-
     $("a.edit_user_group_anchor.ajax").live('click', function (event) {
         event.preventDefault();
         $(this).parents('tr').addClass('current_row');
