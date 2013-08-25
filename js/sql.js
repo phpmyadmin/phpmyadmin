f0b063400cc100d9f3ed16a949e6950bf46ba81e
Atul Pratap Singh <atulpratapsingh05@gmail.com>
2013-08-25 18:45:11 +0530

    fix autoexpand on use query

--- js/sql.js
+++ js/sql.js
@@ -296,6 +296,7 @@ AJAX.registerOnload('sql.js', function() {
                 } else if (typeof data.reload != 'undefined') {
                     // this happens if a USE or DROP command was typed
                     PMA_commonActions.setDb(data.db);
+                    PMA_reloadNavigation();
                     PMA_commonActions.refreshMain(false, function () {
                         if ($('#result_query').length) {
                             $('#result_query').remove();
@@ -306,7 +307,6 @@ AJAX.registerOnload('sql.js', function() {
                                 .prependTo('#page_content');
                         }
                     });
-                    PMA_reloadNavigation();
                 }
                 
                 $sqlqueryresults.show().trigger('makegrid');
