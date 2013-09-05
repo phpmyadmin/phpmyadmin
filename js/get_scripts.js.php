ed4771586f1bdb6e80d4b2502f4e5b07399f784f
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2013-09-05 20:02:16 +0200

    Returned the cache setting to the get_scripts.js.php file

--- js/get_scripts.js.php
+++ js/get_scripts.js.php
@@ -18,10 +18,8 @@ session_write_close();
 
 // Send correct type
 header('Content-Type: text/javascript; charset=UTF-8');
-// Enable browser cache for 1 hour //not
-// ================WARNING=================
-// This must be changed back before merging
-header('Expires: ' . gmdate('D, d M Y H:i:s', time() - 3600) . ' GMT');
+// Enable browser cache for 1 hour
+header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 3600) . ' GMT');
 
 if (! empty($_GET['scripts']) && is_array($_GET['scripts'])) {
     foreach ($_GET['scripts'] as $script) {
