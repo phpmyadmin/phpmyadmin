e8f734e31aa12098c5f4a293d8ab518ed62148ba
Madhura Jayaratne <madhura.cj@gmail.com>
2013-09-19 23:13:47 +0530

    bug #3975 Table grouping isn't implemented properly

--- libraries/navigation/NavigationTree.class.php
+++ libraries/navigation/NavigationTree.class.php
@@ -587,6 +587,14 @@ class PMA_NavigationTree
                     }
                     $groups[$key]->pos2 = $node->pos2;
                     $groups[$key]->pos3 = $node->pos3;
+                    if ($node instanceof Node_Table_Container) {
+                        $groups[$key]->links = array(
+                            'text' => $node->links['text']
+                                . '&amp;tbl_group=' . urlencode($key),
+                            'icon' => $node->links['icon']
+                                . '&amp;tbl_group=' . urlencode($key)
+                        );
+                    }
                     $node->addChild($groups[$key]);
                     foreach ($separators as $separator) {
                         // FIXME: this could be more efficient
