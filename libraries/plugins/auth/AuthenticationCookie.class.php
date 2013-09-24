0b867048925d2403006aec6f7fa11d2aa2215080
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2013-09-24 15:33:21 +0200

    fix coding style : wrap long lines

--- libraries/plugins/auth/AuthenticationCookie.class.php
+++ libraries/plugins/auth/AuthenticationCookie.class.php
@@ -260,10 +260,12 @@ class AuthenticationCookie extends AuthenticationPlugin
         ) {
             // If enabled show captcha to the user on the login screen.
             echo '<script type="text/javascript"
-                    src="https://www.google.com/recaptcha/api/challenge?k=' . $GLOBALS['cfg']['CaptchaLoginPublicKey'] . '">
+                    src="https://www.google.com/recaptcha/api/challenge?k='
+                . $GLOBALS['cfg']['CaptchaLoginPublicKey'] . '">
                  </script>
                  <noscript>
-                    <iframe src="https://www.google.com/recaptcha/api/noscript?k=' . $GLOBALS['cfg']['CaptchaLoginPublicKey'] . '"
+                    <iframe src="https://www.google.com/recaptcha/api/noscript?k='
+                . $GLOBALS['cfg']['CaptchaLoginPublicKey'] . '"
                         height="300" width="500" frameborder="0"></iframe><br>
                     <textarea name="recaptcha_challenge_field" rows="3" cols="40">
                     </textarea>
