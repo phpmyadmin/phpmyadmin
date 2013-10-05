98aeddc71e5850d6d4bd99369c331ffa86fc7452
Marc Delisle <marc@infomarc.info>
2013-10-05 17:44:51 -0400

    bug #4108 Missing refresh by deleting databases

--- js/server_databases.js
+++ js/server_databases.js
@@ -76,6 +76,10 @@ AJAX.registerOnload('server_databases.js', function() {
 
                         $rowsToRemove.remove();
                         $form.find('tbody').PMA_sort_table('.name');
+                        if ($form.find('tbody').find('tr').length == 0) {
+                            // user just dropped the last db on this page
+                            PMA_commonActions.refreshMain();
+                        }
                         PMA_reloadNavigation();
                     } else {
                         $form.find('tr.removeMe').removeClass('removeMe');
