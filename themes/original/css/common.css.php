034886e6205250e2beebf5958e3514ea4c197bd9
Marc Delisle <marc@infomarc.info>
2013-11-20 07:58:01 -0500

    Revert 32b8a06a536d7ff95019296348981549b8ed44c9

--- themes/original/css/common.css.php
+++ themes/original/css/common.css.php
@@ -2095,13 +2095,9 @@ fieldset .disabled-field td {
     color: #FFF !important;
 }
 
-.data {
-    overflow: scroll;
-}
 
 .data_full_width {
     width: 100%;
-    overflow: scroll;
 }
 
 .cDrop {
