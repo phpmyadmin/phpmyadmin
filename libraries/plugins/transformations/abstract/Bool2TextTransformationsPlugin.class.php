b7d8bd7dea9f86995d855cd71bf13bd72a1ec424
Marc Delisle <marc@infomarc.info>
2013-12-03 12:47:45 -0500

    - Fix coding style - Add ChangeLog entry

--- libraries/plugins/transformations/abstract/Bool2TextTransformationsPlugin.class.php
+++ libraries/plugins/transformations/abstract/Bool2TextTransformationsPlugin.class.php
@@ -46,11 +46,17 @@ abstract class Bool2TextTransformationsPlugin extends TransformationsPlugin
     public function applyTransformation($buffer, $options = array(), $meta = '')
     {
         error_log('apply');
-    	if ( ! isset($options[0]) ) $options[0] = 'T';    // default true  option
-    	if ( ! isset($options[1]) ) $options[1] = 'F';    // default false option
-    	
-    	if ( $buffer == '0' ) return $options[1];         // return false label
-    	return $options[0];                               // or true one if nonzero
+        if (! isset($options[0])) {
+            $options[0] = 'T';    // default true  option
+        }
+        if (! isset($options[1])) {
+            $options[1] = 'F';    // default false option
+        }
+ 
+        if ($buffer == '0') {
+            return $options[1];   // return false label
+        }
+        return $options[0];       // or true one if nonzero
     }
 
     /**
