69c24b78bb2d7d50a1d205f7ad13468e95d067d8
Marc Delisle <marc@infomarc.info>
2014-01-01 06:19:39 -0500

    ChangeLog entry and fix explanation for bug #4209
    Signed-off-by: Marc Delisle <marc@infomarc.info>

--- export.php
+++ export.php
@@ -300,6 +300,13 @@ function PMA_isGzHandlerEnabled()
  */
 function PMA_gzencodeNeeded()
 {
+    /*
+     * We should gzencode only if the function exists
+     * but we don't want to compress twice, therefore
+     * gzencode only if transparent compression is not enabled
+     * and gz compression was not asked via $cfg['OBGzip']
+     * but transparent compression does not apply when saving to server
+     */
     if (@function_exists('gzencode')
         && ((! @ini_get('zlib.output_compression')
         && ! PMA_isGzHandlerEnabled())
