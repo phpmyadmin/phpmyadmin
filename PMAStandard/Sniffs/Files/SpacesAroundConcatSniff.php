4de3bf97d7540611784cf7a6d92536918a9a7552
Hugues Peccatte <hugues.peccatte@gmail.com>
2014-01-07 12:17:46 +0100

    Update license and author information.
    Signed-off-by: Hugues Peccatte <hugues.peccatte@gmail.com>

--- PMAStandard/Sniffs/Files/SpacesAroundConcatSniff.php
+++ PMAStandard/Sniffs/Files/SpacesAroundConcatSniff.php
@@ -3,43 +3,28 @@
 /**
  * Class for a sniff to oblige whitespaces before and after a concatenation operator
  *
- * This Sniff check if a whitespace is missing before or after any concatenation operator.
+ * This Sniff check if a whitespace is missing before or after any concatenation
+ * operator.
  * The concatenation operator is identified by the PHP token T_STRING_CONCAT
  * The whitespace is identified by the PHP token T_WHITESPACE
  *
  * PHP version 5
  *
- * LICENSE: CC-BY-SA
- * - The licensor permits others to copy, distribute, display, and perform the work. In return, licenses must give the
- * original author credit.
- * - The licensor permits others to distribute derivative works only under a license identical to the one that governs
- * the licensor's work
- * - The licensor strongly encourages modifications on this software
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE PHP DEVELOPMENT TEAM OR ITS CONTRIBUTORS OR ME BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * @category    PHP
- * @package     PHP_CodeSniffer
- * @author      Nicolas Giraud
- * @since       September, the 12th 2013
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Nicolas Giraud <g.nicolas_89@hotmail.fr>
+ * @since    September, the 12th 2013
  */
 
 /// {{{ Self_Sniffs_Formatting_SpacesAroundConcatSniff
 /**
  * Sniff to oblige whitespaces before and after a concatenation operator
  *
- * @category    PHP
- * @package     PHP_CodeSniffer
- * @subpackage  PMAStandard
- * @name        SpacesAroundConcatSniff
- * @author      Nicolas Giraud
- * @since       September, the 12th 2013
- * @version     Release: 1.0
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @name     SpacesAroundConcatSniff
+ * @author   Nicolas Giraud <g.nicolas_89@hotmail.fr>
+ * @version  Release: 1.0
  */
 class PMAStandard_Sniffs_Files_SpacesAroundConcatSniff implements PHP_CodeSniffer_Sniff
 {
