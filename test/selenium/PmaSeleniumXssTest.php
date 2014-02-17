7f229a2813de8fc2bb664b2623198dd11184190b
Michal Čihař <michal@cihar.com>
2014-02-17 18:23:21 +0100

    Enable XSS Selenium test
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test/selenium/PmaSeleniumXssTest.php
+++ test/selenium/PmaSeleniumXssTest.php
@@ -7,7 +7,7 @@
  * @subpackage Selenium
  */
 
-require_once 'Helper.php';
+require_once 'TestBase.php';
 
 /**
  * PmaSeleniumXSSTest class
@@ -15,29 +15,8 @@ require_once 'Helper.php';
  * @package    PhpMyAdmin-test
  * @subpackage Selenium
  */
-class PmaSeleniumXSSTest extends PHPUnit_Extensions_Selenium2TestCase
+class PmaSeleniumXSSTest extends PMA_SeleniumBase
 {
-    /**
-     * Helper Object
-     *
-     * @var Helper
-     */
-    private $_helper;
-
-    /**
-     * Setup the browser environment to run the selenium test case
-     *
-     * @return void
-     */
-    public function setUp()
-    {
-        $this->_helper = new Helper($this);
-        $this->setBrowser($this->_helper->getBrowserString());
-        $this->setBrowserUrl(
-            TESTSUITE_PHPMYADMIN_HOST . TESTSUITE_PHPMYADMIN_URL
-        );
-    }
-
     /**
      * Tests the SQL query tab with a null query
      *
@@ -45,9 +24,9 @@ class PmaSeleniumXSSTest extends PHPUnit_Extensions_Selenium2TestCase
      */
     public function testQueryTabWithNullValue()
     {
-        $this->_helper->login(TESTSUITE_USER, TESTSUITE_PASSWORD);
+        $this->login(TESTSUITE_USER, TESTSUITE_PASSWORD);
         $this->byLinkText("SQL")->click();
-        $this->_helper->waitForElement("byId", "queryboxf");
+        $this->waitForElement("byId", "queryboxf");
         $this->byId("button_submit_query")->click();
         $this->assertEquals("Missing value in the form!", $this->alertText());
     }
