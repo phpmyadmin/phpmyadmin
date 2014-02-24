bdeddc92793c192510d21b079e474c5fbcb62d63
Michal Čihař <michal@cihar.com>
2014-02-24 14:07:27 +0100

    Remove mistakenly pasted code
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test/selenium/PmaSeleniumDbOperationsTest.php
+++ test/selenium/PmaSeleniumDbOperationsTest.php
@@ -52,9 +52,6 @@ class PMA_SeleniumDbOperationsTest extends PMA_SeleniumBase
             $this->waitForElement(
                 "byXPath",
                 "//span[@id='span_table_comment' and contains(., 'comment_foobar')]"
-            'script' => "$('#topmenu').css('width', '50%')"
-                . ".menuResizer('destroy');",
-            'args' => array()
             )
         );
     }
