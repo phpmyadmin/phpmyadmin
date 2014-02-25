1c732b783bc2232b3e88ecb64bfca99ed25dbfbb
Viduranga Wijesooriya <vpowerrc@gmail.com>
2014-02-25 19:35:02 +0530

    re fixed bug-#4281 Grid edit enum field, dialog disappears when trying to select value
    Signed-off-by: Viduranga Wijesooriya <vpowerrc@gmail.com>

--- js/makegrid.js
+++ js/makegrid.js
@@ -798,7 +798,7 @@ function PMA_makegrid(t, enableResize, enableReorder, enableVisib, enableGridEdi
                         $(g.cEdit).find('.edit_box').val('');
                     });
                 }
-                
+
                 //reset the position of the edit_area div after closing datetime picker
                 $('.edit_area').css({'top' :'0','position':''});
 
@@ -1712,8 +1712,7 @@ function PMA_makegrid(t, enableResize, enableReorder, enableVisib, enableGridEdi
             });
             $('html').click(function (e) {
                 // hide edit cell if the click is not from the datepicker or edit area
-                if (($(e.target).parents().index($('#ui-datepicker-div')) && 
-                   $(e.target).parents().index($('.cEdit .edit_area'))) == -1){ 
+                if ($(e.target).parents().index($('#ui-datepicker-div')) == -1 && $(e.target).parents().index($('.cEdit .edit_area')) == -1){
                    g.hideEditCell();
                 }
             }).keydown(function (e) {
