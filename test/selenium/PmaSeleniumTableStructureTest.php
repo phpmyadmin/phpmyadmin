b93e20c8a9cf3212f5444c97fc55ac0849dd8934
Michal Čihař <michal@cihar.com>
2014-03-13 15:52:57 +0100

    Revert "Avoid not needed click"
    This reverts commit ecc3c4d4b51c046a9cd003eaaad39e11bc6bfecf.

--- test/selenium/PmaSeleniumTableStructureTest.php
+++ test/selenium/PmaSeleniumTableStructureTest.php
@@ -116,6 +116,9 @@ class PMA_SeleniumTableStructureTest extends PMA_SeleniumBase
             . "'Table test_table has been altered successfully')]"
         );
 
+        $this->byLinkText("Structure")->click();
+        $this->waitForElement("byId", "tablestructure");
+
         $this->assertEquals(
             "val3",
             $this->byCssSelector('label[for=checkbox_row_2]')->text()
