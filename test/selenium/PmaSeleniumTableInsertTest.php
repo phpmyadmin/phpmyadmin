833fce8a8d72a6f93eb3927582fa1c7a2a6d905c
Michal Čihař <michal@cihar.com>
2014-03-18 15:10:41 +0100

    Use standard way to navigate to table
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test/selenium/PmaSeleniumTableInsertTest.php
+++ test/selenium/PmaSeleniumTableInsertTest.php
@@ -45,8 +45,7 @@ class PMA_SeleniumTableInsertTest extends PMA_SeleniumBase
     public function setUpPage()
     {
         $this->login();
-        $this->waitForElement('byLinkText', $this->database_name)->click();
-        $this->waitForElement("byLinkText", "test_table");
+        $this->navigateTable('test_table');
     }
 
     /**
