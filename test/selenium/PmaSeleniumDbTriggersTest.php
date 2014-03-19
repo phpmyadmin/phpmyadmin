761ec6bcd09bd10ce0bd9b594e746d2693318cb9
Michal Čihař <michal@cihar.com>
2014-03-19 13:42:47 +0100

    Need to wait for element to be present
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test/selenium/PmaSeleniumDbTriggersTest.php
+++ test/selenium/PmaSeleniumDbTriggersTest.php
@@ -53,7 +53,7 @@ class PMA_SeleniumDbTriggersTest extends PMA_SeleniumBase
     public function setUpPage()
     {
         $this->login();
-        $this->byLinkText($this->database_name)->click();
+        $this->waitForElement('byLinkText', $this->database_name)->click();
         $this->waitForElement(
             "byXPath", "//a[contains(., 'test_table')]"
         );
