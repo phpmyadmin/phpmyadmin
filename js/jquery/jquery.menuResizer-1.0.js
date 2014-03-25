2eb3c363dae9bc3aa91f095e21c885343e7fb2c5
Viduranga Wijesooriya <vpowerrc@gmail.com>
2014-03-25 13:35:36 +0530

    fixed bug-#4337 'More' in Actions area doesn't collapse to fit available space
    Signed-off-by: Viduranga Wijesooriya <vpowerrc@gmail.com>

--- js/jquery/jquery.menuResizer-1.0.js
+++ js/jquery/jquery.menuResizer-1.0.js
@@ -51,7 +51,10 @@
                     .removeClass('tabactive');
                 }
             });
-        $container.children('.clearfloat').before($submenu);
+        $float_element = "<div class='clearfloat'></div>";        
+        $container.children('.clearfloat').detach();       
+        $container.append($submenu);
+        $container.append($float_element);
         setTimeout(function () {
             self.resize();
         }, 4);
