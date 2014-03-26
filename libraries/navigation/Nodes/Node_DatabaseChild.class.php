f8d8c8bd78cc777e778ff7a4dfdcec08f9f4940c
Petr Tomášek <tomy@tomy.cz>
2014-03-26 20:25:24 +0100

    Update Node_DatabaseChild.class.php
    Image lightbulb_off was returned without extension (.png)

--- libraries/navigation/Nodes/Node_DatabaseChild.class.php
+++ libraries/navigation/Nodes/Node_DatabaseChild.class.php
@@ -36,7 +36,7 @@ abstract class Node_DatabaseChild extends Node
                 . '&itemName=' . urldecode($item)
                 . '&dbName=' . urldecode($db) . '"'
                 . ' class="hideNavItem ajax">'
-                . PMA_Util::getImage('lightbulb_off', __('Hide'))
+                . PMA_Util::getImage('lightbulb_off.png', __('Hide'))
                 . '</a></span>';
         }
         return $ret;
