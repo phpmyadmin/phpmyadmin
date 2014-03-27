c02dd60d70641ecdce86cbe472dce8ce1c9d5a6a
Michal Čihař <michal@cihar.com>
2014-03-27 11:35:48 +0100

    Wait for load
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test/selenium/TestBase.php
+++ test/selenium/TestBase.php
@@ -152,6 +152,7 @@ abstract class PMA_SeleniumBase extends PHPUnit_Extensions_Selenium2TestCase
     {
         $result = parent::prepareSession();
         $this->url('');
+        $this->waitForElementNotPresent('byId', 'cfs-style');
         return $result;
     }
 
