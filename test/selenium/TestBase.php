9b8c943a50a0f9f836ef72bde95c0aebb0e476e5
Michal Čihař <michal@cihar.com>
2014-03-27 11:36:13 +0100

    Enable BrowserStack debugging
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test/selenium/TestBase.php
+++ test/selenium/TestBase.php
@@ -65,7 +65,7 @@ abstract class PMA_SeleniumBase extends PHPUnit_Extensions_Selenium2TestCase
             $capabilities = array(
                 'browserstack.user' => $GLOBALS['TESTSUITE_BROWSERSTACK_USER'],
                 'browserstack.key' => $GLOBALS['TESTSUITE_BROWSERSTACK_KEY'],
-                'browserstack.debug' => false,
+                'browserstack.debug' => true,
                 'browserstack.local' => $build_local,
                 'browserstack.localIdentifier' => $build_id,
                 'project' => 'phpMyAdmin',
