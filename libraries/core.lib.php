66e2135fb3a70a59c8cfdb57aa3eeaba88768f52
Michal Čihař <michal@cihar.com>
2014-05-06 13:19:16 +0200

    Remove break after return
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- libraries/core.lib.php
+++ libraries/core.lib.php
@@ -386,11 +386,9 @@ function PMA_arrayMergeRecursive()
     switch(func_num_args()) {
     case 0 :
         return false;
-        break;
     case 1 :
         // when does that happen?
         return func_get_arg(0);
-        break;
     case 2 :
         $args = func_get_args();
         if (! is_array($args[0]) || ! is_array($args[1])) {
@@ -415,13 +413,11 @@ function PMA_arrayMergeRecursive()
             }
         }
         return $args[0];
-        break;
     default :
         $args = func_get_args();
         $args[1] = PMA_arrayMergeRecursive($args[0], $args[1]);
         array_shift($args);
         return call_user_func_array('PMA_arrayMergeRecursive', $args);
-        break;
     }
 }
 
