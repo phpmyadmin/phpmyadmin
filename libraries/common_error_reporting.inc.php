7bdad8efe33bed52d25e13ca1c064736c66fd022
Dhananjay Nakrani <dhananjaynakrani@gmail.com>
2014-05-10 01:48:56 +0530

    Add script that will report all the errors of current-script-run. This script is intended to be used same as common.inc.php, but at the end of each script.
    Signed-off-by: Dhananjay Nakrani <dhananjaynakrani@gmail.com>

---
+++ libraries/common_error_reporting.inc.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * block attempts to directly run this script
+ */
+if (getcwd() == dirname(__FILE__)) {
+    die('Attack stopped');
+}
+
+/** if there were any errors currently write them to a log file. 
+ *	Later it will be changed to submit them to the error reporting server.
+ */
+if($GLOBALS['error_handler']->hasErrors()) {
+	// then log them in a local file.
+	$path = "./pma_error_log.txt";
+	$fp = fopen($path, "w");
+
+	// for each errors in the list
+	foreach($GLOBALS['error_handler']->getCurrentErrors() as $errObj ) {
+		/**
+		 * Following check is to avoid error reported by PMA_warnMissingExtension();
+		 * 
+		 */ 
+		if ($errObj->getLine() && $errObj->getType()) {
+			$str = "\n\n".$errObj->getFile() . "(#"  . $errObj->getLine() . ")\n\t". $errObj->getTitle();
+			// for stack trace
+			// ------------------------------------------------------------------------------------------
+			$backtrace = $errObj->getBacktrace();
+			$error_str= "";	
+			foreach($backtrace as $i=>$stack_frame)
+			{
+			 	$error_str .= "\n \t\t\t Frame[".$i."]: \tfile:".$stack_frame["file"]."\tline:".$stack_frame["line"]."\tfunction:".$stack_frame["function"]."(";
+		 		foreach($stack_frame["args"] as $j=>$arg)
+		 		{
+		 			if($j != 0)
+		 			{
+		 				$error_str .= ", ";
+		 			}
+
+		 			$error_str .= "arg[".$j."] = ".$arg;
+		 		}
+		 		$error_str .= ")";
+			 	
+			 	if($i >= 5)	// MAX 5 of stack frames.
+			 	{
+			 		break;
+			 	}
+			 }
+			// ------------------------------------------------------------------------------------------
+			$retVal = fwrite ($fp , $str);
+			$retVal = fwrite ($fp , $error_str);
+		}
+	}
+	fclose($fp);
+}
+?>
