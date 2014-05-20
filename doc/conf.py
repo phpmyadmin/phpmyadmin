634307329eb6130e43ab1f3bfd1a802c278a88d1
Marc Delisle <marc@infomarc.info>
2014-05-20 08:28:42 -0400

    4.2.3-dev
    Signed-off-by: Marc Delisle <marc@infomarc.info>

--- doc/conf.py
+++ doc/conf.py
@@ -51,7 +51,7 @@ copyright = u'2012 - 2014, The phpMyAdmin devel team'
 # built documents.
 #
 # The short X.Y version.
-version = '4.2.2-dev'
+version = '4.2.3-dev'
 # The full version, including alpha/beta/rc tags.
 release = version
 
