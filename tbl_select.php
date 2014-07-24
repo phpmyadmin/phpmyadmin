484a85a51b1684262a953ba5e1646b689d2feee0
Marc Delisle <marc@infomarc.info>
2014-07-24 05:11:00 -0700

    Clarify variable's origin
    Signed-off-by: Marc Delisle <marc@infomarc.info>

--- tbl_select.php
+++ tbl_select.php
@@ -73,9 +73,9 @@ if (! isset($_POST['columnsToDisplay']) && ! isset($_POST['displayAllColumns']))
     include_once 'libraries/parse_analyze.inc.php';
 
     PMA_executeQueryAndSendQueryResponse(
-        $analyzed_sql_results, false, $db, $table, null, null, null, false, null,
-        null, null, null, $goto, $pmaThemeImage, null, null, null, $sql_query,
-        null, null
+        $analyzed_sql_results, false, $db, $table, null, null, null, false,
+        null, null, null, null, $GLOBALS['goto'], $pmaThemeImage, null,
+        null, null, $sql_query, null, null
     );
 }
 ?>
