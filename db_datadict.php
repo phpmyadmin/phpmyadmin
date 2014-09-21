27b9e16dc6b3f5708d6e9d6857cae500015e9dd2
Marc Delisle <marc@infomarc.info>
2014-09-21 07:34:44 -0400

    Remove unneeded code
    Signed-off-by: Marc Delisle <marc@infomarc.info>

--- db_datadict.php
+++ db_datadict.php
@@ -36,11 +36,7 @@ PMA_Util::checkParameters(array('db'));
 /**
  * Defines the url to return to in case of error in a sql statement
  */
-if ($GLOBALS['PMA_String']->strlen($table)) {
-    $err_url = 'tbl_sql.php?' . PMA_URL_getCommon($db, $table);
-} else {
-    $err_url = 'db_sql.php?' . PMA_URL_getCommon($db);
-}
+$err_url = 'db_sql.php?' . PMA_URL_getCommon($db);
 
 if ($cfgRelation['commwork']) {
     $comment = PMA_getDbComment($db);
