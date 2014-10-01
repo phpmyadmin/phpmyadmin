5118938f103e0022329847b654fcd99c8cb37de7
Marc Delisle <marc@infomarc.info>
2014-10-01 07:20:09 -0400

    4.0.10.4 release
    Signed-off-by: Marc Delisle <marc@infomarc.info>

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -102,7 +102,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '4.0.10.3');
+        $this->set('PMA_VERSION', '4.0.10.4');
         /**
          * @deprecated
          */
