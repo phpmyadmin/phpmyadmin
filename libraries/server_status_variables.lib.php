phpmyadmin/phpmyadmin

commit f9cd1a1a227d193dc8a70decb8e59607deb3e165

Replace mb functions by standard string functions.
Signed-off-by: Hugues Peccatte <hugues.peccatte@gmail.com>

libraries/server_status_variables.lib.php
@@ -253,9 +253,9 @@ function PMA_getHtmlForRenderVariables($ServerStatusData, $alerts, $strShowStatu
                 $retval .= '<span class="allfine">';
             }
         }
-        if ('%' === /*overload*/mb_substr($name, -1, 1)) {
+        if (substr($name, -1)) {
             $retval .= htmlspecialchars(PMA_Util::formatNumber($value, 0, 2)) . ' %';
-        } elseif (/*overload*/mb_strpos($name, 'Uptime') !== false) {
+        } elseif (strpos($name, 'Uptime') !== false) {
             $retval .= htmlspecialchars(
                 PMA_Util::timespanFormat($value)
             );

libraries/Config.class.php
@@ -487,8 +487,8 @@ class PMA_Config
         $commit = false;
         if (! isset($_SESSION['PMA_VERSION_COMMITDATA_' . $hash])) {
             $git_file_name = $git_folder . '/objects/'
-                . /*overload*/mb_substr($hash, 0, 2)
-                . '/' . /*overload*/mb_substr($hash, 2);
+                . substr($hash, 0, 2)
+                . '/' . substr($hash, 2);
             if (file_exists($git_file_name) ) {
                 if (! $commit = @file_get_contents($git_file_name)) {
                     return;
@@ -536,7 +536,7 @@ class PMA_Config
                         }
                     }
                 }
-                $hash = /*overload*/mb_strtolower($hash);
+                $hash = strtolower($hash);
                 foreach ($pack_names as $pack_name) {
                     $index_name = str_replace('.pack', '.idx', $pack_name);
 
@@ -563,7 +563,7 @@ class PMA_Config
                     );
 
                     // find where we should search
-                    $firstbyte = intval(/*overload*/mb_substr($hash, 0, 2), 16);
+                    $firstbyte = intval(substr($hash, 0, 2), 16);
                     // array is indexed from 1 and we need to get
                     // previous entry for start
                     if ($firstbyte == 0) {
@@ -577,7 +577,7 @@ class PMA_Config
                     $found = false;
                     $offset = 8 + (256 * 4);
                     for ($position = $start; $position < $end; $position++) {
-                        $sha = /*overload*/mb_strtolower(
+                        $sha = strtolower(
                             bin2hex(
                                 /*overload*/mb_substr(
                                     $index_data, $offset + ($position * 20), 20

libraries/Error.class.php
@@ -437,7 +437,7 @@ class PMA_Error extends PMA_Message
     {
         $dest = realpath($dest);
 
-        if (/*overload*/mb_substr(PHP_OS, 0, 3) == 'WIN') {
+        if (substr(PHP_OS, 0, 3) == 'WIN') {
             $separator = '\\';
         } else {
             $separator = '/';

libraries/TableSearch.class.php
@@ -409,10 +409,10 @@ EOT;
             if ($column_type == 'date') {
                 $the_class .= ' datefield';
             } elseif ($column_type == 'datetime'
-                || /*overload*/mb_substr($column_type, 0, 9) == 'timestamp'
+                || substr($column_type, 0, 9) == 'timestamp'
             ) {
                 $the_class .= ' datetimefield';
-            } elseif (/*overload*/mb_substr($column_type, 0, 3) == 'bit') {
+            } elseif (substr($column_type, 0, 3) == 'bit') {
                 $the_class .= ' bit';
             }
 

libraries/Tracker.class.php
@@ -759,7 +759,7 @@ class PMA_Tracker
 
         // Parse DROP VIEW statement
         if (! isset($result['identifier'])
-            && /*overload*/mb_substr($query, 0, 10) == 'DROP VIEW '
+            && substr($query, 0, 10) == 'DROP VIEW '
         ) {
             $result['identifier'] = 'DROP VIEW';
 
@@ -774,7 +774,7 @@ class PMA_Tracker
 
         // Parse CREATE DATABASE statement
         if (! isset($result['identifier'])
-            && /*overload*/mb_substr($query, 0, 15) == 'CREATE DATABASE'
+            && substr($query, 0, 15) == 'CREATE DATABASE'
         ) {
             $result['identifier'] = 'CREATE DATABASE';
             $str = str_replace('CREATE DATABASE', '', $query);
@@ -788,7 +788,7 @@ class PMA_Tracker
 
         // Parse ALTER DATABASE statement
         if (! isset($result['identifier'])
-            && /*overload*/mb_substr($query, 0, 14) == 'ALTER DATABASE'
+            && substr($query, 0, 14) == 'ALTER DATABASE'
         ) {
             $result['identifier'] = 'ALTER DATABASE';
             $result['tablename'] = '';
@@ -796,7 +796,7 @@ class PMA_Tracker
 
         // Parse DROP DATABASE statement
         if (! isset($result['identifier'])
-            && /*overload*/mb_substr($query, 0, 13) == 'DROP DATABASE'
+            && substr($query, 0, 13) == 'DROP DATABASE'
         ) {
             $result['identifier'] = 'DROP DATABASE';
             $str = str_replace('DROP DATABASE', '', $query);
@@ -807,7 +807,7 @@ class PMA_Tracker
 
         // Parse CREATE TABLE statement
         if (! isset($result['identifier'])
-            && /*overload*/mb_substr($query, 0, 12) == 'CREATE TABLE'
+            && substr($query, 0, 12) == 'CREATE TABLE'
         ) {
             $result['identifier'] = 'CREATE TABLE';
             $query   = str_replace('IF NOT EXISTS', '', $query);
@@ -818,7 +818,7 @@ class PMA_Tracker
 
         // Parse ALTER TABLE statement
         if (! isset($result['identifier'])
-            && /*overload*/mb_substr($query, 0, 12) == 'ALTER TABLE '
+            && substr($query, 0, 12) == 'ALTER TABLE '
         ) {
             $result['identifier'] = 'ALTER TABLE';
 
@@ -829,7 +829,7 @@ class PMA_Tracker
 
         // Parse DROP TABLE statement
         if (! isset($result['identifier'])
-            && /*overload*/mb_substr($query, 0, 11) == 'DROP TABLE '
+            && substr($query, 0, 11) == 'DROP TABLE '
         ) {
             $result['identifier'] = 'DROP TABLE';
 
@@ -844,9 +844,9 @@ class PMA_Tracker
 
         // Parse CREATE INDEX statement
         if (! isset($result['identifier'])
-            && (/*overload*/mb_substr($query, 0, 12) == 'CREATE INDEX'
-            || /*overload*/mb_substr($query, 0, 19) == 'CREATE UNIQUE INDEX'
-            || /*overload*/mb_substr($query, 0, 20) == 'CREATE SPATIAL INDEX')
+            && (substr($query, 0, 12) == 'CREATE INDEX'
+            || substr($query, 0, 19) == 'CREATE UNIQUE INDEX'
+            || substr($query, 0, 20) == 'CREATE SPATIAL INDEX')
         ) {
              $result['identifier'] = 'CREATE INDEX';
              $prefix = explode('ON ', $query);
@@ -856,7 +856,7 @@ class PMA_Tracker
 
         // Parse DROP INDEX statement
         if (! isset($result['identifier'])
-            && /*overload*/mb_substr($query, 0, 10) == 'DROP INDEX'
+            && substr($query, 0, 10) == 'DROP INDEX'
         ) {
              $result['identifier'] = 'DROP INDEX';
              $prefix = explode('ON ', $query);
@@ -865,7 +865,7 @@ class PMA_Tracker
 
         // Parse RENAME TABLE statement
         if (! isset($result['identifier'])
-            && /*overload*/mb_substr($query, 0, 13) == 'RENAME TABLE '
+            && substr($query, 0, 13) == 'RENAME TABLE '
         ) {
             $result['identifier'] = 'RENAME TABLE';
             $prefix = explode('RENAME TABLE ', $query);
@@ -883,7 +883,7 @@ class PMA_Tracker
         }
         // Parse UPDATE statement
         if (! isset($result['identifier'])
-            && /*overload*/mb_substr($query, 0, 6) == 'UPDATE'
+            && substr($query, 0, 6) == 'UPDATE'
         ) {
             $result['identifier'] = 'UPDATE';
             $prefix  = explode('UPDATE ', $query);
@@ -893,7 +893,7 @@ class PMA_Tracker
 
         // Parse INSERT INTO statement
         if (! isset($result['identifier'])
-            && /*overload*/mb_substr($query, 0, 11) == 'INSERT INTO'
+            && substr($query, 0, 11) == 'INSERT INTO'
         ) {
             $result['identifier'] = 'INSERT';
             $prefix  = explode('INSERT INTO', $query);
@@ -903,7 +903,7 @@ class PMA_Tracker
 
         // Parse DELETE statement
         if (! isset($result['identifier'])
-            && /*overload*/mb_substr($query, 0, 6) == 'DELETE'
+            && substr($query, 0, 6) == 'DELETE'
         ) {
             $result['identifier'] = 'DELETE';
             $prefix  = explode('FROM ', $query);
@@ -913,7 +913,7 @@ class PMA_Tracker
 
         // Parse TRUNCATE statement
         if (! isset($result['identifier'])
-            && /*overload*/mb_substr($query, 0, 8) == 'TRUNCATE'
+            && substr($query, 0, 8) == 'TRUNCATE'
         ) {
             $result['identifier'] = 'TRUNCATE';
             $prefix  = explode('TRUNCATE', $query);
@@ -940,7 +940,7 @@ class PMA_Tracker
             return;
         }
 
-        if (! (/*overload*/mb_substr($query, -1) == ';')) {
+        if (! (substr($query, -1) == ';')) {
             $query = $query . ";\n";
         }
         // Get some information about query

libraries/Util.class.php
@@ -3054,7 +3054,7 @@ class PMA_Util
      */
     public static function isForeignKeySupported($engine)
     {
-        $engine = /*overload*/mb_strtoupper($engine);
+        $engine = strtoupper($engine);
         if (($engine == 'INNODB') || ($engine == 'PBXT')) {
             return true;
         } elseif ($engine == 'NDBCLUSTER' || $engine == 'NDB') {

libraries/display_change_password.lib.php
@@ -40,7 +40,7 @@ function PMA_getHtmlForChangePassword($username, $hostname)
 
     $html .= PMA_URL_getHiddenInputs();
 
-    if (/*overload*/mb_strpos($GLOBALS['PMA_PHP_SELF'], 'server_privileges') !== false) {
+    if (strpos($GLOBALS['PMA_PHP_SELF'], 'server_privileges') !== false) {
         $html .= '<input type="hidden" name="username" '
             . 'value="' . htmlspecialchars($username) . '" />'
             . '<input type="hidden" name="hostname" '

libraries/display_git_revision.lib.php
@@ -26,7 +26,7 @@ function PMA_printGitRevision()
     $GLOBALS['PMA_Config']->checkGitRevision();
 
     // if using a remote commit fast-forwarded, link to GitHub
-    $commit_hash = /*overload*/mb_substr(
+    $commit_hash = substr(
         $GLOBALS['PMA_Config']->get('PMA_VERSION_GIT_COMMITHASH'),
         0,
         7

commit ebcc6c62f20881f7a6bdc53784a223c70d568c90

Fix Status->All status variables to display correct suffix and formatting.
Broken by f9cd1a1a227d193dc8a70decb8e59607deb3e165

Signed-off-by: Daniel Rix <drainx1@live.com>

libraries/server_status_variables.lib.php
@@ -253,7 +253,7 @@ function PMA_getHtmlForRenderVariables($ServerStatusData, $alerts, $strShowStatu
                 $retval .= '<span class="allfine">';
             }
         }
-        if (substr($name, -1)) {
+        if (substr($name, -1) === '%') {
             $retval .= htmlspecialchars(PMA_Util::formatNumber($value, 0, 2)) . ' %';
         } elseif (strpos($name, 'Uptime') !== false) {
             $retval .= htmlspecialchars(