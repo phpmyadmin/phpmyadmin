da44dd4fd7432b915203e3e723a4534a01c12cd9
Madhura Jayaratne <madhura.cj@gmail.com>
2014-11-17 13:05:59 +0530

    bug #4595 [security] Path traversal can lead to leakage of line count
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

--- libraries/error_report.lib.php
+++ libraries/error_report.lib.php
@@ -181,6 +181,20 @@ function PMA_countLines($filename)
 {
     global $LINE_COUNT;
     if (!defined('LINE_COUNTS')) {
+
+        // ensure that the file is inside the phpMyAdmin folder
+        $depath = 1;
+        foreach (explode('/', $filename) as $part) {
+            if ($part == '..') {
+                $depath--;
+            } elseif ($part != '.') {
+                $depath++;
+            }
+            if ($depath < 0) {
+                return 0;
+            }
+        }
+
         $linecount = 0;
         $handle = fopen('./js/' . $filename, 'r');
         while (!feof($handle)) {
