9e9d9c08c7e3e980a6bc2b54e9d2ad0499e9ef09
Marc Delisle <marc@infomarc.info>
2014-11-26 07:46:06 -0500

    Fix typos
    Signed-off-by: Marc Delisle <marc@infomarc.info>

--- libraries/plugins/auth/AuthenticationCookie.class.php
+++ libraries/plugins/auth/AuthenticationCookie.class.php
@@ -62,7 +62,7 @@ class AuthenticationCookie extends AuthenticationPlugin
         $response = PMA_Response::getInstance();
         if ($response->isAjax()) {
             $response->isSuccess(false);
-            // redirect_flag redirects to the loging page
+            // redirect_flag redirects to the login page
             $response->addJSON('redirect_flag', '1');
             if (defined('TESTSUITE')) {
                 return true;
