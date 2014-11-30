942f366e71605bb423d786523ad086396cf62afb
Marc Delisle <marc@infomarc.info>
2014-11-30 06:32:49 -0500

    4.2.13 release
    Signed-off-by: Marc Delisle <marc@infomarc.info>

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -114,7 +114,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '4.2.13-dev');
+        $this->set('PMA_VERSION', '4.2.13');
         /**
          * @deprecated
          */
