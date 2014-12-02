62b2c918d26cc78d1763945e3d44d1a63294a819
Madhura Jayaratne <madhura.cj@gmail.com>
2014-12-02 21:09:28 +0530

    bug #4611 [security] DOS attack with long passwords
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

--- libraries/common.inc.php
+++ libraries/common.inc.php
@@ -856,6 +856,9 @@ if (! defined('PMA_MINIMUM_COMMON')) {
                 . ' ' . $cfg['Server']['auth_type']
             );
         }
+        if (isset($_REQUEST['pma_password'])) {
+            $_REQUEST['pma_password'] = substr($_REQUEST['pma_password'], 0, 256);
+        }
         include_once  './libraries/plugins/auth/' . $auth_class . '.class.php';
         // todo: add plugin manager
         $plugin_manager = null;
@@ -984,6 +987,8 @@ if (! defined('PMA_MINIMUM_COMMON')) {
         if (! $controllink) {
             $controllink = $userlink;
         }
+
+        $auth_plugin->storeUserCredentials();
 
         /* Log success */
         PMA_logUser($cfg['Server']['user']);
