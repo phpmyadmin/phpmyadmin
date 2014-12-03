cc0c82e2a3b3806ac4df76e89a3d52e4bf8d004d
Marc Delisle <marc@infomarc.info>
2014-12-03 09:09:50 -0500

    4.3.0-rc2 release
    Signed-off-by: Marc Delisle <marc@infomarc.info>

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -114,7 +114,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '4.3.0-rc1');
+        $this->set('PMA_VERSION', '4.3.0-rc2');
         /**
          * @deprecated
          */
