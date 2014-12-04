244021ada8a9c41b2dc9f2914d69003a9171e0ff
Madhura Jayaratne <madhura.cj@gmail.com>
2014-12-04 17:03:40 +0530

    Update tablesorter for jquery 1.9+ since there are no new releases of tablesorter
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

--- js/jquery/jquery.tablesorter.js
+++ js/jquery/jquery.tablesorter.js
@@ -861,7 +861,7 @@
                 return /^[-+]?\d*$/.test($.trim(s.replace(/[,.']/g, '')));
             };
             this.clearTableBody = function (table) {
-                if ($.browser.msie) {
+                if (window.navigator.userAgent.indexOf("MSIE ") > 0) {
                     function empty() {
                         while (this.firstChild)
                         this.removeChild(this.firstChild);
