be03cb3f27d151a085c2b556dd94e8c291b5d114
Marc Delisle <marc@infomarc.info>
2015-02-02 06:25:37 +0100

    Remove dead code
    Signed-off-by: Marc Delisle <marc@infomarc.info>

--- libraries/plugins/auth/AuthenticationHttp.class.php
+++ libraries/plugins/auth/AuthenticationHttp.class.php
@@ -271,10 +271,9 @@ class AuthenticationHttp extends AuthenticationPlugin
      *
      * @param string $password New password to set
      *
-     * @return array Additional URL parameters.
+     * @return void 
      */
     public function handlePasswordChange($password)
     {
-        return array('old_usr' => 'relog');
     }
 }
