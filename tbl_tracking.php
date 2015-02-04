4e3cb71a4c786e33b00499e072349d2190c11fbc
Madhura Jayaratne <madhura.cj@gmail.com>
2015-02-04 12:38:43 +0530

    bug #4745 Tracking does not handle views properly
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

--- tbl_tracking.php
+++ tbl_tracking.php
@@ -143,8 +143,9 @@ if ($last_version > 0) {
     );
 }
 
+$type = PMA_Table::isView($GLOBALS['db'], $GLOBALS['table']) ? 'view' : 'table';
 $html .= PMA_getHtmlForDataDefinitionAndManipulationStatements(
-    $url_query, $last_version
+    $url_query, $last_version, $type
 );
 
 $html .= '<br class="clearfloat"/>';
