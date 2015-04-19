bce99300db5b34991e3e300bef5ac843a1fbb0a1
Pranav Sharma <vanarpsharma@gmail.com>
2015-04-20 00:08:39 +0530

    Update progress_sess.php
    Signed-off-by: Pranav Sharma vanarpsharma@gmail.com

--- progress_sess.php
+++ progress_sess.php
@@ -2,12 +2,23 @@
 header("Content-Type: application/json");
 session_id($_COOKIE['phpMyAdmin']);
 session_start();
+if(isset($_SESSION['export_progress']))
+{
 $result = $_SESSION['export_progress'];
-$percentage = $_SESSION['percentage'];
-if ($result==null)
+}
+else
 {
 $result = '';
 }
+if(isset($_SESSION['percentage']))
+{
+$percentage = $_SESSION['percentage'];
+}
+else
+{
+$percentage = '';
+}
 $arr = array('progress_result'=>$result, 'percentage'=>$percentage);
 echo json_encode($arr);
+session_write_close();
 ?>
