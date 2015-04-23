e866ad6e2fe13a77dbd1561ba13bee385b017a19
Pranav Sharma <vanarpsharma@gmail.com>
2015-04-24 02:37:24 +0530

    Last request sets session variables to "".
    Last request sets session variables to null. Added checks for that.
    Signed-off-by: Pranav Sharma vanarpsharma@gmail.com

--- progress_sess.php
+++ progress_sess.php
@@ -5,25 +5,27 @@ if(!isset($_SESSION))
 {
     session_start();
 }
-if(isset($_SESSION['export_progress']))
-{
-$result = $_SESSION['export_progress'];
+if(isset($_REQUEST['done_status'])) {
+    $_SESSION['export_progress'] = "";
+    $_SESSION['percentage']="";
 }
-else
-{
-$result = '';
+else {
+    if(isset($_SESSION['export_progress'])) {
+        $result = $_SESSION['export_progress'];
+    }
+    else {
+        $result = '';
+    }
+    if(isset($_SESSION['percentage'])) {
+        $percentage = $_SESSION['percentage'];
+    }
+    else {
+        $percentage = '';
+    }
+    $response = PMA_Response::getInstance();
+    $response->disable();
+    $response->addJSON("progress_result", $result);
+    $response->addJSON("percentage", $percentage);
 }
-if(isset($_SESSION['percentage']))
-{
-$percentage = $_SESSION['percentage'];
-}
-else
-{
-$percentage = '';
-}
-$response = PMA_Response::getInstance();
-$response->disable();
-$response->addJSON("progress_result", $result);
-$response->addJSON("percentage", $percentage);
 session_write_close();
 ?>
