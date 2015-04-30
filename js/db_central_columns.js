f07fc445929fcbc91cc983a810121d4856df70ba
Madhura Jayaratne <madhura.cj@gmail.com>
2015-04-30 10:07:21 +0530

    bug #4878 Column list of central columns is not cleared
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

--- js/db_central_columns.js
+++ js/db_central_columns.js
@@ -151,7 +151,7 @@ AJAX.registerOnload('db_central_columns.js', function () {
     });
     $('#table-select').change(function(e) {
         var selectvalue = $(this).val();
-        var default_column_select = $('#column-select').html();
+        var default_column_select = $('#column-select option:first');
         var href = "db_central_columns.php";
         var params = {
             'ajax_request' : true,
@@ -163,7 +163,7 @@ AJAX.registerOnload('db_central_columns.js', function () {
         $('#column-select').html('<option value="">' + PMA_messages.strLoading + '</option>');
         if (selectvalue !== "") {
             $.post(href, params, function (data) {
-                $('#column-select').html(default_column_select);
+                $('#column-select').empty().append(default_column_select);
                 $('#column-select').append(data.message);
             });
         }
