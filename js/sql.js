470d61abafc0f32293374eeee26696a3c482566c
Madhura Jayaratne <madhura.cj@gmail.com>
2015-05-08 15:40:47 +0530

    bug #4894 Deleting without confirmation
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

--- js/sql.js
+++ js/sql.js
@@ -155,14 +155,18 @@ AJAX.registerOnload('sql.js', function () {
         var $link = $(this);
         $link.PMA_confirm(question, $link.attr('href'), function (url) {
             $msgbox = PMA_ajaxShowMessage();
-            $.get(url, {'ajax_request': true, 'is_js_confirmed': true}, function (data) {
-                if (data.success) {
-                    PMA_ajaxShowMessage(data.message);
-                    $link.closest('tr').remove();
-                } else {
-                    PMA_ajaxShowMessage(data.error, false);
-                }
-            });
+            if ($link.hasClass('formLinkSubmit')) {
+                submitFormLink($link);
+            } else {
+                $.get(url, {'ajax_request': true, 'is_js_confirmed': true}, function (data) {
+                    if (data.success) {
+                        PMA_ajaxShowMessage(data.message);
+                        $link.closest('tr').remove();
+                    } else {
+                        PMA_ajaxShowMessage(data.error, false);
+                    }
+                });
+            }
         });
     });
 
