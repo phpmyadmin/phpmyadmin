aa78a548031f2dc0ad4fb2346dc0b276dab45dee
Madhura Jayaratne <madhura.cj@gmail.com>
2015-05-20 16:00:55 +0530

    Add back phpMyAdmin specific colors
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

--- themes/pmahomme/css/codemirror.css.php
+++ themes/pmahomme/css/codemirror.css.php
@@ -21,3 +21,37 @@ if (! defined('PMA_MINIMUM_COMMON') && ! defined('TESTSUITE')) {
 #inline_editor_outer .CodeMirror {
     height: <?php echo ceil($GLOBALS['cfg']['TextareaRows'] * 0.4); ?>em;
 }
+
+span.cm-keyword, span.cm-statement-verb {
+    color: #909;
+}
+span.cm-variable {
+    color: black;
+}
+span.cm-comment {
+    color: #808000;
+}
+span.cm-mysql-string {
+    color: #008000;
+}
+span.cm-operator {
+    color: fuchsia;
+}
+span.cm-mysql-word {
+    color: black;
+}
+span.cm-builtin {
+    color: #f00;
+}
+span.cm-variable-2 {
+    color: #f90;
+}
+span.cm-variable-3 {
+    color: #00f;
+}
+span.cm-separator {
+    color: fuchsia;
+}
+span.cm-number {
+    color: teal;
+}
