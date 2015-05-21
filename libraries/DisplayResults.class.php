a59ffb96540293eae9f102f3f024a5c3fe0e6788
Madhura Jayaratne <madhura.cj@gmail.com>
2015-05-21 19:01:57 +0530

    #4912 "Highlight pointer" and "Row marker" doesn't work properly
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

--- libraries/DisplayResults.class.php
+++ libraries/DisplayResults.class.php
@@ -2621,11 +2621,18 @@ class PMA_DisplayResults
                 );
             }
 
-            $alternating_color_class = ($odd_row ? 'odd' : 'even');
+            $tr_class = array();
+            if ($GLOBALS['cfg']['BrowsePointerEnable'] != true) {
+                $tr_class[] = 'nopointer';
+            }
+            if ($GLOBALS['cfg']['BrowseMarkerEnable'] != true) {
+                $tr_class[] = 'nomarker';
+            }
+            $tr_class[] = ($odd_row ? 'odd' : 'even');
             $odd_row = ! $odd_row;
 
             // pointer code part
-            $table_body_html .= '<tr class="' . $alternating_color_class . '">';
+            $table_body_html .= '<tr class="' . implode(' ', $tr_class) . '">';
 
             // 1. Prepares the row
             // 1.1 Results from a "SELECT" statement -> builds the
