a3225dc1edc6f654e6f83fcc13b85193b477f32b
Madhura Jayaratne <madhura.cj@gmail.com>
2015-05-27 12:48:10 +0530

    Fix height issue for SQL editor input transformations
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

--- js/transformations/sql_editor.js
+++ js/transformations/sql_editor.js
@@ -6,10 +6,7 @@
  */
 AJAX.registerOnload('transformations/sql_editor.js', function() {
 
-    $.each($('textarea.transform_sql_editor'), function (i, e) {
-        var height = $(e).css('height');
-        var codemirror_editor = PMA_getSQLEditor($(e));
-        codemirror_editor.getScrollerElement().style.height = height;
-        codemirror_editor.refresh();
+    $('textarea.transform_sql_editor').each(function () {
+        PMA_getSQLEditor($(this));
     });
 });
