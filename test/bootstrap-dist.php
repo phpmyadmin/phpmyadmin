76349057620b68eb5b079d76acde5e8d78c369bc
Isaac Bennetch <bennetch@gmail.com>
2015-05-30 00:05:23 -0400

    Feature request 1675: MySQL and MariaDB functions INET6_ATON and INET6_NTOA
    Signed-off-by: Isaac Bennetch <bennetch@gmail.com>

--- test/bootstrap-dist.php
+++ test/bootstrap-dist.php
@@ -21,6 +21,7 @@ set_include_path(
 define('PHPMYADMIN', 1);
 define('TESTSUITE', 1);
 define('PMA_MYSQL_INT_VERSION', 55000);
+define('PMA_MYSQL_STR_VERSION', '5.50.00');
 
 // Selenium tests setup
 $test_defaults = array(
