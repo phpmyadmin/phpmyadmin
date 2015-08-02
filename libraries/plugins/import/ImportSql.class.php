4a2e04d9a7fd07c8f53122f03481f306d7a0757b
Dan Ungureanu <udan1107@gmail.com>
2015-08-02 22:23:06 +0300

    Removed more dead code.
    Signed-off-by: Dan Ungureanu <udan1107@gmail.com>

--- libraries/plugins/import/ImportSql.class.php
+++ libraries/plugins/import/ImportSql.class.php
@@ -26,61 +26,6 @@ class ImportSql extends ImportPlugin
     const READ_MB_TRUE = 1;
 
     /**
-     * @var string SQL delimiter
-     */
-    private $_delimiter;
-
-    /**
-     * @var int SQL delimiter length
-     */
-    private $_delimiterLength;
-
-    /**
-     * @var bool|int SQL delimiter position or false if not found
-     */
-    private $_delimiterPosition = false;
-
-    /**
-     * @var int Query start position
-     */
-    private $_queryBeginPosition = 0;
-
-    /**
-     * @var int|false First special chars position or false if not found
-     */
-    private $_firstSearchChar = null;
-
-    /**
-     * @var bool Current position is in string
-     */
-    private $_isInString = false;
-
-    /**
-     * @var string Quote of current string or null if out of string
-     */
-    private $_quote = null;
-
-    /**
-     * @var bool Current position is in comment
-     */
-    private $_isInComment = false;
-
-    /**
-     * @var string Current comment opener
-     */
-    private $_openingComment = null;
-
-    /**
-     * @var bool Current position is in delimiter definition
-     */
-    private $_isInDelimiter = false;
-
-    /**
-     * @var string Delimiter keyword
-     */
-    private $_delimiterKeyword = 'DELIMITER ';
-
-    /**
      * @var int Import should be done using multibytes
      */
     private $_readMb = self::READ_MB_FALSE;
@@ -321,8 +266,6 @@ class ImportSql extends ImportPlugin
     {
         $this->_data = ltrim($data);
         $this->_dataLength = $this->_stringFctToUse['strlen']($this->_data);
-        $this->_queryBeginPosition = 0;
-        $this->_delimiterPosition = 0;
 
         return $this->_dataLength;
     }
