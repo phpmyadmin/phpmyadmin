dda7139f199205d4f84c1398dc210b09bab2bf2d
Marc Delisle <marc@infomarc.info>
2015-08-06 14:35:01 -0400

    Add missing dummy queries
    Signed-off-by: Marc Delisle <marc@infomarc.info>

--- libraries/dbi/DBIDummy.class.php
+++ libraries/dbi/DBIDummy.class.php
@@ -615,6 +615,30 @@ $GLOBALS['dummy_queries'] = array(
         'query' => "SELECT TABLE_NAME FROM information_schema.VIEWS WHERE "
             . "TABLE_SCHEMA = 'db' AND TABLE_NAME = 'table'",
         'result' => array()
+    ),
+    array(
+        'query' => "SELECT *, `TABLE_SCHEMA` AS `Db`, "
+            . "`TABLE_NAME` AS `Name`, `TABLE_TYPE` AS `TABLE_TYPE`, "
+            . "`ENGINE` AS `Engine`, `ENGINE` AS `Type`, "
+            . "`VERSION` AS `Version`, `ROW_FORMAT` AS `Row_format`, "
+            . "`TABLE_ROWS` AS `Rows`, `AVG_ROW_LENGTH` AS `Avg_row_length`, "
+            . "`DATA_LENGTH` AS `Data_length`, "
+            . "`MAX_DATA_LENGTH` AS `Max_data_length`, "
+            . "`INDEX_LENGTH` AS `Index_length`, `DATA_FREE` AS `Data_free`, "
+            . "`AUTO_INCREMENT` AS `Auto_increment`, "
+            . "`CREATE_TIME` AS `Create_time`, "
+            . "`UPDATE_TIME` AS `Update_time`, `CHECK_TIME` AS `Check_time`, "
+            . "`TABLE_COLLATION` AS `Collation`, `CHECKSUM` AS `Checksum`, "
+            . "`CREATE_OPTIONS` AS `Create_options`, "
+            . "`TABLE_COMMENT` AS `Comment` "
+            . "FROM `information_schema`.`TABLES` t "
+            . "WHERE BINARY `TABLE_SCHEMA` IN ('db') "
+            . "AND t.`TABLE_NAME` = 'table' ORDER BY Name ASC",
+        'result' => array()
+    ),
+    array(
+        'query' => "SHOW TABLE STATUS FROM `db` WHERE `Name` LIKE 'table%'",
+        'result' => array()
     )
 );
 /**
