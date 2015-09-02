phpmyadmin/phpmyadmin

commit 788c96e8c744c66619cde6fc887b666d581282e6

Fix #11442 MySQL 5.7 and SHOW VARIABLES
Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

libraries/DatabaseInterface.class.php
@@ -323,7 +323,7 @@ class PMA_DatabaseInterface
             'server_language',
             function () {
                 return $GLOBALS['dbi']->fetchValue(
-                    "SHOW VARIABLES LIKE 'language';", 0, 1
+                    "SELECT @@language;", 0, 1
                 );
             }
         );

libraries/Table.class.php
@@ -827,7 +827,7 @@ class PMA_Table
             if (!PMA_DRIZZLE) {
                 SqlParser\Context::setMode(
                     $GLOBALS['dbi']->fetchValue(
-                        "SHOW VARIABLES LIKE 'sql_mode'", 0, 1
+                        "SELECT @@sql_mode", 0, 1
                     )
                 );
             }

libraries/Util.class.php
@@ -1361,7 +1361,7 @@ class PMA_Util
             // (avoid a trip to the server for MySQL before 5.0.37)
             // and do not set a constant as we might be switching servers
             if (defined('PMA_MYSQL_INT_VERSION')
-                && $GLOBALS['dbi']->fetchValue("SHOW VARIABLES LIKE 'profiling'")
+                && $GLOBALS['dbi']->fetchValue("SELECT @@profiling")
             ) {
                 self::cacheSet('profiling_supported', true);
             } else {
@@ -3196,7 +3196,7 @@ class PMA_Util
             return true;
         } elseif ($engine == 'NDBCLUSTER' || $engine == 'NDB') {
             $ndbver = $GLOBALS['dbi']->fetchValue(
-                "SHOW VARIABLES LIKE 'ndb_version_string'"
+                "SELECT @@ndb_version_string"
             );
             return ($ndbver >= 7.3);
         } else {
@@ -4651,7 +4651,7 @@ class PMA_Util
             'lower_case_table_names',
             function () {
                 return $GLOBALS['dbi']->fetchValue(
-                    "SHOW VARIABLES LIKE 'lower_case_table_names'", 0, 1
+                    "SELECT @@lower_case_table_names", 0, 1
                 );
             }
         );

libraries/mysql_charsets.lib.php
@@ -127,7 +127,7 @@ function PMA_getDbCollation($db)
     } else {
         $GLOBALS['dbi']->selectDb($db);
         $return = $GLOBALS['dbi']->fetchValue(
-            'SHOW VARIABLES LIKE \'collation_database\'', 0, 1
+            'SELECT @@collation_database', 0, 1
         );
         if ($db !== $GLOBALS['db']) {
             $GLOBALS['dbi']->selectDb($GLOBALS['db']);
@@ -144,7 +144,7 @@ function PMA_getDbCollation($db)
 function PMA_getServerCollation()
 {
     return $GLOBALS['dbi']->fetchValue(
-        'SHOW VARIABLES LIKE \'collation_server\'', 0, 1
+        'SELECT @@collation_server', 0, 1
     );
 }
 

libraries/operations.lib.php
@@ -362,7 +362,7 @@ function PMA_createDbBeforeCopy()
     // lower_case_table_names=1 `DB` becomes `db`
     if (! PMA_DRIZZLE) {
         $lowerCaseTableNames = $GLOBALS['dbi']->fetchValue(
-            'SHOW VARIABLES LIKE "lower_case_table_names"', 0, 1
+            'SELECT @@lower_case_table_names', 0, 1
         );
         if ($lowerCaseTableNames === '1') {
             $_REQUEST['newname'] = /*overload*/mb_strtolower(

libraries/plugins/import/ImportLdi.class.php
@@ -47,7 +47,7 @@ class ImportLdi extends AbstractImportCsv
             $GLOBALS['cfg']['Import']['ldi_local_option'] = false;
 
             $result = $GLOBALS['dbi']->tryQuery(
-                'SHOW VARIABLES LIKE \'local\\_infile\';'
+                'SELECT @@local_infile;'
             );
             if ($result != false && $GLOBALS['dbi']->numRows($result) > 0) {
                 $tmp = $GLOBALS['dbi']->fetchRow($result);

libraries/server_privileges.lib.php
@@ -1803,7 +1803,7 @@ function PMA_getCurrentAuthenticationPlugin(
         }
     } else {
         $row = $GLOBALS['dbi']->fetchSingleRow(
-            'SHOW VARIABLES like \'default_authentication_plugin\''
+            'SELECT @@default_authentication_plugin'
         );
         $authentication_plugin = $row['Value'];
     }
@@ -1892,7 +1892,7 @@ function PMA_updatePassword($err_url, $username, $hostname)
 
                 // Backup the old value, to be reset later
                 $row = $GLOBALS['dbi']->fetchSingleRow(
-                    'SHOW VARIABLES like \'old_passwords\';'
+                    'SELECT @@old_passwords;'
                 );
                 $orig_value = $row['Value'];
                 // Set the hashing method used by PASSWORD()

templates/database/structure/body_for_table_summary.phtml
@@ -37,12 +37,12 @@ $cell_text = ($approx_rows)
     <?php
         // MySQL <= 5.5.2
         $default_engine = $GLOBALS['dbi']->fetchValue(
-            "SHOW VARIABLES LIKE 'storage_engine';", 0, 1
+            "SELECT @@storage_engine;", 0, 1
         );
         if (empty($default_engine)) {
             // MySQL >= 5.5.3
             $default_engine = $GLOBALS['dbi']->fetchValue(
-                "SHOW VARIABLES LIKE 'default_storage_engine';", 0, 1
+                "SELECT @@default_storage_engine;", 0, 1
             );
         }
     ?>

commit b48c414c0bcf2ae93c15ad02e8fb39ec20a39776

Fix bugs introduced in 788c96e8c744c66619cde6fc887b666d581282e6 while fixing issue #11442
Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

libraries/DatabaseInterface.class.php
@@ -322,9 +322,7 @@ class PMA_DatabaseInterface
         $server_language = PMA_Util::cacheGet(
             'server_language',
             function () {
-                return $GLOBALS['dbi']->fetchValue(
-                    "SELECT @@lc_messages;", 0, 0
-                );
+                return $GLOBALS['dbi']->fetchValue("SELECT @@lc_messages;");
             }
         );
 

libraries/Table.class.php
@@ -826,9 +826,7 @@ class PMA_Table
             // This is not availabile for Drizzle.
             if (!PMA_DRIZZLE) {
                 SqlParser\Context::setMode(
-                    $GLOBALS['dbi']->fetchValue(
-                        "SELECT @@sql_mode", 0, 1
-                    )
+                    $GLOBALS['dbi']->fetchValue("SELECT @@sql_mode")
                 );
             }
 

libraries/Util.class.php
@@ -3195,9 +3195,7 @@ class PMA_Util
         if (($engine == 'INNODB') || ($engine == 'PBXT')) {
             return true;
         } elseif ($engine == 'NDBCLUSTER' || $engine == 'NDB') {
-            $ndbver = $GLOBALS['dbi']->fetchValue(
-                "SELECT @@ndb_version_string"
-            );
+            $ndbver = $GLOBALS['dbi']->fetchValue("SELECT @@ndb_version_string");
             return ($ndbver >= 7.3);
         } else {
             return false;
@@ -4651,7 +4649,7 @@ class PMA_Util
             'lower_case_table_names',
             function () {
                 return $GLOBALS['dbi']->fetchValue(
-                    "SELECT @@lower_case_table_names", 0, 1
+                    "SELECT @@lower_case_table_names"
                 );
             }
         );

libraries/mysql_charsets.lib.php
@@ -126,9 +126,7 @@ function PMA_getDbCollation($db)
         return $GLOBALS['dbi']->fetchValue($sql);
     } else {
         $GLOBALS['dbi']->selectDb($db);
-        $return = $GLOBALS['dbi']->fetchValue(
-            'SELECT @@collation_database', 0, 1
-        );
+        $return = $GLOBALS['dbi']->fetchValue('SELECT @@collation_database');
         if ($db !== $GLOBALS['db']) {
             $GLOBALS['dbi']->selectDb($GLOBALS['db']);
         }
@@ -143,9 +141,7 @@ function PMA_getDbCollation($db)
  */
 function PMA_getServerCollation()
 {
-    return $GLOBALS['dbi']->fetchValue(
-        'SELECT @@collation_server', 0, 1
-    );
+    return $GLOBALS['dbi']->fetchValue('SELECT @@collation_server');
 }
 
 /**

libraries/operations.lib.php
@@ -362,7 +362,7 @@ function PMA_createDbBeforeCopy()
     // lower_case_table_names=1 `DB` becomes `db`
     if (! PMA_DRIZZLE) {
         $lowerCaseTableNames = $GLOBALS['dbi']->fetchValue(
-            'SELECT @@lower_case_table_names', 0, 1
+            'SELECT @@lower_case_table_names'
         );
         if ($lowerCaseTableNames === '1') {
             $_REQUEST['newname'] = /*overload*/mb_strtolower(

libraries/plugins/import/ImportLdi.class.php
@@ -51,7 +51,7 @@ class ImportLdi extends AbstractImportCsv
             );
             if ($result != false && $GLOBALS['dbi']->numRows($result) > 0) {
                 $tmp = $GLOBALS['dbi']->fetchRow($result);
-                if ($tmp[1] == 'ON') {
+                if ($tmp[0] == 'ON') {
                     $GLOBALS['cfg']['Import']['ldi_local_option'] = true;
                 }
             }

libraries/server_privileges.lib.php
@@ -1894,7 +1894,7 @@ function PMA_updatePassword($err_url, $username, $hostname)
                 $row = $GLOBALS['dbi']->fetchSingleRow(
                     'SELECT @@old_passwords;'
                 );
-                $orig_value = $row['Value'];
+                $orig_value = $row['@@old_passwords'];
                 // Set the hashing method used by PASSWORD()
                 // to be 'sha256_password' type
                 $GLOBALS['dbi']->tryQuery('SET old_passwords = 2;');

templates/database/structure/body_for_table_summary.phtml
@@ -37,12 +37,12 @@ $cell_text = ($approx_rows)
     <?php
         // MySQL <= 5.5.2
         $default_engine = $GLOBALS['dbi']->fetchValue(
-            "SELECT @@storage_engine;", 0, 1
+            "SELECT @@storage_engine;"
         );
         if (empty($default_engine)) {
             // MySQL >= 5.5.3
             $default_engine = $GLOBALS['dbi']->fetchValue(
-                "SELECT @@default_storage_engine;", 0, 1
+                "SELECT @@default_storage_engine;"
             );
         }
     ?>

test/classes/plugin/export/PMA_ExportSql_test.php
@@ -812,7 +812,7 @@ class PMA_ExportSql_Test extends PHPUnit_Framework_TestCase
 
         $dbi->expects($this->once())
             ->method('fetchValue')
-            ->with('SELECT @@collation_database', 0, 1)
+            ->with('SELECT @@collation_database')
             ->will($this->returnValue('testcollation'));
 
         $GLOBALS['dbi'] = $dbi;