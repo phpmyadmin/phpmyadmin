30224295f686aa22a648121f132b0c1350f80da8
Hugues Peccatte <hugues.peccatte@gmail.com>
2015-09-12 16:14:22 +0200

    Add missing require.
    Signed-off-by: Hugues Peccatte <hugues.peccatte@gmail.com>

--- test/libraries/PMA_designer_test.php
+++ test/libraries/PMA_designer_test.php
@@ -8,12 +8,11 @@
  * Include to test.
  */
 require_once 'libraries/db_designer.lib.php';
-
 require_once 'libraries/database_interface.inc.php';
-
 require_once 'libraries/php-gettext/gettext.inc';
 require_once 'libraries/url_generating.lib.php';
 require_once 'libraries/relation.lib.php';
+require_once 'libraries/plugin_interface.lib.php';
 
 /**
  * Tests for libraries/db_designer.lib.php
