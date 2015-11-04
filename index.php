c340b18e047de7626164999877ccebb8ee69f13d
ZhiFeng Hu <hufeng1987@gmail.com>
2015-11-04 17:36:54 +0800

    Add support for PHP builtin http server
    Run php builtin http server, just type
    
    cd phpmyadmin; php -S 0.0.0.0:8587
    
    Then open browser http://xxx.xxx.real.ip:8587
    
    Make sure mysql/mysqli were installed and config.inc.php
    configured right
    
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+if (php_sapi_name() === 'cli-server') {
+        $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+            if (__FILE__ !== $path && is_file($path)) {
+                        return false;
+                            }
+            unset($path);
+}
+
 /* vim: set expandtab sw=4 ts=4 sts=4: */
 /**
  * Main loader script
