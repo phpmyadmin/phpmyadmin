54b473ef895d5a164c677e6201624023c345d033
Marc Delisle <marc@infomarc.info>
2015-11-16 07:25:00 -0500

    4.5.3-dev
    Signed-off-by: Marc Delisle <marc@infomarc.info>

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -114,7 +114,7 @@ class PMA_Config
      */
     public function checkSystem()
     {
-        $this->set('PMA_VERSION', '4.5.2-dev');
+        $this->set('PMA_VERSION', '4.5.3-dev');
         /**
          * @deprecated
          */
