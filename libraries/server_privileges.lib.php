bc89aabdf95df41ecfb72e8a1b67db31020d5c4e
Bert-Jan de Lange <info@bert-jan.com>
2015-11-27 13:55:50 +0100

    Fix for not being able to create users on Percona Server
    Inspired by the patch https://github.com/phpmyadmin/phpmyadmin/commit/9bf88cbd32f5f5b4a18bd7ec60e89c5b75bcf947.patch
    
    Signed-off-by: Bert-Jan de Lange <info@bert-jan.com>
    Signed-off-by: Marc Delisle <marc@infomarc.info>

--- libraries/server_privileges.lib.php
+++ libraries/server_privileges.lib.php
@@ -5089,7 +5089,7 @@ function PMA_getSqlQueriesForDisplayAndAddUser($username, $hostname, $password)
 
     // 'IDENTIFIED WITH auth_plugin'
     // is supported by MySQL 5.5.7+
-    if ($serverType == 'MySQL'
+    if (($serverType == 'MySQL' || $serverType == 'Percona Server')
         && PMA_MYSQL_INT_VERSION >= 50507
         && isset($_REQUEST['authentication_plugin'])
     ) {
@@ -5136,7 +5136,7 @@ function PMA_getSqlQueriesForDisplayAndAddUser($username, $hostname, $password)
     // newer MySQL versions
     // and 'CREATE USER ... USING .. VIA ..' syntax for
     // newer MariaDB versions
-    if (($serverType == 'MySQL'
+    if ((($serverType == 'MySQL' || $serverType == 'Percona Server')
         && PMA_MYSQL_INT_VERSION >= 50706)
         || ($serverType == 'MariaDB'
         && PMA_MYSQL_INT_VERSION >= 50200)
