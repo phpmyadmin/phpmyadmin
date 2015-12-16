ca44bbd93b991cf6634db4f5351c1ebc838a6844
Madhura Jayaratne <madhura.cj@gmail.com>
2015-12-16 22:36:51 +1100

    Template plugins page
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

--- libraries/controllers/server/ServerPluginsController.php
+++ libraries/controllers/server/ServerPluginsController.php
@@ -10,6 +10,7 @@
 namespace PMA\libraries\controllers\server;
 
 use PMA\libraries\controllers\Controller;
+use PMA\libraries\Template;
 
 /**
  * Handles viewing server plugin details
@@ -89,73 +90,19 @@ class ServerPluginsController extends Controller
     private function _getPluginTab()
     {
         $html  = '<div id="plugins_plugins">';
-        $html .= '<div id="sectionlinks">';
+        $html .= Template::get('server/plugins/section_links')
+            ->render(array('plugins' => $this->plugins));
 
         foreach ($this->plugins as $plugin_type => $plugin_list) {
-            $key = 'plugins-'
-                . preg_replace('/[^a-z]/', '', /*overload*/mb_strtolower($plugin_type));
-            $html .= '<a href="#' . $key . '">'
-                . htmlspecialchars($plugin_type) . '</a>' . "\n";
-        }
-
-        $html .= '</div>';
-        $html .= '<br />';
-
-        foreach ($this->plugins as $plugin_type => $plugin_list) {
-            $key = 'plugins-'
-                . preg_replace('/[^a-z]/', '', /*overload*/mb_strtolower($plugin_type));
-            sort($plugin_list);
-
-            $html .= '<table class="data_full_width" id="' . $key . '">';
-            $html .= '<caption class="tblHeaders">';
-            $html .=  htmlspecialchars($plugin_type);
-            $html .= '</caption>';
-            $html .= '<thead>';
-            $html .= '<tr>';
-            $html .= '<th>' . __('Plugin') . '</th>';
-            $html .= '<th>' . __('Description') . '</th>';
-            $html .= '<th>' . __('Version') . '</th>';
-            $html .= '<th>' .  __('Author') . '</th>';
-            $html .= '<th>' . __('License') . '</th>';
-            $html .= '</tr>';
-            $html .= '</thead>';
-            $html .= '<tbody>';
-
-            $html .= $this->_getPluginList($plugin_list);
-
-            $html .= '</tbody>';
-            $html .= '</table>';
+            $html .= Template::get('server/plugins/section')
+                ->render(
+                    array(
+                        'plugin_type' => $plugin_type,
+                        'plugin_list' => $plugin_list,
+                    )
+                );
         }
         $html .= '</div>';
-        return $html;
-    }
-
-    /**
-     * Returns the html for plugin List.
-     *
-     * @param array $plugin_list list
-     *
-     * @return string
-     */
-    private function _getPluginList($plugin_list)
-    {
-        $html = "";
-        $odd_row = false;
-        foreach ($plugin_list as $plugin) {
-            $odd_row = !$odd_row;
-            $html .= '<tr class="noclick ' . ($odd_row ? 'odd' : 'even') . '">';
-            $html .= '<th>';
-            $html .= htmlspecialchars($plugin['plugin_name']);
-            if (! $plugin['is_active']) {
-                $html .= '&nbsp;<small class="attention">' . __('disabled') . '</small>';
-            }
-            $html .= '</th>';
-            $html .= '<td>' . htmlspecialchars($plugin['plugin_description']) . '</td>';
-            $html .= '<td>' . htmlspecialchars($plugin['plugin_type_version']) . '</td>';
-            $html .= '<td>' . htmlspecialchars($plugin['plugin_author']) . '</td>';
-            $html .= '<td>' . htmlspecialchars($plugin['plugin_license']) . '</td>';
-            $html .= '</tr>';
-        }
         return $html;
     }
 }
