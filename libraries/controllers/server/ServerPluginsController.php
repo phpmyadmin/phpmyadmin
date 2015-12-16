e23827f67f8632815ea2e92367535a2ec57de5b3
Madhura Jayaratne <madhura.cj@gmail.com>
2015-12-16 22:01:12 +1100

    Create a controller class for server plugins page
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

---
+++ libraries/controllers/server/ServerPluginsController.php
@@ -0,0 +1,154 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+
+/**
+ * Holds the PMA\libraries\controllers\server\ServerPluginsController
+*
+* @package PMA\libraries\controllers\server
+*/
+
+namespace PMA\libraries\controllers\server;
+
+use PMA\libraries\controllers\Controller;
+
+/**
+ * Handles viewing server plugin details
+ *
+ * @package PMA\libraries\controllers\server
+ */
+class ServerPluginsController extends Controller
+{
+    /**
+     * Index action
+     *
+     * @return void
+     */
+    public function indexAction()
+    {
+        include 'libraries/server_common.inc.php';
+
+        $header   = $this->response->getHeader();
+        $scripts  = $header->getScripts();
+        $scripts->addFile('jquery/jquery.tablesorter.js');
+        $scripts->addFile('server_plugins.js');
+
+        $plugins = $this->_getServerPlugins();
+
+        /**
+         * Displays the page
+        */
+        $this->response->addHTML(PMA_getHtmlForSubPageHeader('plugins'));
+        $this->response->addHTML($this->_getPluginTab($plugins));
+    }
+
+    /**
+     * Returns the common SQL used to retrieve plugin data
+     *
+     * @return string SQL
+     */
+    private function _getServerPluginSQL()
+    {
+        return "SELECT plugin_name, plugin_type, (plugin_status = 'ACTIVE') AS is_active,
+                plugin_type_version, plugin_author, plugin_description, plugin_license
+            FROM information_schema.plugins
+            ORDER BY plugin_type, plugin_name";
+    }
+
+    /**
+     * Returns details about server plugins
+     *
+     * @return array server plugins data
+     */
+    private function _getServerPlugins()
+    {
+        $sql = $this->_getServerPluginSQL();
+        $res = $GLOBALS['dbi']->query($sql);
+        $plugins = array();
+        while ($row = $GLOBALS['dbi']->fetchAssoc($res)) {
+            $plugins[$row['plugin_type']][] = $row;
+        }
+        $GLOBALS['dbi']->freeResult($res);
+        ksort($plugins);
+        return $plugins;
+    }
+
+    /**
+     * Returns the html for plugin Tab.
+     *
+     * @param array $plugins list
+     *
+     * @return string
+     */
+    private function _getPluginTab($plugins)
+    {
+        $html  = '<div id="plugins_plugins">';
+        $html .= '<div id="sectionlinks">';
+
+        foreach ($plugins as $plugin_type => $plugin_list) {
+            $key = 'plugins-'
+                . preg_replace('/[^a-z]/', '', /*overload*/mb_strtolower($plugin_type));
+            $html .= '<a href="#' . $key . '">'
+                . htmlspecialchars($plugin_type) . '</a>' . "\n";
+        }
+
+        $html .= '</div>';
+        $html .= '<br />';
+
+        foreach ($plugins as $plugin_type => $plugin_list) {
+            $key = 'plugins-'
+                . preg_replace('/[^a-z]/', '', /*overload*/mb_strtolower($plugin_type));
+            sort($plugin_list);
+
+            $html .= '<table class="data_full_width" id="' . $key . '">';
+            $html .= '<caption class="tblHeaders">';
+            $html .=  htmlspecialchars($plugin_type);
+            $html .= '</caption>';
+            $html .= '<thead>';
+            $html .= '<tr>';
+            $html .= '<th>' . __('Plugin') . '</th>';
+            $html .= '<th>' . __('Description') . '</th>';
+            $html .= '<th>' . __('Version') . '</th>';
+            $html .= '<th>' .  __('Author') . '</th>';
+            $html .= '<th>' . __('License') . '</th>';
+            $html .= '</tr>';
+            $html .= '</thead>';
+            $html .= '<tbody>';
+
+            $html .= $this->_getPluginList($plugin_list);
+
+            $html .= '</tbody>';
+            $html .= '</table>';
+        }
+        $html .= '</div>';
+        return $html;
+    }
+
+    /**
+     * Returns the html for plugin List.
+     *
+     * @param array $plugin_list list
+     *
+     * @return string
+     */
+    private function _getPluginList($plugin_list)
+    {
+        $html = "";
+        $odd_row = false;
+        foreach ($plugin_list as $plugin) {
+            $odd_row = !$odd_row;
+            $html .= '<tr class="noclick ' . ($odd_row ? 'odd' : 'even') . '">';
+            $html .= '<th>';
+            $html .= htmlspecialchars($plugin['plugin_name']);
+            if (! $plugin['is_active']) {
+                $html .= '&nbsp;<small class="attention">' . __('disabled') . '</small>';
+            }
+            $html .= '</th>';
+            $html .= '<td>' . htmlspecialchars($plugin['plugin_description']) . '</td>';
+            $html .= '<td>' . htmlspecialchars($plugin['plugin_type_version']) . '</td>';
+            $html .= '<td>' . htmlspecialchars($plugin['plugin_author']) . '</td>';
+            $html .= '<td>' . htmlspecialchars($plugin['plugin_license']) . '</td>';
+            $html .= '</tr>';
+        }
+        return $html;
+    }
+}
