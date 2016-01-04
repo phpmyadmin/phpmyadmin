5b76c5eddfac79727ee39be91aed9ad8b57aa887
Atul Pratap Singh <atulpratapsingh05@gmail.com>
2016-01-04 17:26:15 +0530

    Fix #11812 Uncaught TypeError: Cannot read property 'parentNode' of null
    Signed-off-by: Atul Pratap Singh <atulpratapsingh05@gmail.com>

--- js/cross_framing_protection.js
+++ js/cross_framing_protection.js
@@ -2,9 +2,15 @@
 /**
  * Conditionally included if framing is not allowed
  */
-if (self == top) {
-    var style_element = document.getElementById("cfs-style");
-    style_element.parentNode.removeChild(style_element);
-} else {
-    top.location = self.location;
+window.onload = function () {
+    if (self == top) {
+        var style_element = document.getElementById("cfs-style");
+        // check if style_element has already been removed
+        // to avoid frequently reported js error
+        if (typeof(style_element) != 'undefined' && style_element != null) {
+            style_element.parentNode.removeChild(style_element);
+        }
+    } else {
+        top.location = self.location;
+    }
 }
