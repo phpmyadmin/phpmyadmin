b39c02b0a82b13d2198276d228051139e6b838d9
Madhura Jayaratne <madhura.cj@gmail.com>
2016-01-15 08:20:15 +1100

    [Security] Fix path disclosure, items 1.4.x, 1.5 and 1.6
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

--- setup/frames/form.inc.php
+++ setup/frames/form.inc.php
@@ -19,9 +19,9 @@ require_once './setup/lib/form_processing.lib.php';
 
 require './libraries/config/setup.forms.php';
 
-$formset_id = isset($_GET['formset']) ? $_GET['formset'] : null;
+$formset_id = PMA_isValid($_GET['formset'], 'scalar') ? $_GET['formset'] : null;
 $mode = isset($_GET['mode']) ? $_GET['mode'] : null;
-if (! isset($forms[$formset_id])) {
+if (! isset($forms[$formset_id]) || substr($formset_id, 0, 1) === '_') {
     PMA_fatalError(__('Incorrect formset, check $formsets array in setup/frames/form.inc.php!'));
 }
 
