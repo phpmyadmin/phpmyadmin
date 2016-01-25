912856b432d794201884c36e5f390d446339b6e4
Michal Čihař <michal@cihar.com>
2016-01-25 12:44:16 +0100

    Use full alphabet to generate random passwords
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- js/functions.js
+++ js/functions.js
@@ -319,7 +319,7 @@ function suggestPassword(passwd_form)
     // restrict the password to just letters and numbers to avoid problems:
     // "editors and viewers regard the password as multiple words and
     // things like double click no longer work"
-    var pwchars = "abcdefhjmnpqrstuvwxyz23456789ABCDEFGHJKLMNPQRSTUVWYXZ";
+    var pwchars = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWYXZ";
     var passwordlength = 16;    // do we want that to be dynamic?  no, keep it simple :)
     var passwd = passwd_form.generated_pw;
     var randomWords = new Int32Array(passwordlength);
