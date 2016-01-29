f1668937772688de3bbd707cee23ef8653756f32
Isaac Bennetch <bennetch@gmail.com>
2016-01-28 23:34:57 -0500

    Release 4.0.10.14
    Signed-off-by: Isaac Bennetch <bennetch@gmail.com>

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -102,7 +102,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '4.0.10.13');
+        $this->set('PMA_VERSION', '4.0.10.14');
         /**
          * @deprecated
          */
