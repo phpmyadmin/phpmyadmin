9204b85fa54dede121b4ab8b37f7d4fed057cf14
Michal Čihař <michal@cihar.com>
2016-02-15 08:50:53 +0100

    Move isStorageSupported to config.js
    This way it's included in the setup as well where it's used.
    
    Fixes #11959
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- libraries/Header.class.php
+++ libraries/Header.class.php
@@ -200,12 +200,12 @@ class PMA_Header
         $this->_scripts->addFile(
             'get_image.js.php?theme=' . $theme_id
         );
+        $this->_scripts->addFile('config.js');
         $this->_scripts->addFile('doclinks.js');
         $this->_scripts->addFile('functions.js');
         $this->_scripts->addFile('navigation.js');
         $this->_scripts->addFile('indexes.js');
         $this->_scripts->addFile('common.js');
-        $this->_scripts->addFile('config.js');
         $this->_scripts->addFile('page_settings.js');
         $this->_scripts->addCode($this->getJsParamsCode());
     }
