81821574b196262742f858ae0560ec64fef4954c
Madhura Jayaratne <madhura.cj@gmail.com>
2016-02-23 22:45:28 +1100

    Merge test files since they both test the same class
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

--- test/classes/SanitizeTest.php
+++ test/classes/SanitizeTest.php
@@ -212,4 +212,39 @@ class SanitizeTest extends PHPUnit_Framework_TestCase
             array('foo', 'apostroph\'', "foo = \"apostroph\\'\";\n"),
         );
     }
+
+    /**
+     * Sanitize::escapeJsString tests
+     *
+     * @param string $target expected output
+     * @param string $source string to be escaped
+     *
+     * @return void
+     *
+     * @dataProvider escapeDataProvider
+     */
+    public function testEscape($target, $source)
+    {
+        $this->assertEquals($target, Sanitize::escapeJsString($source));
+    }
+
+    /**
+     * Data provider for testEscape
+     *
+     * @return array data for testEscape test case
+     */
+    public function escapeDataProvider()
+    {
+        return array(
+            array('\\\';', '\';'),
+            array('\r\n\\\'<scrIpt></\' + \'script>', "\r\n'<scrIpt></sCRIPT>"),
+            array('\\\';[XSS]', '\';[XSS]'),
+            array(
+                    '</\' + \'script></head><body>[HTML]',
+                    '</SCRIPT></head><body>[HTML]'
+            ),
+            array('\"\\\'\\\\\\\'\"', '"\'\\\'"'),
+            array("\\\\\'\'\'\'\'\'\'\'\'\'\'\'\\\\", "\\''''''''''''\\")
+        );
+    }
 }
