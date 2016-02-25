9ec0b598bd0c5a5b63e483801057ab8a22e82527
Michal Čihař <michal@cihar.com>
2016-02-25 09:57:29 +0100

    Urlencode hostname
    This can come from the HTTP header, so we need to be sure to sanitize
    it.
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -1347,7 +1347,7 @@ class PMA_Config
                 $pma_absolute_uri .= '@';
             }
             // Add hostname
-            $pma_absolute_uri .= $url['host'];
+            $pma_absolute_uri .= urlencode($url['host']);
             // Add port, if it not the default one
             if (! empty($url['port'])
                 && (($url['scheme'] == 'http' && $url['port'] != 80)
