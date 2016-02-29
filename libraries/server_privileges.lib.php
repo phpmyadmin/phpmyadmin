7ddce5e39a4e12cd351732955394bc7055c280eb
Madhura Jayaratne <madhura.cj@gmail.com>
2016-02-29 11:24:04 +1100

    Fix XSS in User accounts page
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

--- libraries/server_privileges.lib.php
+++ libraries/server_privileges.lib.php
@@ -2409,7 +2409,7 @@ function PMA_getUsersOverview($result, $db_rights, $link_edit, $pmaThemeImage,
         __('Export'), 'b_tblexport.png', 'export'
     );
     $html_output .= '<input type="hidden" name="initial" '
-        . 'value="' . (isset($_GET['initial']) ? $_GET['initial'] : '') . '" />';
+        . 'value="' . (isset($_GET['initial']) ? htmlspecialchars($_GET['initial']) : '') . '" />';
     $html_output .= '</div>'
         . '<div class="clear_both" style="clear:both"></div>';
 
