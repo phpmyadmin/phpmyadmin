9de4114e6f1fdb8d35d49c421e0e7d65fb04e515
Michal Čihař <michal@cihar.com>
2016-05-11 12:08:06 +0200

    Avoid using too log URLs when getting javascripts
    Some researchers have come with great idea of recommending
    setting "LimitRequestline 512" in Apache, what allows even shorter URLs
    than with MSIE.
    
    I still consider this a really bad idea as most of the applications
    really do not count with so small URL limits, but this error seems to be
    quite widely spread among CentOS users (probably coming from some
    howto).
    
    Fixes #12244
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- libraries/Scripts.class.php
+++ libraries/Scripts.class.php
@@ -83,8 +83,9 @@ class PMA_Scripts
         }
         $separator = PMA_URL_getArgSeparator();
         $static_scripts = '';
-        // Using chunks of 20 files to avoid too long URLs
-        $script_chunks = array_chunk($scripts, 20);
+        // Using chunks of 10 files to avoid too long URLs
+        // as some servers are set to 512 bytes URL limit
+        $script_chunks = array_chunk($scripts, 10);
         foreach ($script_chunks as $script_chunk) {
             $url = 'js/get_scripts.js.php?' . implode($separator, $script_chunk);
 
