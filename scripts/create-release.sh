1cd936e7b3e0a053321cecf708bff1a3769438ed
Michal Čihař <michal@cihar.com>
2016-05-16 11:39:16 +0200

    Remove Weblate configuration on release
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -187,7 +187,7 @@ rm -rf .github
 rm -rf PMAStandard
 
 # Testsuite setup
-rm -f .travis.yml .coveralls.yml .scrutinizer.yml .jshintrc
+rm -f .travis.yml .coveralls.yml .scrutinizer.yml .jshintrc .weblate
 
 # Remove readme for github
 rm -f README.rst
