c1c75f8c35638ba1b1a1ca9b6ac3538e087cb253
Isaac Bennetch <bennetch@gmail.com>
2016-05-25 12:16:14 -0400

    4.6.2 release
    Signed-off-by: Isaac Bennetch <bennetch@gmail.com>

--- libraries/Config.php
+++ libraries/Config.php
@@ -101,7 +101,7 @@ class Config
      */
     public function checkSystem()
     {
-        $this->set('PMA_VERSION', '4.6.2-dev');
+        $this->set('PMA_VERSION', '4.6.2');
         /**
          * @deprecated
          */
