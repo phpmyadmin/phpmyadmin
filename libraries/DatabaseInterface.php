4028783272a8c881f6387eeb1405fdd715f29486
Giovanni Bechis <giovanni@paclan.it>
2016-06-20 19:55:20 +0200

    "SET lc_messages" is not supported on older MySQL versions
    Signed-off-by: Giovanni Bechis <giovanni@paclan.it>

--- libraries/DatabaseInterface.php
+++ libraries/DatabaseInterface.php
@@ -1409,14 +1409,16 @@ class DatabaseInterface
             );
         }
 
-        /* Locale for messages */
-        $locale = LanguageManager::getInstance()->getCurrentLanguage()->getMySQLLocale();
-        if (! empty($locale)) {
-            $this->query(
-                "SET lc_messages = '" . $locale . "';",
-                $link,
-                self::QUERY_STORE
-            );
+        if (PMA_MYSQL_INT_VERSION >  50503) {
+            /* Locale for messages */
+            $locale = LanguageManager::getInstance()->getCurrentLanguage()->getMySQLLocale();
+            if (! empty($locale)) {
+                $this->query(
+                    "SET lc_messages = '" . $locale . "';",
+                    $link,
+                    self::QUERY_STORE
+                );
+            }
         }
     }
 
