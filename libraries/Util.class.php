2989e4943b85e08e8a2e284e597e62ab7c823c0d
Michal Čihař <michal@cihar.com>
2016-06-30 10:04:46 +0200

    Avoid possible path traversal using MySQL username
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- libraries/Util.class.php
+++ libraries/Util.class.php
@@ -2562,7 +2562,7 @@ class PMA_Util
             $dir .= '/';
         }
 
-        return str_replace('%u', $GLOBALS['cfg']['Server']['user'], $dir);
+        return str_replace('%u', PMA_securePath($GLOBALS['cfg']['Server']['user']), $dir);
     }
 
     /**
