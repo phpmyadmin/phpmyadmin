0f87b73ae203d79f74765c97f637a51b87205515
Michal Čihař <michal@cihar.com>
2016-07-13 11:31:55 +0200

    HML encode embedded JSON data
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- libraries/TableSearch.class.php
+++ libraries/TableSearch.class.php
@@ -1180,7 +1180,7 @@ EOT;
             $html_output .= '<center><a href="#" onclick="displayHelp();">'
                 . __('How to use') . '</a></center>';
             $html_output .= '<div id="querydata" style="display:none">'
-                . json_encode($data) . '</div>';
+                . htmlspecialchars(json_encode($data)) . '</div>';
             $html_output .= '<div id="querychart"></div>';
             $html_output .= '<button class="button-reset">'
                 . __('Reset zoom') . '</button>';
