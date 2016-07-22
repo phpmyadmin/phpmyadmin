1d70a1ba64f4bace7a15aa3f153abc81fde4881c
Michal Čihař <michal@cihar.com>
2016-07-22 12:10:11 +0200

    Allow only https outgoing links
    We no longer produce http links, so it's better to filter out these at
    all levels.
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test/libraries/PMA_sanitize_test.php
+++ test/libraries/PMA_sanitize_test.php
@@ -53,8 +53,8 @@ class PMA_Sanitize_Test extends PHPUnit_Framework_TestCase
         unset($GLOBALS['lang']);
         unset($GLOBALS['collation_connection']);
         $this->assertEquals(
-            '<a href="./url.php?url=http%3A%2F%2Fwww.phpmyadmin.net%2F" target="target">link</a>',
-            PMA_sanitize('[a@http://www.phpmyadmin.net/@target]link[/a]')
+            '<a href="./url.php?url=https%3A%2F%2Fwww.phpmyadmin.net%2F" target="target">link</a>',
+            PMA_sanitize('[a@https://www.phpmyadmin.net/@target]link[/a]')
         );
     }
 
