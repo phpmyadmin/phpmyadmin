d48b88284b9e2e34c48fa28ebba8289339a3dcc0
Pavel Rochnyack <pavel2000@ngs.ru>
2016-08-01 01:01:11 +0100

    Translation improvements
    * Improved Russian translation
    * Translated 'Go' button in edit_routine_privileges.phtml
    * Removed whitespace from end of "Table comments: " phrase.
    * Removed \n and whitespaces from phrase in libraries/config/messages.inc.php:813.
    * Unified form of changed phrases in translation files.
    
    Signed-off-by: Pavel Rochnyack <pavel2000@ngs.ru>

--- libraries/config/messages.inc.php
+++ libraries/config/messages.inc.php
@@ -811,8 +811,8 @@ $strConfigServers_userconfig_desc = __(
 $strConfigServers_userconfig_name = __('User preferences storage table');
 $strConfigServers_users_desc = __(
     'Both this table and the user groups table are required to enable the ' .
-    'configurable menus feature; leaving either one of them blank will disable
-    this feature, suggested: [kbd]pma__users[/kbd].'
+    'configurable menus feature; leaving either one of them blank will disable ' .
+    'this feature, suggested: [kbd]pma__users[/kbd].'
 );
 $strConfigServers_users_name = __('Users table');
 $strConfigServers_usergroups_desc = __(
