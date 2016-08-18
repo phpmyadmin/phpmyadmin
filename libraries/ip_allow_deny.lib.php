259694af9a3ce80bd17db04f46fb631693d929b8
Michal Čihař <michal@cihar.com>
2016-08-18 09:15:34 +0200

    Use hash_equals for comparing username in allow/deny rules
    The comparison should happen in constant time to avoid possible leak of
    usernames in rules.
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- libraries/ip_allow_deny.lib.php
+++ libraries/ip_allow_deny.lib.php
@@ -318,7 +318,7 @@ function PMA_allowDeny($type)
 
         // check for username
         if (($rule_data[1] != '%') //wildcarded first
-            && ($rule_data[1] != $username)
+            && (! hash_equals($rule_data[1], $username))
         ) {
             continue;
         }
