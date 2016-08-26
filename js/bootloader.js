dac6577fba2fa261a6d27cd435c14564c8e34497
Seralahthan <e11381@ce.pdn.ac.lk>
2016-08-26 10:25:27 +0530

    bug fix for #12447 Outdated JavaScript Libraries

---
+++ js/bootloader.js
@@ -0,0 +1,4 @@
+var jq = document.createElement('script');
+jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js";
+var head = document.getElementsByTagName('head');
+head[0].appendChild(jq);
