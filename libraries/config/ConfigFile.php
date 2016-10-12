eef59d6c487102b8d78f4393096a448c9136bfa1
Deven Bansod <devenbansod.bits@gmail.com>
2016-10-12 13:53:11 +0530

    Fix order of testing DBConnection with different parameters in Config method
    In setup, try to follow what the docs say.
    'Password login is still tried first, but as fallback, no password method is tried.'
    
    Fix #12472
    
    Signed-off-by: Deven Bansod <devenbansod.bits@gmail.com>

--- libraries/config/ConfigFile.php
+++ libraries/config/ConfigFile.php
@@ -416,7 +416,9 @@ class ConfigFile
         $dsn = 'mysqli://';
         if ($this->getValue("$path/auth_type") == 'config') {
             $dsn .= $this->getValue("$path/user");
-            if (! $this->getValue("$path/nopassword")) {
+            if (! $this->getValue("$path/nopassword")
+                || ! empty($this->getValue("$path/password"))
+            ) {
                 $dsn .= ':***';
             }
             $dsn .= '@';
