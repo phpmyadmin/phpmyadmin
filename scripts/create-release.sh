c365e794706d1cb51099c7ae88fd4d15a43380a5
Michal Čihař <michal@cihar.com>
2016-10-12 17:08:19 +0200

    Do cleanup of simple-math for release as well
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -208,6 +208,7 @@ if [ ! -d libraries/tcpdf ] ; then
         vendor/phpmyadmin/sql-parser/tests/ \
         vendor/phpmyadmin/sql-parser/tools/ \
         vendor/phpmyadmin/motranslator/tests/ \
+        vendor/phpmyadmin/simple-math/tests/ \
         vendor/phpmyadmin/shapefile/tests/ \
         vendor/phpmyadmin/shapefile/examples/ \
         vendor/phpmyadmin/shapefile/data/ \
