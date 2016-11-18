b8a9b0f30d3d83746967ef457cca641e014c32a5
osaid <osaid.nasir@gmail.com>
2016-11-18 20:54:34 +0530

    Share code for getting temporary directory
    Changed Encoding.php and File.php.
    
    Fixes #12514
    
    Signed-off-by: Osaid <osaid.nasir@gmail.com>
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- libraries/config/ConfigFile.php
+++ libraries/config/ConfigFile.php
@@ -537,4 +537,24 @@ class ConfigFile
         }
         return $c;
     }
+
+    /**
+     * Returns temporary directory
+     *
+     * @return string
+     */
+    public function getDefaultTempDirectory()
+    {
+        $tmp_subdir = null;
+        if (! empty($GLOBALS['cfg']['TempDir']) && @is_writable($GLOBALS['cfg']['TempDir'])) {
+            $tmp_subdir = $GLOBALS['cfg']['TempDir'];
+        } else {
+            $tmp_subdir = ini_get('upload_tmp_dir');
+            if (empty($tmp_subdir)) {
+                $tmp_subdir = sys_get_temp_dir();
+            }
+            $tmp_subdir = rtrim($tmp_subdir, DIRECTORY_SEPARATOR);
+        }
+        return $tmp_subdir;
+    }
 }
