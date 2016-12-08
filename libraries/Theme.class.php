bd3677f161977bf0cc800cae82e65355bf49f342
Michal Čihař <michal@cihar.com>
2016-12-08 11:26:51 +0100

    Do not provide fallback to cookie for font size
    * This should be already handled by Config class
    * Injecting cookie value to CSS could be security risk
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- libraries/Theme.class.php
+++ libraries/Theme.class.php
@@ -433,9 +433,6 @@ class PMA_Theme
         if (!is_null($fs)) {
             return $fs;
         }
-        if (isset($_COOKIE['pma_fontsize'])) {
-            return htmlspecialchars($_COOKIE['pma_fontsize']);
-        }
         return '82%';
     }
 
