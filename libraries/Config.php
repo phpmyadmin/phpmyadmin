2a989a23bb66f3cc9b754714775c0c8ad3c8d1c5
Isaac Bennetch <bennetch@gmail.com>
2017-01-25 23:12:48 -0500

    Prepare for 4.8.0-dev
    Signed-off-by: Isaac Bennetch <bennetch@gmail.com>

--- libraries/Config.php
+++ libraries/Config.php
@@ -103,7 +103,7 @@ class Config
      */
     public function checkSystem()
     {
-        $this->set('PMA_VERSION', '4.7.0-dev');
+        $this->set('PMA_VERSION', '4.8.0-dev');
         /**
          * @deprecated
          */
