62c6e73cc5601ecd6cf839874c038a991c7b5b97
Isaac Bennetch <bennetch@gmail.com>
2017-01-25 19:25:29 -0500

    Prepare for 4.7.0-beta1
    Signed-off-by: Isaac Bennetch <bennetch@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -51,7 +51,7 @@ copyright = u'2012 - 2016, The phpMyAdmin devel team'
 # built documents.
 #
 # The short X.Y version.
-version = '4.7.0-dev'
+version = '4.7.0-beta1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
