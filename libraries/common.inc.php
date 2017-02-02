186f20770c9b76860ca70689d086b3bea68e6b72
Thomas E. Horner <github.com@t-horner.com>
2017-02-02 09:27:06 +0100

    Update common.inc.php
    Signed-off-by: Thomas Horner <github-phpmyadmin@t-horner.com>

--- libraries/common.inc.php
+++ libraries/common.inc.php
@@ -361,8 +361,8 @@ if (PMA_checkPageValidity($_REQUEST['back'], $goto_whitelist)) {
 $token_mismatch = true;
 $token_provided = false;
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (PMA_isValid($_SESSION[' PMA_token ']) && PMA_isValid($_POST['token'])) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && PMA_isValid($_SESSION[' PMA_token '])) {
+    if (PMA_isValid($_POST['token'])) {
         $token_provided = true;
         $token_mismatch = ! hash_equals($_SESSION[' PMA_token '], $_POST['token']);
     }
