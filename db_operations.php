913d0a8994e2099fc94e09987f46acb92b0e33ce
TheSaturn <saturn@thesaturn.me>
2017-03-19 14:09:38 +0700

    Adds error if copy database name have the same name is the current one #13099

--- db_operations.php
+++ db_operations.php
@@ -48,6 +48,8 @@ if (strlen($GLOBALS['db']) > 0
 
     if (! isset($_REQUEST['newname']) || strlen($_REQUEST['newname']) === 0) {
         $message = PMA\libraries\Message::error(__('The database name is empty!'));
+    } else if($_REQUEST['newname'] === $_REQUEST['db']) {
+        $message = PMA\libraries\Message::error(__('The new database name is equal to the current one!'));
     } else {
         $_error = false;
         if ($move || ! empty($_REQUEST['create_database_before_copying'])) {
