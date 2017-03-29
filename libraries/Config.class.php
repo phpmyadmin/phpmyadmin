9a9a6ab709eb6b1b2e82b7a15851ae93a31e9e45
Isaac Bennetch <bennetch@gmail.com>
2017-03-28 20:54:38 -0400

    Prepare for 4.0.10.20
    Signed-off-by: Isaac Bennetch <bennetch@gmail.com>

--- libraries/Config.class.php
+++ libraries/Config.class.php
@@ -102,7 +102,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '4.0.10.19');
+        $this->set('PMA_VERSION', '4.0.10.20');
         /**
          * @deprecated
          */
