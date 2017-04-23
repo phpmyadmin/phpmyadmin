05969547094a7c236ae56e6d197c3fea6e0c9a24
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2017-04-23 22:46:53 +0100

    Create autoload.php
    Add new autoload.php file for customization

---
+++ autoload.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Includes the autoloader created by Composer.
+ *
+ * @see composer.json
+ * @see libraries/common.inc.php
+ */
+
+return require __DIR__ . '/vendor/autoload.php';
