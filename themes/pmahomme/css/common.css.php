b6850df247b928ee70e232d5aa2d947b25d17c2f
Abhilash Mathews <amthekkel@gmail.com>
2017-06-08 18:05:21 +0100

    Table Ops layout #13145 -- modified js function and removed dynamic adjustment of child boxes within boxContainer, removed operations_half_width css rule . Signed-off-by: Abhilash Mathews amthekkel@gmail.com

--- themes/pmahomme/css/common.css.php
+++ themes/pmahomme/css/common.css.php
@@ -1740,10 +1740,6 @@ li.no_bullets {
     width: 35%;
 }
 
-.operations_half_width {
-    width: 48%;
-    float: <?php echo $left; ?>;
-}
 .operations_half_width input[type=text],
 .operations_half_width input[type=password],
 .operations_half_width input[type=number],
