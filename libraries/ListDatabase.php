85747b9ae99bd6110578c7f467ee99ad91324b7a
Michal Čihař <michal@cihar.com>
2017-07-10 14:34:12 +0200

    Use usort() instead of natsort()
    The natsoft has side effect of producit associated array, what is later
    represented as object if encoded to JSON and breaking code expect array
    instead.
    
    Fixes #13465
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- libraries/ListDatabase.php
+++ libraries/ListDatabase.php
@@ -105,7 +105,7 @@ class ListDatabase extends ListAbstract
         }
 
         if ($GLOBALS['cfg']['NaturalOrder']) {
-            natsort($database_list);
+            usort($database_list, 'strnatcasecmp');
         } else {
             // need to sort anyway, otherwise information_schema
             // goes at the top
