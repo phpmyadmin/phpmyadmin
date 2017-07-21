570e82615551f3e8bfe9282a799834e5223aad3e
Maurício Meneghini Fauth <mauriciofauth@gmail.com>
2017-07-20 22:57:18 -0300

    Move util.lib function into Util class
    Move PMA\Util\get function to PhpMyAdmin\Util::getValueByKey
    
    Signed-off-by: Maurício Meneghini Fauth <mauriciofauth@gmail.com>

--- libraries/classes/Util.php
+++ libraries/classes/Util.php
@@ -4903,4 +4903,29 @@ class Util
             @set_time_limit($GLOBALS['cfg']['ExecTimeLimit']);
         }
     }
+
+    /**
+     * Access to a multidimensional array by dot notation
+     *
+     * @param array        $array   List of values
+     * @param string|array $path    Path to searched value
+     * @param mixed        $default Default value
+     *
+     * @return mixed Searched value
+     */
+    public static function getValueByKey($array, $path, $default = null)
+    {
+        if (is_string($path)) {
+            $path = explode('.', $path);
+        }
+        $p = array_shift($path);
+        while (isset($p)) {
+            if (!isset($array[$p])) {
+                return $default;
+            }
+            $array = $array[$p];
+            $p = array_shift($path);
+        }
+        return $array;
+    }
 }
