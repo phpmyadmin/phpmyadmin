cc488403503d1222eb771faeb16b0161efe29fe1
Maurício Meneghini Fauth <mauriciofauth@gmail.com>
2017-07-26 16:47:11 -0300

    Port some templates to Twig
    Signed-off-by: Maurício Meneghini Fauth <mauriciofauth@gmail.com>

--- libraries/classes/Controllers/Table/TableGisVisualizationController.php
+++ libraries/classes/Controllers/Table/TableGisVisualizationController.php
@@ -198,13 +198,14 @@ class TableGisVisualizationController extends TableController
         $html = Template::get('table/gis_visualization/gis_visualization')->render(
             array(
                 'url_params' => $this->url_params,
-                'downloadUrl' => $downloadUrl,
-                'labelCandidates' => $labelCandidates,
-                'spatialCandidates' => $spatialCandidates,
-                'visualizationSettings' => $this->visualizationSettings,
+                'download_url' => $downloadUrl,
+                'label_candidates' => $labelCandidates,
+                'spatial_candidates' => $spatialCandidates,
+                'visualization_settings' => $this->visualizationSettings,
                 'sql_query' => $this->sql_query,
                 'visualization' => $this->visualization->toImage('svg'),
-                'drawOl' => $this->visualization->asOl()
+                'draw_ol' => $this->visualization->asOl(),
+                'pma_theme_image' => $GLOBALS['pmaThemeImage']
             )
         );
 
