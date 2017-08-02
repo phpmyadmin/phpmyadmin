4f80efb47741d7664e800f3fbc7f64cfaf46833c
Michal Čihař <michal@cihar.com>
2017-08-02 09:27:17 +0200

    Remove no longer valid comment
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- libraries/classes/Util.php
+++ libraries/classes/Util.php
@@ -1965,9 +1965,6 @@ class Util
      * Called by each script that needs parameters, it displays
      * an error message and, by default, stops the execution.
      *
-     * Not sure we could use a strMissingParameter message here,
-     * would have to check if the error message file is always available
-     *
      * @param string[] $params The names of the parameters needed by the calling
      *                         script
      *
