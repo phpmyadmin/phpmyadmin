1a406bec74eea50e3b78b433710426303816108f
akritianand <anand.akriti@gmail.com>
2017-08-27 14:05:58 +0530

    Solved issue number #13617
    Unnecessary data should not come in clipboard copied table - SOLVED

--- js/sql.js
+++ js/sql.js
@@ -259,10 +259,6 @@ AJAX.registerOnload('sql.js', function () {
         textArea.value += '\t\t' + window.location.href;
         textArea.value += '\n';
 
-        $('.notice,.success').each(function(){
-            textArea.value += $(this).text() + '\n\n';
-        });
-
         $('.sql pre').each(function() {
             textArea.value += $(this).text() + '\n\n';
         });
