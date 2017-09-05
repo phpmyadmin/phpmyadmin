205138fdf7fb6a9647c3186fe1d46debabb492da
Michal Čihař <michal@cihar.com>
2017-09-05 12:12:13 +0200

    Add tests for getting all configuration descriptions
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test/classes/config/DescriptionTest.php
+++ test/classes/config/DescriptionTest.php
@@ -45,4 +45,51 @@ class DescriptionTest extends PMATestCase
             ),
         );
     }
+
+    /**
+     * Assertion for getting description key
+     *
+     * @return void
+     */
+    public function assertGet($key)
+    {
+        $this->assertNotNull(Descriptions::get($key, 'name'));
+        $this->assertNotNull(Descriptions::get($key, 'desc'));
+        $this->assertNotNull(Descriptions::get($key, 'cmt'));
+    }
+
+    /**
+     * Test getting all names for configuratons
+     */
+    public function testAll()
+    {
+        $nested = array(
+            'Export',
+            'Import',
+            'Schema',
+            'DBG',
+            'DefaultTransformations',
+            'SQLQuery',
+        );
+
+        $cfg = array();
+        include './libraries/config.default.php';
+        foreach ($cfg as $key => $value) {
+            $this->assertGet($key);
+            if ($key == 'Servers') {
+                foreach ($value[1] as $item => $val) {
+                    $this->assertGet($key . '/1/' . $item);
+                    if ($item == 'AllowDeny') {
+                        foreach ($val as $second => $val2) {
+                            $this->assertGet($key . '/1/' . $item . '/' . $second);
+                        }
+                    }
+                }
+            } elseif (in_array($key, $nested)) {
+                foreach ($value as $item => $val) {
+                    $this->assertGet($key . '/' . $item);
+                }
+            }
+        }
+    }
 }
