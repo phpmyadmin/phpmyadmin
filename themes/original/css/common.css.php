139f9ebc9a1fe9ba52e09ab93da22f1255ccddac
Jakub Stanecki <staneckijakub@gmail.com>
2017-09-11 01:31:32 +0200

    Fix edit index field size bug
    Signed-off-by: Jakub Stanecki <staneckijakub@gmail.com>

--- themes/original/css/common.css.php
+++ themes/original/css/common.css.php
@@ -3349,3 +3349,7 @@ body .ui-dialog .ui-button-text-only .ui-button-text {
         margin: 4px;
     }
 }
+#index_frm > #index_edit_fields > .index_info > #index_columns > tbody > tr > td > select[name="index[columns][names][]"],
+#index_frm > #index_edit_fields > .index_info > div > input[name="index[Key_name]"] {
+    width: 100%;
+}
