3b3e2edc42b66a7285a71c1b6e2558446812ad64
Maurício Meneghini Fauth <mauriciofauth@gmail.com>
2017-09-28 22:09:05 -0300

    Refactor PMA_extractColumnSpec_test
    Move PMA_extractColumnSpec_test to UtilTest
    
    Signed-off-by: Maurício Meneghini Fauth <mauriciofauth@gmail.com>

--- test/classes/UtilTest.php
+++ test/classes/UtilTest.php
@@ -723,4 +723,140 @@ class UtilTest extends \PMATestCase
             array('@PHPMYADMIN@', 'phpMyAdmin PMA_VERSION'),
         );
     }
+
+    /**
+     * Test case for parsing SHOW COLUMNS output
+     *
+     * @param string $in  Column specification
+     * @param array  $out Expected value
+     *
+     * @return void
+     *
+     * @covers PhpMyAdmin\Util::extractColumnSpec
+     * @dataProvider providerExtractColumnSpec
+     */
+    public function testExtractColumnSpec($in, $out)
+    {
+        $GLOBALS['cfg']['LimitChars'] = 1000;
+
+        $this->assertEquals(
+            $out, Util::extractColumnSpec($in)
+        );
+    }
+
+    /**
+     * Data provider for testExtractColumnSpec
+     *
+     * @return array
+     */
+    public function providerExtractColumnSpec()
+    {
+        return array(
+            array(
+                "SET('a','b')",
+                array(
+                    'type' => 'set',
+                    'print_type' => "set('a', 'b')",
+                    'binary' => false,
+                    'unsigned' => false,
+                    'zerofill' => false,
+                    'spec_in_brackets' => "'a','b'",
+                    'enum_set_values' => array('a', 'b'),
+                    'attribute' => ' ',
+                    'can_contain_collation' => true,
+                    'displayed_type' => "set('a', 'b')"
+                ),
+            ),
+            array(
+                "SET('\'a','b')",
+                array(
+                    'type' => 'set',
+                    'print_type' => "set('\'a', 'b')",
+                    'binary' => false,
+                    'unsigned' => false,
+                    'zerofill' => false,
+                    'spec_in_brackets' => "'\'a','b'",
+                    'enum_set_values' => array("'a", 'b'),
+                    'attribute' => ' ',
+                    'can_contain_collation' => true,
+                    'displayed_type' => "set('\'a', 'b')"
+                ),
+            ),
+            array(
+                "SET('''a','b')",
+                array(
+                    'type' => 'set',
+                    'print_type' => "set('''a', 'b')",
+                    'binary' => false,
+                    'unsigned' => false,
+                    'zerofill' => false,
+                    'spec_in_brackets' => "'''a','b'",
+                    'enum_set_values' => array("'a", 'b'),
+                    'attribute' => ' ',
+                    'can_contain_collation' => true,
+                    'displayed_type' => "set('''a', 'b')"
+                ),
+            ),
+            array(
+                "ENUM('a&b', 'b''c\\'d', 'e\\\\f')",
+                array(
+                    'type' => 'enum',
+                    'print_type' => "enum('a&b', 'b''c\\'d', 'e\\\\f')",
+                    'binary' => false,
+                    'unsigned' => false,
+                    'zerofill' => false,
+                    'spec_in_brackets' => "'a&b', 'b''c\\'d', 'e\\\\f'",
+                    'enum_set_values' => array('a&b', 'b\'c\'d', 'e\\f'),
+                    'attribute' => ' ',
+                    'can_contain_collation' => true,
+                    'displayed_type' => "enum('a&amp;b', 'b''c\\'d', 'e\\\\f')"
+                ),
+            ),
+            array(
+                "INT UNSIGNED zerofill",
+                array(
+                    'type' => 'int',
+                    'print_type' => 'int',
+                    'binary' => false,
+                    'unsigned' => true,
+                    'zerofill' => true,
+                    'spec_in_brackets' => '',
+                    'enum_set_values' => array(),
+                    'attribute' => 'UNSIGNED ZEROFILL',
+                    'can_contain_collation' => false,
+                    'displayed_type' => "int"
+                ),
+            ),
+            array(
+                "VARCHAR(255)",
+                array(
+                    'type' => 'varchar',
+                    'print_type' => 'varchar(255)',
+                    'binary' => false,
+                    'unsigned' => false,
+                    'zerofill' => false,
+                    'spec_in_brackets' => '255',
+                    'enum_set_values' => array(),
+                    'attribute' => ' ',
+                    'can_contain_collation' => true,
+                    'displayed_type' => "varchar(255)"
+                ),
+            ),
+            array(
+                "VARBINARY(255)",
+                array(
+                    'type' => 'varbinary',
+                    'print_type' => 'varbinary(255)',
+                    'binary' => false,
+                    'unsigned' => false,
+                    'zerofill' => false,
+                    'spec_in_brackets' => '255',
+                    'enum_set_values' => array(),
+                    'attribute' => ' ',
+                    'can_contain_collation' => false,
+                    'displayed_type' => "varbinary(255)"
+                ),
+            ),
+        );
+    }
 }
