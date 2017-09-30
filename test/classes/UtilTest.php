02f993e4f96f9fd943375fa3e0d25cac2cfbb32c
Maurício Meneghini Fauth <mauriciofauth@gmail.com>
2017-09-30 13:33:45 -0300

    Refactor PMA_showDocu_test
    Move PMA_showDocu_test into UtilTest
    
    Signed-off-by: Maurício Meneghini Fauth <mauriciofauth@gmail.com>

--- test/classes/UtilTest.php
+++ test/classes/UtilTest.php
@@ -2050,4 +2050,22 @@ class UtilTest extends \PMATestCase
             }
         }
     }
+
+    /**
+     * Test for Util::showDocu
+     *
+     * @return void
+     *
+     * @covers PhpMyAdmin\Util::showDocu
+     */
+    public function testShowDocu()
+    {
+        $GLOBALS['server'] = '99';
+        $GLOBALS['cfg']['ServerDefault'] = 1;
+
+        $this->assertEquals(
+            '<a href="./url.php?url=https%3A%2F%2Fdocs.phpmyadmin.net%2Fen%2Flatest%2Fpage.html%23anchor" target="documentation"><img src="themes/dot.gif" title="Documentation" alt="Documentation" class="icon ic_b_help" /></a>',
+            Util::showDocu('page', 'anchor')
+        );
+    }
 }
