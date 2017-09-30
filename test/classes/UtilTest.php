b4eda45cfbbee841e4693f22ec3895f3387e379a
Maurício Meneghini Fauth <mauriciofauth@gmail.com>
2017-09-30 11:33:21 -0300

    Refactor PMA_getDropdown_test
    Move PMA_getDropdown_test into UtilTest
    
    Signed-off-by: Maurício Meneghini Fauth <mauriciofauth@gmail.com>

--- test/classes/UtilTest.php
+++ test/classes/UtilTest.php
@@ -1244,4 +1244,97 @@ class UtilTest extends \PMATestCase
             "<root><div id=\"$id\">\n</div></root>"
         );
     }
+
+    /**
+     * Test for getDropdown
+     *
+     * @return void
+     *
+     * @covers PhpMyAdmin\Util::getDropdown
+     */
+    public function testGetDropdownEmpty()
+    {
+        $name = "test_dropdown_name";
+        $choices = array();
+        $active_choice = null;
+        $id = "test_&lt;dropdown&gt;_name";
+
+        $result = '<select name="' . htmlspecialchars($name) . '" id="'
+            . htmlspecialchars($id) . '">' . "\n" . '</select>' . "\n";
+
+        $this->assertEquals(
+            $result,
+            Util::getDropdown(
+                $name, $choices, $active_choice, $id
+            )
+        );
+    }
+
+    /**
+     * Test for getDropdown
+     *
+     * @return void
+     *
+     * @covers PhpMyAdmin\Util::getDropdown
+     */
+    public function testGetDropdown()
+    {
+        $name = "&test_dropdown_name";
+        $choices = array("value_1" => "label_1", "value&_2\"" => "label_2");
+        $active_choice = null;
+        $id = "test_&lt;dropdown&gt;_name";
+
+        $result = '<select name="' . htmlspecialchars($name) . '" id="'
+            . htmlspecialchars($id) . '">';
+        foreach ($choices as $one_choice_value => $one_choice_label) {
+            $result .= "\n" . '<option value="' . htmlspecialchars($one_choice_value) . '"';
+            if ($one_choice_value == $active_choice) {
+                $result .= ' selected="selected"';
+            }
+            $result .= '>' . htmlspecialchars($one_choice_label) . '</option>';
+        }
+        $result .= "\n" . '</select>' . "\n";
+
+        $this->assertEquals(
+            $result,
+            Util::getDropdown(
+                $name, $choices, $active_choice, $id
+            )
+        );
+    }
+
+    /**
+     * Test for getDropdown
+     *
+     * @return void
+     *
+     * @covers PhpMyAdmin\Util::getDropdown
+     */
+    public function testGetDropdownWithActive()
+    {
+        $name = "&test_dropdown_name";
+        $choices = array("value_1" => "label_1", "value&_2\"" => "label_2");
+        $active_choice = "value&_2\"";
+        $id = "test_&lt;dropdown&gt;_name";
+
+        $result = '<select name="' . htmlspecialchars($name) . '" id="'
+            . htmlspecialchars($id) . '">';
+        foreach ($choices as $one_choice_value => $one_choice_label) {
+            $result .= "\n";
+            $result .= '<option value="' . htmlspecialchars($one_choice_value) . '"';
+            if ($one_choice_value == $active_choice) {
+                $result .= ' selected="selected"';
+            }
+            $result .= '>' . htmlspecialchars($one_choice_label) . '</option>';
+        }
+        $result .= "\n";
+        $result .= '</select>' . "\n";
+
+        $this->assertEquals(
+            $result,
+            Util::getDropdown(
+                $name, $choices, $active_choice, $id
+            )
+        );
+    }
 }
