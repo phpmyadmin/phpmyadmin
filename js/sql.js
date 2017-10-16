446ec298124dc61875d9ba0263ec840a3feef1ff
akritianand <anand.akriti@gmail.com>
2017-10-16 19:14:00 +0530

    Issue#13727
    Replaced 1 with [Asc/Desc]

--- js/sql.js
+++ js/sql.js
@@ -283,11 +283,26 @@ AJAX.registerOnload('sql.js', function () {
 
         $('.sql pre').each(function () {
             textArea.value += $(this).text() + '\n\n';
-        });
+        }); 
 
         $('.table_results .column_heading a').each(function () {
-            textArea.value += $(this).text() + '\t';
-        });
+            //document.getElementsByTagName("small")[38].innerHTML;
+            var res = $(this).text().split("   1");
+                if (typeof res[1] === 'undefined'){
+                    textArea.value += res + '\t';
+                }
+                else {
+                    var order = $(".soimg").attr("alt");
+                        if (order == "Ascending"){
+                            order = "Asc";
+                        }
+                        else {
+                            order = "Desc";
+                        }
+                    res = res[0] + " [" + order + "] " + res[1];
+                    textArea.value += res + '\t';
+                }
+        }); 
 
         textArea.value += '\n';
         $('.table_results tbody tr').each(function () {
