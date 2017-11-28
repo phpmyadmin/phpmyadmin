9ebd88061cc786fb79ccdbea2c3f4c0d520640ed
Michal Čihař <michal@cihar.com>
2017-11-28 10:55:41 +0100

    Set $sql_query as well when changing password
    Issue #13737
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- libraries/server_privileges.lib.php
+++ libraries/server_privileges.lib.php
@@ -1913,7 +1913,7 @@ function PMA_updatePassword($err_url, $username, $hostname)
                 . $GLOBALS['dbi']->escapeString($username)
                 . "'@'" . $GLOBALS['dbi']->escapeString($hostname) . "'"
                 . " = PASSWORD ('";
-            $local_query = $query_prefix
+            $sql_query = $local_query = $query_prefix
                 . $GLOBALS['dbi']->escapeString($_POST['pma_pw']) . "')";
         } else if ($serverType == 'MariaDB'
             && PMA_MYSQL_INT_VERSION >= 50200
