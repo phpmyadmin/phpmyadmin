8f9f8aa3b5dba8df743f3dffea3fb5731acb2e38
Michal Čihař <michal@cihar.com>
2017-12-15 12:35:00 +0100

    Adjust RTE oprations to use POST
    Issue #13866
    
    Signed-off-by: Michal Čihař <michal@cihar.com>
    (cherry picked from commit 2bc87216f0c39e11ea8efd6bedd903aa8ffb51f9)
    Signed-off-by: Maurício Meneghini Fauth <mauriciofauth@gmail.com>

--- libraries/rte/rte_list.lib.php
+++ libraries/rte/rte_list.lib.php
@@ -8,6 +8,7 @@
 use PMA\libraries\Response;
 use PMA\libraries\URL;
 use PMA\libraries\Template;
+use PMA\libraries\Util;
 use PhpMyAdmin\SqlParser\Statements\CreateStatement;
 
 if (! defined('PHPMYADMIN')) {
@@ -305,13 +306,11 @@ function PMA_RTN_getRowForList($routine, $rowclass = '')
     }
     $retval .= "            </td>\n";
     $retval .= "            <td>\n";
-    $retval .= '                <a ' . $ajax_class['drop']
-                                         . ' href="sql.php'
-                                         . $url_query
-                                         . '&amp;sql_query=' . urlencode($sql_drop)
-                                         . '&amp;goto=db_routines.php'
-                                         . urlencode("?db={$db}")
-                                         . '" >' . $titles['Drop'] . "</a>\n";
+    $retval .= Util::linkOrButton(
+        'sql.php' . $url_query . '&amp;sql_query=' . urlencode($sql_drop) . '&amp;goto=db_routines.php' . urlencode("?db={$db}"),
+        $titles['Drop'],
+        ['class' => 'ajax drop_anchor']
+    );
     $retval .= "            </td>\n";
     $retval .= "            <td>\n";
     $retval .= "                 {$routine['type']}\n";
@@ -381,14 +380,11 @@ function PMA_TRI_getRowForList($trigger, $rowclass = '')
     $retval .= "            </td>\n";
     $retval .= "            <td>\n";
     if (PMA\libraries\Util::currentUserHasPrivilege('TRIGGER', $db)) {
-        $retval .= '                <a ' . $ajax_class['drop']
-                                         . ' href="sql.php'
-                                         . $url_query
-                                         . '&amp;sql_query='
-                                         . urlencode($trigger['drop'])
-                                         . '&amp;goto=db_triggers.php'
-                                         . urlencode("?db={$db}")
-                                         . '" >' . $titles['Drop'] . "</a>\n";
+        $retval .= Util::linkOrButton(
+            'sql.php' . $url_query . '&amp;sql_query=' . urlencode($trigger['drop']) . '&amp;goto=db_triggers.php' . urlencode("?db={$db}"),
+            $titles['Drop'],
+            ['class' => 'ajax drop_anchor']
+        );
     } else {
         $retval .= "                {$titles['NoDrop']}\n";
     }
@@ -462,13 +458,11 @@ function PMA_EVN_getRowForList($event, $rowclass = '')
     $retval .= "            </td>\n";
     $retval .= "            <td>\n";
     if (PMA\libraries\Util::currentUserHasPrivilege('EVENT', $db)) {
-        $retval .= '                <a ' . $ajax_class['drop']
-                                         . ' href="sql.php'
-                                         . $url_query
-                                         . '&amp;sql_query=' . urlencode($sql_drop)
-                                         . '&amp;goto=db_events.php'
-                                         . urlencode("?db={$db}")
-                                         . '" >' . $titles['Drop'] . "</a>\n";
+        $retval .= Util::linkOrButton(
+            'sql.php' . $url_query . '&amp;sql_query=' . urlencode($sql_drop) . '&amp;goto=db_events.php' . urlencode("?db={$db}"),
+            $titles['Drop'],
+            ['class' => 'ajax drop_anchor']
+        );
     } else {
         $retval .= "                {$titles['NoDrop']}\n";
     }
