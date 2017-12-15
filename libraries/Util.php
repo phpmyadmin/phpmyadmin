39b8020857bc69be37299ad06b404fb0e7e62ef9
Michal Čihař <michal@cihar.com>
2017-12-15 11:05:54 +0100

    Fixed displaying of formatted numeric values for some locales
    The code was wrongly assuming that . is always decimal separator, what
    it is not and can be even thousands separator.
    
    Fixes #13865
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- libraries/Util.php
+++ libraries/Util.php
@@ -1422,6 +1422,10 @@ class Util
             7 => 'Z',
             8 => 'Y'
         );
+        /* l10n: Decimal separator */
+        $decimal_sep = __('.');
+        /* l10n: Thousands separator */
+        $thousands_sep = __(',');
 
         // check for negative value to retain sign
         if ($value < 0) {
@@ -1459,24 +1463,20 @@ class Util
         $formattedValue = number_format(
             $value,
             $digits_right,
-            /* l10n: Decimal separator */
-            __('.'),
-            /* l10n: Thousands separator */
-            __(',')
+            $decimal_sep,
+            $thousands_sep
         );
         // If we don't want any zeros, remove them now
-        if ($noTrailingZero && strpos($formattedValue, '.') !== false) {
-            $formattedValue = preg_replace('/\.?0+$/', '', $formattedValue);
+        if ($noTrailingZero && strpos($formattedValue, $decimal_sep) !== false) {
+            $formattedValue = preg_replace('/' . preg_quote($decimal_sep) . '?0+$/', '', $formattedValue);
         }
 
         if ($originalValue != 0 && floatval($value) == 0) {
             return ' <' . number_format(
                 (1 / pow(10, $digits_right)),
                 $digits_right,
-                /* l10n: Decimal separator */
-                __('.'),
-                /* l10n: Thousands separator */
-                __(',')
+                $decimal_sep,
+                $thousands_sep
             )
             . ' ' . $unit;
         }
