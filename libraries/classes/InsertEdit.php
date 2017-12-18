32a9d406d4dc407087b0c4d79119270043e3441d
Saksham Gupta <shucon01@gmail.com>
2017-12-19 00:50:25 +0530

    Allow Custom number of rows to be inserted Enhances : https://github.com/phpmyadmin/phpmyadmin/issues/13578
    Signed-off-by: Saksham Gupta <shucon01@gmail.com>

--- libraries/classes/InsertEdit.php
+++ libraries/classes/InsertEdit.php
@@ -1468,18 +1468,8 @@ class InsertEdit
                     . ' value="' . htmlspecialchars(trim($where_clause)) . '" />' . "\n";
             }
         }
-        $tmp = '<select name="insert_rows" id="insert_rows">' . "\n";
-        $option_values = array(1, 2, 5, 10, 15, 20, 30, 40);
-
-        foreach ($option_values as $value) {
-            $tmp .= '<option value="' . $value . '"';
-            if ($value == $GLOBALS['cfg']['InsertRows']) {
-                $tmp .= ' selected="selected"';
-            }
-            $tmp .= '>' . $value . '</option>' . "\n";
-        }
-
-        $tmp .= '</select>' . "\n";
+        $tmp = '<input type="number" name="insert_rows" id="insert_rows">' . "\n";
+        $tmp .= '</input>' . "\n";
         $html_output .= "\n" . sprintf(__('Continue insertion with %s rows'), $tmp);
         unset($tmp);
         $html_output .= '</form>' . "\n";
