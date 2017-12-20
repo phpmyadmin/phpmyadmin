3c1605f4d0770b44e64eb527748d2012b29cc23c
Andreas Grip <andreas@grubblare.com>
2017-12-20 11:36:31 +0100

    Update DatabaseInterface.php
    ignore errors if variable lc_messages is not supported by the server

--- libraries/classes/DatabaseInterface.php
+++ libraries/classes/DatabaseInterface.php
@@ -1446,7 +1446,7 @@ class DatabaseInterface
         /* Locale for messages */
         $locale = LanguageManager::getInstance()->getCurrentLanguage()->getMySQLLocale();
         if (! empty($locale)) {
-            $this->query(
+            $this->tryQuery(
                 "SET lc_messages = '" . $locale . "';",
                 DatabaseInterface::CONNECT_USER,
                 self::QUERY_STORE
