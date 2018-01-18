d4e3ba109e49cc62201c204585d2f584eac647f1
yanping <xyp.xiong@gmail.com>
2018-01-18 11:03:56 -0500

    fix #13124 Table SQL: Align Query and Columns fields
    Signed-off-by: yanping <xyp.xiong@gmail.com>

--- themes/pmahomme/css/common.css.php
+++ themes/pmahomme/css/common.css.php
@@ -1622,6 +1622,7 @@ div#tablefieldscontainer {
 div#tablefieldscontainer select {
     width: 100%;
     background: #fff;
+    height: <?php echo ceil($GLOBALS['cfg']['TextareaRows'] * 1.2)-3.35; ?>em;
     /* height: 12em; */
 }
 
