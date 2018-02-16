e92b3572104dda3d86e7a987f0555aa6d0d54a69
nulll-pointer <nulll-pointer@users.noreply.github.com>
2018-02-16 20:13:10 +0530

    Fix table horizontal scrolling (#14001)
    Fix table horizontal scrolling
    
    Fix #13959

--- themes/original/css/common.css.php
+++ themes/original/css/common.css.php
@@ -3329,6 +3329,10 @@ body .ui-dialog .ui-button-text-only .ui-button-text {
     display: none;
 }
 
+.responsivetable {
+    overflow-x: auto;
+}
+
 @media only screen and (max-width: 768px) {
     /* For mobile phones: */
     #main_pane_left {
