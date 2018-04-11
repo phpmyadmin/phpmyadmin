1a107ac92e3dfe3de2023d8bd78d22fc26a28b32
Maurício Meneghini Fauth <mauriciofauth@gmail.com>
2018-04-11 18:27:29 -0300

    Upgrade to PHP 7.1
    Signed-off-by: Maurício Meneghini Fauth <mauriciofauth@gmail.com>

--- libraries/common.inc.php
+++ libraries/common.inc.php
@@ -55,9 +55,9 @@ if (getcwd() == dirname(__FILE__)) {
  * Minimum PHP version; can't call Core::fatalError() which uses a
  * PHP 5 function, so cannot easily localize this message.
  */
-if (version_compare(PHP_VERSION, '5.5.0', 'lt')) {
+if (version_compare(PHP_VERSION, '7.1.0', 'lt')) {
     die(
-        'PHP 5.5+ is required. <br /> Currently installed version is: '
+        'PHP 7.1+ is required. <br /> Currently installed version is: '
         . phpversion()
     );
 }
--- test/Environment_test.php
+++ test/Environment_test.php
@@ -28,8 +28,8 @@ class Environment_Test extends TestCase
     public function testPhpVersion()
     {
         $this->assertTrue(
-            version_compare('5.5', phpversion(), '<='),
-            'phpMyAdmin requires PHP 5.5 or above'
+            version_compare('7.1.0', phpversion(), '<='),
+            'phpMyAdmin requires PHP 7.1 or above'
         );
     }
 
