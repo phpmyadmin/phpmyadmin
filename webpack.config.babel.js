0e5980426bb5c45f5c1b76ec2496b1febe076803
Piyush Vijay <piyushvijay.1997@gmail.com>
2018-05-19 04:27:30 +0530

    new webpack configuration for exposing global variables as libraries Signed-off-by: Piyush Vijay <piyushvijay.1997@gmail.com>

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,30 +3,33 @@ import webpack from 'webpack';
 
 // let dev server port be 3307
 
+var mode = 'development';
+var module = {
+    rules: [
+        { test: /\.(js)$/, use: 'babel-loader', exclude: /node_modules/ }
+    ]
+};
+var resolve = {
+    extensions: ['.js']
+};
+var devServer = {
+    // port number of dev server
+    port: 3307,
+    // hot: true,
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    }
+};
+var plugins = [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+];
 
-/*'./js/es6/chart.js',
-    './js/es6/common.js',
-    './js/es6/config.js',
-    './js/es6/console.js',
-    './js/es6/cross_framing_protection.js',
-    './js/es6/db_central_columns.js',
-    './js/es6/db_multi_table_query',
-    './js/es6/db_operations.js',
-    './js/es6/db_qbe.js',
-    './js/es6/db_search.js',
-    './js/es6/db_structure.js',
-    './js/es6/db_tracking.js',
-    './js/es6/doclinks.js',
-    './js/es6/error_reporting.js',
-    './js/es6/export_output.js',
-    './js/es6/.js',
-    './js/es6/ajax.js',
-    './js/es6/ajax.js',
-    './js/es6/ajax.js',
-*/
-export default {
+export default [{
     // envionment either development or production
-    mode: 'development',
+    mode: mode,
     entry: {
         ajax: './js/src/ajax.js',
         chart: './js/src/chart.js',
@@ -89,26 +92,31 @@ export default {
         path: path.resolve(__dirname, 'dist'),
         publicPath: 'http://localhost:3007/dist'
     },
-    module: {
-        rules: [
-            { test: /\.(js)$/, use: 'babel-loader', exclude: /node_modules/ },
-        ],
-    },
+    module: module,
     // devtool: 'source-map',
-    resolve: {
-        extensions: ['.js'],
+    resolve: resolve,
+    devServer: devServer,
+    plugins: plugins
+},
+{
+    // envionment either development or production
+    mode: 'development',
+    entry: {
+        pma_common_params: './js/src/global/pma_common_params',
+        ajax_global: './js/src/global/ajax_global.js',
+        jQuery: './js/src/global/jquery.js'
     },
-    devServer: {
-        port: 3307,
-        // hot: true,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-        },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'js/dist/global'),
+        // url on which dev server will run
+        publicPath: 'http://localhost:3007/js/dist/global/',
+        library: '[name]',
+        libraryTarget: 'umd'
     },
-    plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
-    ],
-}
+    module: module,
+    // devtool: 'source-map',
+    resolve: resolve,
+    devServer: devServer,
+    plugins: plugins
+}];
