a349504b0eb859a798cbe7c4bfd35f1a4d8c4199
Isaac Bennetch <bennetch@gmail.com>
2018-05-24 22:37:57 -0400

    Prepare for 4.8.1 release
    Signed-off-by: Isaac Bennetch <bennetch@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -51,7 +51,7 @@ copyright = u'2012 - 2018, The phpMyAdmin devel team'
 # built documents.
 #
 # The short X.Y version.
-version = '4.8.1-dev'
+version = '4.8.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
