cc8398379f3cff3a2f210a3def6cdbce82cd6c16
William Desportes <williamdes@wdes.fr>
2018-07-01 20:45:39 +0200

    Fix 'Variable $destImage might not be defined.'
    ------ -------------------------------------------
      Line   transformation_wrapper.php
     ------ -------------------------------------------
      158    Variable $destImage might not be defined.
      171    Variable $destImage might not be defined.
      174    Variable $destImage might not be defined.
      177    Variable $destImage might not be defined.
     ------ -------------------------------------------
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- transformation_wrapper.php
+++ transformation_wrapper.php
@@ -149,30 +149,29 @@ if (! isset($_REQUEST['resize'])) {
 
     if ($_REQUEST['resize']) {
         $destImage = imagecreatetruecolor($destWidth, $destHeight);
-    }
 
-    // ImageCopyResized($destImage, $srcImage, 0, 0, 0, 0,
-    // $destWidth, $destHeight, $srcWidth, $srcHeight);
-    // better quality but slower:
-    imagecopyresampled(
-        $destImage,
-        $srcImage,
-        0,
-        0,
-        0,
-        0,
-        $destWidth,
-        $destHeight,
-        $srcWidth,
-        $srcHeight
-    );
-
-    if ($_REQUEST['resize'] == 'jpeg') {
-        imagejpeg($destImage, null, 75);
-    }
-    if ($_REQUEST['resize'] == 'png') {
-        imagepng($destImage);
+        // ImageCopyResized($destImage, $srcImage, 0, 0, 0, 0,
+        // $destWidth, $destHeight, $srcWidth, $srcHeight);
+        // better quality but slower:
+        imagecopyresampled(
+            $destImage,
+            $srcImage,
+            0,
+            0,
+            0,
+            0,
+            $destWidth,
+            $destHeight,
+            $srcWidth,
+            $srcHeight
+        );
+        if ($_REQUEST['resize'] == 'jpeg') {
+            imagejpeg($destImage, null, 75);
+        }
+        if ($_REQUEST['resize'] == 'png') {
+            imagepng($destImage);
+        }
+        imagedestroy($destImage);
     }
     imagedestroy($srcImage);
-    imagedestroy($destImage);
 }
