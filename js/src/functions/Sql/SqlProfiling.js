c9ac456d970c14ae3e3a35155d71ead3d2e9dc87
Piyush Vijay <piyushvijay.1997@gmail.com>
2018-07-13 09:12:36 +0530

    Separating basic functions needed for initialising codemirror and inline codemirror, saving and getting saved queries and making profiling charts. Signed-Off-By: Piyush Vijay <piyushvijay.1997@gmail.com>

---
+++ js/src/functions/Sql/SqlProfiling.js
@@ -0,0 +1,57 @@
+import { createProfilingChart } from '../chart';
+
+/*
+ * Profiling Chart
+ */
+export function makeProfilingChart () {
+    if ($('#profilingchart').length === 0 ||
+        $('#profilingchart').html().length !== 0 ||
+        !$.jqplot || !$.jqplot.Highlighter || !$.jqplot.PieRenderer
+    ) {
+        return;
+    }
+
+    var data = [];
+    $.each(JSON.parse($('#profilingChartData').html()), function (key, value) {
+        data.push([key, parseFloat(value)]);
+    });
+
+    // Remove chart and data divs contents
+    $('#profilingchart').html('').show();
+    $('#profilingChartData').html('');
+
+    createProfilingChart('profilingchart', data);
+}
+
+/*
+ * initialize profiling data tables
+ */
+export function initProfilingTables () {
+    if (!$.tablesorter) {
+        return;
+    }
+
+    $('#profiletable').tablesorter({
+        widgets: ['zebra'],
+        sortList: [[0, 0]],
+        textExtraction: function (node) {
+            if (node.children.length > 0) {
+                return node.children[0].innerHTML;
+            } else {
+                return node.innerHTML;
+            }
+        }
+    });
+
+    $('#profilesummarytable').tablesorter({
+        widgets: ['zebra'],
+        sortList: [[1, 1]],
+        textExtraction: function (node) {
+            if (node.children.length > 0) {
+                return node.children[0].innerHTML;
+            } else {
+                return node.innerHTML;
+            }
+        }
+    });
+}
