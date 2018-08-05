65fb4202c5a015855906722b182910dfafec1929
Piyush Vijay <piyushvijay.1997@gmail.com>
2018-08-06 01:55:08 +0530

    Edit php files for changes related to tbl_change.js Signed-Off-By: Piyush Vijay <piyushvijay.1997@gmail.com>

--- libraries/classes/Controllers/Table/TableSearchController.php
+++ libraries/classes/Controllers/Table/TableSearchController.php
@@ -213,10 +213,9 @@ class TableSearchController extends TableController
                 ->getScripts()
                 ->addFiles(
                     [
-                        'makegrid.js',
-                        'sql.js',
+                        'sql',
                         'tbl_select.js',
-                        'tbl_change.js',
+                        'tbl_change',
                         'vendor/jquery/jquery.uitablefilter.js',
                         'gis_data_editor.js',
                     ]
@@ -245,8 +244,7 @@ class TableSearchController extends TableController
                 ->getScripts()
                 ->addFiles(
                     [
-                        'makegrid.js',
-                        'sql.js',
+                        'sql',
                         'vendor/jqplot/jquery.jqplot.js',
                         'vendor/jqplot/plugins/jqplot.canvasTextRenderer.js',
                         'vendor/jqplot/plugins/jqplot.canvasAxisLabelRenderer.js',
@@ -254,7 +252,7 @@ class TableSearchController extends TableController
                         'vendor/jqplot/plugins/jqplot.highlighter.js',
                         'vendor/jqplot/plugins/jqplot.cursor.js',
                         'tbl_zoom_plot_jqplot.js',
-                        'tbl_change.js',
+                        'tbl_change',
                     ]
                 );
 
