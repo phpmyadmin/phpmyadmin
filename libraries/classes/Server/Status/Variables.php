6d5719d8c2ec782b84a13811336251535b7524b1
Maurício Meneghini Fauth <mauriciofauth@gmail.com>
2018-11-03 14:59:08 -0300

    Retrieve parameters from $_POST in server_status_variables.php
    Signed-off-by: Maurício Meneghini Fauth <mauriciofauth@gmail.com>

--- libraries/classes/Server/Status/Variables.php
+++ libraries/classes/Server/Status/Variables.php
@@ -30,23 +30,23 @@ class Variables
     public static function getHtmlForFilter(Data $serverStatusData)
     {
         $filterAlert = '';
-        if (! empty($_REQUEST['filterAlert'])) {
+        if (! empty($_POST['filterAlert'])) {
             $filterAlert = ' checked="checked"';
         }
         $filterText = '';
-        if (! empty($_REQUEST['filterText'])) {
-            $filterText = htmlspecialchars($_REQUEST['filterText']);
+        if (! empty($_POST['filterText'])) {
+            $filterText = htmlspecialchars($_POST['filterText']);
         }
         $dontFormat = '';
-        if (! empty($_REQUEST['dontFormat'])) {
+        if (! empty($_POST['dontFormat'])) {
             $dontFormat = ' checked="checked"';
         }
 
         $retval  = '';
         $retval .= '<fieldset id="tableFilter">';
         $retval .= '<legend>' . __('Filters') . '</legend>';
-        $retval .= '<form action="server_status_variables.php'
-            . Url::getCommon() . '">';
+        $retval .= '<form action="server_status_variables.php" method="post">';
+        $retval .= Url::getHiddenInputs();
         $retval .= '<input type="submit" value="' . __('Refresh') . '" />';
         $retval .= '<div class="formelement">';
         $retval .= '<label for="filterText">' . __('Containing the word:') . '</label>';
@@ -66,8 +66,8 @@ class Variables
 
         foreach ($serverStatusData->sections as $section_id => $section_name) {
             if (isset($serverStatusData->sectionUsed[$section_id])) {
-                if (! empty($_REQUEST['filterCategory'])
-                    && $_REQUEST['filterCategory'] == $section_id
+                if (! empty($_POST['filterCategory'])
+                    && $_POST['filterCategory'] == $section_id
                 ) {
                     $selected = ' selected="selected"';
                 } else {
@@ -113,7 +113,8 @@ class Variables
                 if ('doc' == $link_name) {
                     $retval .= Util::showMySQLDocu($link_url);
                 } else {
-                    $retval .= '<a href="' . $link_url . '">' . $link_name . '</a>';
+                    $retval .= '<a href="' . $link_url['url'] . '" data-post="' . $link_url['params'] . '">'
+                        . $link_name . '</a>';
                 }
                 $i++;
             }
@@ -309,7 +310,8 @@ class Variables
                     if ('doc' == $link_name) {
                         $retval .= Util::showMySQLDocu($link_url);
                     } else {
-                        $retval .= ' <a href="' . $link_url . '">' . $link_name . '</a>';
+                        $retval .= ' <a href="' . $link_url['url'] . '" data-post="' . $link_url['params'] . '">'
+                            . $link_name . '</a>';
                     }
                 }
                 unset($link_url, $link_name);
