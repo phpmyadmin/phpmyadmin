4f8b9527ee3b58b326c09144a016d92249780a9a
mokraemer <github@mokraemer.de>
2018-11-09 15:18:26 +0100

    Fix left menu not showing on Opera 12.16 (#14619)
    Left database tree is not shown if size computes to 0 or less. This will ensure to have a min height.
    
    Fixes #14591 
    
    Signed-off-by: Marc Krämer <github@mokraemer.de>

--- js/navigation.js
+++ js/navigation.js
@@ -1301,9 +1301,12 @@ var ResizeHandler = function () {
         var $nav_tree   = $('#pma_navigation_tree');
         var $nav_header = $('#pma_navigation_header');
         var $nav_tree_content = $('#pma_navigation_tree_content');
-        $nav_tree.height($nav.height() - $nav_header.height());
+				var height = ($nav.height() - $nav_header.height());
+
+				height = height > 50 ? height : 800; //keep min. height
+        $nav_tree.height(height);
         if ($nav_tree_content.length > 0) {
-            $nav_tree_content.height($nav_tree.height() - $nav_tree_content.position().top);
+            $nav_tree_content.height(height - $nav_tree_content.position().top);
         } else {
             // TODO: in fast filter search response there is no #pma_navigation_tree_content, needs to be added in php
             $nav_tree.css({
