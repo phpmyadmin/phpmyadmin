34972f0132c6e04fc324ad422f2fc609df7a22ec
Maurício Meneghini Fauth <mauriciofauth@gmail.com>
2018-11-10 17:54:44 -0200

    Retrieve parameters from $_POST in server binlog
    Signed-off-by: Maurício Meneghini Fauth <mauriciofauth@gmail.com>

--- libraries/classes/Controllers/Server/ServerBinlogController.php
+++ libraries/classes/Controllers/Server/ServerBinlogController.php
@@ -55,15 +55,15 @@ class ServerBinlogController extends Controller
         include_once 'libraries/server_common.inc.php';
 
         $url_params = array();
-        if (! isset($_REQUEST['log'])
-            || ! array_key_exists($_REQUEST['log'], $this->binary_logs)
+        if (! isset($_POST['log'])
+            || ! array_key_exists($_POST['log'], $this->binary_logs)
         ) {
-            $_REQUEST['log'] = '';
+            $_POST['log'] = '';
         } else {
-            $url_params['log'] = $_REQUEST['log'];
+            $url_params['log'] = $_POST['log'];
         }
 
-        if (!empty($_REQUEST['dontlimitchars'])) {
+        if (!empty($_POST['dontlimitchars'])) {
             $url_params['dontlimitchars'] = 1;
         }
 
@@ -89,7 +89,7 @@ class ServerBinlogController extends Controller
             array(
                 'url_params' => $url_params,
                 'binary_logs' => $this->binary_logs,
-                'log' => $_REQUEST['log'],
+                'log' => $_POST['log'],
             )
         );
     }
@@ -106,16 +106,16 @@ class ServerBinlogController extends Controller
         /**
          * Need to find the real end of rows?
          */
-        if (! isset($_REQUEST['pos'])) {
+        if (! isset($_POST['pos'])) {
             $pos = 0;
         } else {
             /* We need this to be a integer */
-            $pos = (int) $_REQUEST['pos'];
+            $pos = (int) $_POST['pos'];
         }
 
         $sql_query = 'SHOW BINLOG EVENTS';
-        if (! empty($_REQUEST['log'])) {
-            $sql_query .= ' IN \'' . $_REQUEST['log'] . '\'';
+        if (! empty($_POST['log'])) {
+            $sql_query .= ' IN \'' . $_POST['log'] . '\'';
         }
         $sql_query .= ' LIMIT ' . $pos . ', ' . intval($GLOBALS['cfg']['MaxRows']);
 
@@ -134,7 +134,7 @@ class ServerBinlogController extends Controller
             $num_rows = 0;
         }
 
-        if (empty($_REQUEST['dontlimitchars'])) {
+        if (empty($_POST['dontlimitchars'])) {
             $dontlimitchars = false;
         } else {
             $dontlimitchars = true;
@@ -192,8 +192,8 @@ class ServerBinlogController extends Controller
                 $this_url_params['pos'] = $pos - $GLOBALS['cfg']['MaxRows'];
             }
 
-            $html .= '<a href="server_binlog.php'
-                . Url::getCommon($this_url_params) . '"';
+            $html .= '<a href="server_binlog.php" data-post="'
+                . Url::getCommon($this_url_params, '') . '"';
             if (Util::showIcons('TableNavigationLinksMode')) {
                 $html .= ' title="' . _pgettext('Previous page', 'Previous') . '">';
             } else {
@@ -215,7 +215,7 @@ class ServerBinlogController extends Controller
             $tempTitle = __('Show Full Queries');
             $tempImgMode = 'full';
         }
-        $html .= '<a href="server_binlog.php' . Url::getCommon($this_url_params)
+        $html .= '<a href="server_binlog.php" data-post="' . Url::getCommon($this_url_params, '')
             . '" title="' . $tempTitle . '">'
             . '<img src="' . $GLOBALS['pmaThemeImage'] . 's_' . $tempImgMode
             . 'text.png" alt="' . $tempTitle . '" /></a>';
@@ -225,8 +225,8 @@ class ServerBinlogController extends Controller
         if ($num_rows >= $GLOBALS['cfg']['MaxRows']) {
             $this_url_params = $url_params;
             $this_url_params['pos'] = $pos + $GLOBALS['cfg']['MaxRows'];
-            $html .= ' - <a href="server_binlog.php'
-                . Url::getCommon($this_url_params)
+            $html .= ' - <a href="server_binlog.php" data-post="'
+                . Url::getCommon($this_url_params, '')
                 . '"';
             if (Util::showIcons('TableNavigationLinksMode')) {
                 $html .= ' title="' . _pgettext('Next page', 'Next') . '">';
