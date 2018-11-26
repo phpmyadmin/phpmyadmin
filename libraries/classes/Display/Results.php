5e8642b387b4ba6126cff0fa16496dc814b8c6ef
Maurício Meneghini Fauth <mauriciofauth@gmail.com>
2018-11-26 13:40:00 -0200

    Fix DocBlocks issues
    Signed-off-by: Maurício Meneghini Fauth <mauriciofauth@gmail.com>

--- libraries/classes/Display/Results.php
+++ libraries/classes/Display/Results.php
@@ -1389,9 +1389,9 @@ class Results
     /**
      * Prepare sort by key dropdown - html code segment
      *
-     * @param Index[]     $indexes            the indexes of the table for sort criteria
-     * @param array|null  $sort_expression    the sort expression
-     * @param string      $unsorted_sql_query the unsorted sql query
+     * @param Index[]    $indexes            the indexes of the table for sort criteria
+     * @param array|null $sort_expression    the sort expression
+     * @param string     $unsorted_sql_query the unsorted sql query
      *
      * @return  string  $drop_down_html         html content
      *
