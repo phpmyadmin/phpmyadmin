b293ff5f234ef493336ed8638f623a12164d359e
Eddie Chang <eddietcchang@gmail.com>
2018-12-07 16:28:48 +1100

    Fix https://github.com/phpmyadmin/phpmyadmin-security/issues/260 Stored Cross-Site Scripting (XSS) in navigation tree
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

--- libraries/classes/Navigation/NavigationTree.php
+++ libraries/classes/Navigation/NavigationTree.php
@@ -762,7 +762,7 @@ class NavigationTree
                 }
 
                 $groups[$key] = new Node(
-                    $key,
+                    htmlspecialchars($key),
                     Node::CONTAINER,
                     true
                 );
