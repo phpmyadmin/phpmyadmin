phpmyadmin/phpmyadmin

commit c1cdaac2f465dd6b9e17f9f35fd46861ad703a6d

Retrieve parameters from $_POST in export
Signed-off-by: Maurício Meneghini Fauth <mauriciofauth@gmail.com>

export.php
@@ -227,39 +227,39 @@ $filename = '';
 $separate_files = '';
 
 // Is it a quick or custom export?
-if (isset($_REQUEST['quick_or_custom'])
-    && $_REQUEST['quick_or_custom'] == 'quick'
+if (isset($_POST['quick_or_custom'])
+    && $_POST['quick_or_custom'] == 'quick'
 ) {
     $quick_export = true;
 } else {
     $quick_export = false;
 }
 
-if ($_REQUEST['output_format'] == 'astext') {
+if ($_POST['output_format'] == 'astext') {
     $asfile = false;
 } else {
     $asfile = true;
-    if (isset($_REQUEST['as_separate_files'])
-        && ! empty($_REQUEST['as_separate_files'])
+    if (isset($_POST['as_separate_files'])
+        && ! empty($_POST['as_separate_files'])
     ) {
-        if (isset($_REQUEST['compression'])
-            && ! empty($_REQUEST['compression'])
-            && $_REQUEST['compression'] == 'zip'
+        if (isset($_POST['compression'])
+            && ! empty($_POST['compression'])
+            && $_POST['compression'] == 'zip'
         ) {
-            $separate_files = $_REQUEST['as_separate_files'];
+            $separate_files = $_POST['as_separate_files'];
         }
     }
-    if (in_array($_REQUEST['compression'], $compression_methods)) {
-        $compression = $_REQUEST['compression'];
+    if (in_array($_POST['compression'], $compression_methods)) {
+        $compression = $_POST['compression'];
         $buffer_needed = true;
     }
-    if (($quick_export && ! empty($_REQUEST['quick_export_onserver']))
-        || (! $quick_export && ! empty($_REQUEST['onserver']))
+    if (($quick_export && ! empty($_POST['quick_export_onserver']))
+        || (! $quick_export && ! empty($_POST['onserver']))
     ) {
         if ($quick_export) {
-            $onserver = $_REQUEST['quick_export_onserver'];
+            $onserver = $_POST['quick_export_onserver'];
         } else {
-            $onserver = $_REQUEST['onserver'];
+            $onserver = $_POST['onserver'];
         }
         // Will we save dump on server?
         $save_on_server = ! empty($cfg['SaveDir']) && $onserver;
@@ -297,9 +297,9 @@ if ((!empty($parser->statements[0]))
 ) {
     $aliases = \PhpMyAdmin\SqlParser\Utils\Misc::getAliases($parser->statements[0], $db);
 }
-if (!empty($_REQUEST['aliases'])) {
-    $aliases = Export::mergeAliases($aliases, $_REQUEST['aliases']);
-    $_SESSION['tmpval']['aliases'] = $_REQUEST['aliases'];
+if (!empty($_POST['aliases'])) {
+    $aliases = Export::mergeAliases($aliases, $_POST['aliases']);
+    $_SESSION['tmpval']['aliases'] = $_POST['aliases'];
 }
 
 /**
@@ -446,7 +446,7 @@ do {
         if (!isset($table_data) || !is_array($table_data)) {
             $table_data = array();
         }
-        if (!empty($_REQUEST['structure_or_data_forced'])) {
+        if (!empty($_POST['structure_or_data_forced'])) {
             $table_structure = $tables;
             $table_data = $tables;
         }

libraries/classes/Export.php
@@ -341,9 +341,9 @@ class Export
             . preg_replace('@[/\\\\]@', '_', $filename);
 
         if (@file_exists($save_filename)
-            && ((! $quick_export && empty($_REQUEST['onserver_overwrite']))
+            && ((! $quick_export && empty($_POST['onserver_overwrite']))
             || ($quick_export
-            && $_REQUEST['quick_export_onserver_overwrite'] != 'saveitover'))
+            && $_POST['quick_export_onserver_overwrite'] != 'saveitover'))
         ) {
             $message = Message::error(
                 __(
@@ -469,45 +469,45 @@ class Export
         $html = '<div>';
 
         /**
-         * Displays a back button with all the $_REQUEST data in the URL
+         * Displays a back button with all the $_POST data in the URL
          * (store in a variable to also display after the textarea)
          */
         $back_button = '<p id="export_back_button">[ <a href="';
         if ($export_type == 'server') {
-            $back_button .= 'server_export.php' . Url::getCommon();
+            $back_button .= 'server_export.php" data-post="' . Url::getCommon([], '');
         } elseif ($export_type == 'database') {
-            $back_button .= 'db_export.php' . Url::getCommon(array('db' => $db));
+            $back_button .= 'db_export.php" data-post="' . Url::getCommon(array('db' => $db), '');
         } else {
-            $back_button .= 'tbl_export.php' . Url::getCommon(
+            $back_button .= 'tbl_export.php" data-post="' . Url::getCommon(
                 array(
                     'db' => $db, 'table' => $table
-                )
+                ), ''
             );
         }
 
         // Convert the multiple select elements from an array to a string
-        if ($export_type == 'server' && isset($_REQUEST['db_select'])) {
-            $_REQUEST['db_select'] = implode(",", $_REQUEST['db_select']);
+        if ($export_type == 'server' && isset($_POST['db_select'])) {
+            $_POST['db_select'] = implode(",", $_POST['db_select']);
         } elseif ($export_type == 'database') {
-            if (isset($_REQUEST['table_select'])) {
-                $_REQUEST['table_select'] = implode(",", $_REQUEST['table_select']);
+            if (isset($_POST['table_select'])) {
+                $_POST['table_select'] = implode(",", $_POST['table_select']);
             }
-            if (isset($_REQUEST['table_structure'])) {
-                $_REQUEST['table_structure'] = implode(
+            if (isset($_POST['table_structure'])) {
+                $_POST['table_structure'] = implode(
                     ",",
-                    $_REQUEST['table_structure']
+                    $_POST['table_structure']
                 );
-            } elseif (empty($_REQUEST['structure_or_data_forced'])) {
-                $_REQUEST['table_structure'] = '';
+            } elseif (empty($_POST['structure_or_data_forced'])) {
+                $_POST['table_structure'] = '';
             }
-            if (isset($_REQUEST['table_data'])) {
-                $_REQUEST['table_data'] = implode(",", $_REQUEST['table_data']);
-            } elseif (empty($_REQUEST['structure_or_data_forced'])) {
-                $_REQUEST['table_data'] = '';
+            if (isset($_POST['table_data'])) {
+                $_POST['table_data'] = implode(",", $_POST['table_data']);
+            } elseif (empty($_POST['structure_or_data_forced'])) {
+                $_POST['table_data'] = '';
             }
         }
 
-        foreach ($_REQUEST as $name => $value) {
+        foreach ($_POST as $name => $value) {
             if (!is_array($value)) {
                 $back_button .= '&amp;' . urlencode($name) . '=' . urlencode($value);
             }

libraries/classes/Display/Export.php
@@ -62,11 +62,11 @@ class Export
      */
     public function getHtmlForSelectOptions($tmpSelect = '')
     {
-        // Check if the selected databases are defined in $_GET
+        // Check if the selected databases are defined in $_POST
         // (from clicking Back button on export.php)
-        if (isset($_GET['db_select'])) {
-            $_GET['db_select'] = urldecode($_GET['db_select']);
-            $_GET['db_select'] = explode(",", $_GET['db_select']);
+        if (isset($_POST['db_select'])) {
+            $_POST['db_select'] = urldecode($_POST['db_select']);
+            $_POST['db_select'] = explode(",", $_POST['db_select']);
         }
 
         $databases = [];
@@ -75,8 +75,8 @@ class Export
                 continue;
             }
             $isSelected = false;
-            if (isset($_GET['db_select'])) {
-                if (in_array($currentDb, $_GET['db_select'])) {
+            if (isset($_POST['db_select'])) {
+                if (in_array($currentDb, $_POST['db_select'])) {
                     $isSelected = true;
                 }
             } elseif (!empty($tmpSelect)) {
@@ -121,14 +121,14 @@ class Export
         global $cfg;
 
         // If the export method was not set, the default is quick
-        if (isset($_GET['export_method'])) {
-            $cfg['Export']['method'] = $_GET['export_method'];
+        if (isset($_POST['export_method'])) {
+            $cfg['Export']['method'] = $_POST['export_method'];
         } elseif (! isset($cfg['Export']['method'])) {
             $cfg['Export']['method'] = 'quick';
         }
 
-        if (empty($sqlQuery) && isset($_GET['sql_query'])) {
-            $sqlQuery = $_GET['sql_query'];
+        if (empty($sqlQuery) && isset($_POST['sql_query'])) {
+            $sqlQuery = $_POST['sql_query'];
         }
 
         return Template::get('display/export/hidden_inputs')->render([
@@ -138,7 +138,7 @@ class Export
             'export_method' => $cfg['Export']['method'],
             'single_table' => $singleTable,
             'sql_query' => $sqlQuery,
-            'template_id' => isset($_GET['template_id']) ? $_GET['template_id'] : '',
+            'template_id' => isset($_POST['template_id']) ? $_POST['template_id'] : '',
         ]);
     }
 
@@ -176,7 +176,7 @@ class Export
 
         return Template::get('display/export/template_options')->render([
             'templates' => $templates,
-            'selected_template' => !empty($_GET['template_id']) ? $_GET['template_id'] : null,
+            'selected_template' => !empty($_POST['template_id']) ? $_POST['template_id'] : null,
         ]);
     }
 
@@ -188,8 +188,8 @@ class Export
     private function getHtmlForOptionsMethod()
     {
         global $cfg;
-        if (isset($_GET['quick_or_custom'])) {
-            $exportMethod = $_GET['quick_or_custom'];
+        if (isset($_POST['quick_or_custom'])) {
+            $exportMethod = $_POST['quick_or_custom'];
         } else {
             $exportMethod = $cfg['Export']['method'];
         }
@@ -264,9 +264,9 @@ class Export
         $numberOfRows = $tableObject->countRecords();
 
         return Template::get('display/export/options_rows')->render([
-            'allrows' => isset($_GET['allrows']) ? $_GET['allrows'] : null,
-            'limit_to' => isset($_GET['limit_to']) ? $_GET['limit_to'] : null,
-            'limit_from' => isset($_GET['limit_from']) ? $_GET['limit_from'] : null,
+            'allrows' => isset($_POST['allrows']) ? $_POST['allrows'] : null,
+            'limit_to' => isset($_POST['limit_to']) ? $_POST['limit_to'] : null,
+            'limit_from' => isset($_POST['limit_from']) ? $_POST['limit_from'] : null,
             'unlim_num_rows' => $unlimNumRows,
             'number_of_rows' => $numberOfRows,
         ]);
@@ -357,8 +357,8 @@ class Export
         );
         $msg->addParamHtml('</a>');
 
-        if (isset($_GET['filename_template'])) {
-            $filenameTemplate = $_GET['filename_template'];
+        if (isset($_POST['filename_template'])) {
+            $filenameTemplate = $_POST['filename_template'];
         } else {
             if ($exportType == 'database') {
                 $filenameTemplate = $GLOBALS['PMA_Config']->getUserValue(
@@ -408,8 +408,8 @@ class Export
     private function getHtmlForOptionsOutputCompression()
     {
         global $cfg;
-        if (isset($_GET['compression'])) {
-            $selectedCompression = $_GET['compression'];
+        if (isset($_POST['compression'])) {
+            $selectedCompression = $_POST['compression'];
         } elseif (isset($cfg['Export']['compression'])) {
             $selectedCompression = $cfg['Export']['compression'];
         } else {
@@ -442,7 +442,7 @@ class Export
     private function getHtmlForOptionsOutputRadio()
     {
         return Template::get('display/export/options_output_radio')->render([
-            'has_repopulate' => isset($_GET['repopulate']),
+            'has_repopulate' => isset($_POST['repopulate']),
             'export_asfile' => $GLOBALS['cfg']['Export']['asfile'],
         ]);
     }
@@ -505,8 +505,8 @@ class Export
             'export_type' => $exportType,
             'is_checked_lock_tables' => $isCheckedLockTables,
             'is_checked_asfile' => $isCheckedAsfile,
-            'repopulate' => isset($_GET['repopulate']),
-            'lock_tables' => isset($_GET['lock_tables']),
+            'repopulate' => isset($_POST['repopulate']),
+            'lock_tables' => isset($_POST['lock_tables']),
             'save_dir' => isset($cfg['SaveDir']) ? $cfg['SaveDir'] : null,
             'is_encoding_supported' => Encoding::isSupported(),
             'options_output_save_dir' => $optionsOutputSaveDir,
@@ -658,8 +658,8 @@ class Export
     ) {
         $cfgRelation = $this->relation->getRelationsParam();
 
-        if (isset($_REQUEST['single_table'])) {
-            $GLOBALS['single_table'] = $_REQUEST['single_table'];
+        if (isset($_POST['single_table'])) {
+            $GLOBALS['single_table'] = $_POST['single_table'];
         }
 
         /* Scan for plugins */
@@ -731,8 +731,8 @@ class Export
      */
     public function handleTemplateActions(array $cfgRelation)
     {
-        if (isset($_REQUEST['templateId'])) {
-            $id = $GLOBALS['dbi']->escapeString($_REQUEST['templateId']);
+        if (isset($_POST['templateId'])) {
+            $id = $GLOBALS['dbi']->escapeString($_POST['templateId']);
         } else {
             $id = '';
         }
@@ -741,16 +741,16 @@ class Export
            . Util::backquote($cfgRelation['export_templates']);
         $user = $GLOBALS['dbi']->escapeString($GLOBALS['cfg']['Server']['user']);
 
-        switch ($_REQUEST['templateAction']) {
+        switch ($_POST['templateAction']) {
         case 'create':
             $query = "INSERT INTO " . $templateTable . "("
                 . " `username`, `export_type`,"
                 . " `template_name`, `template_data`"
                 . ") VALUES ("
                 . "'" . $user . "', "
-                . "'" . $GLOBALS['dbi']->escapeString($_REQUEST['exportType'])
-                . "', '" . $GLOBALS['dbi']->escapeString($_REQUEST['templateName'])
-                . "', '" . $GLOBALS['dbi']->escapeString($_REQUEST['templateData'])
+                . "'" . $GLOBALS['dbi']->escapeString($_POST['exportType'])
+                . "', '" . $GLOBALS['dbi']->escapeString($_POST['templateName'])
+                . "', '" . $GLOBALS['dbi']->escapeString($_POST['templateData'])
                 . "');";
             break;
         case 'load':
@@ -759,7 +759,7 @@ class Export
             break;
         case 'update':
             $query = "UPDATE " . $templateTable . " SET `template_data` = "
-              . "'" . $GLOBALS['dbi']->escapeString($_REQUEST['templateData']) . "'"
+              . "'" . $GLOBALS['dbi']->escapeString($_POST['templateData']) . "'"
               . " WHERE `id` = " . $id  . " AND `username` = '" . $user . "'";
             break;
         case 'delete':
@@ -782,12 +782,12 @@ class Export
         }
 
         $response->setRequestStatus(true);
-        if ('create' == $_REQUEST['templateAction']) {
+        if ('create' == $_POST['templateAction']) {
             $response->addJSON(
                 'data',
-                $this->getOptionsForTemplates($_REQUEST['exportType'])
+                $this->getOptionsForTemplates($_POST['exportType'])
             );
-        } elseif ('load' == $_REQUEST['templateAction']) {
+        } elseif ('load' == $_POST['templateAction']) {
             $data = null;
             while ($row = $GLOBALS['dbi']->fetchAssoc(
                 $result, DatabaseInterface::CONNECT_CONTROL

db_export.php
@@ -56,8 +56,8 @@ if ($num_tables < 1) {
 } // end if
 
 $multi_values  = '<div class="export_table_list_container">';
-if (isset($_GET['structure_or_data_forced'])) {
-    $force_val = htmlspecialchars($_GET['structure_or_data_forced']);
+if (isset($_POST['structure_or_data_forced'])) {
+    $force_val = htmlspecialchars($_POST['structure_or_data_forced']);
 } else {
     $force_val = 0;
 }
@@ -84,19 +84,19 @@ if (!empty($_POST['selected_tbl']) && empty($table_select)) {
     $table_select = $_POST['selected_tbl'];
 }
 
-// Check if the selected tables are defined in $_GET
+// Check if the selected tables are defined in $_POST
 // (from clicking Back button on export.php)
 foreach (array('table_select', 'table_structure', 'table_data') as $one_key) {
-    if (isset($_GET[$one_key])) {
-        $_GET[$one_key] = urldecode($_GET[$one_key]);
-        $_GET[$one_key] = explode(",", $_GET[$one_key]);
+    if (isset($_POST[$one_key])) {
+        $_POST[$one_key] = urldecode($_POST[$one_key]);
+        $_POST[$one_key] = explode(",", $_POST[$one_key]);
     }
 }
 
 foreach ($tables as $each_table) {
-    if (isset($_GET['table_select']) && is_array($_GET['table_select'])) {
+    if (isset($_POST['table_select']) && is_array($_POST['table_select'])) {
         $is_checked = Export::getCheckedClause(
-            $each_table['Name'], $_GET['table_select']
+            $each_table['Name'], $_POST['table_select']
         );
     } elseif (isset($table_select)) {
         $is_checked = Export::getCheckedClause(
@@ -105,16 +105,16 @@ foreach ($tables as $each_table) {
     } else {
         $is_checked = ' checked="checked"';
     }
-    if (isset($_GET['table_structure']) && is_array($_GET['table_structure'])) {
+    if (isset($_POST['table_structure']) && is_array($_POST['table_structure'])) {
         $structure_checked = Export::getCheckedClause(
-            $each_table['Name'], $_GET['table_structure']
+            $each_table['Name'], $_POST['table_structure']
         );
     } else {
         $structure_checked = $is_checked;
     }
-    if (isset($_GET['table_data']) && is_array($_GET['table_data'])) {
+    if (isset($_POST['table_data']) && is_array($_POST['table_data'])) {
         $data_checked = Export::getCheckedClause(
-            $each_table['Name'], $_GET['table_data']
+            $each_table['Name'], $_POST['table_data']
         );
     } else {
         $data_checked = $is_checked;

commit 348f1fbd2e93d8579f84d35329a48d10e27d600d

Fix export issues/warnings
- Fix php warning quick_export_onserver_overwrite
- Fix issues introduced by c1cdaac2f465dd6b9e17f9f35fd46861ad703a6d
- Fix refresh an export displayed as text (token had mimatch because it was urlencoded)

Signed-off-by: William Desportes <williamdes@wdes.fr>

export.php
@@ -21,14 +21,6 @@ use PhpMyAdmin\Response;
  * Get the variables sent or posted to this script and a core script
  */
 include_once 'libraries/common.inc.php';
-/**
- * If we are sending the export file (as opposed to just displaying it
- * as text), we have to bypass the usual PhpMyAdmin\Response mechanism
- */
-if (isset($_POST['output_format']) && $_POST['output_format'] == 'sendit') {
-    $response = Response::getInstance();
-    $response->disable();
-}
 
 $response = Response::getInstance();
 $header   = $response->getHeader();
@@ -46,6 +38,7 @@ if (isset($_GET['check_time_out'])) {
     }
     exit;
 }
+
 /**
  * Sets globals from $_POST
  *
@@ -264,6 +257,14 @@ if ($_POST['output_format'] == 'astext') {
         // Will we save dump on server?
         $save_on_server = ! empty($cfg['SaveDir']) && $onserver;
     }
+}
+
+/**
+ * If we are sending the export file (as opposed to just displaying it
+ * as text), we have to bypass the usual PhpMyAdmin\Response mechanism
+ */
+if (isset($_POST['output_format']) && $_POST['output_format'] == 'sendit' && ! $save_on_server) {
+    $response->disable();
 }
 
 // Generate error url and check for needed variables

libraries/classes/Export.php
@@ -336,6 +336,11 @@ class Export
     {
         $file_handle = null;
         $message = '';
+        $doNotSaveItOver = true;
+
+        if(isset($_POST['quick_export_onserver_overwrite'])) {
+            $doNotSaveItOver = $_POST['quick_export_onserver_overwrite'] != 'saveitover';
+        }
 
         $save_filename = Util::userDir($GLOBALS['cfg']['SaveDir'])
             . preg_replace('@[/\\\\]@', '_', $filename);
@@ -343,7 +348,7 @@ class Export
         if (@file_exists($save_filename)
             && ((! $quick_export && empty($_POST['onserver_overwrite']))
             || ($quick_export
-            && $_POST['quick_export_onserver_overwrite'] != 'saveitover'))
+            && $doNotSaveItOver))
         ) {
             $message = Message::error(
                 __(
@@ -486,24 +491,13 @@ class Export
         }
 
         // Convert the multiple select elements from an array to a string
-        if ($export_type == 'server' && isset($_POST['db_select'])) {
-            $_POST['db_select'] = implode(",", $_POST['db_select']);
-        } elseif ($export_type == 'database') {
-            if (isset($_POST['table_select'])) {
-                $_POST['table_select'] = implode(",", $_POST['table_select']);
+        if ($export_type == 'database') {
+            $structOrDataForced = empty($_POST['structure_or_data_forced']);
+            if ($structOrDataForced && ! isset($_POST['table_structure'])) {
+                $_POST['table_structure'] = [];
             }
-            if (isset($_POST['table_structure'])) {
-                $_POST['table_structure'] = implode(
-                    ",",
-                    $_POST['table_structure']
-                );
-            } elseif (empty($_POST['structure_or_data_forced'])) {
-                $_POST['table_structure'] = '';
-            }
-            if (isset($_POST['table_data'])) {
-                $_POST['table_data'] = implode(",", $_POST['table_data']);
-            } elseif (empty($_POST['structure_or_data_forced'])) {
-                $_POST['table_data'] = '';
+            if ($structOrDataForced && ! isset($_POST['table_data'])) {
+                $_POST['table_data'] = [];
             }
         }
 
@@ -520,11 +514,11 @@ class Export
         foreach($_POST as $name => $value) {
             if (is_array($value)) {
                 foreach($value as $val) {
-                    $refreshButton .= '<input type="hidden" name="' . urlencode((string) $name) . '[]" value="' . urlencode((string) $val) . '">';
+                    $refreshButton .= '<input type="hidden" name="' . htmlentities((string) $name) . '[]" value="' . htmlentities((string) $val) . '">';
                 }
             }
             else {
-                $refreshButton .= '<input type="hidden" name="' . urlencode((string) $name) . '" value="' . urlencode((string) $value) . '">';
+                $refreshButton .= '<input type="hidden" name="' . htmlentities((string) $name) . '" value="' . htmlentities((string) $value) . '">';
             }
         }
         $refreshButton .= '</form>';