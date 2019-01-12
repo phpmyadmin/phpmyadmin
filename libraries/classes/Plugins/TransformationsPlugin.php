8b246760e67deba47f6967fb7dfd01c614e6503e
Hugues Peccatte <hugues.peccatte@gmail.com>
2019-01-13 00:21:51 +0100

    Remove useless semicolon
    Signed-off-by: Hugues Peccatte <hugues.peccatte@gmail.com>

--- libraries/classes/Plugins/TransformationsPlugin.php
+++ libraries/classes/Plugins/TransformationsPlugin.php
@@ -28,7 +28,6 @@ abstract class TransformationsPlugin implements TransformationsInterface
      */
     public function applyTransformationNoWrap(array $options = [])
     {
-        ;
     }
 
     /**
