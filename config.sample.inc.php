6c0f07090ee63d132eb167e124eae0616981613a
Ketan Yekale <ketan.yekale@inscripts.com>
2019-02-18 19:53:20 +0530

    Fix for: HY000/2002: No such file or directory
    PhpMyAdmin error on MacOS:
    
    mysqli_real_connect(): (HY000/2002): No such file or directory

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -32,6 +32,7 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['socket'] = '/tmp/mysql.sock';
 
 /**
  * phpMyAdmin configuration storage settings.
