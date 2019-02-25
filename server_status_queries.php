e149a6188f53c7d40103a420f7dbaf2f364f1c00
Maurício Meneghini Fauth <mauriciofauth@gmail.com>
2019-02-25 00:53:57 -0300

    Create Server\Status\QueriesController
    Extracts server_status_queries.php and Server\Status\Queries logic to
    the controller.
    
    Signed-off-by: Maurício Meneghini Fauth <mauriciofauth@gmail.com>

--- server_status_queries.php
+++ server_status_queries.php
@@ -1,6 +1,5 @@
 <?php
 /* vim: set expandtab sw=4 ts=4 sts=4: */
-
 /**
  * Displays query statistics for the server
  *
@@ -8,10 +7,9 @@
  */
 declare(strict_types=1);
 
+use PhpMyAdmin\Controllers\Server\Status\QueriesController;
 use PhpMyAdmin\Response;
-use PhpMyAdmin\Message;
 use PhpMyAdmin\Server\Status\Data;
-use PhpMyAdmin\Server\Status\Queries;
 
 if (! defined('ROOT_PATH')) {
     define('ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR);
@@ -21,13 +19,16 @@ require_once ROOT_PATH . 'libraries/common.inc.php';
 require_once ROOT_PATH . 'libraries/server_common.inc.php';
 require_once ROOT_PATH . 'libraries/replication.inc.php';
 
-$serverStatusData = new Data();
-
 $response = Response::getInstance();
-$header   = $response->getHeader();
-$scripts  = $header->getScripts();
 
-// for charting
+$controller = new QueriesController(
+    $response,
+    $GLOBALS['dbi'],
+    new Data()
+);
+
+$header = $response->getHeader();
+$scripts = $header->getScripts();
 $scripts->addFile('chart.js');
 $scripts->addFile('vendor/jqplot/jquery.jqplot.js');
 $scripts->addFile('vendor/jqplot/plugins/jqplot.pieRenderer.js');
@@ -37,17 +38,4 @@ $scripts->addFile('vendor/jquery/jquery.tablesorter.js');
 $scripts->addFile('server_status_sorter.js');
 $scripts->addFile('server_status_queries.js');
 
-// Add the html content to the response
-$response->addHTML('<div>');
-$response->addHTML($serverStatusData->getMenuHtml());
-if ($serverStatusData->dataLoaded) {
-    $response->addHTML(Queries::getHtmlForQueryStatistics($serverStatusData));
-} else {
-    $response->addHTML(
-        Message::error(
-            __('Not enough privilege to view query statistics.')
-        )->getDisplay()
-    );
-}
-$response->addHTML('</div>');
-exit;
+$response->addHTML($controller->index());
