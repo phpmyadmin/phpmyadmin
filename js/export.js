4b855c9a47e99bc1a070ff9183626da5a15b913c
William Desportes <williamdes@wdes.fr>
2019-03-01 20:21:11 +0100

    Fix code style
    Fix code style
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- js/export.js
+++ js/export.js
@@ -311,7 +311,7 @@ AJAX.registerOnload('export.js', function () {
     // When MS Excel is selected as the Format automatically Switch to Character Set as windows-1252
     $('#plugins').change(function () {
         var selected_plugin_name = $('#plugins').find('option:selected').val();
-        if (selected_plugin_name == "excel") {
+        if (selected_plugin_name === 'excel') {
             $('#select_charset').val('windows-1252');
         } else {
             $('#select_charset').val('utf-8');
