b0a777de5020f7e2c23ee9aa43d1ab1df956631c
Maurício Meneghini Fauth <mauriciofauth@gmail.com>
2019-03-02 20:35:07 -0300

    Fix coding standard error on PhpMyAdmin\Export
    Signed-off-by: Maurício Meneghini Fauth <mauriciofauth@gmail.com>

--- libraries/classes/Export.php
+++ libraries/classes/Export.php
@@ -520,11 +520,10 @@ class Export
         $refreshButton .= '[ <a class="disableAjax" onclick="$(this).parent().submit()">' . __('Refresh') . '</a> ]';
         foreach ($_POST as $name => $value) {
             if (is_array($value)) {
-                foreach($value as $val) {
+                foreach ($value as $val) {
                     $refreshButton .= '<input type="hidden" name="' . htmlentities((string) $name) . '[]" value="' . htmlentities((string) $val) . '">';
                 }
-            }
-            else {
+            } else {
                 $refreshButton .= '<input type="hidden" name="' . htmlentities((string) $name) . '" value="' . htmlentities((string) $value) . '">';
             }
         }
