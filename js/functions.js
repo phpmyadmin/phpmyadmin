27f7b7a75fa9421e310b0d1d9e7bc34accb96fb9
William Desportes <williamdes@wdes.fr>
2019-03-16 20:58:57 +0100

    Fix, prevent multiple error reporting modals
    See: fa303ee2923f43ee9483c3bc3c880cee7c4812d0
    Seee: #14359
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- js/functions.js
+++ js/functions.js
@@ -5049,7 +5049,7 @@ function configSet (key, value, only_local) {
         success: function (data) {
             // Updating value in local storage.
             if (! data.success) {
-                if(data.error) {
+                if (data.error) {
                     PMA_ajaxShowMessage(data.error);
                 } else {
                     PMA_ajaxShowMessage(data.message);
