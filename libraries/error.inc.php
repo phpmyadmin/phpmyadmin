10cd180442dcd7571500f0b37d84fa4206ee5f18
William Desportes <williamdes@wdes.fr>
2019-03-25 18:56:14 +0100

    Fix #15095 - Wrong format of comments
    Fixes commit: 5ba97aa1de76fe7a6f5eb2efe3c568ba5c43bedf
    Closes: #15108
    Closes: #15107
    Closes: #15097
    Fixes: #15095
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- libraries/error.inc.php
+++ libraries/error.inc.php
@@ -25,6 +25,33 @@ if (! defined('TESTSUITE')) {
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <title>phpMyAdmin</title>
     <meta charset="utf-8">
+    <style type="text/css">
+    html {
+        padding: 0;
+        margin: 0;
+    }
+    body  {
+        font-family: sans-serif;
+        font-size: small;
+        color: #000000;
+        background-color: #F5F5F5;
+        margin: 1em;
+    }
+    h1 {
+        margin: 0;
+        padding: 0.3em;
+        font-size: 1.4em;
+        font-weight: bold;
+        color: #ffffff;
+        background-color: #ff0000;
+    }
+    p {
+        margin: 0;
+        padding: 0.5em;
+        border: 0.1em solid red;
+        background-color: #ffeeee;
+    }
+    </style>
 </head>
 <body>
 <h1>phpMyAdmin - <?php echo $error_header; ?></h1>
