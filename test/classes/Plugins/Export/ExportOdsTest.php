3e3a5ce7e24954f2f979e4b46b84f6d15de20a98
William Desportes <williamdes@wdes.fr>
2019-05-07 14:11:24 +0200

    Fix failing tests for #14588
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- test/classes/Plugins/Export/ExportOdsTest.php
+++ test/classes/Plugins/Export/ExportOdsTest.php
@@ -272,9 +272,11 @@ class ExportOdsTest extends PmaTestCase
 
         $flags = array();
         $a = new stdClass;
+        $a->type = '';
         $flags[] = $a;
 
         $a = new stdClass;
+        $a->type = '';
         $a->blob = true;
         $flags[] = $a;
 
@@ -396,6 +398,20 @@ class ExportOdsTest extends PmaTestCase
             ->getMock();
 
         $flags = array();
+        $a = new \stdClass();
+        $a->blob = false;
+        $a->numeric = false;
+        $a->type = 'string';
+        $a->name = 'fna\"me';
+        $a->length = 20;
+        $flags[] = $a;
+        $b = new \stdClass();
+        $b->blob = false;
+        $b->numeric = false;
+        $b->type = 'string';
+        $b->name = 'fnam/<e2';
+        $b->length = 20;
+        $flags[] = $b;
 
         $dbi->expects($this->once())
             ->method('getFieldsMeta')
