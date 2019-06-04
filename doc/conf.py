004848d93f974236ed6d8d58f5fc18d006a6e53c
Isaac Bennetch <bennetch@gmail.com>
2019-06-04 12:05:18 -0400

    Prepare version 4.9.0.1
    Signed-off-by: Isaac Bennetch <bennetch@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -51,7 +51,7 @@ copyright = u'2012 - 2018, The phpMyAdmin devel team'
 # built documents.
 #
 # The short X.Y version.
-version = '4.9.1-dev'
+version = '4.9.0.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
