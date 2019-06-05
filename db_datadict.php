72237fee89a5cb2e263faed917acd64c9ac9190a
Hugues Peccatte <hugues.peccatte@gmail.com>
2019-06-05 21:47:59 +0200

    Define correctly the DataDictionaryController parameters
    Signed-off-by: Hugues Peccatte <hugues.peccatte@gmail.com>

--- db_datadict.php
+++ db_datadict.php
@@ -8,41 +8,19 @@
 declare(strict_types=1);
 
 use PhpMyAdmin\Controllers\Database\DataDictionaryController;
-use PhpMyAdmin\Di\Container;
 use PhpMyAdmin\Response;
 use PhpMyAdmin\Util;
-use Symfony\Component\DependencyInjection\Definition;
 
 if (! defined('ROOT_PATH')) {
     define('ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR);
 }
 
-global $db;
-
 require_once ROOT_PATH . 'libraries/common.inc.php';
 
 Util::checkParameters(['db']);
 
-$container = Container::getDefaultContainer();
-$container->set(Response::class, Response::getInstance());
-
 /** @var Response $response */
 $response = $containerBuilder->get(Response::class);
-
-/* Define dependencies for the concerned controller */
-$dependency_definitions = [
-    'db' => $container->get('db'),
-];
-
-/** @var Definition $definition */
-$definition = $containerBuilder->getDefinition(DataDictionaryController::class);
-array_map(
-    static function (string $parameterName, $value) use ($definition) {
-        $definition->replaceArgument($parameterName, $value);
-    },
-    array_keys($dependency_definitions),
-    $dependency_definitions
-);
 
 /** @var DataDictionaryController $controller */
 $controller = $containerBuilder->get(DataDictionaryController::class);
