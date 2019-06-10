8de3080d45b24f10f8e67f9033e19b55fe435e9e
Hugues Peccatte <hugues.peccatte@gmail.com>
2019-06-10 22:08:35 +0200

    Remove Di\Container in tbl_get_field.php
    Signed-off-by: Hugues Peccatte <hugues.peccatte@gmail.com>

--- tbl_get_field.php
+++ tbl_get_field.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
 
 use PhpMyAdmin\Core;
 use PhpMyAdmin\DatabaseInterface;
-use PhpMyAdmin\Di\Container;
 use PhpMyAdmin\Mime;
 use PhpMyAdmin\Response;
 
@@ -17,18 +16,19 @@ if (! defined('ROOT_PATH')) {
     define('ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR);
 }
 
-global $db, $table;
-
 require_once ROOT_PATH . 'libraries/common.inc.php';
 
-$container = Container::getDefaultContainer();
-$container->set(Response::class, Response::getInstance());
-
 /** @var Response $response */
-$response = $container->get(Response::class);
+$response = $containerBuilder->get(Response::class);
 
 /** @var DatabaseInterface $dbi */
-$dbi = $container->get(DatabaseInterface::class);
+$dbi = $containerBuilder->get(DatabaseInterface::class);
+
+/** @var string $db */
+$db = $containerBuilder->getParameter('db');
+
+/** @var string $table */
+$table = $containerBuilder->getParameter('table');
 
 $response->disable();
 
