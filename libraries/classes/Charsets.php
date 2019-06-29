48676c2270eedac777421151a68357830b709396
Maurício Meneghini Fauth <mauricio@fauth.dev>
2019-06-29 14:54:48 -0300

    Add GB18030 Chinese collations description
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- libraries/classes/Charsets.php
+++ libraries/classes/Charsets.php
@@ -382,10 +382,17 @@ class Charsets
                     case 'koi8r':
                         $name = _pgettext('Collation', 'Russian');
                         break;
-                    // Simplified Chinese charsets
+                    // Chinese charsets
                     case 'gb2312':
                     case 'gbk':
                         $name = _pgettext('Collation', 'Simplified Chinese');
+                        break;
+                    case 'big5':
+                        $name = _pgettext('Collation', 'Traditional Chinese');
+                        break;
+                    case 'gb18030':
+                        $name = _pgettext('Collation', 'Chinese');
+                        $unicode = true;
                         break;
                     // Japanese charsets
                     case 'sjis':
@@ -403,9 +410,6 @@ class Charsets
                     case 'armscii8':
                     case 'armscii':
                         $name = _pgettext('Collation', 'Armenian');
-                        break;
-                    case 'big5':
-                        $name = _pgettext('Collation', 'Traditional Chinese');
                         break;
                     case 'cp1251':
                         $name = _pgettext('Collation', 'Cyrillic');
