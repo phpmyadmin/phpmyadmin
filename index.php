54b7100c8362862054bc97d891f988ae363dae09
Maurício Meneghini Fauth <mauricio@fauth.dev>
2019-08-19 21:05:01 -0300

    Use the router for the gis data editor
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- index.php
+++ index.php
@@ -98,6 +98,9 @@ if (isset($_GET['route']) || isset($_POST['route'])) {
         $routes->addRoute(['GET', 'POST'], '/export', function () {
             require_once ROOT_PATH . 'libraries/entry_points/export.php';
         });
+        $routes->addRoute(['GET', 'POST'], '/gis_data_editor', function () {
+            require_once ROOT_PATH . 'libraries/entry_points/gis_data_editor.php';
+        });
         $routes->addRoute(['GET', 'POST'], '/import', function () {
             require_once ROOT_PATH . 'libraries/entry_points/import.php';
         });
