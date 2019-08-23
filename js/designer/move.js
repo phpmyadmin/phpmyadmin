phpmyadmin/phpmyadmin

commit a23f2ba51a93af0c5eedf91646aa9e04b9bc3a12

Fix some bugs with strange table and database names
- Fix export
- Fix split on . to have db and table
- Replace some $_REQUEST occurences
- Add translations to "add tables from another database" : "None" > strNone
- Refactor the ugly code that used globals to use an object "DesignerTable"
- Fix save a new added table
- Fix delete page items
- Fix more bugs

Fixes: #15446
Fixes: #13370
Fixes: #14945
Closes: #15438

Signed-off-by: William Desportes <williamdes@wdes.fr>

js/designer/move.js
@@ -549,25 +549,25 @@ function Toggle_fullscreen () {
     saveValueInConfig('full_screen', value_sent);
 }
 
-function addTableToTablesList(index, table_dom) {
+function addTableToTablesList (index, table_dom) {
     var db = $(table_dom).find('.small_tab_pref').attr('db');
-    var table = $(table_dom).find('.small_tab_pref').attr('table_name_small');
-    var db_encoded = encodeURIComponent(db);
-    var table_encoded = encodeURIComponent(table);
+    var table = $(table_dom).find('.small_tab_pref').attr('table_name');
+    var db_encoded = $(table_dom).find('.small_tab_pref').attr('db_url');
+    var table_encoded = $(table_dom).find('.small_tab_pref').attr('table_name_url');
     var $new_table_line = $('<tr>' +
         '    <td title="' + PMA_messages.strStructure + '"' +
         '        width="1px"' +
         '        class="L_butt2_1">' +
         '        <img alt=""' +
-        '            db="' + db + '"' +
-        '            table_name="' + table + '"' +
+        '            db="' + db_encoded + '"' +
+        '            table_name="' + table_encoded + '"' +
         '            class="scroll_tab_struct"' +
         '            src="' + pmaThemeImage + 'designer/exec.png"/>' +
         '    </td>' +
         '    <td width="1px">' +
         '        <input class="scroll_tab_checkbox"' +
         '            title="' + PMA_messages.strHide + '"' +
-        '            id="check_vis_' + encodeURI(db) + '.' + table_encoded + '"' +
+        '            id="check_vis_' + db_encoded + '.' + table_encoded + '"' +
         '            style="margin:0;"' +
         '            type="checkbox"' +
         '            value="' + db_encoded + '.' + table_encoded + '"' +
@@ -597,7 +597,7 @@ function Add_Other_db_tables () {
         var db = $('#add_table_from').val();
         var table = $('#add_table').val();
 
-        //Check if table already imported or not.
+        // Check if table already imported or not.
         var $table = $('[id="' + encodeURIComponent(db) + '.' + encodeURIComponent(table) + '"]');
         if ($table.length !== 0) {
             PMA_ajaxShowMessage(
@@ -615,11 +615,14 @@ function Add_Other_db_tables () {
             'table' : table,
             'server': PMA_commonParams.get('server')
         }, function (data) {
-            var $new_table_dom = $(data.message);
-            $new_table_dom.find('a').first().remove();
-            $('#container-form').append($new_table_dom);
-            enableTableEvents(null, $new_table_dom);
-            addTableToTablesList(null, $new_table_dom);
+            var $newTableDom = $(data.message);
+            $newTableDom.find('a').first().remove();
+            $('#container-form').append($newTableDom);
+            enableTableEvents(null, $newTableDom);
+            addTableToTablesList(null, $newTableDom);
+            var dbEncoded = $($newTableDom).find('.small_tab_pref').attr('db_url');
+            var tableEncoded = $($newTableDom).find('.small_tab_pref').attr('table_name_url');
+            j_tabs[dbEncoded + '.' + tableEncoded] = 1;
         });
         $(this).dialog('close');
     };
@@ -628,10 +631,10 @@ function Add_Other_db_tables () {
     };
 
     var $select_db = $('<select id="add_table_from"></select>');
-    $select_db.append('<option value="">None</option>');
+    $select_db.append('<option value="">' + PMA_messages.strNone + '</option>');
 
     var $select_table = $('<select id="add_table"></select>');
-    $select_table.append('<option value="">None</option>');
+    $select_table.append('<option value="">' + PMA_messages.strNone + '</option>');
 
     $.post('sql.php', {
         'ajax_request' : true,
@@ -670,7 +673,11 @@ function Add_Other_db_tables () {
                 'server': PMA_commonParams.get('server')
             }, function (data) {
                 $select_table.html('');
-                $(data.message).find('table.table_results.data.ajax').find('td.data').each(function () {
+                var rows = $(data.message).find('table.table_results.data.ajax').find('td.data');
+                if (rows.length === 0) {
+                    $select_table.append('<option value="">' + PMA_messages.strNone + '</option>');
+                }
+                rows.each(function () {
                     var val = $(this)[0].innerHTML;
                     $select_table.append('<option value="' + val + '">' + val + '</option>');
                 });
@@ -705,11 +712,15 @@ function Get_url_pos (forceString) {
     if (designer_tables_enabled || forceString) {
         var poststr = '';
         var argsep = PMA_commonParams.get('arg_separator');
+        var i = 1;
         for (var key in j_tabs) {
-            poststr += argsep + 't_x[' + decodeURIComponent(key) + ']=' + parseInt(document.getElementById(key).style.left, 10);
-            poststr += argsep + 't_y[' + decodeURIComponent(key) + ']=' + parseInt(document.getElementById(key).style.top, 10);
-            poststr += argsep + 't_v[' + decodeURIComponent(key) + ']=' + (document.getElementById('id_tbody_' + key).style.display === 'none' ? 0 : 1);
-            poststr += argsep + 't_h[' + decodeURIComponent(key) + ']=' + (document.getElementById('check_vis_' + key).checked ? 1 : 0);
+            poststr += argsep + 't_x[' + i + ']=' + parseInt(document.getElementById(key).style.left, 10);
+            poststr += argsep + 't_y[' + i + ']=' + parseInt(document.getElementById(key).style.top, 10);
+            poststr += argsep + 't_v[' + i + ']=' + (document.getElementById('id_tbody_' + key).style.display === 'none' ? 0 : 1);
+            poststr += argsep + 't_h[' + i + ']=' + (document.getElementById('check_vis_' + key).checked ? 1 : 0);
+            poststr += argsep + 't_db[' + i + ']=' + $(document.getElementById(key)).attr('db_url');
+            poststr += argsep + 't_tbl[' + i + ']=' + $(document.getElementById(key)).attr('table_name_url');
+            i++;
         }
         return poststr;
     } else {
@@ -718,7 +729,10 @@ function Get_url_pos (forceString) {
             if (document.getElementById('check_vis_' + key).checked) {
                 var x = parseInt(document.getElementById(key).style.left, 10);
                 var y = parseInt(document.getElementById(key).style.top, 10);
-                var tbCoords = new TableCoordinate(db, key.split('.')[1], -1, x, y);
+                var tbCoords = new TableCoordinate(
+                    $(document.getElementById(key)).attr('db_url'),
+                    $(document.getElementById(key)).attr('table_name_url'),
+                    -1, x, y);
                 coords.push(tbCoords);
             }
         }
@@ -729,8 +743,8 @@ function Get_url_pos (forceString) {
 function Save2 (callback) {
     if (designer_tables_enabled) {
         var argsep = PMA_commonParams.get('arg_separator');
-        var poststr = argsep + 'operation=savePage' + argsep + 'save_page=same' + argsep + 'ajax_request=true';
-        poststr += argsep + 'server=' + server + argsep + 'db=' + db + argsep + 'selected_page=' + selected_page;
+        var poststr = 'operation=savePage' + argsep + 'save_page=same' + argsep + 'ajax_request=true';
+        poststr += argsep + 'server=' + server + argsep + 'db=' + encodeURIComponent(db) + argsep + 'selected_page=' + selected_page;
         poststr += Get_url_pos();
 
         var $msgbox = PMA_ajaxShowMessage(PMA_messages.strProcessingRequest);
@@ -1176,7 +1190,7 @@ function Load_page (page) {
         if (page !== null) {
             param_page = argsep + 'page=' + page;
         }
-        $('<a href="db_designer.php?server=' + server + argsep + 'db=' + encodeURI(db) + param_page + '"></a>')
+        $('<a href="db_designer.php?server=' + server + argsep + 'db=' + encodeURIComponent(db) + param_page + '"></a>')
             .appendTo($('#page_content'))
             .click();
     } else {

db_designer.php
@@ -19,33 +19,25 @@ $designerCommon = new Common();
 if (isset($_POST['dialog'])) {
 
     if ($_POST['dialog'] == 'edit') {
-        $html = $databaseDesigner->getHtmlForEditOrDeletePages($GLOBALS['db'], 'editPage');
+        $html = $databaseDesigner->getHtmlForEditOrDeletePages($_POST['db'], 'editPage');
     } elseif ($_POST['dialog'] == 'delete') {
-        $html = $databaseDesigner->getHtmlForEditOrDeletePages($GLOBALS['db'], 'deletePage');
+        $html = $databaseDesigner->getHtmlForEditOrDeletePages($_POST['db'], 'deletePage');
     } elseif ($_POST['dialog'] == 'save_as') {
-        $html = $databaseDesigner->getHtmlForPageSaveAs($GLOBALS['db']);
+        $html = $databaseDesigner->getHtmlForPageSaveAs($_POST['db']);
     } elseif ($_POST['dialog'] == 'export') {
         $html = $databaseDesigner->getHtmlForSchemaExport(
-            $GLOBALS['db'], $_POST['selected_page']
+            $_POST['db'], $_POST['selected_page']
         );
     } elseif ($_POST['dialog'] == 'add_table') {
-        $script_display_field = $designerCommon->getTablesInfo();
-        $required = $GLOBALS['db'] . '.' . $GLOBALS['table'];
-        $tab_column = $designerCommon->getColumnsInfo();
-        $tables_all_keys = $designerCommon->getAllKeys();
-        $tables_pk_or_unique_keys = $designerCommon->getPkOrUniqueKeys();
-
-        $req_key = array_search($required, $GLOBALS['designer']['TABLE_NAME']);
-
-        $GLOBALS['designer']['TABLE_NAME'] = array($GLOBALS['designer']['TABLE_NAME'][$req_key]);
-        $GLOBALS['designer_url']['TABLE_NAME'] = array($GLOBALS['designer_url']['TABLE_NAME'][$req_key]);
-        $GLOBALS['designer_url']['TABLE_NAME_SMALL'] = array($GLOBALS['designer_url']['TABLE_NAME_SMALL'][$req_key]);
-        $GLOBALS['designer']['TABLE_NAME_SMALL'] = array($GLOBALS['designer']['TABLE_NAME_SMALL'][$req_key]);
-        $GLOBALS['designer_out']['TABLE_NAME_SMALL'] = array($GLOBALS['designer_out']['TABLE_NAME_SMALL'][$req_key]);
-        $GLOBALS['designer']['TABLE_TYPE'] = array($GLOBALS['designer_url']['TABLE_TYPE'][$req_key]);
-        $GLOBALS['designer_out']['OWNER'] = array($GLOBALS['designer_out']['OWNER'][$req_key]);
+        // Pass the db and table to the getTablesInfo so we only have the table we asked for
+        $script_display_field = $designerCommon->getTablesInfo($_POST['db'], $_POST['table']);
+        $tab_column = $designerCommon->getColumnsInfo($script_display_field);
+        $tables_all_keys = $designerCommon->getAllKeys($script_display_field);
+        $tables_pk_or_unique_keys = $designerCommon->getPkOrUniqueKeys($script_display_field);
 
         $html = $databaseDesigner->getDatabaseTables(
+            $_POST['db'],
+            $script_display_field,
             array(), -1, $tab_column,
             $tables_all_keys, $tables_pk_or_unique_keys
         );
@@ -66,7 +58,7 @@ if (isset($_POST['operation'])) {
         if ($_POST['save_page'] == 'same') {
             $page = $_POST['selected_page'];
         } else { // new
-            $page = $designerCommon->createNewPage($_POST['selected_value'], $GLOBALS['db']);
+            $page = $designerCommon->createNewPage($_POST['selected_value'], $_POST['db']);
             $response->addJSON('id', $page);
         }
         $success = $designerCommon->saveTablePositions($page);
@@ -110,11 +102,6 @@ if (isset($_POST['operation'])) {
 require 'libraries/db_common.inc.php';
 
 $script_display_field = $designerCommon->getTablesInfo();
-$tab_column = $designerCommon->getColumnsInfo();
-$script_tables = $designerCommon->getScriptTabs();
-$tables_pk_or_unique_keys = $designerCommon->getPkOrUniqueKeys();
-$tables_all_keys = $designerCommon->getAllKeys();
-$classes_side_menu = $databaseDesigner->returnClassNamesFromMenuButtons();
 
 $display_page = -1;
 $selected_page = null;
@@ -132,7 +119,30 @@ if ($display_page != -1) {
     $selected_page = $designerCommon->getPageName($display_page);
 }
 $tab_pos = $designerCommon->getTablePositions($display_page);
-$script_contr = $designerCommon->getScriptContr();
+
+$fullTableNames = [];
+
+foreach($script_display_field as $designerTable) {
+    $fullTableNames[] = $designerTable->getDbTableString();
+}
+
+foreach($tab_pos as $position) {
+    if (! in_array($position['dbName'] . '.' . $position['tableName'], $fullTableNames)) {
+        foreach($designerCommon->getTablesInfo($position['dbName'], $position['tableName']) as $designerTable) {
+            $script_display_field[] = $designerTable;
+        }
+    }
+}
+
+
+$tab_column = $designerCommon->getColumnsInfo($script_display_field);
+$script_tables = $designerCommon->getScriptTabs($script_display_field);
+$tables_pk_or_unique_keys = $designerCommon->getPkOrUniqueKeys($script_display_field);
+$tables_all_keys = $designerCommon->getAllKeys($script_display_field);
+$classes_side_menu = $databaseDesigner->returnClassNamesFromMenuButtons();
+
+
+$script_contr = $designerCommon->getScriptContr($script_display_field);
 
 $params = array('lang' => $GLOBALS['lang']);
 if (isset($_GET['db'])) {
@@ -192,6 +202,8 @@ $response->addHTML($databaseDesigner->getHtmlTableList());
 
 $response->addHTML(
     $databaseDesigner->getDatabaseTables(
+        $_GET['db'],
+        $script_display_field,
         $tab_pos, $display_page, $tab_column,
         $tables_all_keys, $tables_pk_or_unique_keys
     )

libraries/classes/Database/Designer.php
@@ -291,22 +291,25 @@ class Designer
     {
         return Template::get('database/designer/table_list')->render([
             'theme' => $GLOBALS['PMA_Theme'],
-            'table_names' => $GLOBALS['designer']['TABLE_NAME'],
         ]);
     }
 
     /**
      * Get HTML to display tables on designer page
      *
-     * @param array $tab_pos                  tables positions
-     * @param int   $display_page             page number of the selected page
-     * @param array $tab_column               table column info
-     * @param array $tables_all_keys          all indices
-     * @param array $tables_pk_or_unique_keys unique or primary indices
+     * @param string $db                       The database name from the request
+     * @param array  $designerTables           The designer tables
+     * @param array  $tab_pos                  tables positions
+     * @param int    $display_page             page number of the selected page
+     * @param array  $tab_column               table column info
+     * @param array  $tables_all_keys          all indices
+     * @param array  $tables_pk_or_unique_keys unique or primary indices
      *
      * @return string html
      */
     public function getDatabaseTables(
+        $db,
+        array $designerTables,
         array $tab_pos,
         $display_page,
         array $tab_column,
@@ -315,20 +318,14 @@ class Designer
     ) {
         return Template::get('database/designer/database_tables')->render([
             'db' => $GLOBALS['db'],
-            'get_db' => $_GET['db'],
+            'get_db' => $db,
             'has_query' => isset($_REQUEST['query']),
             'tab_pos' => $tab_pos,
             'display_page' => $display_page,
             'tab_column' => $tab_column,
             'tables_all_keys' => $tables_all_keys,
             'tables_pk_or_unique_keys' => $tables_pk_or_unique_keys,
-            'table_names' => $GLOBALS['designer']['TABLE_NAME'],
-            'table_names_url' => $GLOBALS['designer_url']['TABLE_NAME'],
-            'table_names_small' => $GLOBALS['designer']['TABLE_NAME_SMALL'],
-            'table_names_small_url' => $GLOBALS['designer_url']['TABLE_NAME_SMALL'],
-            'table_names_small_out' => $GLOBALS['designer_out']['TABLE_NAME_SMALL'],
-            'table_types' => $GLOBALS['designer']['TABLE_TYPE'],
-            'owner_out' => $GLOBALS['designer_out']['OWNER'],
+            'tables' => $designerTables,
             'theme' => $GLOBALS['PMA_Theme'],
         ]);
     }

commit 8590357612116753514f01c4b1c49b3bcd335ef8

Fix a bug introduced by a23f2ba51a93af0c5
[ci skip]

Signed-off-by: William Desportes <williamdes@wdes.fr>

js/designer/move.js
@@ -1992,7 +1992,7 @@ function enableTableEvents(index, element) {
         Small_tab($(this).attr('table_name'), 1);
     });
     $(element).on('click', '.small_tab_pref_1', function () {
-        Start_tab_upd($(this).attr('db'), $(this).attr('table_name_small'));
+        Start_tab_upd($(this).attr('db_url'), $(this).attr('table_name_url'));
     });
     $(element).on('click', '.select_all_store_col', function () {
         var params = ($(this).attr('store_column_param')).split(',');