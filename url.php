5d2266ae41f57ddb0434562dda5f7a492471fc60
Maurício Meneghini Fauth <mauricio@fauth.dev>
2019-09-05 20:35:22 -0300

    Fix missing global declaration
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- url.php
+++ url.php
@@ -15,6 +15,8 @@ if (! defined('ROOT_PATH')) {
     define('ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR);
 }
 
+global $containerBuilder;
+
 /**
  * Gets core libraries and defines some variables
  */
