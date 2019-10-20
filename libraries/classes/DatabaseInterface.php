phpmyadmin/phpmyadmin

commit b8650115be7cc2d79d231723b7a6f1961e834d27

Fix #14433 - "You do not have privileges to manipulate with the users!" on root superadmin
Signed-off-by: William Desportes <williamdes@wdes.fr>

libraries/classes/DatabaseInterface.php
@@ -2330,7 +2330,11 @@ class DatabaseInterface
     {
         if (count($this->_current_user) == 0) {
             $user = $this->getCurrentUser();
-            $this->_current_user = explode("@", $user);
+            if ($user === '@') {// Request did not succeed, please do not cache
+                return $user;
+            } else {
+                $this->_current_user = explode("@", $user);
+            }
         }
         return $this->_current_user;
     }

commit 83602a6b72f49536b6a5417aa568604d14ea596c

Fix #14433 - return an empty array
Fixes: b8650115be7cc2d79d231723b7a6f1961e834d27

Signed-off-by: William Desportes <williamdes@wdes.fr>

libraries/classes/DatabaseInterface.php
@@ -2331,7 +2331,7 @@ class DatabaseInterface
         if (count($this->_current_user) == 0) {
             $user = $this->getCurrentUser();
             if ($user === '@') {// Request did not succeed, please do not cache
-                return $user;
+                return [];
             } else {
                 $this->_current_user = explode("@", $user);
             }