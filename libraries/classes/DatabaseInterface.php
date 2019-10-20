phpmyadmin/phpmyadmin

commit 83602a6b72f49536b6a5417aa568604d14ea596c

Fix #14433 - return an empty array
Fixes: b8650115be7cc2d79d231723b7a6f1961e834d27

Signed-off-by: William Desportes <williamdes@wdes.fr>

libraries/classes/DatabaseInterface.php
@@ -2331,7 +2331,7 @@ class DatabaseInterface
         if (count($this->_current_user) == 0) {
             $user = $this->getCurrentUser();
             if ($user === '@') {// Request did not succeed, please do not cache
-                return $user;
+                return [];
             } else {
                 $this->_current_user = explode("@", $user);
             }

commit af344bb4c1456fec5382a535733a2e3dddddbb8d

Fix #14433 - return an array with an empty user and empty host
Fixes: 83602a6b72f49536b6a5417aa568604d14ea596c
This is an unexpected change, detected by unit tests

Signed-off-by: William Desportes <williamdes@wdes.fr>

libraries/classes/DatabaseInterface.php
@@ -2331,7 +2331,7 @@ class DatabaseInterface
         if (count($this->_current_user) == 0) {
             $user = $this->getCurrentUser();
             if ($user === '@') {// Request did not succeed, please do not cache
-                return [];
+                return ['', ''];
             } else {
                 $this->_current_user = explode("@", $user);
             }