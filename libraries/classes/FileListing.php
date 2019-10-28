ba370e2311751dd1a48bd36ad5b1be9a5b846e2e
Oleg Abrazhaev <seyferseed@gmail.com>
2019-10-28 19:31:15 +0100

    fix code review
    Signed-off-by: Oleg Abrazhaev <seyferseed@gmail.com>

--- libraries/classes/FileListing.php
+++ libraries/classes/FileListing.php
@@ -65,14 +65,10 @@ class FileListing
             return false;
         }
 
-        $sanitizedList = array_map(function (string $entry) {
-            return htmlspecialchars($entry);
-        }, $list);
-
         $template = new Template();
 
         return $template->render('file_select_options', [
-            'filesList' => $sanitizedList,
+            'filesList' => $list,
             'active' => $active
         ]);
     }
