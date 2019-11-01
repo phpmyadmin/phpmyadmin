b3b8a84783e8531c39272470f246f4be5611741c
Hugues Peccatte <hugues.peccatte@gmail.com>
2019-11-01 17:12:31 +0100

    Replace fake loop by try/catch with exceptions
    Signed-off-by: Hugues Peccatte <hugues.peccatte@gmail.com>

---
+++ libraries/classes/Exceptions/ExportException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Export exception
+ *
+ * @package PhpMyAdmin
+ */
+declare(strict_types=1);
+
+namespace PhpMyAdmin\Exceptions;
+
+use Exception;
+
+/**
+ * Export exception
+ *
+ * @package PhpMyAdmin
+ */
+class ExportException extends Exception
+{
+
+}
