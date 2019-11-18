f5f751f86914f540b767aa1cf24a7c0fb65dedd0
Shailesh Jain <jainshailesh91@gmail.com>
2019-11-18 22:45:07 +0100

    Fix #15465 - profiling output when the result has no rows
    Fixes: #15465
    
    Signed-off-by: Shailesh Jain <jainshailesh91@gmail.com>

--- libraries/classes/Sql.php
+++ libraries/classes/Sql.php
@@ -1465,6 +1465,7 @@ EOT;
      * @param DisplayResults $displayResultsObject DisplayResult instance
      * @param array          $extra_data           extra data
      * @param string         $pmaThemeImage        uri of the theme image
+     * @param array|null     $profiling_results    profiling results
      * @param object         $result               executed query results
      * @param string         $sql_query            sql query
      * @param string         $complete_query       complete sql query
@@ -1473,7 +1474,7 @@ EOT;
      */
     private function getQueryResponseForNoResultsReturned(array $analyzed_sql_results, $db,
         $table, $message_to_show, $num_rows, $displayResultsObject, $extra_data,
-        $pmaThemeImage, $result, $sql_query, $complete_query
+        $pmaThemeImage, $profiling_results, $result, $sql_query, $complete_query
     ) {
         if ($this->isDeleteTransformationInfo($analyzed_sql_results)) {
             $this->deleteTransformationInfo($db, $table, $analyzed_sql_results);
@@ -1530,6 +1531,12 @@ EOT;
                     $pmaThemeImage, $url_query, $displayParts,
                     false, 0, $num_rows, true, $result,
                     $analyzed_sql_results, true
+                );
+
+                $html_output .= $this->getHtmlForProfilingChart(
+                    $url_query,
+                    $db,
+                    isset($profiling_results) ? $profiling_results : []
                 );
 
                 $html_output .= $displayResultsObject->getCreateViewQueryResultOp(
@@ -2244,7 +2251,7 @@ EOT;
                 $analyzed_sql_results, $db, $table,
                 isset($message_to_show) ? $message_to_show : null,
                 $num_rows, $displayResultsObject, $extra_data,
-                $pmaThemeImage, isset($result) ? $result : null,
+                $pmaThemeImage, $profiling_results, isset($result) ? $result : null,
                 $sql_query, isset($complete_query) ? $complete_query : null
             );
         } else {
