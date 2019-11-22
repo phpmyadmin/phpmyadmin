3f9ac1dc98cf445cf6079a247eefb680f96aeb10
William Desportes <williamdes@wdes.fr>
2019-11-23 00:05:43 +0100

    Fix engine can be null
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- libraries/classes/Database/Designer/Common.php
+++ libraries/classes/Database/Designer/Common.php
@@ -60,7 +60,7 @@ class Common
             $designerTables[] = new DesignerTable(
                                     $db,
                                     $one_table['TABLE_NAME'],
-                                    $one_table['ENGINE'],
+                                    is_string($one_table['ENGINE']) ? $one_table['ENGINE'] : '',
                                     $DF
                                 );
         }
