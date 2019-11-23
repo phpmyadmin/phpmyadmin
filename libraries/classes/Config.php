8fbd20ba31dd55d73fb10bc9aad47de92cce6d84
Isaac Bennetch <bennetch@gmail.com>
2019-11-23 06:48:13 -0500

    Prepare for version 4.9.3-dev
    Signed-off-by: Isaac Bennetch <bennetch@gmail.com>

--- libraries/classes/Config.php
+++ libraries/classes/Config.php
@@ -116,7 +116,7 @@ class Config
      */
     public function checkSystem()
     {
-        $this->set('PMA_VERSION', '4.9.2');
+        $this->set('PMA_VERSION', '4.9.3-dev');
         /* Major version */
         $this->set(
             'PMA_MAJOR_VERSION',
