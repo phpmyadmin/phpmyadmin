9d3f7519c990b3b4b34991b9cf8b1884ceef177b
Maurício Meneghini Fauth <mauricio@fauth.dev>
2019-12-21 14:54:40 -0300

    Revert "Fix #14433 - "You do not have privileges to manipulate with the users!" on root superadmin"
    This reverts commit b8650115be7cc2d79d231723b7a6f1961e834d27.
    
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- libraries/classes/DatabaseInterface.php
+++ libraries/classes/DatabaseInterface.php
@@ -2330,11 +2330,7 @@ class DatabaseInterface
     {
         if (count($this->_current_user) == 0) {
             $user = $this->getCurrentUser();
-            if ($user === '@') {// Request did not succeed, please do not cache
-                return ['', ''];
-            } else {
-                $this->_current_user = explode("@", $user);
-            }
+            $this->_current_user = explode("@", $user);
         }
         return $this->_current_user;
     }
