62eecc8f5d3f5dcfdb5201bf91f7542bb74b9262
Maurício Meneghini Fauth <mauricio@fauth.dev>
2019-12-26 14:10:08 -0300

    Fix Server\Status\MonitorControllerTest tests
    Adds missing $response->setAjax(true) calls.
    
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- test/classes/Controllers/Server/Status/MonitorControllerTest.php
+++ test/classes/Controllers/Server/Status/MonitorControllerTest.php
@@ -230,8 +230,11 @@ class MonitorControllerTest extends TestCase
 
         $GLOBALS['dbi'] = $dbi;
 
+        $response = Response::getInstance();
+        $response->setAjax(true);
+
         $controller = new MonitorController(
-            Response::getInstance(),
+            $response,
             $GLOBALS['dbi'],
             new Template(),
             $this->data,
@@ -302,8 +305,11 @@ class MonitorControllerTest extends TestCase
 
         $GLOBALS['dbi'] = $dbi;
 
+        $response = Response::getInstance();
+        $response->setAjax(true);
+
         $controller = new MonitorController(
-            Response::getInstance(),
+            $response,
             $GLOBALS['dbi'],
             new Template(),
             $this->data,
@@ -359,8 +365,11 @@ class MonitorControllerTest extends TestCase
         $dbi->expects($this->any())->method('fetchResult')
             ->will($this->returnValue($value));
 
+        $response = Response::getInstance();
+        $response->setAjax(true);
+
         $controller = new MonitorController(
-            Response::getInstance(),
+            $response,
             $dbi,
             new Template(),
             $this->data,
@@ -401,8 +410,11 @@ class MonitorControllerTest extends TestCase
         $dbi->expects($this->at(5))->method('fetchAssoc')
             ->will($this->returnValue(false));
 
+        $response = Response::getInstance();
+        $response->setAjax(true);
+
         $controller = new MonitorController(
-            Response::getInstance(),
+            $response,
             $dbi,
             new Template(),
             $this->data,
