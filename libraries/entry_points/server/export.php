48a7ce660a225a96fc9338817d03f3fa374eb0f2
Maurício Meneghini Fauth <mauricio@fauth.dev>
2020-01-03 16:20:30 -0300

    Create Server\ExportController controller
    Extracts the server export entry point logic to the controller.
    
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- libraries/entry_points/server/export.php
+++ libraries/entry_points/server/export.php
@@ -1,55 +1,17 @@
 <?php
 /**
- * object the server export page
- *
  * @package PhpMyAdmin
  */
 declare(strict_types=1);
 
-use PhpMyAdmin\Config\PageSettings;
-use PhpMyAdmin\Display\Export;
-use PhpMyAdmin\Response;
+use PhpMyAdmin\Controllers\Server\ExportController;
 
 if (! defined('PHPMYADMIN')) {
     exit;
 }
 
-global $db, $table, $sql_query, $num_tables, $unlim_num_rows, $tmp_select, $select_item, $multi_values;
+global $containerBuilder;
 
-require_once ROOT_PATH . 'libraries/server_common.inc.php';
-
-PageSettings::showGroup('Export');
-
-$response = Response::getInstance();
-$header   = $response->getHeader();
-$scripts  = $header->getScripts();
-$scripts->addFile('export.js');
-
-$export_page_title = __('View dump (schema) of databases') . "\n";
-
-$displayExport = new Export();
-
-$select_item = $tmp_select ?? '';
-$multi_values = $displayExport->getHtmlForSelectOptions($select_item);
-
-if (! isset($sql_query)) {
-    $sql_query = '';
-}
-if (! isset($num_tables)) {
-    $num_tables = 0;
-}
-if (! isset($unlim_num_rows)) {
-    $unlim_num_rows = 0;
-}
-$response = Response::getInstance();
-$response->addHTML(
-    $displayExport->getDisplay(
-        'server',
-        $db,
-        $table,
-        $sql_query,
-        $num_tables,
-        $unlim_num_rows,
-        $multi_values
-    )
-);
+/** @var ExportController $controller */
+$controller = $containerBuilder->get(ExportController::class);
+$controller->index();
