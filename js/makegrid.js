phpmyadmin/phpmyadmin

commit b93ae5004436ca45d81cac6a0777e2dc617d97a7

Fixed some `:first :last` deprecated selectors (Batch-1)
Signed-off-by: Yash Bothra <yashrajbothra786@gmail.com>

js/makegrid.js
@@ -253,9 +253,9 @@ var makeGrid = function (t, enableResize, enableReorder, enableVisib, enableGrid
          */
         reposRsz: function () {
             $(g.cRsz).find('div').hide();
-            var $firstRowCols = $(g.t).find('tr:first th.draggable:visible');
+            var $firstRowCols = $(g.t).find('tr').first().find('th.draggable:visible');
             var $resizeHandles = $(g.cRsz).find('div').removeClass('condition');
-            $(g.t).find('table.pma_table').find('thead th:first').removeClass('before-condition');
+            $(g.t).find('table.pma_table').find('thead th').first().removeClass('before-condition');
             for (var n = 0, l = $firstRowCols.length; n < l; n++) {
                 var $col = $($firstRowCols[n]);
                 var colWidth;
@@ -274,7 +274,7 @@ var makeGrid = function (t, enableResize, enableReorder, enableVisib, enableGrid
                 }
             }
             if ($($resizeHandles[0]).hasClass('condition')) {
-                $(g.t).find('thead th:first').addClass('before-condition');
+                $(g.t).find('thead th').first().addClass('before-condition');
             }
             $(g.cRsz).css('height', $(g.t).height());
         },
@@ -512,7 +512,7 @@ var makeGrid = function (t, enableResize, enableReorder, enableVisib, enableGrid
             g.sendColPrefs();
 
             // check visible first row headers count
-            g.visibleHeadersCount = $(g.t).find('tr:first th.draggable:visible').length;
+            g.visibleHeadersCount = $(g.t).find('tr').first().find('th.draggable:visible').length;
             g.refreshRestoreButton();
         },
 
@@ -1383,7 +1383,7 @@ var makeGrid = function (t, enableResize, enableReorder, enableVisib, enableGrid
                                 var tools = $resultQuery.find('.tools').wrap('<p>').parent().html();
                                 // sqlOuter and tools will not be present if 'Show SQL queries' configuration is off
                                 if (typeof sqlOuter !== 'undefined' && typeof tools !== 'undefined') {
-                                    $(g.o).find('.result_query:not(:last)').remove();
+                                    $(g.o).find('.result_query').not().last().remove();
                                     var $existingQuery = $(g.o).find('.result_query');
                                     // If two query box exists update query in second else add a second box
                                     if ($existingQuery.find('div.sqlOuter').length > 1) {
@@ -1563,7 +1563,7 @@ var makeGrid = function (t, enableResize, enableReorder, enableVisib, enableGrid
             g.cRsz.className = 'cRsz';
 
             // get data columns in the first row of the table
-            var $firstRowCols = $(g.t).find('tr:first th.draggable');
+            var $firstRowCols = $(g.t).find('tr').first().find('th.draggable');
 
             // create column borders
             $firstRowCols.each(function () {
@@ -1599,7 +1599,7 @@ var makeGrid = function (t, enableResize, enableReorder, enableVisib, enableGrid
             g.reorderHint = Messages.strColOrderHint;
 
             // get data columns in the first row of the table
-            var $firstRowCols = $(g.t).find('tr:first th.draggable');
+            var $firstRowCols = $(g.t).find('tr').first().find('th.draggable');
 
             // initialize column order
             var $colOrder = $(g.o).find('.col_order');   // check if column order is passed from PHP
@@ -1694,7 +1694,7 @@ var makeGrid = function (t, enableResize, enableReorder, enableVisib, enableGrid
             g.showAllColText = Messages.strShowAllCol;
 
             // get data columns in the first row of the table
-            var $firstRowCols = $(g.t).find('tr:first th.draggable');
+            var $firstRowCols = $(g.t).find('tr').first().find('th.draggable');
 
             var i;
             // initialize column visibility
@@ -2161,14 +2161,14 @@ var makeGrid = function (t, enableResize, enableReorder, enableVisib, enableGrid
     g.o = $(t).parents('.sqlqueryresults');
 
     // get data columns in the first row of the table
-    var $firstRowCols = $(t).find('tr:first th.draggable');
+    var $firstRowCols = $(t).find('tr').first().find('th.draggable');
 
     // initialize visible headers count
     g.visibleHeadersCount = $firstRowCols.filter(':visible').length;
 
     // assign first column (actions) span
-    if (! $(t).find('tr:first th:first').hasClass('draggable')) {  // action header exist
-        g.actionSpan = $(t).find('tr:first th:first').prop('colspan');
+    if (! $(t).find('tr').first().find('th').first().hasClass('draggable')) {  // action header exist
+        g.actionSpan = $(t).find('tr').first().find('th').first().prop('colspan');
     } else {
         g.actionSpan = 0;
     }

js/config.js
@@ -615,10 +615,10 @@ function setupConfigTabs () {
                 e.preventDefault();
                 setTab($(this).attr('href').substr(1));
             })
-            .filter(':first')
+            .first()
             .parent()
             .addClass('active');
-        $this.find('div.tabs_contents fieldset').hide().filter(':first').show();
+        $this.find('div.tabs_contents fieldset').hide().first().show();
     });
 }
 

js/database/central_columns.js
@@ -80,7 +80,7 @@ AJAX.registerOnload('database/central_columns.js', function () {
     });
     $('#add_new').find('td').each(function () {
         if ($(this).attr('name') !== 'undefined') {
-            $(this).find('input,select:first').attr('name', $(this).attr('name'));
+            $(this).find('input,select').first().attr('name', $(this).attr('name'));
         }
     });
     $('#field_0_0').attr('required','required');
@@ -138,7 +138,7 @@ AJAX.registerOnload('database/central_columns.js', function () {
         var rownum = $(this).data('rownum');
         $('#f_' + rownum + ' td').each(function () {
             if ($(this).attr('name') !== 'undefined') {
-                $(this).find(':input[type!="hidden"],select:first')
+                $(this).find(':input[type!="hidden"],select').first()
                     .attr('name', $(this).attr('name'));
             }
         });
@@ -192,7 +192,7 @@ AJAX.registerOnload('database/central_columns.js', function () {
     });
     $('#table-select').on('change', function () {
         var selectValue = $(this).val();
-        var defaultColumnSelect = $('#column-select').find('option:first');
+        var defaultColumnSelect = $('#column-select').find('option').first();
         var href = 'index.php?route=/database/central-columns';
         var params = {
             'ajax_request' : true,

js/database/structure.js
@@ -50,7 +50,7 @@ DatabaseStructure.adjustTotals = function () {
     /**
      * @var $allTr jQuery object that references all the rows in the list of tables
      */
-    var $allTr = $('#tablesForm').find('table.data tbody:first tr');
+    var $allTr = $('#tablesForm').find('table.data tbody').first().find('tr');
     // New summary values for the table
     var tableSum = $allTr.length;
     var rowsSum = 0;

js/database/tracking.js
@@ -12,7 +12,7 @@ AJAX.registerTeardown('database/tracking.js', function () {
  */
 AJAX.registerOnload('database/tracking.js', function () {
     var $versions = $('#versions');
-    $versions.find('tr:first th').append($('<div class="sorticon"></div>'));
+    $versions.find('tr').first().find('th').append($('<div class="sorticon"></div>'));
     $versions.tablesorter({
         sortList: [[1, 0]],
         headers: {
@@ -25,7 +25,7 @@ AJAX.registerOnload('database/tracking.js', function () {
     });
 
     var $noVersions = $('#noversions');
-    $noVersions.find('tr:first th').append($('<div class="sorticon"></div>'));
+    $noVersions.find('tr').first().find('th').append($('<div class="sorticon"></div>'));
     $noVersions.tablesorter({
         sortList: [[1, 0]],
         headers: {

js/export.js
@@ -863,7 +863,7 @@ Export.addAlias = function (type, name, field, value) {
 
     var row = $('#alias_data tfoot tr').clone();
     row.find('th').text(type);
-    row.find('td:first').text(name);
+    row.find('td').first().text(name);
     row.find('input').attr('name', field);
     row.find('input').val(value);
     row.find('.alias_remove').on('click', function () {

js/functions.js
@@ -2942,7 +2942,7 @@ AJAX.registerOnload('functions.js', function () {
                             /**
                              * @var curr_last_row   Object referring to the last <tr> element in {@link tablesTable}
                              */
-                            var currLastRow = $(tablesTable).find('tr:last');
+                            var currLastRow = $(tablesTable).find('tr').last();
                             /**
                              * @var curr_last_row_index_string   String containing the index of {@link currLastRow}
                              */
@@ -3416,7 +3416,7 @@ var $enumEditorDialog = null;
 AJAX.registerOnload('functions.js', function () {
     $(document).on('click', 'a.open_enum_editor', function () {
         // Get the name of the column that is being edited
-        var colname = $(this).closest('tr').find('input:first').val();
+        var colname = $(this).closest('tr').find('input').first().val();
         var title;
         var i;
         // And use it to make up a title for the page
@@ -3541,7 +3541,7 @@ AJAX.registerOnload('functions.js', function () {
             buttons: buttonOptions,
             open: function () {
                 // Focus the "Go" button after opening the dialog
-                $(this).closest('.ui-dialog').find('.ui-dialog-buttonpane button:first').trigger('focus');
+                $(this).closest('.ui-dialog').find('.ui-dialog-buttonpane button').first().trigger('focus');
             },
             close: function () {
                 $(this).remove();
@@ -3687,7 +3687,7 @@ AJAX.registerOnload('functions.js', function () {
                     }
                     return false;
                 });
-                $(this).closest('.ui-dialog').find('.ui-dialog-buttonpane button:first').trigger('focus');
+                $(this).closest('.ui-dialog').find('.ui-dialog-buttonpane button').first().trigger('focus');
             },
             close: function () {
                 $('#col_list').off('click', '.pick');
@@ -3714,7 +3714,7 @@ AJAX.registerOnload('functions.js', function () {
                     Functions.getImage('b_drop') +
                     '</td></tr>'
                 )
-                .find('tr:last')
+                .find('tr').last()
                 .show('fast');
         }
     });
@@ -3785,7 +3785,7 @@ AJAX.registerOnload('functions.js', function () {
         while (rowsToAdd--) {
             var $indexColumns = $('#index_columns');
             var $newrow = $indexColumns
-                .find('tbody > tr:first')
+                .find('tbody > tr').first()
                 .clone()
                 .appendTo(
                     $indexColumns.find('tbody')
@@ -4585,7 +4585,7 @@ AJAX.registerOnload('functions.js', function () {
                 // was also prevented in IE
                 $(this).trigger('blur');
 
-                $(this).closest('.ui-dialog').find('.ui-button:first').trigger('click');
+                $(this).closest('.ui-dialog').find('.ui-button').first().trigger('click');
             }
         }); // end $(document).on()
     }
@@ -4954,8 +4954,8 @@ AJAX.registerOnload('functions.js', function () {
 
             // There could be multiple submit buttons on the same form,
             // we assume all of them behave identical and just click one.
-            if (! $form.find('input[type="submit"]:first') ||
-                ! $form.find('input[type="submit"]:first').trigger('click')
+            if (! $form.find('input[type="submit"]').first() ||
+                ! $form.find('input[type="submit"]').first().trigger('click')
             ) {
                 $form.trigger('submit');
             }

js/menu_resizer.js
@@ -79,7 +79,7 @@
     MenuResizer.prototype.resize = function () {
         var wmax = this.widthCalculator.call(this.$container);
         var windowWidth = $(window).width();
-        var $submenu = this.$container.find('.submenu:last');
+        var $submenu = this.$container.find('.submenu').last();
         var submenuW = $submenu.outerWidth(true);
         var $submenuUl = $submenu.find('ul');
         var $li = this.$container.find('> li');

commit bc89e83869a5a668d3434b700bbbbd287c481113

Fix broken remove logic
Got broken in commit: b93ae50

Signed-off-by: Tobias Speicher <rootcommander@gmail.com>

js/makegrid.js
@@ -1378,7 +1378,7 @@ var makeGrid = function (t, enableResize, enableReorder, enableVisib, enableGrid
                                 var tools = $resultQuery.find('.tools').wrap('<p>').parent().html();
                                 // sqlOuter and tools will not be present if 'Show SQL queries' configuration is off
                                 if (typeof sqlOuter !== 'undefined' && typeof tools !== 'undefined') {
-                                    $(g.o).find('.result_query').not().last().remove();
+                                    $(g.o).find('.result_query').not($(g.o).find('.result_query').last()).remove();
                                     var $existingQuery = $(g.o).find('.result_query');
                                     // If two query box exists update query in second else add a second box
                                     if ($existingQuery.find('div.sqlOuter').length > 1) {