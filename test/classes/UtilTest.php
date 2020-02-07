phpmyadmin/phpmyadmin

commit 3db8949ad3836db24e5cf182cbe715296654f56e

Fix #15200 - server-side HTTPS detection misses support for Forwarded HTTP Extension (RFC 7239)
Fixes: #15200
Signed-off-by: William Desportes <williamdes@wdes.fr>

libraries/classes/Util.php
@@ -4967,4 +4967,27 @@ class Util
     {
         return ctype_digit((string) $input);
     }
+
+    /**
+     * Get the protocol from the RFC 7239 Forwarded header
+     * @param string $headerContents The Forwarded header contents
+     * @return string the protocol http/https
+     */
+    public static function getProtoFromForwardedHeader(string $headerContents): string
+    {
+        if (strpos($headerContents, '=') !== false) {// does not contain any equal sign
+            $hops = explode(',', $headerContents);
+            $parts = explode(';', $hops[0]);
+            foreach ($parts as $part) {
+                [
+                    $keyName,
+                    $value,
+                ] = explode('=', $part);
+                if (strtolower(trim($keyName)) === 'proto') {
+                    return empty($value) ? '' : $value;
+                }
+            }
+        }
+        return '';
+    }
 }

test/classes/UtilTest.php
@@ -2682,4 +2682,97 @@ class UtilTest extends PmaTestCase
             ],
         ];
     }
+
+    /**
+     * Test for Util::getProtoFromForwardedHeader
+     *
+     * @param string $header The http Forwarded header
+     * @param string $proto  The protocol http/https
+     *
+     * @return void
+     *
+     * @dataProvider providerForwardedHeaders
+     */
+    public function testGetProtoFromForwardedHeader(string $header, string $proto): void
+    {
+        $protocolDetected = Util::getProtoFromForwardedHeader($header);
+        $this->assertEquals($proto, $protocolDetected);
+    }
+
+    /**
+     * Data provider for Util::getProtoFromForwardedHeader test
+     * @source https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Forwarded MDN docs
+     * @source https://www.nginx.com/resources/wiki/start/topics/examples/forwarded/ Nginx docs
+     *
+     * @return array
+     */
+    public function providerForwardedHeaders(): array
+    {
+        return [
+            [
+                '',
+                '',
+            ],
+            [
+                '=',
+                '',
+            ],
+            [
+                'https',
+                '',
+            ],
+            [
+                'https',
+                '',
+            ],
+            [
+                '=https',
+                '',
+            ],
+            [
+                '=http',
+                '',
+            ],
+            [
+                'For="[2001:db8:cafe::17]:4711"',
+                '',
+            ],
+            [
+                'for=192.0.2.60;proto=http;by=203.0.113.43',
+                'http',
+            ],
+            [
+                'for=192.0.2.43, for=198.51.100.17',
+                '',
+            ],
+            [
+                'for=123.34.567.89',
+                '',
+            ],
+            [
+                'for=192.0.2.43, for="[2001:db8:cafe::17]"',
+                '',
+            ],
+            [
+                'for=12.34.56.78;host=example.com;proto=https, for=23.45.67.89',
+                'https',
+            ],
+            [
+                'for=12.34.56.78, for=23.45.67.89;secret=egah2CGj55fSJFs, for=10.1.2.3',
+                '',
+            ],
+            [
+                'for=injected;by="',
+                '',
+            ],
+            [
+                'for=injected;by=", for=real',
+                '',
+            ],
+            [
+                'for=192.0.2.60;proto=http;by=203.0.113.43',
+                'http',
+            ],
+        ];
+    }
 }

libraries/classes/Config.php
@@ -1361,6 +1361,9 @@ class Config
         } elseif (strtolower(Core::getenv('HTTP_CLOUDFRONT_FORWARDED_PROTO')) === 'https') {
             // Amazon CloudFront, issue #15621
             $is_https = true;
+        } elseif (Util::getProtoFromForwardedHeader(Core::getenv('HTTP_FORWARDED')) === 'https') {
+            // RFC 7239 Forwarded header
+            $is_https = true;
         } elseif (Core::getenv('SERVER_PORT') == 443) {
             $is_https = true;
         }

test/classes/ConfigTest.php
@@ -485,6 +485,7 @@ class ConfigTest extends PmaTestCase
      *
      * @param string $scheme          http scheme
      * @param string $https           https
+     * @param string $forwarded       forwarded header
      * @param string $uri             request uri
      * @param string $lb              http https from lb
      * @param string $front           http front end https
@@ -498,10 +499,11 @@ class ConfigTest extends PmaTestCase
      *
      * @dataProvider httpsParams
      */
-    public function testIsHttps($scheme, $https, $uri, $lb, $front, $proto, $protoCloudFront, $pmaAbsoluteUri, $port, $expected): void
+    public function testIsHttps($scheme, $https, string $forwarded, $uri, $lb, $front, $proto, $protoCloudFront, $pmaAbsoluteUri, $port, $expected): void
     {
         $_SERVER['HTTP_SCHEME'] = $scheme;
         $_SERVER['HTTPS'] = $https;
+        $_SERVER['HTTP_FORWARDED'] = $forwarded;
         $_SERVER['REQUEST_URI'] = $uri;
         $_SERVER['HTTP_HTTPS_FROM_LB'] = $lb;
         $_SERVER['HTTP_FRONT_END_HTTPS'] = $front;
@@ -528,6 +530,7 @@ class ConfigTest extends PmaTestCase
                 '',
                 '',
                 '',
+                '',
                 'http',
                 '',
                 '',
@@ -536,6 +539,7 @@ class ConfigTest extends PmaTestCase
             ],
             [
                 'http',
+                '',
                 '',
                 'http://',
                 '',
@@ -552,6 +556,7 @@ class ConfigTest extends PmaTestCase
                 '',
                 '',
                 '',
+                '',
                 'http',
                 '',
                 '',
@@ -560,6 +565,7 @@ class ConfigTest extends PmaTestCase
             ],
             [
                 'http',
+                '',
                 '',
                 '',
                 '',
@@ -575,6 +581,7 @@ class ConfigTest extends PmaTestCase
                 '',
                 '',
                 '',
+                '',
                 'on',
                 'http',
                 '',
@@ -586,6 +593,7 @@ class ConfigTest extends PmaTestCase
                 'http',
                 '',
                 '',
+                '',
                 'on',
                 '',
                 'http',
@@ -596,6 +604,7 @@ class ConfigTest extends PmaTestCase
             ],
             [
                 'http',
+                '',
                 '',
                 'https://',
                 '',
@@ -612,6 +621,7 @@ class ConfigTest extends PmaTestCase
                 '',
                 '',
                 '',
+                '',
                 'http',
                 '',
                 '',
@@ -624,6 +634,7 @@ class ConfigTest extends PmaTestCase
                 '',
                 '',
                 '',
+                '',
                 'http',
                 '',
                 '',
@@ -632,6 +643,7 @@ class ConfigTest extends PmaTestCase
             ],
             [
                 'http',
+                '',
                 '',
                 '',
                 '',
@@ -648,6 +660,7 @@ class ConfigTest extends PmaTestCase
                 '',
                 '',
                 '',
+                '',
                 'https',
                 'http',
                 '',
@@ -663,11 +676,13 @@ class ConfigTest extends PmaTestCase
                 '',
                 '',
                 '',
+                '',
                 80,
                 true,
             ],
             [
                 'http',
+                '',
                 '',
                 '',
                 '',
@@ -679,6 +694,7 @@ class ConfigTest extends PmaTestCase
                 false,
             ],
             [
+                '',
                 '',
                 '',
                 '',
@@ -698,9 +714,23 @@ class ConfigTest extends PmaTestCase
                 '',
                 '',
                 '',
+                '',
                 'http://127.0.0.1',
                 80,
                 false,
+            ],
+            [
+                '',
+                '',
+                'for=12.34.56.78;host=example.com;proto=https, for=23.45.67.89',
+                '',
+                '',
+                '',
+                '',
+                '',
+                'http://127.0.0.1',
+                80,
+                true,
             ],
         ];
     }

commit 6729c89b1775b48f6fb636e27279ab94c91f79da

Fix php notices on #15200 - server-side HTTPS detection misses support for Forwarded HTTP Extension (RFC 7239)
Ref: https://github.com/phpmyadmin/phpmyadmin/commit/3db8949ad3836db24e5cf182cbe715296654f56e#commitcomment-37079212
Fixes: 3db8949ad3836db24e5cf182cbe715296654f56e

Signed-off-by: William Desportes <williamdes@wdes.fr>

libraries/classes/Util.php
@@ -4979,12 +4979,15 @@ class Util
             $hops = explode(',', $headerContents);
             $parts = explode(';', $hops[0]);
             foreach ($parts as $part) {
-                [
-                    $keyName,
-                    $value,
-                ] = explode('=', $part);
-                if (strtolower(trim($keyName)) === 'proto') {
-                    return empty($value) ? '' : $value;
+                $keyValueArray = explode('=', $part, 2);
+                if (count($keyValueArray) === 2) {
+                    [
+                        $keyName,
+                        $value,
+                    ] = $keyValueArray;
+                    if (strtolower(trim($keyName)) === 'proto' && in_array($value, ['http', 'https'])) {
+                        return empty($value) ? '' : $value;
+                    }
                 }
             }
         }

test/classes/UtilTest.php
@@ -2789,6 +2789,18 @@ class UtilTest extends PmaTestCase
                 'for=192.0.2.60;proto=http;by=203.0.113.43',
                 'http',
             ],
+            [
+                'for=12.34.45.67;secret="special;proto=abc;test=1";proto=http,for=23.45.67.89',
+                'http',
+            ],
+            [
+                'for=12.34.45.67;secret="special;proto=abc;test=1";proto=418,for=23.45.67.89',
+                '',
+            ],
+            /*[ // this test case is very special and would need a different implementation
+                'for=12.34.45.67;secret="special;proto=http;test=1";proto=https,for=23.45.67.89',
+                'https'
+            ]*/
         ];
     }
 }