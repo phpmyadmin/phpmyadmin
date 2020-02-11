abd4b8d657b91912b6a64bc98ff67daf639ebed0
Jayati Shrivastava <gaurijove@gmail.com>
2020-02-12 01:56:04 +0530

    Fixes #15882 - Replace kbd tag by code tag
    Fixes #15882
    
    Signed-off-by: Jayati Shrivastava <gaurijove@gmail.com>

--- libraries/classes/Config/Descriptions.php
+++ libraries/classes/Config/Descriptions.php
@@ -488,8 +488,8 @@ class Descriptions
             'NavigationLogoLink_desc' => __('URL where logo in the navigation panel will point to.'),
             'NavigationLogoLink_name' => __('Logo link URL'),
             'NavigationLogoLinkWindow_desc' => __(
-                'Open the linked page in the main window ([kbd]main[/kbd]) or in a new one '
-                . '([kbd]new[/kbd]).'
+                'Open the linked page in the main window ([code]main[/code]) or in a new one '
+                . '([code]new[/code]).'
             ),
             'NavigationLogoLinkWindow_name' => __('Logo link target'),
             'NavigationDisplayServers_desc' => __('Display server choice at the top of the navigation panel.'),
