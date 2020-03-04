ae182cddb5909b1c80e786fd8d33f991cfe9c1f2
William Desportes <williamdes@wdes.fr>
2020-03-04 01:48:24 +0100

    Ref #16005 - Move services loader to a php array
    Signed-off-by: William Desportes <williamdes@wdes.fr>

---
+++ libraries/services_loader.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Symfony\Component\DependencyInjection\Reference;
+
+return function (ContainerConfigurator $configurator) {
+    $services = $configurator->services();
+    $loadServices = function (array $servicesFile, ServicesConfigurator $services): void {
+        foreach ($servicesFile['services'] as $serviceName => $service) {
+            if (is_string($service)) {
+                $services->alias($serviceName, $service);
+            } else {
+                $theService = $services->set($serviceName, $service['class']);
+                if ($service['arguments'] !== null) {
+                    foreach ($service['arguments'] as &$argumentName) {
+                        if ($argumentName[0] === '@') {
+                            $services->alias($serviceName, substr($argumentName, 1));
+                            $argumentName = new Reference(substr($argumentName, 1));
+                        }
+                    }
+                    $theService->args($service['arguments']);
+                }
+                if ($service['factory'] !== null) {
+                    $theService->factory($service['factory']);
+                }
+            }
+        }
+    };
+
+    $servicesFile = include __DIR__ . '/services.php';
+    $loadServices($servicesFile, $services);
+    $servicesFile = include __DIR__ . '/services_controllers.php';
+    $loadServices($servicesFile, $services);
+};
