b43e3fb5f16d9252001878b5d7f8f313f1189ddd
William Desportes <williamdes@wdes.fr>
2020-03-12 19:37:40 +0100

    Rewrite some php parts of the Designer code
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- libraries/classes/Database/Designer/DesignerTable.php
+++ libraries/classes/Database/Designer/DesignerTable.php
@@ -23,6 +23,12 @@ class DesignerTable implements JsonSerializable
 
     /** @var string|null */
     private $displayField;
+
+    /**
+     * The associated columns
+     *
+     * @var DesignerColumn[]
+     */
     private $columns = [];
 
     /**
@@ -122,7 +128,7 @@ class DesignerTable implements JsonSerializable
      */
     public function getDisplayFieldBase64(): string
     {
-        return base64_encode($this->getDisplayField());
+        return base64_encode($this->getDisplayField() ?? '');
     }
 
     /**
@@ -145,6 +151,9 @@ class DesignerTable implements JsonSerializable
         return $this->columns;
     }
 
+    /**
+     * @return array
+     */
     public function jsonSerialize()
     {
         return [
