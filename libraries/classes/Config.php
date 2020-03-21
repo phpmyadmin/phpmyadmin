3133be3e828f5ccf1803d2fccc469db4ae78e09b
Isaac Bennetch <bennetch@gmail.com>
2020-03-20 23:01:37 -0400

    Prepare for 5.0.2
    Signed-off-by: Isaac Bennetch <bennetch@gmail.com>

--- libraries/classes/Config.php
+++ libraries/classes/Config.php
@@ -113,7 +113,7 @@ class Config
      */
     public function checkSystem(): void
     {
-        $this->set('PMA_VERSION', '5.0.2-dev');
+        $this->set('PMA_VERSION', '5.0.2');
         /* Major version */
         $this->set(
             'PMA_MAJOR_VERSION',
