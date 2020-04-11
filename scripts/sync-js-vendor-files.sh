b557a9aa6ab38d14df7590807a2f067285ccc8d4
William Desportes <williamdes@wdes.fr>
2020-04-11 22:20:45 +0200

    Add more vendors to the JS sync vendor script
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- scripts/sync-js-vendor-files.sh
+++ scripts/sync-js-vendor-files.sh
@@ -6,8 +6,22 @@
 # This script will sync JS node_modules files with existing files in vendor folder.
 # Warning: this will not add any more files, it uses existing files in vendor folder.
 
-ROOT_DIR="$(dirname $0)/../"
+ROOT_DIR="$(realpath $(dirname $0)/../)"
 echo "Using root dir: $ROOT_DIR"
 
+echo 'Updating codemirror'
 cd ./js/vendor/codemirror
 find * -type f -print -exec cp ../../../node_modules/codemirror/{} {} \;
+cd ${ROOT_DIR}
+
+echo 'Updating jquery'
+cp ./node_modules/jquery/dist/jquery.min.js ./js/vendor/jquery/jquery.min.js
+cp ./node_modules/jquery/dist/jquery.min.map ./js/vendor/jquery/jquery.min.map
+cp ./node_modules/jquery/LICENSE.txt ./js/vendor/jquery/MIT-LICENSE.txt
+echo 'Updating jquery-migrate'
+cp ./node_modules/jquery-migrate/dist/jquery-migrate.js ./js/vendor/jquery/jquery-migrate.js
+echo 'Updating bootstrap'
+cp ./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js ./js/vendor/bootstrap/bootstrap.bundle.min.js
+cp ./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map ./js/vendor/bootstrap/bootstrap.bundle.min.js.map
+
+echo 'Done.'
