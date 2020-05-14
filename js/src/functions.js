2fa6bb2e4cd42fb18b4e1f03630212543a406ae5
Yash Bothra <yashrajbothra786@gmail.com>
2020-05-15 01:27:22 +0530

    Sql for Default Function
    Signed-off-by: Yash Bothra <yashrajbothra786@gmail.com>

--- js/src/functions.js
+++ js/src/functions.js
@@ -430,9 +430,10 @@ Functions.sprintf = function () {
  */
 Functions.hideShowDefaultValue = function ($defaultType) {
     if ($defaultType.val() === 'USER_DEFINED_VALUE') {
+        $defaultType.siblings('.default_function').hide();
         $defaultType.siblings('.default_value').show().trigger('focus');
     } else if ($defaultType.val() === 'USER_DEFINED_FUNCTION') {
-        $defaultType.siblings('.default_function').show().trigger('focus');
+        $defaultType.siblings('.default_function, .default_value').show().trigger('focus');
     } else {
         $defaultType.siblings('.default_value, .default_function').hide();
         if ($defaultType.val() === 'NULL') {
@@ -463,6 +464,7 @@ Functions.hideShowExpression = function ($virtuality) {
 Functions.verifyColumnsProperties = function () {
     $('select.column_type').each(function () {
         Functions.showNoticeForEnum($(this));
+        Functions.updateDefaultFunction($(this));
     });
     $('select.default_type').each(function () {
         Functions.hideShowDefaultValue($(this));
@@ -2551,6 +2553,32 @@ Functions.showNoticeForEnum = function (selectElement) {
 };
 
 /**
+ * Update the function for particular type
+ */
+Functions.updateDefaultFunction = function (selectElement) {
+    var selectedType = selectElement.val();
+    var params = {
+        'ajax_request': true,
+        'type': selectedType,
+    };
+    var url = 'index.php?route=/ajax/defaultFuncs';
+    $.post(url, params, function (response) {
+        if (response.success === true) {
+            var selectBox = $('#field_' + selectElement.attr('id').substring(6,7) + '_4').siblings('select');
+            selectBox.find('option').remove();
+            $.each(response.default_functions, function (id, value) {
+                var option = $('<option></option>');
+                option.text(value);
+                option.attr('value', value);
+                $('#field_' + selectElement.attr('id').substring(6,7) + '_4').siblings('select').append(option);
+            });
+        } else {
+            Functions.ajaxShowMessage(response.error, false);
+        }
+    });
+};
+
+/**
  * Creates a Profiling Chart. Used in sql.js
  * and in server/status/monitor.js
  */
@@ -3312,6 +3340,7 @@ AJAX.registerOnload('functions.js', function () {
     // needs on() to work also in the Create Table dialog
     $(document).on('change', 'select.column_type', function () {
         Functions.showNoticeForEnum($(this));
+        Functions.updateDefaultFunction($(this));
     });
     $(document).on('change', 'select.default_type', function () {
         Functions.hideShowDefaultValue($(this));
--- libraries/classes/Table/ColumnsDefinition.php
+++ libraries/classes/Table/ColumnsDefinition.php
@@ -158,7 +158,7 @@ final class ColumnsDefinition
             $regenerate = 1;
         }
 
-        $foreigners = $relation->getForeigners($db, $table, '', 'foreign');
+         $foreigners = $relation->getForeigners($db, $table, '', 'foreign');
         $child_references = null;
         // From MySQL 5.6.6 onwards columns with foreign keys can be renamed.
         // Hence, no need to get child references
@@ -461,6 +461,7 @@ final class ColumnsDefinition
 
             $default_value = '';
             $type_upper = mb_strtoupper($type);
+            $default_function = $GLOBALS['dbi']->types->getFunctions($type_upper);
 
             // For a TIMESTAMP, do not show the string "CURRENT_TIMESTAMP" as a default value
             if (isset($columnMeta['DefaultValue'])) {
