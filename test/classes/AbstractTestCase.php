8624fbbe97b3c3815b1cd3f198f7271d173143f0
William Desportes <williamdes@wdes.fr>
2020-05-20 21:10:15 +0200

    Create AbstractTestCase class
    Signed-off-by: William Desportes <williamdes@wdes.fr>

---
+++ test/classes/AbstractTestCase.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpMyAdmin\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Abstract class to hold some usefull methods used in tests
+ * And make tests clean
+ */
+abstract class AbstractTestCase extends TestCase
+{
+
+
+}
