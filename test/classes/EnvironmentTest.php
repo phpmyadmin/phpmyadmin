c4da7f6a9a82d37950087552c38e2ea7271eeb0f
William Desportes <williamdes@wdes.fr>
2020-05-24 13:23:53 +0200

    Remove useless testMySQL
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- test/classes/EnvironmentTest.php
+++ test/classes/EnvironmentTest.php
@@ -6,12 +6,8 @@ declare(strict_types=1);
 
 namespace PhpMyAdmin\Tests;
 
-use PDO;
 use PhpMyAdmin\Tests\AbstractTestCase;
-use Throwable;
 use const PHP_VERSION;
-use function constant;
-use function preg_match;
 use function version_compare;
 
 /**
@@ -30,43 +26,5 @@ class EnvironmentTest extends AbstractTestCase
             version_compare('7.1.3', PHP_VERSION, '<='),
             'phpMyAdmin requires PHP 7.1.3 or above'
         );
-    }
-
-    /**
-     * Tests MySQL connection
-     *
-     * @return void
-     */
-    public function testMySQL()
-    {
-        AbstractTestCase::defineTestingGlobals();
-        try {
-            $pdo = new PDO(
-                'mysql:host=' . $GLOBALS['TESTSUITE_SERVER'] . ';port=' . $GLOBALS['TESTSUITE_PORT'],
-                $GLOBALS['TESTSUITE_USER'],
-                $GLOBALS['TESTSUITE_PASSWORD']
-            );
-            $this->assertNull(
-                $pdo->errorCode(),
-                'Error when trying to connect to database'
-            );
-
-            $pdo->exec('SHOW DATABASES;');
-            $this->assertEquals(
-                0,
-                $pdo->errorCode(),
-                'Error trying to show tables for database'
-            );
-        } catch (Throwable $e) {
-            $this->markTestSkipped('Error: ' . $e->getMessage());
-        }
-
-        // Check id MySQL server is 5 version
-        preg_match(
-            '/^(\d+)?\.(\d+)?\.(\*|\d+)/',
-            $pdo->getAttribute(constant('PDO::ATTR_SERVER_VERSION')),
-            $version_parts
-        );
-        $this->assertEquals(5, $version_parts[1]);
     }
 }
