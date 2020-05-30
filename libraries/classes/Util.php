5a1c5acccc5f9b97a5392faa91ce07731402b47a
CommanderRoot <CommanderRoot@users.noreply.github.com>
2020-05-30 03:26:45 +0200

    Fix #16147
    Signed-off-by: Tobias Speicher <rootcommander@gmail.com>

--- libraries/classes/Util.php
+++ libraries/classes/Util.php
@@ -2828,7 +2828,7 @@ class Util
                 $groupTable,
                 $GLOBALS['dbi']->getTablesFull(
                     $db,
-                    $groupWithSeparator,
+                    $groupWithSeparator !== false ? $groupWithSeparator : '',
                     $groupWithSeparator !== false,
                     $limit_offset,
                     $limit_count,
