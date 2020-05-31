2b733efcf7ba383162fc36a68606c491b5b174c2
ululalbab <shishir64092@gmail.com>
2020-05-31 20:50:23 +0600

    Update config.sample.inc.php

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -29,7 +29,7 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['compress'] = false;
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
 
 /**
  * phpMyAdmin configuration storage settings.
