a7be49f06f5c5196c53c80012c0894a9727eecaa
Maurício Meneghini Fauth <mauricio@fauth.dev>
2020-07-06 16:56:19 -0300

    Remove TESTSUITE constant from js/messages.php
    Related to https://github.com/phpmyadmin/phpmyadmin/issues/15247
    
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- js/messages.php
+++ js/messages.php
@@ -13,36 +13,29 @@ if (! defined('ROOT_PATH')) {
     // phpcs:enable
 }
 
-if (! defined('TESTSUITE')) {
-    chdir('..');
+chdir('..');
 
-    // Send correct type:
-    header('Content-Type: text/javascript; charset=UTF-8');
+// Send correct type.
+header('Content-Type: text/javascript; charset=UTF-8');
 
-    // Cache output in client - the nocache query parameter makes sure that this
-    // file is reloaded when config changes
-    header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 3600) . ' GMT');
+// Cache output in client - the nocache query parameter makes sure that this file is reloaded when config changes.
+header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 3600) . ' GMT');
 
-    // Avoid loading the full common.inc.php because this would add many
-    // non-js-compatible stuff like DOCTYPE
-    // phpcs:disable PSR1.Files.SideEffects
-    define('PMA_MINIMUM_COMMON', true);
-    define('PMA_PATH_TO_BASEDIR', '../');
-    define('PMA_NO_SESSION', true);
-    // phpcs:enable
+// Avoid loading the full common.inc.php because this would add many non-js-compatible stuff like DOCTYPE.
+// phpcs:disable PSR1.Files.SideEffects
+define('PMA_MINIMUM_COMMON', true);
+define('PMA_PATH_TO_BASEDIR', '../');
+define('PMA_NO_SESSION', true);
+// phpcs:enable
 
-    require_once ROOT_PATH . 'libraries/common.inc.php';
-}
+require_once ROOT_PATH . 'libraries/common.inc.php';
 
 $buffer = OutputBuffering::getInstance();
 $buffer->start();
-if (! defined('TESTSUITE')) {
-    register_shutdown_function(
-        static function () {
-            echo OutputBuffering::getInstance()->getContents();
-        }
-    );
-}
+
+register_shutdown_function(static function () {
+    echo OutputBuffering::getInstance()->getContents();
+});
 
 /** @var JavaScriptMessagesController $controller */
 $controller = $containerBuilder->get(JavaScriptMessagesController::class);
