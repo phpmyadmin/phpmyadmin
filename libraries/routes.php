045e8600075fb5e7d1832aa2f30d17abbbd5b9ee
Maurício Meneghini Fauth <mauricio@fauth.dev>
2020-07-15 23:02:56 -0300

    Add return types to routes functions
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- libraries/routes.php
+++ libraries/routes.php
@@ -97,15 +97,15 @@ if (! defined('PHPMYADMIN')) {
     exit;
 }
 
-return static function (RouteCollector $routes) {
-    $routes->addGroup('', static function (RouteCollector $routes) {
+return static function (RouteCollector $routes): void {
+    $routes->addGroup('', static function (RouteCollector $routes): void {
         $routes->addRoute(['GET', 'POST'], '[/]', [HomeController::class, 'index']);
         $routes->post('/set-theme', [HomeController::class, 'setTheme']);
         $routes->post('/collation-connection', [HomeController::class, 'setCollationConnection']);
         $routes->addRoute(['GET', 'POST'], '/recent-table', [HomeController::class, 'reloadRecentTablesList']);
         $routes->addRoute(['GET', 'POST'], '/git-revision', [HomeController::class, 'gitRevision']);
     });
-    $routes->addGroup('/ajax', static function (RouteCollector $routes) {
+    $routes->addGroup('/ajax', static function (RouteCollector $routes): void {
         $routes->post('/list-databases', [AjaxController::class, 'databases']);
         $routes->post('/list-tables', [AjaxController::class, 'tables']);
         $routes->post('/list-columns', [AjaxController::class, 'columns']);
@@ -115,14 +115,14 @@ return static function (RouteCollector $routes) {
     $routes->addRoute(['GET', 'POST'], '/browse-foreigners', [BrowseForeignersController::class, 'index']);
     $routes->get('/changelog', [ChangeLogController::class, 'index']);
     $routes->addRoute(['GET', 'POST'], '/check-relations', [CheckRelationsController::class, 'index']);
-    $routes->addGroup('/database', static function (RouteCollector $routes) {
+    $routes->addGroup('/database', static function (RouteCollector $routes): void {
         $routes->addRoute(['GET', 'POST'], '/central-columns', [CentralColumnsController::class, 'index']);
         $routes->get('/data-dictionary', [DataDictionaryController::class, 'index']);
         $routes->addRoute(['GET', 'POST'], '/designer', [DesignerController::class, 'index']);
         $routes->addRoute(['GET', 'POST'], '/events', [EventsController::class, 'index']);
         $routes->addRoute(['GET', 'POST'], '/export', [DatabaseExportController::class, 'index']);
         $routes->addRoute(['GET', 'POST'], '/import', [DatabaseImportController::class, 'index']);
-        $routes->addGroup('/multi-table-query', static function (RouteCollector $routes) {
+        $routes->addGroup('/multi-table-query', static function (RouteCollector $routes): void {
             $routes->get('', [MultiTableQueryController::class, 'index']);
             $routes->get('/tables', [MultiTableQueryController::class, 'table']);
             $routes->post('/query', [MultiTableQueryController::class, 'displayResults']);
@@ -131,12 +131,12 @@ return static function (RouteCollector $routes) {
         $routes->addRoute(['GET', 'POST'], '/qbe', [QueryByExampleController::class, 'index']);
         $routes->addRoute(['GET', 'POST'], '/routines', [RoutinesController::class, 'index']);
         $routes->addRoute(['GET', 'POST'], '/search', [SearchController::class, 'index']);
-        $routes->addGroup('/sql', static function (RouteCollector $routes) {
+        $routes->addGroup('/sql', static function (RouteCollector $routes): void {
             $routes->addRoute(['GET', 'POST'], '', [DatabaseSqlController::class, 'index']);
             $routes->post('/autocomplete', [SqlAutoCompleteController::class, 'index']);
             $routes->post('/format', [SqlFormatController::class, 'index']);
         });
-        $routes->addGroup('/structure', static function (RouteCollector $routes) {
+        $routes->addGroup('/structure', static function (RouteCollector $routes): void {
             $routes->addRoute(['GET', 'POST'], '', [StructureController::class, 'index']);
             $routes->post('/add-prefix', [StructureController::class, 'addPrefix']);
             $routes->post('/add-prefix-table', [StructureController::class, 'addPrefixTable']);
@@ -185,7 +185,7 @@ return static function (RouteCollector $routes) {
     $routes->addRoute(['GET', 'POST'], '/navigation', [NavigationController::class, 'index']);
     $routes->addRoute(['GET', 'POST'], '/normalization', [NormalizationController::class, 'index']);
     $routes->get('/phpinfo', [PhpInfoController::class, 'index']);
-    $routes->addGroup('/preferences', static function (RouteCollector $routes) {
+    $routes->addGroup('/preferences', static function (RouteCollector $routes): void {
         $routes->addRoute(['GET', 'POST'], '/export', [PreferencesExportController::class, 'index']);
         $routes->addRoute(['GET', 'POST'], '/features', [FeaturesController::class, 'index']);
         $routes->addRoute(['GET', 'POST'], '/import', [PreferencesImportController::class, 'index']);
@@ -196,15 +196,15 @@ return static function (RouteCollector $routes) {
         $routes->addRoute(['GET', 'POST'], '/two-factor', [TwoFactorController::class, 'index']);
     });
     $routes->addRoute(['GET', 'POST'], '/schema-export', [SchemaExportController::class, 'index']);
-    $routes->addGroup('/server', static function (RouteCollector $routes) {
+    $routes->addGroup('/server', static function (RouteCollector $routes): void {
         $routes->addRoute(['GET', 'POST'], '/binlog', [BinlogController::class, 'index']);
         $routes->get('/collations', [CollationsController::class, 'index']);
-        $routes->addGroup('/databases', static function (RouteCollector $routes) {
+        $routes->addGroup('/databases', static function (RouteCollector $routes): void {
             $routes->addRoute(['GET', 'POST'], '', [DatabasesController::class, 'index']);
             $routes->post('/create', [DatabasesController::class, 'create']);
             $routes->post('/destroy', [DatabasesController::class, 'destroy']);
         });
-        $routes->addGroup('/engines', static function (RouteCollector $routes) {
+        $routes->addGroup('/engines', static function (RouteCollector $routes): void {
             $routes->get('', [EnginesController::class, 'index']);
             $routes->get('/{engine}[/{page}]', [EnginesController::class, 'show']);
         });
@@ -214,10 +214,10 @@ return static function (RouteCollector $routes) {
         $routes->addRoute(['GET', 'POST'], '/privileges', [PrivilegesController::class, 'index']);
         $routes->addRoute(['GET', 'POST'], '/replication', [ReplicationController::class, 'index']);
         $routes->addRoute(['GET', 'POST'], '/sql', [ServerSqlController::class, 'index']);
-        $routes->addGroup('/status', static function (RouteCollector $routes) {
+        $routes->addGroup('/status', static function (RouteCollector $routes): void {
             $routes->get('', [StatusController::class, 'index']);
             $routes->get('/advisor', [AdvisorController::class, 'index']);
-            $routes->addGroup('/monitor', static function (RouteCollector $routes) {
+            $routes->addGroup('/monitor', static function (RouteCollector $routes): void {
                 $routes->get('', [MonitorController::class, 'index']);
                 $routes->post('/chart', [MonitorController::class, 'chartingData']);
                 $routes->post('/slow-log', [MonitorController::class, 'logDataTypeSlow']);
@@ -225,7 +225,7 @@ return static function (RouteCollector $routes) {
                 $routes->post('/log-vars', [MonitorController::class, 'loggingVars']);
                 $routes->post('/query', [MonitorController::class, 'queryAnalyzer']);
             });
-            $routes->addGroup('/processes', static function (RouteCollector $routes) {
+            $routes->addGroup('/processes', static function (RouteCollector $routes): void {
                 $routes->addRoute(['GET', 'POST'], '', [ProcessesController::class, 'index']);
                 $routes->post('/refresh', [ProcessesController::class, 'refresh']);
                 $routes->post('/kill/{id:\d+}', [ProcessesController::class, 'kill']);
@@ -234,13 +234,13 @@ return static function (RouteCollector $routes) {
             $routes->addRoute(['GET', 'POST'], '/variables', [StatusVariables::class, 'index']);
         });
         $routes->addRoute(['GET', 'POST'], '/user-groups', [UserGroupsController::class, 'index']);
-        $routes->addGroup('/variables', static function (RouteCollector $routes) {
+        $routes->addGroup('/variables', static function (RouteCollector $routes): void {
             $routes->get('', [VariablesController::class, 'index']);
             $routes->get('/get/{name}', [VariablesController::class, 'getValue']);
             $routes->post('/set/{name}', [VariablesController::class, 'setValue']);
         });
     });
-    $routes->addGroup('/sql', static function (RouteCollector $routes) {
+    $routes->addGroup('/sql', static function (RouteCollector $routes): void {
         $routes->addRoute(['GET', 'POST'], '', [SqlController::class, 'index']);
         $routes->post('/get-relational-values', [SqlController::class, 'getRelationalValues']);
         $routes->post('/get-enum-values', [SqlController::class, 'getEnumValues']);
@@ -248,19 +248,19 @@ return static function (RouteCollector $routes) {
         $routes->get('/get-default-fk-check-value', [SqlController::class, 'getDefaultForeignKeyCheckValue']);
         $routes->post('/set-column-preferences', [SqlController::class, 'setColumnOrderOrVisibility']);
     });
-    $routes->addGroup('/table', static function (RouteCollector $routes) {
+    $routes->addGroup('/table', static function (RouteCollector $routes): void {
         $routes->addRoute(['GET', 'POST'], '/add-field', [AddFieldController::class, 'index']);
-        $routes->addGroup('/change', static function (RouteCollector $routes) {
+        $routes->addGroup('/change', static function (RouteCollector $routes): void {
             $routes->addRoute(['GET', 'POST'], '', [ChangeController::class, 'index']);
             $routes->post('/rows', [ChangeController::class, 'rows']);
         });
         $routes->addRoute(['GET', 'POST'], '/chart', [ChartController::class, 'index']);
         $routes->addRoute(['GET', 'POST'], '/create', [CreateController::class, 'index']);
-        $routes->addGroup('/delete', static function (RouteCollector $routes) {
+        $routes->addGroup('/delete', static function (RouteCollector $routes): void {
             $routes->post('/confirm', [DeleteController::class, 'confirm']);
             $routes->post('/rows', [DeleteController::class, 'rows']);
         });
-        $routes->addGroup('/export', static function (RouteCollector $routes) {
+        $routes->addGroup('/export', static function (RouteCollector $routes): void {
             $routes->addRoute(['GET', 'POST'], '', [TableExportController::class, 'index']);
             $routes->post('/rows', [TableExportController::class, 'rows']);
         });
@@ -275,7 +275,7 @@ return static function (RouteCollector $routes) {
         $routes->addRoute(['GET', 'POST'], '/replace', [ReplaceController::class, 'index']);
         $routes->addRoute(['GET', 'POST'], '/search', [TableSearchController::class, 'index']);
         $routes->addRoute(['GET', 'POST'], '/sql', [TableSqlController::class, 'index']);
-        $routes->addGroup('/structure', static function (RouteCollector $routes) {
+        $routes->addGroup('/structure', static function (RouteCollector $routes): void {
             $routes->addRoute(['GET', 'POST'], '', [TableStructureController::class, 'index']);
             $routes->post('/add-key', [TableStructureController::class, 'addKey']);
             $routes->post('/browse', [TableStructureController::class, 'browse']);
@@ -299,13 +299,13 @@ return static function (RouteCollector $routes) {
         $routes->addRoute(['GET', 'POST'], '/zoom-search', [ZoomSearchController::class, 'index']);
     });
     $routes->get('/themes', [ThemesController::class, 'index']);
-    $routes->addGroup('/transformation', static function (RouteCollector $routes) {
+    $routes->addGroup('/transformation', static function (RouteCollector $routes): void {
         $routes->addRoute(['GET', 'POST'], '/overview', [TransformationOverviewController::class, 'index']);
         $routes->addRoute(['GET', 'POST'], '/wrapper', [TransformationWrapperController::class, 'index']);
     });
     $routes->addRoute(['GET', 'POST'], '/user-password', [UserPasswordController::class, 'index']);
     $routes->addRoute(['GET', 'POST'], '/version-check', [VersionCheckController::class, 'index']);
-    $routes->addGroup('/view', static function (RouteCollector $routes) {
+    $routes->addGroup('/view', static function (RouteCollector $routes): void {
         $routes->addRoute(['GET', 'POST'], '/create', [ViewCreateController::class, 'index']);
         $routes->addRoute(['GET', 'POST'], '/operations', [ViewOperationsController::class, 'index']);
     });
