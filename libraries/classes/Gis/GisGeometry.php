61d59bc96e84f26f2983eaade1c2915c0150a47d
William Desportes <williamdes@wdes.fr>
2020-12-10 12:03:16 +0100

    Fix a typo and add a return typehint
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- libraries/classes/Gis/GisGeometry.php
+++ libraries/classes/Gis/GisGeometry.php
@@ -237,7 +237,7 @@ abstract class GisGeometry
      *
      * @access protected
      */
-    protected function extractPoints($point_set, $scale_data, $linear = false)
+    protected function extractPoints($point_set, $scale_data, $linear = false): array
     {
         $points_arr = [];
 
@@ -247,16 +247,16 @@ abstract class GisGeometry
         foreach ($points as $point) {
             $point = str_replace(['(', ')'], '', $point);
             // Extract coordinates of the point
-            $cordinates = explode(' ', $point);
+            $coordinates = explode(' ', $point);
 
-            if (isset($cordinates[0], $cordinates[1]) && trim($cordinates[0]) != '' && trim($cordinates[1]) != '') {
+            if (isset($coordinates[0], $coordinates[1]) && trim($coordinates[0]) != '' && trim($coordinates[1]) != '') {
                 if ($scale_data != null) {
-                    $x = ($cordinates[0] - $scale_data['x']) * $scale_data['scale'];
+                    $x = ($coordinates[0] - $scale_data['x']) * $scale_data['scale'];
                     $y = $scale_data['height']
-                        - ($cordinates[1] - $scale_data['y']) * $scale_data['scale'];
+                        - ($coordinates[1] - $scale_data['y']) * $scale_data['scale'];
                 } else {
-                    $x = floatval(trim($cordinates[0]));
-                    $y = floatval(trim($cordinates[1]));
+                    $x = floatval(trim($coordinates[0]));
+                    $y = floatval(trim($coordinates[1]));
                 }
             } else {
                 $x = 0;
