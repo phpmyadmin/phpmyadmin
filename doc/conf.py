546ea643521c176250e5e2462e4bd4f0e92ec606
William Desportes <williamdes@wdes.fr>
2020-12-31 23:07:11 +0100

    Happy new year !
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- doc/conf.py
+++ doc/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'phpMyAdmin'
-copyright = u'2012 - 2020, The phpMyAdmin devel team'
+copyright = u'2012 - 2021, The phpMyAdmin devel team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
