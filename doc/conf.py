0a9dc7eab06f621d58e35e0ccf317ecb4dd062fe
SavvyOsive <savvy.osive@gmail.com>
2021-01-21 20:00:25 +0530

    Added pydata-sphinx-theme in conf.py
    Signed-off-by: SavvyOsive <savvy.osive@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -98,7 +98,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'pydata_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
