8d0431d1eafbd236ad0defec95530181044170eb
William Desportes <williamdes@wdes.fr>
2021-02-17 20:20:57 +0100

    Ref #16654 - Fix bulk delete button on Selenium tests
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- test/selenium/CreateDropDatabaseTest.php
+++ test/selenium/CreateDropDatabaseTest.php
@@ -76,7 +76,7 @@ class CreateDropDatabaseTest extends TestBase
         $this->scrollToElement($dbElement, 0, 20);
         $dbElement->click();
 
-        $multSubmit = $this->byCssSelector('button.mult_submit');
+        $multSubmit = $this->byCssSelector('button#bulkActionDropButton');
         $this->scrollToElement($multSubmit);
         $multSubmit->click();
         $this->byCssSelector('button.submitOK')->click();
