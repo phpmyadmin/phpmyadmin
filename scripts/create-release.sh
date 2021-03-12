d5b8473e8f634957c739f7fe254bfa42fc691d71
William Desportes <williamdes@wdes.fr>
2021-03-12 14:29:48 +0100

    Remove more tcpdf useless files
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -147,11 +147,16 @@ cleanup_composer_vendors() {
         vendor/tecnickcom/tcpdf/examples/ \
         vendor/tecnickcom/tcpdf/tools/ \
         vendor/tecnickcom/tcpdf/fonts/ae_fonts_*/ \
-        vendor/tecnickcom/tcpdf/fonts/dejavu-fonts-ttf-2.33/ \
+        vendor/tecnickcom/tcpdf/fonts/dejavu-fonts-ttf-2.34/ \
         vendor/tecnickcom/tcpdf/fonts/freefont-*/ \
         vendor/tecnickcom/tcpdf/include/sRGB.icc \
         vendor/tecnickcom/tcpdf/.git \
         vendor/tecnickcom/tcpdf/.github/ \
+        vendor/tecnickcom/tcpdf/tcpdf_barcodes_1d.php \
+        vendor/tecnickcom/tcpdf/tcpdf_barcodes_2d.php \
+        vendor/tecnickcom/tcpdf/tcpdf_import.php \
+        vendor/tecnickcom/tcpdf/tcpdf_parser.php \
+        vendor/tecnickcom/tcpdf/include/barcodes/ \
         vendor/bacon/bacon-qr-code/phpunit.xml.dist \
         vendor/bacon/bacon-qr-code/test/ \
         vendor/dasprid/enum/phpunit.xml.dist \
