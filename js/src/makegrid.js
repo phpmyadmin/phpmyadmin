e462bd8ffc59d436a694e4c89a17dceb32313f87
William Desportes <williamdes@wdes.fr>
2021-03-31 13:47:48 +0200

    Fix #16765 - Revert "55880451eb193f7e1bad90df365b67d54eded5a6"
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- js/src/makegrid.js
+++ js/src/makegrid.js
@@ -1240,11 +1240,7 @@ var makeGrid = function (t, enableResize, enableReorder, enableVisib, enableGrid
                             fieldsType.push('hex');
                         }
                         fieldsNull.push('');
-                        // Convert \n to \r\n to be consistent with form submitted value.
-                        // The internal browser representation has to be just \n
-                        // while form submitted value \r\n, see specification:
-                        // https://www.w3.org/TR/html5/forms.html#the-textarea-element
-                        fields.push($thisField.data('value').replace(/\n/g, '\r\n'));
+                        fields.push($thisField.data('value'));
 
                         var cellIndex = $thisField.index('.to_be_saved');
                         if ($thisField.is(':not(.relation, .enum, .set, .bit)')) {
