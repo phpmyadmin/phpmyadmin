1c8f9b4d23d405093d6fb5cd0f31f12be6d6da08
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2021-04-26 14:32:03 +0200

    Replace secure.php.net with www.php.net and remove Polish and Persian from PHP documentation
    Signed-off-by: Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>

--- libraries/classes/Controllers/HomeController.php
+++ libraries/classes/Controllers/HomeController.php
@@ -350,7 +350,7 @@ class HomeController extends AbstractController
             if ($gc_time < $cfg['LoginCookieValidity']) {
                 trigger_error(
                     __(
-                        'Your PHP parameter [a@https://secure.php.net/manual/en/session.' .
+                        'Your PHP parameter [a@https://www.php.net/manual/en/session.' .
                         'configuration.php#ini.session.gc-maxlifetime@_blank]session.' .
                         'gc_maxlifetime[/a] is lower than cookie validity configured ' .
                         'in phpMyAdmin, because of this, your login might expire sooner ' .
