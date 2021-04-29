1bb715dc646fceb34a98ddf621a643ec5b75af58
William Desportes <williamdes@wdes.fr>
2021-04-29 20:33:40 +0200

    Fix #16871 - PHP error on normalisation without a primary key index
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- libraries/classes/Normalization.php
+++ libraries/classes/Normalization.php
@@ -283,7 +283,7 @@ class Normalization
         $hasPrimaryKey = '0';
         $legendText = __('Step 1.') . $step . ' ' . $stepTxt;
         $extra = '';
-        if ($primary) {
+        if ($primary !== false) {
             $headText = __('Primary key already exists.');
             $subText = __('Taking you to next step…');
             $hasPrimaryKey = '1';
@@ -386,7 +386,7 @@ class Normalization
             . '<input class="btn btn-secondary" type="submit" value="' . __('No repeating group')
             . '" onclick="goToStep4();">';
         $primary = Index::getPrimary($table, $db);
-        $primarycols = $primary->getColumns();
+        $primarycols = $primary === false ? [] : $primary->getColumns();
         $pk = [];
         foreach ($primarycols as $col) {
             $pk[] = $col->getName();
@@ -413,7 +413,7 @@ class Normalization
     {
         $legendText = __('Step 2.') . '1 ' . __('Find partial dependencies');
         $primary = Index::getPrimary($table, $db);
-        $primarycols = $primary->getColumns();
+        $primarycols = $primary === false ? [] : $primary->getColumns();
         $pk = [];
         $subText = '';
         $selectPkForm = '';
@@ -630,7 +630,7 @@ class Normalization
                 continue;
             }
             $primary = Index::getPrimary($table, $db);
-            $primarycols = $primary->getColumns();
+            $primarycols = $primary === false ? [] : $primary->getColumns();
             $pk = [];
             foreach ($primarycols as $col) {
                 $pk[] = $col->getName();
@@ -873,7 +873,7 @@ class Normalization
         $cnt = 0;
         foreach ($tables as $table) {
             $primary = Index::getPrimary($table, $db);
-            $primarycols = $primary->getColumns();
+            $primarycols = $primary === false ? [] : $primary->getColumns();
             $selectTdForm = '';
             $pk = [];
             foreach ($primarycols as $col) {
@@ -1001,7 +1001,7 @@ class Normalization
         );
         $totalRows = $totalRowsRes[0];
         $primary = Index::getPrimary($table, $db);
-        $primarycols = $primary->getColumns();
+        $primarycols = $primary === false ? [] : $primary->getColumns();
         $pk = [];
         foreach ($primarycols as $col) {
             $pk[] = Util::backquote($col->getName());
