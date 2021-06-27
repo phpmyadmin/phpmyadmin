5c19230a6653bb67022f92183b2f6bde20df3a1d
William Desportes <williamdes@wdes.fr>
2021-06-27 12:50:04 +0200

    Fix warning "Undefined index: ods_recognize_percentages" on ImportOds
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- libraries/classes/Plugins/Import/ImportOds.php
+++ libraries/classes/Plugins/Import/ImportOds.php
@@ -246,7 +246,8 @@ class ImportOds extends ImportPlugin
      */
     protected function getValue($cell_attrs, $text)
     {
-        if ($_REQUEST['ods_recognize_percentages']
+        if (isset($_REQUEST['ods_recognize_percentages'])
+            && $_REQUEST['ods_recognize_percentages']
             && ! strcmp(
                 'percentage',
                 (string) $cell_attrs['value-type']
