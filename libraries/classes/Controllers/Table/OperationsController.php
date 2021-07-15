f724f648e1019976cbabbbff21d3a735ce58eba4
William Desportes <williamdes@wdes.fr>
2021-07-15 02:13:03 +0200

    Fix #16976 - Wrong link when a table is moved from a database to another
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- libraries/classes/Controllers/Table/OperationsController.php
+++ libraries/classes/Controllers/Table/OperationsController.php
@@ -156,6 +156,9 @@ class OperationsController extends AbstractController
             $this->response->addJSON('message', $message);
 
             if ($message->isSuccess()) {
+                if (isset($_POST['submit_move'], $_POST['target_db'])) {
+                    $db = $_POST['target_db'];// Used in Header::getJsParams()
+                }
                 $this->response->addJSON('db', $db);
 
                 return;
