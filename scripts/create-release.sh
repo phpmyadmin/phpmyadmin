adc30f62760f8fda7732cfd482c79f2bd2964396
Maurício Meneghini Fauth <mauricio@fauth.dev>
2022-01-26 15:55:56 -0300

    Bump VERSION_SERIES to 5.3 in scripts/create-release.sh
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -20,7 +20,7 @@ set -e
 KITS="all-languages english source"
 COMPRESSIONS="zip-7z txz tgz"
 # The version series this script is allowed to handle
-VERSION_SERIES="5.2"
+VERSION_SERIES="5.3"
 
 # Process parameters
 
