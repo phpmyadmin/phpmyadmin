a8c0d62285db469f03dc5bfc1ff41fda37fd07d1
Isaac Bennetch <bennetch@gmail.com>
2022-02-10 22:01:50 -0500

    Prepare for 4.9.10
    Signed-off-by: Isaac Bennetch <bennetch@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -51,7 +51,7 @@ copyright = u'2012 - 2018, The phpMyAdmin devel team'
 # built documents.
 #
 # The short X.Y version.
-version = '4.9.9'
+version = '4.9.10'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- libraries/classes/Config.php
+++ libraries/classes/Config.php
@@ -116,7 +116,7 @@ class Config
      */
     public function checkSystem()
     {
-        $this->set('PMA_VERSION', '4.9.9');
+        $this->set('PMA_VERSION', '4.9.10');
         /* Major version */
         $this->set(
             'PMA_MAJOR_VERSION',
