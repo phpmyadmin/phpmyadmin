6f6913855f11f8fc3f5f03a31dc739b6d35d0eda
William Desportes <williamdes@wdes.fr>
2022-03-06 10:32:09 +0100

    Add one more file to the create release exclude list
    Ref: https://github.com/twigphp/Twig/issues/3658
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -185,6 +185,7 @@ cleanup_composer_vendors() {
         vendor/twig/twig/.editorconfig \
         vendor/twig/twig/.php_cs.dist \
         vendor/twig/twig/drupal_test.sh \
+        vendor/twig/twig/.php-cs-fixer.dist.php \
         vendor/twig/twig/src/Test/ \
         vendor/psr/log/Psr/Log/Test/ \
         vendor/paragonie/constant_time_encoding/tests/ \
