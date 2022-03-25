cbd23e5c0f1e732320028724cc2341a948786f3d
Maurício Meneghini Fauth <mauricio@fauth.dev>
2022-03-25 16:27:40 -0300

    Skip GitTest when remote commit info is unavailable
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- test/classes/GitTest.php
+++ test/classes/GitTest.php
@@ -233,6 +233,13 @@ class GitTest extends AbstractTestCase
 
         $commit = $this->object->checkGitRevision();
 
+        if (
+            $commit === null
+            && ! isset($_SESSION['PMA_VERSION_REMOTECOMMIT_17bf8b7309919f8ac593d7c563b31472780ee83b'])
+        ) {
+            $this->markTestSkipped('Unable to get remote commit information.');
+        }
+
         $this->assertIsArray($commit);
         $this->assertArrayHasKey('hash', $commit);
         $this->assertEquals('17bf8b7309919f8ac593d7c563b31472780ee83b', $commit['hash']);
@@ -358,6 +365,13 @@ class GitTest extends AbstractTestCase
         );
 
         $commit = $this->object->checkGitRevision();
+
+        if (
+            $commit === null
+            && ! isset($_SESSION['PMA_VERSION_REMOTECOMMIT_17bf8b7309919f8ac593d7c563b31472780ee83b'])
+        ) {
+            $this->markTestSkipped('Unable to get remote commit information.');
+        }
 
         $this->assertIsArray($commit);
         $this->assertArrayHasKey('hash', $commit);
