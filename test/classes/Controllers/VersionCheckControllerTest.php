2933498bc06c31b130eeb006f5bbe47d9a31b4be
Maurício Meneghini Fauth <mauricio@fauth.dev>
2022-03-31 18:35:55 -0300

    Improve VersionCheckControllerTest tests
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- test/classes/Controllers/VersionCheckControllerTest.php
+++ test/classes/Controllers/VersionCheckControllerTest.php
@@ -10,31 +10,25 @@ use PhpMyAdmin\Tests\AbstractTestCase;
 use PhpMyAdmin\Tests\Stubs\ResponseRenderer;
 use PhpMyAdmin\VersionInformation;
 
-use function json_encode;
-use function time;
-
-use const PHP_VERSION_ID;
-
 /**
  * @covers \PhpMyAdmin\Controllers\VersionCheckController
  */
 class VersionCheckControllerTest extends AbstractTestCase
 {
-    public function testInvoke(): void
+    public function testWithLatestCompatibleVersion(): void
     {
         $_GET = [];
-        $GLOBALS['cfg']['VersionCheck'] = true;
-        $versionInfo = [
+        $versionInfo = (object) [
             'date' => '2022-02-11',
             'version' => '5.1.3',
             'releases' => [
-                [
+                (object) [
                     'date' => '2022-02-11',
                     'php_versions' => '>=7.1,<8.1',
                     'version' => '5.1.3',
                     'mysql_versions' => '>=5.5',
                 ],
-                [
+                (object) [
                     'date' => '2022-02-11',
                     'php_versions' => '>=5.5,<8.0',
                     'version' => '4.9.10',
@@ -42,20 +36,67 @@ class VersionCheckControllerTest extends AbstractTestCase
                 ],
             ],
         ];
-        $_SESSION['cache'] = [];
-        $_SESSION['cache']['version_check'] = [
-            'response' => json_encode($versionInfo),
-            'timestamp' => time(),
-        ];
 
-        (new VersionCheckController(new ResponseRenderer(), new Template(), new VersionInformation()))();
+        $versionInformation = $this->createMock(VersionInformation::class);
+        $versionInformation->expects($this->once())->method('getLatestVersion')->willReturn($versionInfo);
+        $versionInformation->expects($this->once())->method('getLatestCompatibleVersion')
+            ->with($this->equalTo($versionInfo->releases))
+            ->willReturn(['version' => '5.1.3', 'date' => '2022-02-11']);
+
+        (new VersionCheckController(new ResponseRenderer(), new Template(), $versionInformation))();
 
         $output = $this->getActualOutputForAssertion();
         $this->assertTrue(isset($_GET['ajax_request']));
-        if (PHP_VERSION_ID < 80100) {
-            $this->assertSame('{"version":"5.1.3","date":"2022-02-11"}', $output);
-        } else {
-            $this->assertSame('{"version":"","date":""}', $output);
-        }
+        $this->assertSame('{"version":"5.1.3","date":"2022-02-11"}', $output);
+    }
+
+    public function testWithoutLatestCompatibleVersion(): void
+    {
+        $_GET = [];
+        $versionInfo = (object) [
+            'date' => '2022-02-11',
+            'version' => '5.1.3',
+            'releases' => [
+                (object) [
+                    'date' => '2022-02-11',
+                    'php_versions' => '>=7.1,<8.1',
+                    'version' => '5.1.3',
+                    'mysql_versions' => '>=5.5',
+                ],
+                (object) [
+                    'date' => '2022-02-11',
+                    'php_versions' => '>=5.5,<8.0',
+                    'version' => '4.9.10',
+                    'mysql_versions' => '>=5.5',
+                ],
+            ],
+        ];
+
+        $versionInformation = $this->createMock(VersionInformation::class);
+        $versionInformation->expects($this->once())->method('getLatestVersion')->willReturn($versionInfo);
+        $versionInformation->expects($this->once())->method('getLatestCompatibleVersion')
+            ->with($this->equalTo($versionInfo->releases))
+            ->willReturn(null);
+
+        (new VersionCheckController(new ResponseRenderer(), new Template(), $versionInformation))();
+
+        $output = $this->getActualOutputForAssertion();
+        $this->assertTrue(isset($_GET['ajax_request']));
+        $this->assertSame('{"version":"","date":""}', $output);
+    }
+
+    public function testWithoutLatestVersion(): void
+    {
+        $_GET = [];
+
+        $versionInformation = $this->createMock(VersionInformation::class);
+        $versionInformation->expects($this->once())->method('getLatestVersion')->willReturn(null);
+        $versionInformation->expects($this->never())->method('getLatestCompatibleVersion');
+
+        (new VersionCheckController(new ResponseRenderer(), new Template(), $versionInformation))();
+
+        $output = $this->getActualOutputForAssertion();
+        $this->assertTrue(isset($_GET['ajax_request']));
+        $this->assertSame('[]', $output);
     }
 }
