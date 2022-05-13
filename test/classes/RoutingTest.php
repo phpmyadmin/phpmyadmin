680e59eb5f390279d09cc071a1b951942be5d593
Maurício Meneghini Fauth <mauricio@fauth.dev>
2022-05-13 16:45:21 -0300

    Ignore invalid routes cache file
    If the `libraries/cache/routes.cache.php` file is invalid (usually from
    5.1), ignores it and try to write a new one instead of failing.
    
    Fixes https://github.com/phpmyadmin/phpmyadmin/issues/17522.
    
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- test/classes/RoutingTest.php
+++ test/classes/RoutingTest.php
@@ -8,6 +8,11 @@ use FastRoute\Dispatcher;
 use PhpMyAdmin\Controllers\HomeController;
 use PhpMyAdmin\Routing;
 
+use function copy;
+
+use const CACHE_DIR;
+use const ROOT_PATH;
+
 /**
  * @covers \PhpMyAdmin\Routing
  */
@@ -23,6 +28,52 @@ class RoutingTest extends AbstractTestCase
         $this->assertSame(
             [Dispatcher::FOUND, HomeController::class, []],
             $dispatcher->dispatch('GET', '/')
+        );
+    }
+
+    public function testGetDispatcherWithValidCacheFile(): void
+    {
+        $GLOBALS['cfg']['environment'] = null;
+
+        $this->assertDirectoryIsWritable(CACHE_DIR);
+        $this->assertTrue(copy(
+            ROOT_PATH . 'test/test_data/routes/routes-valid.cache.txt',
+            CACHE_DIR . 'routes.cache.php'
+        ));
+
+        $dispatcher = Routing::getDispatcher();
+        $this->assertInstanceOf(Dispatcher::class, $dispatcher);
+        $this->assertSame(
+            [Dispatcher::FOUND, HomeController::class, []],
+            $dispatcher->dispatch('GET', '/')
+        );
+
+        $this->assertFileEquals(
+            CACHE_DIR . 'routes.cache.php',
+            ROOT_PATH . 'test/test_data/routes/routes-valid.cache.txt'
+        );
+    }
+
+    public function testGetDispatcherWithInvalidCacheFile(): void
+    {
+        $GLOBALS['cfg']['environment'] = null;
+
+        $this->assertDirectoryIsWritable(CACHE_DIR);
+        $this->assertTrue(copy(
+            ROOT_PATH . 'test/test_data/routes/routes-invalid.cache.txt',
+            CACHE_DIR . 'routes.cache.php'
+        ));
+
+        $dispatcher = Routing::getDispatcher();
+        $this->assertInstanceOf(Dispatcher::class, $dispatcher);
+        $this->assertSame(
+            [Dispatcher::FOUND, HomeController::class, []],
+            $dispatcher->dispatch('GET', '/')
+        );
+
+        $this->assertFileNotEquals(
+            CACHE_DIR . 'routes.cache.php',
+            ROOT_PATH . 'test/test_data/routes/routes-invalid.cache.txt'
         );
     }
 
