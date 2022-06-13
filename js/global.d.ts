b3001e4d6452d5f3297d3939d8be8f273d808f60
Maurício Meneghini Fauth <mauricio@fauth.dev>
2022-06-13 16:45:26 -0300

    Add more type definitions to JavaScript
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

---
+++ js/global.d.ts
@@ -0,0 +1,8 @@
+// js/messages.php
+declare var Messages: { [p: string]: string };
+
+// templates/javascript/variables.twig
+declare var firstDayOfCalendar: string;
+declare var themeImagePath: string;
+declare var mysqlDocTemplate: string;
+declare var maxInputVars: number;
