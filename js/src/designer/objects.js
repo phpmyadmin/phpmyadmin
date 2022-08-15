abb871be8938f889e106b7c32c9878799606988b
William Desportes <williamdes@wdes.fr>
2022-08-15 17:20:32 +0200

    Add back some dev comments on Designer objects
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- js/src/designer/objects.js
+++ js/src/designer/objects.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 var DesignerObjects = {
     PdfPage: function (dbName, pageDescr, tblCords) {
+        // no dot set the auto increment before put() in the database
+        // issue #12900
         // eslint-disable-next-line no-unused-vars
         var pgNr;
         this.dbName = dbName;
@@ -8,6 +10,9 @@ var DesignerObjects = {
         this.tblCords = tblCords;
     },
     TableCoordinate: function (dbName, tableName, pdfPgNr, x, y) {
+        // no dot set the auto increment before put() in the database
+        // issue #12900
+        // var id;
         this.dbName = dbName;
         this.tableName = tableName;
         this.pdfPgNr = pdfPgNr;
