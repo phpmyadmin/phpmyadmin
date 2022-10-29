phpmyadmin/phpmyadmin

commit 8424e75907b2631de017cf012c724de7a68af7ab

Fix "Link not found!" on foreign columns
It also generated invalid queries with no column name to select.

Signed-off-by: William Desportes <williamdes@wdes.fr>

libraries/classes/Display/Results.php
@@ -4416,7 +4416,10 @@ class Results
             $relation = $map[$meta->name];
             // Field to display from the foreign table?
             $dispval = '';
-            if ($relation[2] !== '') {
+
+            // Check that we have a valid column name
+            // Relation::getDisplayField() returns false by default
+            if ($relation[2] !== '' && $relation[2] !== false) {
                 $dispval = $this->getFromForeign($relation, $whereComparison);
             }
 

commit e97af7568e47ce6c58decd63fd38c5a6595dbeb8

Fix psalm baseline issues introduced by 8424e75907
Ref: 8424e75907b2631de017cf012c724de7a68af7ab

Signed-off-by: William Desportes <williamdes@wdes.fr>

libraries/classes/Display/Results.php
@@ -4412,7 +4412,7 @@ class Results
         }
 
         if (isset($map[$meta->name])) {
-            /** @var array<int, string> $relation */
+            /** @var array{0: string, 1: string, 2: string|false, 3: string} $relation */
             $relation = $map[$meta->name];
             // Field to display from the foreign table?
             $dispval = '';