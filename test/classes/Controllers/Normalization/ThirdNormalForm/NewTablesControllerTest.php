aabdbe7357cb334fd4dead437cd7a5de61c94da8
Maurício Meneghini Fauth <mauricio@fauth.dev>
2022-11-23 17:28:31 -0300

    Update PHPStan and Psalm baselines
    - Fix coding standard error
    - Remove strict option from composer validate because the sql-parser is
      pointing to a commit-ref
    
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- test/classes/Controllers/Normalization/ThirdNormalForm/NewTablesControllerTest.php
+++ test/classes/Controllers/Normalization/ThirdNormalForm/NewTablesControllerTest.php
@@ -60,7 +60,6 @@ class NewTablesControllerTest extends AbstractTestCase
             ['pd', null, $pd],
         ]);
 
-
         $controller = new NewTablesController(
             $response,
             $template,
