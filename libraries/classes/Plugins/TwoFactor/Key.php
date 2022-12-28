f6a03d405d5c4d4ad6e356ed654917a79418207c
Maurício Meneghini Fauth <mauricio@fauth.dev>
2022-12-28 15:49:41 -0300

    Improve type inference of TwoFactor::$config
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- libraries/classes/Plugins/TwoFactor/Key.php
+++ libraries/classes/Plugins/TwoFactor/Key.php
@@ -19,6 +19,7 @@ use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
 
 use function __;
+use function is_array;
 use function is_object;
 use function json_decode;
 use function json_encode;
@@ -41,7 +42,10 @@ class Key extends TwoFactorPlugin
     public function __construct(TwoFactor $twofactor)
     {
         parent::__construct($twofactor);
-        if (isset($this->twofactor->config['settings']['registrations'])) {
+        if (
+            isset($this->twofactor->config['settings']['registrations'])
+            && is_array($this->twofactor->config['settings']['registrations'])
+        ) {
             return;
         }
 
