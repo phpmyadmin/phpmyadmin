89adbcfe9b30e53f3802522c72ce2cefba35bddb
William Desportes <williamdes@wdes.fr>
2023-01-20 13:12:22 +0400

    Fix PSR-7 implementation for GET and test implementation for POST
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- test/classes/Http/Factory/ServerRequestFactoryTest.php
+++ test/classes/Http/Factory/ServerRequestFactoryTest.php
@@ -8,6 +8,7 @@ use GuzzleHttp\Psr7\HttpFactory as GuzzleHttpFactory;
 use Laminas\Diactoros\ServerRequestFactory as LaminasServerRequestFactory;
 use Nyholm\Psr7\Factory\Psr17Factory as NyholmPsr17Factory;
 use PhpMyAdmin\Http\Factory\ServerRequestFactory;
+use PhpMyAdmin\Http\ServerRequest;
 use PhpMyAdmin\Tests\AbstractTestCase;
 use Psr\Http\Message\ServerRequestFactoryInterface;
 use Slim\Psr7\Factory\ServerRequestFactory as SlimServerRequestFactory;
@@ -72,19 +73,20 @@ class ServerRequestFactoryTest extends AbstractTestCase
      *
      * @dataProvider dataProviderPsr7Implementations
      */
-    public function testPsr7Implementation(string $className, string $humanName): void
+    public function testPsr7ImplementationGet(string $className, string $humanName): void
     {
         $this->testOrSkip($className, $humanName);
 
         $_GET['foo'] = 'bar';
+        $_GET['blob'] = 'baz';
         $_SERVER['QUERY_STRING'] = 'foo=bar&blob=baz';
         $_SERVER['REQUEST_URI'] = '/test-page.php';
-        $_SERVER['REQUEST_METHOD'] = 'PATCH';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
         $_SERVER['HTTP_HOST'] = 'phpmyadmin.local';
 
         $request = ServerRequestFactory::createFromGlobals();
         $this->assertSame(
-            'PATCH',
+            'GET',
             $request->getMethod()
         );
         $this->assertSame(
@@ -110,6 +112,79 @@ class ServerRequestFactoryTest extends AbstractTestCase
             'foo' => 'bar',
             'blob' => 'baz',
         ], $request->getQueryParams());
+    }
+
+    public function testCreateServerRequestFromGlobals(): void
+    {
+        $_GET['foo'] = 'bar';
+        $_GET['blob'] = 'baz';
+        $_POST['input1'] = 'value1';
+        $_POST['input2'] = 'value2';
+        $_POST['input3'] = '';
+        $_SERVER['QUERY_STRING'] = 'foo=bar&blob=baz';
+        $_SERVER['REQUEST_URI'] = '/test-page.php';
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $_SERVER['HTTP_HOST'] = 'phpmyadmin.local';
+
+        $creator = $this->getMockBuilder(ServerRequestFactory::class)
+            ->setMethods(['getallheaders'])
+            ->getMock();
+
+        $creator
+            ->method('getallheaders')
+            ->willReturn(['Content-Type' => 'application/x-www-form-urlencoded']);
+
+        $serverRequest = $this->callFunction(
+            $creator,
+            ServerRequestFactory::class,
+            'createServerRequestFromGlobals',
+            [$creator]
+        );
+
+        $request = new ServerRequest($serverRequest);
+
+        $this->assertSame(
+            ['application/x-www-form-urlencoded'],
+            $request->getHeader('Content-Type')
+        );
+        $this->assertSame(
+            'POST',
+            $request->getMethod()
+        );
+        $this->assertSame(
+            'http://phpmyadmin.local/test-page.php?foo=bar&blob=baz',
+            $request->getUri()->__toString()
+        );
+        $this->assertTrue(
+            $request->isPost()
+        );
+        $this->assertSame(
+            'default',
+            $request->getParam('not-exists', 'default')
+        );
+        $this->assertSame(
+            'bar',
+            $request->getParam('foo')
+        );
+        $this->assertSame(
+            'baz',
+            $request->getParam('blob')
+        );
+        $this->assertSame([
+            'foo' => 'bar',
+            'blob' => 'baz',
+        ], $request->getQueryParams());
+
+        $this->assertSame([
+            'input1' => 'value1',
+            'input2' => 'value2',
+            'input3' => '',
+        ], $request->getParsedBody());
+
+        $this->assertNull($request->getParsedBodyParam('foo'));
+        $this->assertSame('value1', $request->getParsedBodyParam('input1'));
+        $this->assertSame('value2', $request->getParsedBodyParam('input2'));
+        $this->assertSame('', $request->getParsedBodyParam('input3', 'default'));
     }
 
     /**
