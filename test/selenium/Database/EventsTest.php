5f54c3ec81be271c352ffdd39a2a94f095ac908a
Maurício Meneghini Fauth <mauricio@fauth.dev>
2023-01-24 18:01:00 -0300

    Fix failing Selenium tests
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- test/selenium/Database/EventsTest.php
+++ test/selenium/Database/EventsTest.php
@@ -111,7 +111,7 @@ class EventsTest extends TestBase
                 ->click()// Click to free the mouse
                 ->perform();
 
-        $this->byXPath("//button[contains(., 'Go')]")->click();
+        $this->byCssSelector('div.ui-dialog-buttonset button:nth-child(1)')->click();
 
         $this->waitForElement(
             'xpath',
@@ -174,7 +174,7 @@ class EventsTest extends TestBase
         $this->byName('item_interval_value')->clear();
         $this->byName('item_interval_value')->sendKeys('2');
 
-        $this->byXPath("//button[contains(., 'Go')]")->click();
+        $this->byCssSelector('div.ui-dialog-buttonset button:nth-child(1)')->click();
 
         $this->waitForElement(
             'xpath',
