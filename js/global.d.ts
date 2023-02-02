3bafef8f2b3c5461e572879bb4c77e4b98d11a05
Maurício Meneghini Fauth <mauricio@fauth.dev>
2023-02-02 01:06:20 -0300

    Add type info for some jQuery plugins
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- js/global.d.ts
+++ js/global.d.ts
@@ -8,3 +8,23 @@ declare var mysqlDocTemplate: string;
 declare var maxInputVars: number;
 
 declare function sprintf(format: string, ...values: (string|number)[]): string;
+
+interface JQuery {
+    getPostData: () => string;
+
+    sortableTable: (method: any) => any;
+
+    noSelect: (p: any) => any;
+
+    menuResizer: (method: string|Function) => any;
+
+    confirm: (question: string, url: string, callbackFn: Function, openCallback?: Function) => boolean;
+
+    sortTable: (textSelector: string) => JQuery<HTMLElement>;
+
+    filterByValue: (value: any) => any;
+}
+
+interface JQueryStatic {
+    timepicker: JQueryUI.Datepicker;
+}
