affd2b9d7f62a6438bda2c64a5c1200d93ebc94f
Kamil Tekiela <tekiela246@gmail.com>
2023-02-14 21:18:29 +0000

    Enable checkTooWideReturnTypesInProtectedAndPublicMethods
    Signed-off-by: Kamil Tekiela <tekiela246@gmail.com>

--- libraries/classes/Dbal/MysqliResult.php
+++ libraries/classes/Dbal/MysqliResult.php
@@ -13,6 +13,7 @@ use PhpMyAdmin\FieldMetadata;
 use Webmozart\Assert\Assert;
 
 use function array_column;
+use function array_key_exists;
 use function is_array;
 use function is_bool;
 use function is_string;
@@ -104,7 +105,11 @@ final class MysqliResult implements ResultInterface
             $row = $this->fetchRow();
         }
 
-        return $row[$field] ?? false;
+        if (! array_key_exists($field, $row)) {
+            return false;
+        }
+
+        return $row[$field];
     }
 
     /**
