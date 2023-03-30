8764673ccd03c493afd6d9e15273a1f92869332e
Maurício Meneghini Fauth <mauricio@fauth.dev>
2023-03-30 14:53:50 -0300

    Add ts-loader package
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -98,14 +98,17 @@ module.exports = [
         module: {
             rules: [
                 {
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: [
+                        { loader: 'babel-loader', options: { presets:  ['@babel/preset-env'] } },
+                        { loader: 'ts-loader' }
+                    ],
+                },
+                {
                     test: /\.js$/,
                     exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets:  ['@babel/preset-env'],
-                        },
-                    },
+                    use: { loader: 'babel-loader', options: { presets:  ['@babel/preset-env'] } },
                 },
             ],
         },
