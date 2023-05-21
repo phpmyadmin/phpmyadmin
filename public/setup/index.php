bbbc0f86d433d929280c164f26aa646662155c1b
rohit-sharma-1802 <59288117+rohit-sharma-1802@users.noreply.github.com>
2023-05-21 23:42:11 +0530

    Update index.php
    Remove the line declare(strict_types=1); as it is not necessary in this context.
    Remove the comment // phpcs:enable as it is unnecessary in this code.
    
    Modify the error message when the PHP version is not compatible. Instead of using the HTML <p> tags, it's better to use plain text and concatenate the PHP version directly into the string. Also, consider providing instructions on how to upgrade the PHP version. 
    Replace the @is_readable() function usage with is_readable() to handle any potential errors properly. Then, update the error message when the AUTOLOAD_FILE is missing or not readable. Instead of using HTML tags, use plain text for consistency. Also, consider providing a direct link to the relevant documentation.

--- public/setup/index.php
+++ public/setup/index.php
@@ -1,31 +1,28 @@
 <?php
 
-declare(strict_types=1);
-
 use PhpMyAdmin\Common;
 
-// phpcs:disable PSR1.Files.SideEffects
 if (! defined('ROOT_PATH')) {
     define('ROOT_PATH', dirname(__DIR__, 2) . DIRECTORY_SEPARATOR);
 }
 
 define('PHPMYADMIN', true);
-// phpcs:enable
 
 if (PHP_VERSION_ID < 80100) {
-    die('<p>PHP 8.1.0+ is required.</p><p>Currently installed version is: ' . PHP_VERSION . '</p>');
+    die('PHP 8.1.0+ is required.' . PHP_EOL
+        . 'Currently installed version is: ' . PHP_VERSION . PHP_EOL
+        . 'Please upgrade your PHP installation to meet the requirements.');
 }
 
 require_once ROOT_PATH . 'libraries/constants.php';
 
-if (! @is_readable(AUTOLOAD_FILE)) {
-    die(
-        '<p>File <samp>' . AUTOLOAD_FILE . '</samp> missing or not readable.</p>'
-        . '<p>Most likely you did not run Composer to '
-        . '<a href="https://docs.phpmyadmin.net/en/latest/setup.html#installing-from-git">'
-        . 'install library files</a>.</p>'
-    );
+if (!is_readable(AUTOLOAD_FILE)) {
+    die('File "' . AUTOLOAD_FILE . '" is missing or not readable.' . PHP_EOL
+        . 'Most likely you did not run Composer to install library files.' . PHP_EOL
+        . 'Please refer to the documentation on how to install library files: '
+        . 'https://docs.phpmyadmin.net/en/latest/setup.html#installing-from-git');
 }
+
 
 require AUTOLOAD_FILE;
 
