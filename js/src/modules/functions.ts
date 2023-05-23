d0e1b3a0932fe92d186b782456b7b729342a1c6c
Maurício Meneghini Fauth <mauricio@fauth.dev>
2023-05-23 18:17:16 -0300

    Fix missing params when using Edit inline on SQL message box
    Fixes https://github.com/phpmyadmin/phpmyadmin/issues/18440
    
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- js/src/modules/functions.ts
+++ js/src/modules/functions.ts
@@ -1130,7 +1130,7 @@ function onloadSqlQueryEditEvents () {
 
         var fkCheck = $(this).parent().find('#fk_checks').is(':checked');
 
-        var $form = $('a.inline_edit_sql').prev('form');
+        var $form = $('a.inline_edit_sql').parent().parent().find('form');
         var $fakeForm = $('<form>', { action: 'index.php?route=/import', method: 'post' })
             .append($form.find('input[name=server], input[name=db], input[name=table], input[name=token]').clone())
             .append($('<input>', { type: 'hidden', name: 'show_query', value: 1 }))
