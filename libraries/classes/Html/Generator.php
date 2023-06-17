5f7499abb0e7861b7044303d0d0cb4f41f40a69f
Maurício Meneghini Fauth <mauricio@fauth.dev>
2023-06-17 15:26:19 -0300

    Fix profiling chart not loading when profiling is activated
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- libraries/classes/Html/Generator.php
+++ libraries/classes/Html/Generator.php
@@ -669,7 +669,7 @@ class Generator
             $retval .= '</div>';
 
             $retval .= '<div class="tools d-print-none">';
-            $retval .= '<form action="' . Url::getFromRoute('/sql') . '" method="post">';
+            $retval .= '<form action="' . Url::getFromRoute('/sql') . '" method="post" class="disableAjax">';
             $retval .= Url::getHiddenInputs($GLOBALS['db'], $GLOBALS['table']);
             $retval .= '<input type="hidden" name="sql_query" value="'
                 . htmlspecialchars($sqlQuery) . '">';
