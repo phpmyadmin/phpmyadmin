f862ade53b5ffe8d1da0f43e1fb70090449888fe
Maurício Meneghini Fauth <mauricio@fauth.dev>
2023-07-05 10:55:42 -0300

    Improve unit tests for the Navigation\NodeFactory class
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- test/classes/Navigation/NodeFactoryTest.php
+++ test/classes/Navigation/NodeFactoryTest.php
@@ -6,6 +6,7 @@ namespace PhpMyAdmin\Tests\Navigation;
 
 use PhpMyAdmin\Navigation\NodeFactory;
 use PhpMyAdmin\Navigation\Nodes\Node;
+use PhpMyAdmin\Navigation\Nodes\NodeDatabase;
 use PhpMyAdmin\Tests\AbstractTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
 
@@ -45,5 +46,25 @@ class NodeFactoryTest extends AbstractTestCase
         $this->assertEquals('default', $node->name);
         $this->assertEquals(Node::CONTAINER, $node->type);
         $this->assertTrue($node->isGroup);
+    }
+
+    public function testDatabaseNode(): void
+    {
+        $node = NodeFactory::getInstance(NodeDatabase::class, 'database_name');
+        $this->assertInstanceOf(NodeDatabase::class, $node);
+        $this->assertEquals('database_name', $node->name);
+        $this->assertEquals(Node::OBJECT, $node->type);
+        $this->assertFalse($node->isGroup);
+    }
+
+    public function testGetInstanceForNewNode(): void
+    {
+        $node = NodeFactory::getInstanceForNewNode('New', 'new_database italics');
+        $this->assertEquals('New', $node->name);
+        $this->assertEquals(Node::OBJECT, $node->type);
+        $this->assertFalse($node->isGroup);
+        $this->assertEquals('New', $node->title);
+        $this->assertTrue($node->isNew);
+        $this->assertEquals('new_database italics', $node->classes);
     }
 }
