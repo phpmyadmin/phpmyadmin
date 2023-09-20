5e8ec6924060da707e8e1aa86e6149e73d2783ac
Maurício Meneghini Fauth <mauricio@fauth.dev>
2023-09-20 14:14:28 -0300

    Improve git hash info error handling in home.js
    Instead of showing a generic error message alert when an error occurs,
    it displays an error message at the same place where the success output
    goes.
    
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- js/src/home.js
+++ js/src/home.js
@@ -105,13 +105,15 @@ const GitInfo = {
                 'server': CommonParams.get('server'),
                 'ajax_request': true,
                 'no_debug': true
-            },
-            function (data) {
-                if (typeof data !== 'undefined' && data.success === true) {
-                    $(data.message).insertAfter('#li_pma_version');
-                }
             }
-        );
+        ).done(function (data) {
+            if (typeof data !== 'undefined' && data.success === true) {
+                $(data.message).insertAfter('#li_pma_version');
+            }
+        }).fail(function () {
+            const gitHashInfoLi = '<li id="li_pma_version_git" class="list-group-item">' + window.Messages.errorLoadingGitInformation + '</li>';
+            $(gitHashInfoLi).insertAfter('#li_pma_version');
+        });
     }
 };
 
