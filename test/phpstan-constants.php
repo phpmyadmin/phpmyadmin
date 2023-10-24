941c79508503e984c5249b45bd2abfe36cf79b49
Maurício Meneghini Fauth <mauricio@fauth.dev>
2023-10-24 17:24:06 -0300

    Move ./cache directory to ./app/cache
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- test/phpstan-constants.php
+++ test/phpstan-constants.php
@@ -3,5 +3,5 @@
 declare(strict_types=1);
 
 define('ROOT_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
-define('CACHE_DIR', ROOT_PATH . 'cache' . DIRECTORY_SEPARATOR);
+define('CACHE_DIR', ROOT_PATH . 'app' . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR);
 define('VERSION_SUFFIX', '');
