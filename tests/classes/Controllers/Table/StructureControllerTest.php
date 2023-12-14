6f228e7fc5c3a0d2dddd842ebaa387394ac2c28c
Maurício Meneghini Fauth <mauricio@fauth.dev>
2023-12-14 13:36:17 -0300

    Fix Table\StructureControllerTest failing test
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- tests/classes/Controllers/Table/StructureControllerTest.php
+++ tests/classes/Controllers/Table/StructureControllerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PhpMyAdmin\Tests\Controllers\Table;
 
+use PhpMyAdmin\Charsets;
 use PhpMyAdmin\Config;
 use PhpMyAdmin\Config\PageSettings;
 use PhpMyAdmin\ConfigStorage\Relation;
@@ -52,10 +53,22 @@ class StructureControllerTest extends AbstractTestCase
         $config->settings['ShowPropertyComments'] = false;
         (new ReflectionProperty(Relation::class, 'cache'))->setValue(null, null);
         (new ReflectionProperty(Template::class, 'twig'))->setValue(null, null);
+        (new ReflectionProperty(Charsets::class, 'collations'))->setValue(null, []);
 
         $this->dummyDbi->addSelectDb('test_db');
         $this->dummyDbi->addSelectDb('test_db');
         $this->dummyDbi->addResult('SELECT 1 FROM `test_db`.`test_table` LIMIT 1;', [[1]]);
+        $this->dummyDbi->addResult(
+            'SHOW COLLATION',
+            [
+                ['utf8mb4_general_ci', 'utf8mb4', '45', 'Yes', 'Yes', '1'],
+                ['armscii8_general_ci', 'armscii8', '32', 'Yes', 'Yes', '1'],
+                ['utf8_general_ci', 'utf8', '33', 'Yes', 'Yes', '1'],
+                ['utf8_bin', 'utf8', '83', '', 'Yes', '1'],
+                ['latin1_swedish_ci', 'latin1', '8', 'Yes', 'Yes', '1'],
+            ],
+            ['Collation', 'Charset', 'Id', 'Default', 'Compiled', 'Sortlen'],
+        );
         // phpcs:disable Generic.Files.LineLength.TooLong
         $this->dummyDbi->addResult(
             'SELECT * FROM `information_schema`.`PARTITIONS` WHERE `TABLE_SCHEMA` = \'test_db\' AND `TABLE_NAME` = \'test_table\'',
@@ -150,6 +163,8 @@ class StructureControllerTest extends AbstractTestCase
         ]);
 
         $this->assertSame($expected, $response->getHTMLResult());
+
+        $this->dummyDbi->assertAllSelectsConsumed();
         $this->dummyDbi->assertAllQueriesConsumed();
     }
 }
