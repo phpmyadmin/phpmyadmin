1b913bb1da6bde9ddfa6334d9f9c751de4a77cbf
Kamil Tekiela <tekiela246@gmail.com>
2023-12-27 17:47:33 +0100

    Privatize StructureController's elemets
    This class cannot be made final because it's mocked, but it's never extended and should not be extended.
    
    Signed-off-by: Kamil Tekiela <tekiela246@gmail.com>

--- src/Controllers/Table/StructureController.php
+++ src/Controllers/Table/StructureController.php
@@ -48,7 +48,7 @@ use function strtotime;
  */
 class StructureController extends AbstractController
 {
-    protected readonly Table $tableObj;
+    private readonly Table $tableObj;
 
     public function __construct(
         ResponseRenderer $response,
@@ -150,7 +150,7 @@ class StructureController extends AbstractController
      * @param (string|int)[] $columnsWithIndex       Columns with index
      * @psalm-param non-empty-string $route
      */
-    protected function displayStructure(
+    private function displayStructure(
         RelationParameters $relationParameters,
         array $columnsWithUniqueIndex,
         Index|null $primaryIndex,
@@ -292,7 +292,7 @@ class StructureController extends AbstractController
     /**
      * Get HTML snippet for display table statistics
      */
-    protected function getTableStats(
+    private function getTableStats(
         bool $isSystemSchema,
         bool $tableIsAView,
         string $tableStorageEngine,
