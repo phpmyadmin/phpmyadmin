1f85fe91b9760503f361a5073e18139bf701493b
Joao <joaopedro.oliveirajava@gmail.com>
2024-11-24 19:51:46 -0300

    Saved query - Replace the popup by handling the behavior through the button
    Signed-off-by: João Pedro Oliveira Rocha <joaopedro.oliveirajava@gmail.com>
    Signed-off-by: jpoliveira08 <joaopedro.oliveirajava@gmail.com>

--- resources/js/src/sql.ts
+++ resources/js/src/sql.ts
@@ -52,6 +52,8 @@ function autoSave (query): void {
         } else {
             window.Cookies.set(key, query, { path: CommonParams.get('rootPath') });
         }
+
+        checkSavedQuery();
     }
 }
 
@@ -309,8 +311,6 @@ const insertQuery = function (queryType) {
         } else if (window.Cookies.get(key, { path: CommonParams.get('rootPath') })) {
             // @ts-ignore
             setQuery(window.Cookies.get(key, { path: CommonParams.get('rootPath') }));
-        } else {
-            ajaxShowMessage(window.Messages.strNoAutoSavedQuery);
         }
 
         return;
@@ -1269,15 +1269,20 @@ function getAutoSavedKey () {
 }
 
 function checkSavedQuery () {
-    var key = Sql.getAutoSavedKey();
+    let key = Sql.getAutoSavedKey();
+    let buttonGetAutoSavedQuery = $('#saved');
 
-    if (isStorageSupported('localStorage') &&
-        typeof window.localStorage.getItem(key) === 'string') {
-        ajaxShowMessage(window.Messages.strPreviousSaveQuery);
-        // @ts-ignore
-    } else if (window.Cookies.get(key, { path: CommonParams.get('rootPath') })) {
-        ajaxShowMessage(window.Messages.strPreviousSaveQuery);
+    let isAutoSavedInLocalStorage = isStorageSupported('localStorage') && (typeof window.localStorage.getItem(key) === 'string');
+    // @ts-ignore
+    let isAutoSavedInCookie = window.Cookies.get(key, { path: CommonParams.get('rootPath') });
+
+    if (isAutoSavedInLocalStorage || isAutoSavedInCookie) {
+        buttonGetAutoSavedQuery.prop('disabled', false);
+
+        return;
     }
+
+    buttonGetAutoSavedQuery.prop('disabled', true);
 }
 
 AJAX.registerOnload('sql.js', function () {
