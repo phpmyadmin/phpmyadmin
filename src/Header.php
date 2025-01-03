a6144ad5afdce1d7929ef6995a57128809e331e3
Kamil Tekiela <tekiela246@gmail.com>
2025-01-03 16:57:08 +0000

    Convert DisableShortcutKeys to $config
    Signed-off-by: Kamil Tekiela <tekiela246@gmail.com>

--- src/Header.php
+++ src/Header.php
@@ -238,7 +238,7 @@ class Header
             $this->scripts->addFile('drag_drop_import.js');
         }
 
-        if (! $this->config->get('DisableShortcutKeys')) {
+        if (! $this->config->config->DisableShortcutKeys) {
             $this->scripts->addFile('shortcuts_handler.js');
         }
 
