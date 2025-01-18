a5de3a2c5d1850b6b09307d0c3ed790daa124c78
faissaloux <fwahabali@gmail.com>
2025-01-19 00:34:09 +0100

    Search operators EMPTY/NOT EMPTY
    Signed-off-by: faissaloux <fwahabali@gmail.com>

--- resources/js/src/table/select.ts
+++ resources/js/src/table/select.ts
@@ -95,8 +95,8 @@ AJAX.registerOnload('table/select.js', function () {
         var unaryFunctions = [
             'IS NULL',
             'IS NOT NULL',
-            '= \'\'',
-            '!= \'\''
+            'EMPTY',
+            'NOT EMPTY',
         ];
 
         var geomUnaryFunctions = [
