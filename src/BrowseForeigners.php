a39f05672fc91a669c044abc895be6bc095eb2cf
Kamil Tekiela <tekiela246@gmail.com>
2025-02-04 20:52:46 +0000

    Fixed mixed type hints
    Signed-off-by: Kamil Tekiela <tekiela246@gmail.com>

--- src/BrowseForeigners.php
+++ src/BrowseForeigners.php
@@ -35,13 +35,13 @@ class BrowseForeigners
     /**
      * Function to get html for one relational key
      *
-     * @param int     $horizontalCount    the current horizontal count
-     * @param string  $header             table header
-     * @param mixed[] $keys               all the keys
-     * @param int     $indexByKeyname     index by keyname
-     * @param mixed[] $descriptions       descriptions
-     * @param int     $indexByDescription index by description
-     * @param string  $currentValue       current value on the edit form
+     * @param int      $horizontalCount    the current horizontal count
+     * @param string   $header             table header
+     * @param mixed[]  $keys               all the keys
+     * @param int      $indexByKeyname     index by keyname
+     * @param string[] $descriptions       descriptions
+     * @param int      $indexByDescription index by description
+     * @param string   $currentValue       current value on the edit form
      *
      * @return array{string, int, int} the generated html
      */
