b456b929d13c4a1a5c9d99af76c4072923ef28d7
huai.wang <huai.wang@vossic.com>
2025-02-12 22:49:41 +0800

    word style
    Signed-off-by: huai.wang <huai.wang@vossic.com>

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = '';
+$cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
