801e4e9ae9a4b78dc754f1611a6a494c5660d469
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2025-07-02 14:56:12 +0200

    Fix test
    Signed-off-by: Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>

--- libraries/classes/Plugins/Export/ExportPdf.php
+++ libraries/classes/Plugins/Export/ExportPdf.php
@@ -15,7 +15,7 @@ use TCPDF;
 
 use function __;
 use function class_exists;
-use function extension_exists;
+use function extension_loaded;
 
 /**
  * Produce a PDF report (export) from a query
@@ -333,6 +333,6 @@ class ExportPdf extends ExportPlugin
 
     public static function isAvailable(): bool
     {
-        return class_exists(TCPDF::class) && extension_exists('curl');
+        return class_exists(TCPDF::class) && extension_loaded('curl');
     }
 }
