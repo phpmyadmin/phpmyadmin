435bd0fc7c715e118715cb9b9b4b730a218fefbc
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2025-09-01 00:33:04 +0200

    Fix
    Signed-off-by: Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -568,7 +568,11 @@ PACKAGE_LIST='tecnickcom/tcpdf pragmarx/google2fa-qrcode bacon/bacon-qr-code cod
 for PACKAGES in $PACKAGE_LIST
 do
     PKG_VERSION="$(get_composer_package_version "$PACKAGES")"
-    PACKAGES_VERSIONS="$PACKAGES_VERSIONS \"$PACKAGES:$PKG_VERSION\""
+    if echo "$PKG_VERSION" | grep -q '||'; then
+        PACKAGES_VERSIONS="$PACKAGES_VERSIONS \"$PACKAGES:$PKG_VERSION\""
+    else
+        PACKAGES_VERSIONS="$PACKAGES_VERSIONS $PACKAGES:$PKG_VERSION"
+    fi
 done
 
 echo "* Installing composer packages '$PACKAGES_VERSIONS'"
