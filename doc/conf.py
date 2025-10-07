cabbe159aebb35594167cb3bf891924a9abf324f
Isaac Bennetch <bennetch@gmail.com>
2025-10-07 16:37:57 -0400

    Prepare for version 5.2.3
    Signed-off-by: Isaac Bennetch <bennetch@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -51,7 +51,7 @@ copyright = u'2012 - 2025, The phpMyAdmin devel team'
 # built documents.
 #
 # The short X.Y version.
-version = '5.2.3-dev'
+version = '5.2.3'
 # The full version, including alpha/beta/rc tags.
 release = version
 
