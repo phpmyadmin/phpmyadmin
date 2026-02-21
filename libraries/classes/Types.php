09771b1efdeea64fa50b0ab69cea9f09afb32033
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2026-02-21 20:19:53 +0100

    Fix
    Signed-off-by: Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>

--- libraries/classes/Types.php
+++ libraries/classes/Types.php
@@ -446,7 +446,8 @@ class Types
                 return __('128-bit UUID (Universally Unique Identifier)');
 
             case 'VECTOR':
-                return __('Stores vector data optimized for operations such as similarity search and machine learning applications');
+                return __('Stores vector data optimized for operations '
+                    . 'such as similarity search and machine learning applications');
         }
 
         return '';
