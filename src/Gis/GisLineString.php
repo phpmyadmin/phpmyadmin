2311a0173b37def439f536db82296af49584110f
Kamil Tekiela <tekiela246@gmail.com>
2026-08-21 20:23:16 +0100

    Create Color DTO
    Signed-off-by: Kamil Tekiela <tekiela246@gmail.com>

--- src/Gis/GisLineString.php
+++ src/Gis/GisLineString.php
@@ -9,6 +9,7 @@ namespace PhpMyAdmin\Gis;
 
 use PhpMyAdmin\Gis\Ds\Extent;
 use PhpMyAdmin\Gis\Ds\ScaleData;
+use PhpMyAdmin\Image\Color;
 use PhpMyAdmin\Image\ImageWrapper;
 use TCPDF;
 
@@ -44,19 +45,18 @@ class GisLineString extends GisGeometry
      *
      * @param string    $spatial   GIS POLYGON object
      * @param string    $label     Label for the GIS POLYGON object
-     * @param int[]     $color     Color for the GIS POLYGON object
      * @param ScaleData $scaleData Array containing data related to scaling
      */
     public function prepareRowAsPng(
         string $spatial,
         string $label,
-        array $color,
+        Color $color,
         ScaleData $scaleData,
         ImageWrapper $image,
     ): void {
         // allocate colors
-        $black = $image->colorAllocate(0, 0, 0);
-        $lineColor = $image->colorAllocate(...$color);
+        $black = $image->colorAllocate(Color::black());
+        $lineColor = $image->colorAllocate($color);
 
         // Trim to remove leading 'LINESTRING(' and trailing ')'
         $lineString = mb_substr($spatial, 11, -1);
@@ -96,17 +96,16 @@ class GisLineString extends GisGeometry
      *
      * @param string    $spatial   GIS LINESTRING object
      * @param string    $label     Label for the GIS LINESTRING object
-     * @param int[]     $color     Color for the GIS LINESTRING object
      * @param ScaleData $scaleData Array containing data related to scaling
      */
     public function prepareRowAsPdf(
         string $spatial,
         string $label,
-        array $color,
+        Color $color,
         ScaleData $scaleData,
         TCPDF $pdf,
     ): void {
-        $lineStyle = ['all' => ['width' => 1.5, 'color' => $color]];
+        $lineStyle = ['all' => ['width' => 1.5, 'color' => $color->toArray()]];
 
         // Trim to remove leading 'LINESTRING(' and trailing ')'
         $linestring = mb_substr($spatial, 11, -1);
@@ -128,17 +127,16 @@ class GisLineString extends GisGeometry
      *
      * @param string    $spatial   GIS LINESTRING object
      * @param string    $label     Label for the GIS LINESTRING object
-     * @param int[]     $color     Color for the GIS LINESTRING object
      * @param ScaleData $scaleData Array containing data related to scaling
      *
      * @return string the code related to a row in the GIS dataset
      */
-    public function prepareRowAsSvg(string $spatial, string $label, array $color, ScaleData $scaleData): string
+    public function prepareRowAsSvg(string $spatial, string $label, Color $color, ScaleData $scaleData): string
     {
         $options = [
             'class' => 'linestring vector',
             'fill' => 'none',
-            'stroke' => sprintf('#%02x%02x%02x', $color[0], $color[1], $color[2]),
+            'stroke' => sprintf('#%02x%02x%02x', $color->red, $color->green, $color->blue),
             'stroke-width' => 2,
         ];
         if ($label !== '') {
