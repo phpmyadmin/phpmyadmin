dafd0e587d63af2ef89ca593fcb24e455d18ad90
Kamil Tekiela <tekiela246@gmail.com>
2026-08-22 00:48:52 +0100

    Narrow Current::$whereClause to array|null
    Signed-off-by: Kamil Tekiela <tekiela246@gmail.com>

--- src/Current.php
+++ src/Current.php
@@ -13,8 +13,8 @@ final class Current
     public static string $sqlQuery = '';
     public static Message|null $message = null;
     public static string $lang = 'en';
-    /** @var array<string>|string|null */
-    public static array|string|null $whereClause = null;
+    /** @var array<string>|null */
+    public static array|null $whereClause = null;
     public static string|null $displayQuery = null;
     public static string|null $charset = null;
     public static string|null $completeQuery = null;
